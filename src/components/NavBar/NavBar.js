7e43b42af962a9ac958970932c0a3c53f65437d7
Igor Nikiforov <desola2@inbox.ru>
2021-10-20 20:55:25 +0300

    Create NavBar comp placeholder, add styles
    * Import Athiti 400, 700 weight font
    
    * Import Comfortaa 300, 700 weight font
    
    * Add container and body bg styles

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import cl from './NavBar.module.scss'
+
+const NavBar = () => {
+	return (
+		<nav className={cl.navbar}>
+			<Link to='/game'>Game</Link>
+		</nav>
+	)
+}
+
+export default NavBar
