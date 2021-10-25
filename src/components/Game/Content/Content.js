93a85d049199d1c0bb938a20fba46c482bf4a6f6
Igor Nikiforov <desola2@inbox.ru>
2021-10-25 18:09:52 +0300

    Add video and plays sections

--- src/components/Game/Content/Content.js
+++ src/components/Game/Content/Content.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import cl from './Content.module.scss';
 import $ from 'jquery';
+import { useParams } from 'react-router';
 import ContentSituationsList from '../ContentSituationsList/ContentSituationsList';
+import PlaysImg from 'images/plays.jpg'
 
 const Content = ({ viewMode, contentSituationsList }) => {
+  const { tab } = useParams();
+
+  const renderTab = tab => {
+    switch (tab) {
+      case 'videos':
+        return (
+          <iframe
+            width='100%'
+            height='100%'
+            src='https://www.youtube.com/embed/gN7RdRGmBF4?autoplay=1'
+            title='YouTube video player'
+            frameBorder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen></iframe>
+        );
+				case 'plays':
+					return <img src={PlaysImg} alt="plays" width='100%'/>
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={cl.content}>
       <ContentSituationsList situations={contentSituationsList} />
       <div className={cl.situationsControls}></div>
-      <div className={cl.graphics}></div>
+      <div className={cl.graphics}>
+        {renderTab(tab)}
+      </div>
     </div>
   );
 };
