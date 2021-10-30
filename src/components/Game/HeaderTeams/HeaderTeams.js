cce58b2b3a0b8592170e6edc3c116ba28e7abcad
Igor Nikiforov <desola2@inbox.ru>
2021-10-30 14:17:49 +0300

    Rework HeaderTeams comp

--- src/components/Game/HeaderTeams/HeaderTeams.js
+++ src/components/Game/HeaderTeams/HeaderTeams.js
@@ -2,10 +2,12 @@ import React from 'react';
 import cl from './HeaderTeams.module.scss';
 
 const HeaderTeams = ({ names }) => {
+  const getShortName = name => (name.length > 8 ? name.slice(0, 7) + '…' : name);
+
   return (
     <div className={cl.teamNames}>
-      <p className={cl.teamAttack}>{names.attack_team}</p>
-      <p className={cl.teamDefence}>{names.defence_team}</p>
+      <p className={cl.guests}>{getShortName(names[0])}</p>
+      <p className={cl.owners}>{getShortName(names[1])}</p>
     </div>
   );
 };
