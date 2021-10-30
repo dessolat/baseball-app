cf2cf4bce04d7196b600413cb0b5999d77e3420d
Igor Nikiforov <desola2@inbox.ru>
2021-10-30 16:10:11 +0300

    Rework useSummaryRHE hook

--- src/hooks/useSummaryRHE.js
+++ src/hooks/useSummaryRHE.js
@@ -1,13 +1,14 @@
 import { useMemo } from 'react';
 
-const useSummaryRHE = innings =>
+const useSummaryRHE = (innings, inningNumber) =>
   useMemo(() => {
     const initialRHE = { bot_err: 0, top_err: 0, bot_hits: 0, bot_runs: 0, top_hits: 0, top_runs: 0 };
+    const newInnings = inningNumber === null ? innings.slice() : innings.slice(0, inningNumber);
 
-    return innings.reduce((sum, inning) => {
+    return newInnings.reduce((sum, inning) => {
       Object.keys(sum).forEach(field => (sum[field] += inning[field] || 0));
       return sum;
     }, initialRHE);
-  }, [innings]);
+  }, [innings, inningNumber]);
 
 export default useSummaryRHE;
