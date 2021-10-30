a5bcf9def7bc69196036e9aa8297a8f64b7d7761
Igor Nikiforov <desola2@inbox.ru>
2021-10-30 14:20:47 +0300

    Rework HeaderInfo comp

--- src/components/Game/HeaderInfo/HeaderInfo.js
+++ src/components/Game/HeaderInfo/HeaderInfo.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import cl from './HeaderInfo.module.scss';
+import useSummaryRHE from 'hooks/useSummaryRHE';
 
-const HeaderInfo = ({data}) => {
+const HeaderInfo = ({ innings }) => {
+	const summaryRHE = useSummaryRHE(innings)
+
   return (
     <div className={cl.info}>
       <div>
         <span>R</span>
-        <span>{data.attack.r}</span>
-        <span>{data.defence.r}</span>
+        <span>{summaryRHE.top_runs}</span>
+        <span>{summaryRHE.bot_runs}</span>
       </div>
       <div>
         <span>H</span>
-        <span>{data.attack.h}</span>
-        <span>{data.defence.h}</span>
+        <span>{summaryRHE.top_hits}</span>
+        <span>{summaryRHE.bot_hits}</span>
       </div>
       <div>
         <span>E</span>
-        <span>{data.attack.e}</span>
-        <span>{data.defence.e}</span>
+        <span>{summaryRHE.top_err}</span>
+        <span>{summaryRHE.bot_err}</span>
       </div>
     </div>
   );
