96197fe12d5ccca153e242660b1f908273890185
Igor Nikiforov <desola2@inbox.ru>
2021-11-03 22:55:50 +0300

    Create ContentSituations components, add styles
    * ContentSituations, ContentSituationsList comps
    
    * Add styles

--- src/components/Game/ContentSituationsList/ContentSituationsList.js
+++ src/components/Game/ContentSituationsList/ContentSituationsList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import cl from './ContentSituationsList.module.scss';
 import ContentSituationsListItem from '../ContentSituationsListItem/ContentSituationsListItem';
 
-const ContentSituationsList = ({ innings, inningNumber }) => {
+const ContentSituationsList = ({ innings, inningNumber, setEvents, currentCard, setCurrentCard }) => {
   const cards = [];
   const newInnings =
     inningNumber !== null ? innings.filter(inning => inning.number === inningNumber) : innings;
@@ -11,10 +11,24 @@ const ContentSituationsList = ({ innings, inningNumber }) => {
     inning['bottom/owners']?.forEach(owner => cards.push({ inning_number: inning.number, ...owner }));
   });
 
+  const situationClick = player => {
+		setCurrentCard({who_id: player['who id'], inning_number: player.inning_number})
+    const events = [];
+    player.moments.forEach(moment => {
+      if (moment.events) moment.events.forEach(event => events.push(event.description));
+    });
+    setEvents(events);
+  };
+
   return (
     <ul className={cl.list}>
       {cards.map(card => (
-        <ContentSituationsListItem key={card.inning_number + ' ' + card.who} player={card} />
+        <ContentSituationsListItem
+          key={card.inning_number + ' ' + card.who}
+          player={card}
+          situationClick={situationClick}
+					currentCard={currentCard}
+        />
       ))}
     </ul>
   );
