74402df65e745f65f63ae1c778ef0ea0deed2ca9
Igor Nikiforov <desola2@inbox.ru>
2021-11-05 23:47:43 +0300

    Add SET_FULL_DATA action to gameReducer

--- src/redux/gameReducer.js
+++ src/redux/gameReducer.js
@@ -1,8 +1,14 @@
-const defaultState = []
+const defaultState = [];
+
+const SET_FULL_DATA = 'SET_FULL_DATA';
 
 export const gameReducer = (state = defaultState, action) => {
-	switch (action) {
-		default:
-			return state
-	}
-}
+  switch (action) {
+    case SET_FULL_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const setFullData = data => ({ type: SET_FULL_DATA, payload: data });
