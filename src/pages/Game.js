0ee0e11524ba9d797a67e03e57f0bc516d37f600
Igor Nikiforov <desola2@inbox.ru>
2021-11-06 00:28:39 +0300

    Add setFullData dispatch to Game comp

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
 import { filterSituationsList } from '../data';
-import fullData from 'baseball.json';
 import Header from 'components/Game/Header/Header';
 import Filters from 'components/Game/Filters/Filters';
 import Content from 'components/Game/Content/Content';
+import { useDispatch } from 'react-redux';
+import { setFullData } from 'redux/gameReducer';
 
 const Game = () => {
   const [situationFilter, setSituationFilter] = useState('All');
   const [inningNumber, setInningNumber] = useState(null);
   const [viewMode, setViewMode] = useState('mode-1');
-  const [jsonData, setJsonData] = useState(fullData);
+  const dispatch = useDispatch();
+	
 
   const selectJSON = e => {
-    const files = e.target.files;
-
+		const files = e.target.files;
+		
     for (let i = 0, f; (f = files[i]); i++) {
-      const reader = new FileReader();
-
+			const reader = new FileReader();
+			
       reader.onload = (function (theFile) {
-        return e => {
-          try {
-            const json = JSON.parse(e.target.result);
-            setJsonData(json)
+				return e => {
+					try {
+						const json = JSON.parse(e.target.result);
+						dispatch(setFullData(json))
           } catch (ex) {
             alert('ex when trying to parse json = ' + ex);
           }
@@ -29,7 +31,7 @@ const Game = () => {
       })(f);
       reader.readAsText(f);
     }
-  }
+  };
 
   return (
     <>
@@ -37,9 +39,9 @@ const Game = () => {
         type='file'
         onChange={selectJSON}
         style={{ position: 'fixed', left: 0, top: 0 }}
-				accept='application/json'
+        accept='application/json'
       />
-      <Header {...jsonData} inningNumber={inningNumber} setInningNumber={setInningNumber} />
+      <Header inningNumber={inningNumber} setInningNumber={setInningNumber} />
       <Filters
         situationFilter={situationFilter}
         setSituationFilter={setSituationFilter}
@@ -47,7 +49,7 @@ const Game = () => {
         setViewMode={setViewMode}
         situations={filterSituationsList}
       />
-      <Content viewMode={viewMode} innings={jsonData.innings} inningNumber={inningNumber} />
+      <Content viewMode={viewMode} inningNumber={inningNumber} />
     </>
   );
 };
