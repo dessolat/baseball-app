45ba288001605901b354f5650311ba97641cab9b
Igor Nikiforov <desola2@inbox.ru>
2021-11-07 22:21:11 +0300

    Add SET_INNING_NUMBER action type to gameReducer

--- src/redux/gameReducer.js
+++ src/redux/gameReducer.js
@@ -1,16 +1,20 @@
 import fullData from 'baseball.json';
 
-const defaultState = { innings: fullData.innings, preview: fullData.preview };
+const defaultState = { innings: fullData.innings, preview: fullData.preview, inningNumber: null };
 
 const SET_FULL_DATA = 'SET_FULL_DATA';
+const SET_INNING_NUMBER = 'SET_INNING_NUMBER';
 
 export const gameReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_FULL_DATA:
       return { ...state, innings: action.payload.innings, preview: action.payload.preview };
+    case SET_INNING_NUMBER:
+      return { ...state, inningNumber: action.payload };
     default:
       return state;
   }
 };
 
 export const setFullData = data => ({ type: SET_FULL_DATA, payload: data });
+export const setInningNumber = number => ({ type: SET_INNING_NUMBER, payload: number });
