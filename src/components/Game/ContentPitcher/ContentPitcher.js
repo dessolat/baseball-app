fc65a1af1276b16f057c4ee013bbefb6bf5e5d8f
Igor Nikiforov <desola2@inbox.ru>
2021-11-08 16:51:24 +0300

    Create ContentPitcher comp
    * Move styles from Content to ContentPitcher comp

---
+++ src/components/Game/ContentPitcher/ContentPitcher.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import cl from './ContentPitcher.module.scss';
+
+const ContentPitcher = ({ currentCard }) => {
+  const pitcherName = currentCard.moments?.slice(-1)[0].pitcher.pitches_name;
+
+  return <p className={cl.playerName}>Pitcher: {currentCard.moments && <span>{pitcherName}</span>}</p>;
+};
+
+export default ContentPitcher;
