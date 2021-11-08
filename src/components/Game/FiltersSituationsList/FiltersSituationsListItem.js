13c66572ac8772d99b84f92b366f4bd1e84d5f3f
Igor Nikiforov <desola2@inbox.ru>
2021-11-08 22:17:44 +0300

    Add capitalize func to FiltersSituationsListItem

--- src/components/Game/FiltersSituationsList/FiltersSituationsListItem.js
+++ src/components/Game/FiltersSituationsList/FiltersSituationsListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const FiltersSituationsListItem = ({ situationFilter, situation, handleClick, cl }) => {
-  const classes = situationFilter === situation ? cl.active : ''
-	
-	return (
+  const classes = situationFilter === situation ? cl.active : '';
+  
+	const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+  return (
     <li>
       <button name={situation} onClick={handleClick}>
-        <span className={classes}>{situation}</span>
+        <span className={classes}>{capitalizeFirstLetter(situation)}</span>
       </button>
     </li>
   );
