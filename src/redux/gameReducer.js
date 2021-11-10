bae671a01f22b981c86f7ae08ceca9ac622c5294
Igor Nikiforov <desola2@inbox.ru>
2021-11-10 16:49:08 +0300

    Add SET_VIEW_MODE action type to gameReducer

--- src/redux/gameReducer.js
+++ src/redux/gameReducer.js
@@ -1,10 +1,17 @@
 import fullData from 'baseball.json';
 
-const defaultState = { innings: fullData.innings, preview: fullData.preview, inningNumber: null, situationFilter: 'All' };
+const defaultState = {
+  innings: fullData.innings,
+  preview: fullData.preview,
+  inningNumber: null,
+  situationFilter: 'All',
+  viewMode: 'mode-1'
+};
 
 const SET_FULL_DATA = 'SET_FULL_DATA';
 const SET_INNING_NUMBER = 'SET_INNING_NUMBER';
-const SET_SITUATION_FILTER = 'SET_SITUATION_FILTER'
+const SET_SITUATION_FILTER = 'SET_SITUATION_FILTER';
+const SET_VIEW_MODE = 'SET_VIEW_MODE'
 
 export const gameReducer = (state = defaultState, action) => {
   switch (action.type) {
@@ -14,6 +21,8 @@ export const gameReducer = (state = defaultState, action) => {
       return { ...state, inningNumber: action.payload };
     case SET_SITUATION_FILTER:
       return { ...state, situationFilter: action.payload };
+    case SET_VIEW_MODE:
+      return { ...state, viewMode: action.payload };
     default:
       return state;
   }
@@ -22,3 +31,4 @@ export const gameReducer = (state = defaultState, action) => {
 export const setFullData = payload => ({ type: SET_FULL_DATA, payload });
 export const setInningNumber = payload => ({ type: SET_INNING_NUMBER, payload });
 export const setSituationFilter = payload => ({ type: SET_SITUATION_FILTER, payload });
+export const setViewMode = payload => ({ type: SET_VIEW_MODE, payload });
