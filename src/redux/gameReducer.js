fef27cfebbf5584dc3bcc74e799d6c185a9ed6c9
Igor Nikiforov <desola2@inbox.ru>
2021-11-10 16:20:51 +0300

    Add SET_SITUATION_FILTER action to gameReducer

--- src/redux/gameReducer.js
+++ src/redux/gameReducer.js
@@ -1,9 +1,10 @@
 import fullData from 'baseball.json';
 
-const defaultState = { innings: fullData.innings, preview: fullData.preview, inningNumber: null };
+const defaultState = { innings: fullData.innings, preview: fullData.preview, inningNumber: null, situationFilter: 'All' };
 
 const SET_FULL_DATA = 'SET_FULL_DATA';
 const SET_INNING_NUMBER = 'SET_INNING_NUMBER';
+const SET_SITUATION_FILTER = 'SET_SITUATION_FILTER'
 
 export const gameReducer = (state = defaultState, action) => {
   switch (action.type) {
@@ -11,10 +12,13 @@ export const gameReducer = (state = defaultState, action) => {
       return { ...state, innings: action.payload.innings, preview: action.payload.preview };
     case SET_INNING_NUMBER:
       return { ...state, inningNumber: action.payload };
+    case SET_SITUATION_FILTER:
+      return { ...state, situationFilter: action.payload };
     default:
       return state;
   }
 };
 
-export const setFullData = data => ({ type: SET_FULL_DATA, payload: data });
-export const setInningNumber = number => ({ type: SET_INNING_NUMBER, payload: number });
+export const setFullData = payload => ({ type: SET_FULL_DATA, payload });
+export const setInningNumber = payload => ({ type: SET_INNING_NUMBER, payload });
+export const setSituationFilter = payload => ({ type: SET_SITUATION_FILTER, payload });
