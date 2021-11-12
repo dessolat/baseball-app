3b6dc7023e078740b807add3ca0fd58c33042aec
Igor Nikiforov <desola2@inbox.ru>
2021-11-12 12:56:18 +0300

    Move fetch logic from Game comp to hook
    * Move logic from Game comp to useGameFetch hook

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,53 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import Header from 'components/Game/Header/Header';
 import Filters from 'components/Game/Filters/Filters';
 import Content from 'components/Game/Content/Content';
 import { useSelector, useDispatch } from 'react-redux';
-import { setFullData } from 'redux/gameReducer';
 import Loader from 'components/UI/loaders/Loader/Loader';
 import ErrorLoader from 'components/UI/loaders/ErrorLoader/ErrorLoader';
+import useGameFetch from 'hooks/useGameFetch';
 
 const Game = () => {
   const [situations, setSituations] = useState(['All']);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const innings = useSelector(state => state.game.innings);
-  const preview = useSelector(state => state.game.preview);
   const dispatch = useDispatch();
-  const ref = useRef();
+  const [error, isLoading, intervalRef, getFullData] = useGameFetch('http://84.201.172.216:3030/game_280');
 
   useEffect(() => {
-    const getFullData = async () => {
-      try {
-        const resp = await axios.get('http://84.201.172.216:3030/game_280');
-        dispatch(setFullData(resp.data));
-      } catch (err) {
-        setError(err.message);
-      }
-    };
-
-    setIsLoading(true);
-
-    axios
-      .get('http://84.201.172.216:3030/game_280')
-      .then(resp => {
-        dispatch(setFullData(resp.data));
-      })
-      .catch(err => setError(err.message))
-      .finally(() => {
-        setIsLoading(false);
-        ref.current = setInterval(getFullData, 4000);
-      });
+    const interval = intervalRef.current;
+    dispatch(getFullData(true));
 
     return () => {
-      clearInterval(ref.current);
+      clearInterval(interval);
     };
+    // eslint-disable-next-line
   }, []);
-
-  useEffect(() => {
-    error && setError(null);
-  }, [innings, preview]);
 
   return (
     <>
