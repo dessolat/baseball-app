f77916e13f23b59081f25826c501bb5119d1f95f
Igor Nikiforov <desola2@inbox.ru>
2021-11-17 23:39:23 +0300

    Add batter_moment number to EventsListItem

--- src/components/Game/ContentVideoEventsList/ContentVideoEventsListItem.js
+++ src/components/Game/ContentVideoEventsList/ContentVideoEventsListItem.js
@@ -2,7 +2,20 @@ import React from 'react';
 import cl from './ContentVideoEventsList.module.scss';
 
 const ContentVideoEventsListItem = ({ event }) => {
-  return <li className={cl[event.circ_color_pitch]}>{event.circ_text_pitch}</li>;
+  return (
+    <>
+      {event.circ_color_play && (
+        <li className={cl[event.circ_color_play]} data-before={event.batter_moment}>
+          {event.circ_text_play}
+        </li>
+      )}
+      {event.circ_color_pitch && (
+        <li className={cl[event.circ_color_pitch]} data-before={event.batter_moment}>
+          {event.circ_text_pitch}
+        </li>
+      )}
+    </>
+  );
 };
 
 export default ContentVideoEventsListItem;
