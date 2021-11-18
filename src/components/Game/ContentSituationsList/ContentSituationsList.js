8d1b77a61b6a01a78401276205d67478a18b6cd3
Igor Nikiforov <desola2@inbox.ru>
2021-11-18 20:14:02 +0300

    Disable card on/off toggle in ContentSituationList

--- src/components/Game/ContentSituationsList/ContentSituationsList.js
+++ src/components/Game/ContentSituationsList/ContentSituationsList.js
@@ -6,25 +6,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setPlaybackMode } from 'redux/gameReducer';
 
 const ContentSituationsList = ({ cards, currentCard, setCurrentCard }, ref) => {
-	const playbackMode = useSelector(state => state.game.playbackMode)
-	const dispatch = useDispatch()
+  const playbackMode = useSelector(state => state.game.playbackMode);
+  const dispatch = useDispatch();
 
   const situationClick = (player, number) => {
-		playbackMode === 'play' && dispatch(setPlaybackMode('pause'))
-    if (currentCard.row_number !== number) {
-      setCurrentCard({ ...player, row_number: number });
-      return;
-    }
-    setCurrentCard({});
+    playbackMode === 'play' && dispatch(setPlaybackMode('pause'));
+    if (currentCard.row_number === number) return;
+    setCurrentCard({ ...player, row_number: number });
   };
 
   return (
     <ul className={cl.list}>
       {cards.map((card, i) => (
         <ContentSituationsListItem
-          // key={card.inning_number + ' ' + card.who}
           key={uuidv4()}
-					ref={ref}
+          ref={ref}
           player={card}
           number={i}
           situationClick={situationClick}
