a5c7f6e9e921e5329aafe205e504a8ed82a89860
Igor Nikiforov <desola2@inbox.ru>
2021-11-19 16:29:26 +0300

    Add gameId handle in Game comp

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,27 +6,39 @@ import { useSelector, useDispatch } from 'react-redux';
 import Loader from 'components/UI/loaders/Loader/Loader';
 import ErrorLoader from 'components/UI/loaders/ErrorLoader/ErrorLoader';
 import useGameFetch from 'hooks/useGameFetch';
+import GameIdForm from 'components/Game/GameIdForm/GameIdForm';
 
 const Game = () => {
   const innings = useSelector(state => state.game.innings);
+  const gameId = useSelector(state => state.game.gameId);
   const dispatch = useDispatch();
-  // const [error, isLoading, intervalRef, getFullData] = useGameFetch('http://84.201.172.216:3030/game_280');
-  // const [error, isLoading, intervalRef, getFullData] = useGameFetch('http://192.168.1.5:3001/');
+  const [error, isLoading, cancelTokenRef, intervalRef, getFullData] = useGameFetch(
+    'http://84.201.172.216:3030/game_279'
+  );
+  // const [error, isLoading, intervalRef, getFullData] = useGameFetch('http://192.168.1.3:3001/');
   // const [error, isLoading, intervalRef, getFullData] = useGameFetch('http://localhost:3001/');
-  const [error, isLoading, intervalRef, getFullData] = useGameFetch('https://fierce-harbor-71147.herokuapp.com/');
+  // const [error, isLoading, intervalRef, getFullData] = useGameFetch('https://fierce-harbor-71147.herokuapp.com/');
 
   useEffect(() => {
-    const interval = intervalRef.current;
     dispatch(getFullData(true));
-
     return () => {
-      clearInterval(interval);
+      // eslint-disable-next-line
+      clearInterval(intervalRef.current);
     };
     // eslint-disable-next-line
   }, []);
 
+  useEffect(() => {
+    if (gameId === 0) return;
+    typeof cancelTokenRef.current != 'undefined' && cancelTokenRef.current.cancel(null);
+    clearInterval(intervalRef.current);
+    dispatch(getFullData(true, 'http://84.201.172.216:3030/game_' + gameId));
+    // eslint-disable-next-line
+  }, [gameId]);
+
   return (
     <>
+      <GameIdForm isLoading={isLoading} />
       {error ? (
         <ErrorLoader error={error} />
       ) : isLoading ? (
