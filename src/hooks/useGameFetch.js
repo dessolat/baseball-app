677a239e7f1bd5ca1d27f05af3d210fbbd8e9b01
Igor Nikiforov <desola2@inbox.ru>
2021-11-19 14:42:04 +0300

    Add cancelToken, change url in useGameFetch hook
    * Add cancelTokenRef for canceling fetch request
    
    * Change innerUrl var

--- src/hooks/useGameFetch.js
+++ src/hooks/useGameFetch.js
@@ -1,27 +1,32 @@
 import { useState, useEffect, useRef } from 'react';
-import { setFullData } from 'redux/gameReducer'
+import { setFullData } from 'redux/gameReducer';
 import { useSelector } from 'react-redux';
-import axios from 'axios'
+import axios from 'axios';
 
 const useGameFetch = url => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-	const innings = useSelector(state => state.game.innings);
+  const innings = useSelector(state => state.game.innings);
   const intervalRef = useRef();
   const dataRef = useRef({});
+	const cancelTokenRef = useRef()
 
-	useEffect(() => {
+  useEffect(() => {
     error && setError(null);
-		// eslint-disable-next-line
+    // eslint-disable-next-line
   }, [innings]);
 
   const getFullData =
-    (firstTime = false) =>
+    (firstTime = false, innerUrl = url) =>
     async dispatch => {
+      
+
+      cancelTokenRef.current = axios.CancelToken.source();
+
       try {
         firstTime && setIsLoading(true);
         // const resp = await axios.get(firstTime ? url + '0' : url);
-        const resp = await axios.get(url);
+        const resp = await axios.get(innerUrl, { cancelToken: cancelTokenRef.current.token });
         if (JSON.stringify(dataRef.current) === JSON.stringify(resp.data)) return;
         dataRef.current = resp.data;
         dispatch(setFullData(resp.data));
@@ -30,12 +35,14 @@ const useGameFetch = url => {
       } finally {
         if (firstTime) {
           setIsLoading(false);
-          intervalRef.current = setInterval(() => {dispatch(getFullData())}, 3000);
+          intervalRef.current = setInterval(() => {
+            dispatch(getFullData(false, innerUrl));
+          }, 3000);
         }
       }
     };
 
-  return [error, isLoading, intervalRef, getFullData];
+  return [error, isLoading, cancelTokenRef, intervalRef, getFullData];
 };
 
 export default useGameFetch;
