e900aef3a4de7cbbe1af76f0c2d58aa3254ba2d4
Igor Nikiforov <desola2@inbox.ru>
2021-11-21 23:13:56 +0300

    Rework ContentSituationsListItemComp, minor styles

--- src/components/Game/ContentSituationsListItem/ContentSituationsListItem.js
+++ src/components/Game/ContentSituationsListItem/ContentSituationsListItem.js
@@ -1,22 +1,26 @@
 import React, { forwardRef } from 'react';
 import cl from './ContentSituationsListItem.module.scss';
 import PortraitImg from 'images/portrait.png';
-import Ellipse from 'components/UI/icons/Ellipse';
-import Rectangle from 'components/UI/icons/Rectangle';
+import Ellipses from 'components/UI/icons/Ellipses/Ellipses';
+import RectanglesEllipses from 'components/UI/icons/RectanglesEllipses/RectanglesEllipses';
+import ContentCardSimple from '../ContentCardSimple/ContentCardSimple';
+import ContentCardComplex from '../ContentCardComplex/ContentCardComplex';
 
-const ContentSituationsListItem = ({ player, number, situationClick, currentCard }, ref) => {
-  const eventsSummary = [];
-  const lastMoment = player.moments.slice(-1)[0];
-  const { r1, r2, r3, outs, balls, strikes } = lastMoment?.table || 0;
-  
-    lastMoment?.events?.forEach(event => eventsSummary.push(event.description));
-  
+const ContentSituationsListItem = ({ player, situationClick, currentCard }, ref) => {
   const classNames = [cl.listItem];
   player.who_id === currentCard.who_id &&
     player.inning_number === currentCard.inning_number &&
     classNames.push(cl.active);
 
-	// Обработать карточку здесь, разбить её на несколько подкарточек под одним выделением (нужно будет создать еще компонент)
+  //Filling situationsArr with moments where icons.rect_text exists
+  const situationsArr = [];
+  player.moments.forEach(
+    (moment, i) =>
+      moment.icons?.rect_text &&
+      (moment.icons.rect_text !== 'Replacement'
+        ? situationsArr.push(moment)
+        : i !== 0 && situationsArr.push(moment))
+  );
 
   return (
     <li
@@ -26,30 +30,12 @@ const ContentSituationsListItem = ({ player, number, situationClick, currentCard
           : null
       }
       className={classNames.join(' ')}
-      onClick={() => situationClick(player, number)}>
-      <div className={cl.portraitRectanglesWrapper}>
-        <img className={cl.portrait} src={PortraitImg} alt='Portrait' />
-        <div className={cl.rectangles}>
-          <Rectangle className={cl.topRectangle + ' ' + cl.absolute} fill={r2 && '#FFAB00'} />
-          <Rectangle className={cl.leftRectangle + ' ' + cl.absolute} fill={r3 && '#FFAB00'} />
-          <Rectangle className={cl.rightRectangle + ' ' + cl.absolute} fill={r1 && '#FFAB00'} />
-          <Ellipse className={cl.leftEllipse + ' ' + cl.absolute} fill={outs > 0 && '#1A4C96'} />
-          <Ellipse className={cl.rightEllipse + ' ' + cl.absolute} fill={outs > 1 && '#1A4C96'} />
-        </div>
-      </div>
-      <div className={cl.textEllipsesWrapper}>
-        <p className={cl.text}>{`${player.hit_order}. ${player.who}`}</p>
-        <p className={cl.text}>{eventsSummary.join('.')}</p>
-        <div className={cl.ellipsesWrapper}>
-          <div className={cl.ellipses}>
-            <Ellipse fill={balls > 0 && '#2B9D6A'} />
-            <Ellipse fill={balls > 1 && '#2B9D6A'} />
-            <Ellipse fill={balls > 2 && '#2B9D6A'} />
-            <Ellipse fill={strikes > 0 && '#E2001C'} />
-            <Ellipse fill={strikes > 1 && '#E2001C'} />
-          </div>
-        </div>
-      </div>
+      onClick={() => situationClick(player)}>
+      {situationsArr.length > 0 ? (
+        <ContentCardComplex player={player} situationsArr={situationsArr} />
+      ) : (
+        <ContentCardSimple player={player} />
+      )}
     </li>
   );
 };
