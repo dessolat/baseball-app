6d2546255b20446fcce68847cbd2590f76f403c4
Igor Nikiforov <desola2@inbox.ru>
2021-11-22 14:53:31 +0300

    Create ContentCardComplex comp, add styles

---
+++ src/components/Game/ContentCardComplex/ContentCardComplex.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentCardComplexFooter from '../ContentCardComplexFooter/ContentCardComplexFooter';
+import ContentCardComplexBody from '../ContentCardComplexBody/ContentCardComplexBody';
+import ContentCardComplexHeader from '../ContentCardComplexHeader/ContentCardComplexHeader';
+
+const ContentCardComplex = ({ player, situationsArr }) => {
+  const lastMoment = player.moments.slice(-1)[0];
+  return (
+    <div>
+      <ContentCardComplexHeader player={player} sit={situationsArr[0]} />
+      {situationsArr.slice(1).map((sit, i) => (
+        <ContentCardComplexBody key={i} sit={sit} />
+      ))}
+      {(!lastMoment.icons.rect_text || lastMoment.icons.rect_text === 'Replacement') && (
+        <ContentCardComplexFooter lastMoment={lastMoment} />
+      )}
+    </div>
+  );
+};
+
+export default ContentCardComplex;
