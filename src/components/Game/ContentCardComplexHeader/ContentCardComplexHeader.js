fa4e62c890d107827a22238c9b790eb165db114d
Igor Nikiforov <desola2@inbox.ru>
2021-11-22 14:51:23 +0300

    Refactor Complexheader

--- src/components/Game/ContentCardComplexHeader/ContentCardComplexHeader.js
+++ src/components/Game/ContentCardComplexHeader/ContentCardComplexHeader.js
@@ -3,6 +3,7 @@ import cl from './ContentCardComplexHeader.module.scss';
 import PortraitImg from 'images/portrait.png';
 import Ellipses from 'components/UI/icons/Ellipses/Ellipses';
 import RectanglesEllipses from 'components/UI/icons/RectanglesEllipses/RectanglesEllipses';
+import ContentCardReplacement from '../ContentCardReplacement/ContentCardReplacement';
 
 const ContentCardComplexHeader = ({ player, sit }) => {
   const { r1, r2, r3, outs, balls, strikes } = sit.table;
@@ -14,16 +15,19 @@ const ContentCardComplexHeader = ({ player, sit }) => {
       <div className={cl.top}>
         <div className={cl.textWrapper}>
           <p className={cl.playerName}>{`${player.hit_order}. ${player.who}`}</p>
-          <p className={cl.text}>{eventsSummary.join('.')}</p>
+          <p className={cl.text}>{sit.icons.rect_text !== 'Replacement' ? eventsSummary.join('.') : ''}</p>
         </div>
-				<img className={cl.portrait} src={PortraitImg} alt='Portrait' />
+        <img className={cl.portrait} src={PortraitImg} alt='Portrait' />
       </div>
-
       <div className={cl.bottom}>
         <RectanglesEllipses r1={r1} r2={r2} r3={r3} outs={outs} />
-        <div className={cl.rectText + ' ' + cl[sit.icons.rect_color]}>
-          {sit.icons.rect_text.toUpperCase()}
-        </div>
+
+        {sit.icons.rect_text !== 'Replacement' && (
+          <div className={cl.rectText + ' ' + cl[sit.icons.rect_color]}>
+            {sit.icons.rect_text.toUpperCase()}
+          </div>
+        )}
+
         {sit.icons.score_own !== undefined && (
           <div className={cl.rectText + ' ' + cl[sit.icons.rect_color]}>
             {sit.icons.score_gue + ' - ' + sit.icons.score_own}
@@ -31,6 +35,8 @@ const ContentCardComplexHeader = ({ player, sit }) => {
         )}
         <Ellipses balls={balls} strikes={strikes} />
       </div>
+
+      {sit.icons.rect_text === 'Replacement' && <ContentCardReplacement text={eventsSummary.join('.')} />}
     </div>
   );
 };
