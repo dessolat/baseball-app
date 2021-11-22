b4ce43002a51b7a5beba31d46365e16d0a4e67b8
Igor Nikiforov <desola2@inbox.ru>
2021-11-22 14:51:57 +0300

    Create ContentCardComplexFooter comp, add styles

---
+++ src/components/Game/ContentCardComplexFooter/ContentCardComplexFooter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import cl from './ContentCardComplexFooter.module.scss';
+import Ellipses from 'components/UI/icons/Ellipses/Ellipses';
+import RectanglesEllipses from 'components/UI/icons/RectanglesEllipses/RectanglesEllipses';
+
+const ContentCardComplexFooter = ({ lastMoment }) => {
+	const eventsSummary = [];
+  const { r1, r2, r3, outs, balls, strikes } = lastMoment?.table || 0;
+
+  lastMoment?.events?.forEach(event => eventsSummary.push(event.description));
+
+  return (
+    <div className={cl.footer}>
+      <p className={cl.text}>{eventsSummary.join('.')}</p>
+      <div className={cl.rectanglesEllipsesWrapper}>
+        <RectanglesEllipses r1={r1} r2={r2} r3={r3} outs={outs} />
+        <Ellipses balls={balls} strikes={strikes} />
+      </div>
+    </div>
+  );
+};
+
+export default ContentCardComplexFooter;
