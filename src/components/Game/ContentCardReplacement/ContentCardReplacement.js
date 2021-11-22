558061490efb6d13b9609623d2a9d1085b58d0cd
Igor Nikiforov <desola2@inbox.ru>
2021-11-22 14:52:19 +0300

    Create ContentCardReplacement comp, add styles

---
+++ src/components/Game/ContentCardReplacement/ContentCardReplacement.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import cl from './ContentCardReplacement.module.scss';
+import PortraitImg from 'images/portrait.png';
+
+const ContentCardReplacement = ({ text = '' }) => {
+  return (
+    <div className={cl.replace}>
+      <img className={cl.portrait} src={PortraitImg} alt='Portrait' />
+      <p className={cl.text}>{text}</p>
+      <img className={cl.portrait} src={PortraitImg} alt='Portrait' />
+    </div>
+  );
+};
+
+export default ContentCardReplacement;
