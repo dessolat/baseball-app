d5a994a980bd456bad9b8817b360f2dd3cf32c68
Igor Nikiforov <desola2@inbox.ru>
2021-11-23 19:27:19 +0300

    Add toggling situationFilter in FiltersSituations

--- src/components/Game/FiltersSituationsList/FiltersSituationsListItem.js
+++ src/components/Game/FiltersSituationsList/FiltersSituationsListItem.js
@@ -4,19 +4,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setPlaybackMode, setSituationFilter } from 'redux/gameReducer';
 
 const FiltersSituationsListItem = ({ situation }) => {
-	const situationFilter = useSelector(state => state.game.situationFilter)
-	const playbackMode = useSelector(state => state.game.playbackMode)
-	const dispatch = useDispatch();
+  const situationFilter = useSelector(state => state.game.situationFilter);
+  const playbackMode = useSelector(state => state.game.playbackMode);
+  const dispatch = useDispatch();
   const classes = situationFilter === situation.name ? cl.active : '';
 
   const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
-	const handleClick = e => {
-		playbackMode !== 'pause' && dispatch(setPlaybackMode('pause'))
-		dispatch(setSituationFilter(e.currentTarget.name))
-	}
+  const handleClick = e => {
+    playbackMode !== 'pause' && dispatch(setPlaybackMode('pause'));
+		const newSituationFilter = situationFilter !== e.currentTarget.name ? e.currentTarget.name : 'All'
+    dispatch(setSituationFilter(newSituationFilter));
+  };
   return (
     <li>
-      <button name={situation.name} onClick={handleClick} data-count={situation.count} className={situation.count <= 1 ? cl.hidden : ''}>
+      <button
+        name={situation.name}
+        onClick={handleClick}
+        data-count={situation.count}
+        className={situation.count <= 1 ? cl.hidden : ''}>
         <span className={classes}>{capitalizeFirstLetter(situation.name)}</span>
       </button>
     </li>
