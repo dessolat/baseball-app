18c9e81352aea8ae15f1542bc0e3fd312119977f
Igor Nikiforov <desola2@inbox.ru>
2021-11-23 17:21:20 +0300

    Add 2 more ellipses in Ellipses UI comp
    * Change width to 46px

--- src/components/UI/icons/Ellipses/Ellipses.js
+++ src/components/UI/icons/Ellipses/Ellipses.js
@@ -8,8 +8,10 @@ const Ellipses = ({balls, strikes}) => {
       <Ellipse fill={balls > 0 && '#2B9D6A'} />
       <Ellipse fill={balls > 1 && '#2B9D6A'} />
       <Ellipse fill={balls > 2 && '#2B9D6A'} />
+      <Ellipse fill={balls > 3 && '#2B9D6A'} />
       <Ellipse fill={strikes > 0 && '#E2001C'} />
       <Ellipse fill={strikes > 1 && '#E2001C'} />
+      <Ellipse fill={strikes > 2 && '#E2001C'} />
     </div>
   );
 };
