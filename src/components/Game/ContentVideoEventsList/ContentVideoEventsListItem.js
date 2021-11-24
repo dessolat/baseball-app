612823669c0d5c6f6555ab88473157901b3f05e4
Igor Nikiforov <desola2@inbox.ru>
2021-11-24 16:05:17 +0300

    Change appearance order in VideoEventsListItem
    * Make pitch circle appear first

--- src/components/Game/ContentVideoEventsList/ContentVideoEventsListItem.js
+++ src/components/Game/ContentVideoEventsList/ContentVideoEventsListItem.js
@@ -5,20 +5,16 @@ import cl from './ContentVideoEventsList.module.scss';
 const ContentVideoEventsListItem = ({ event }) => {
   return (
     <>
-      {event.circ_color_play && (
-        <li
-          className={event.circ_text_play !== 'R' ? cl[event.circ_color_play] : ''}
-          data-before={
-            (event.circ_text_pitch && event.circ_text_play === 'CS') || event.circ_text_play === 'R'
-              ? ''
-              : event.batter_moment
-          }>
-          {event.circ_text_play !== 'R' ? event.circ_text_play : <ReplaceEvent />}
-        </li>
-      )}
       {event.circ_color_pitch && (
         <li className={cl[event.circ_color_pitch]} data-before={event.batter_moment}>
           {event.circ_text_pitch}
+        </li>
+      )}
+      {event.circ_color_play && (
+        <li
+          className={event.circ_text_play !== 'R' ? cl[event.circ_color_play] : ''}
+          data-before={event.circ_text_pitch || event.circ_text_play === 'R' ? '' : event.batter_moment}>
+          {event.circ_text_play !== 'R' ? event.circ_text_play : <ReplaceEvent />}
         </li>
       )}
     </>
