f001e6c0e297a210b8aed9baba128392df44a3cb
Igor Nikiforov <desola2@inbox.ru>
2021-11-25 23:27:06 +0300

    refactor: create RectScore UI icon comp

---
+++ src/components/UI/icons/Rects/RectScore.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import cl from './Rects.module.scss';
+
+const RectScore = ({icons}) => {
+	const classes = [cl.score, cl.rectText, cl[icons.rect_color]]
+
+  return (
+    <div className={classes.join(' ')}>
+      {icons.score_gue + ' - ' + icons.score_own}
+    </div>
+  );
+};
+
+export default RectScore;
