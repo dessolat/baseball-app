0d26eaef776bd52789f5229a267ef35c10f0cb14
Igor Nikiforov <desola2@inbox.ru>
2021-11-25 23:26:19 +0300

    fix: rename attr

--- src/components/UI/buttons/PlayOnline/PlayOnline.js
+++ src/components/UI/buttons/PlayOnline/PlayOnline.js
@@ -9,7 +9,7 @@ const PlayOnline = ({ className, ...props }) => {
           d='M7.40515 16.6719L0.673439 10.875H3.70153H3.95153V10.625V0.25H10.8546V3.77778V10.625V10.875H11.1046H14.3132L7.40515 16.6719Z'
           fill='#1A4C96'
           stroke='black'
-          stroke-width='0.5'
+          strokeWidth='0.5'
         />
       </svg>
     </button>
