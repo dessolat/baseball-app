016263afbca8ff424e2b192f332e21b1275d6c17
Igor Nikiforov <desola2@inbox.ru>
2021-11-25 23:30:56 +0300

    refactor: recompose code with new comps

--- src/components/Game/ContentCardComplexBody/ContentCardComplexBody.js
+++ src/components/Game/ContentCardComplexBody/ContentCardComplexBody.js
@@ -2,7 +2,9 @@ import React from 'react';
 import cl from './ContentCardComplexBody.module.scss';
 import Ellipses from 'components/UI/icons/Ellipses/Ellipses';
 import RectanglesEllipses from 'components/UI/icons/RectanglesEllipses/RectanglesEllipses';
-import ContentCardReplacement from '../ContentCardReplacement/ContentCardReplacement'
+import ContentCardReplacement from '../ContentCardReplacement/ContentCardReplacement';
+import RectText from 'components/UI/icons/Rects/RectText';
+import RectScore from 'components/UI/icons/Rects/RectScore';
 
 const ContentCardComplexBody = ({ sit }) => {
   const { r1, r2, r3, outs, balls, strikes } = sit.table;
@@ -10,22 +12,21 @@ const ContentCardComplexBody = ({ sit }) => {
   sit.events.forEach(event => eventsSummary.push(event.description));
 
   return (
-		<>
-    {sit.icons.rect_text !== 'Replacement' ? <div>
-      <p className={cl.text}>{eventsSummary.join('.')}</p>
-      <div className={cl.bottom}>
-        <RectanglesEllipses r1={r1} r2={r2} r3={r3} outs={outs} />
-        <div className={cl.rectText + ' ' + cl[sit.icons.rect_color]}>
-          {sit.icons.rect_text.toUpperCase()}
+    <>
+      {sit.icons.rect_text !== 'Replacement' ? (
+        <div>
+          <p className={cl.text}>{eventsSummary.join('.')}</p>
+          <div className={cl.bottom}>
+            <RectanglesEllipses r1={r1} r2={r2} r3={r3} outs={outs} />
+            <RectText icons={sit.icons} />
+            {sit.icons.score_own !== undefined && <RectScore icons={sit.icons} />}
+            <Ellipses balls={balls} strikes={strikes} />
+          </div>
         </div>
-        {sit.icons.score_own !== undefined && (
-          <div className={cl.rectText + ' ' + cl[sit.icons.rect_color]}>{sit.icons.score_gue + ' - ' + sit.icons.score_own}</div>
-        )}
-        <Ellipses balls={balls} strikes={strikes} />
-      </div>
-    </div> : <ContentCardReplacement text={eventsSummary.join('.')}/>}
-		</>
-		
+      ) : (
+        <ContentCardReplacement text={eventsSummary.join('.')} />
+      )}
+    </>
   );
 };
 
