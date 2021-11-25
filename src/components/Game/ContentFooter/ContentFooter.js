b5ee07db99fa6e85054b764d20aaf7fbc90b5c0d
Igor Nikiforov <desola2@inbox.ru>
2021-11-25 18:05:26 +0300

    refactor: create ContentFooter comp

---
+++ src/components/Game/ContentFooter/ContentFooter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import cl from './ContentFooter.module.scss';
+import ContentPitcher from '../ContentPitcher/ContentPitcher';
+import ContentControls from '../ContentControls/ContentControls';
+
+const ContentFooter = () => {
+  return (
+    <div className={cl.controlsWrapper}>
+      <ContentPitcher />
+      <ContentControls />
+    </div>
+  );
+};
+
+export default ContentFooter;
