797c9e64776a28a3a2004a1e0989b2ffa2bfa804
Igor Nikiforov <desola2@inbox.ru>
2021-11-26 19:22:54 +0300

    refactor: recompose code with new comps

--- src/components/Game/ContentCardComplexHeader/ContentCardComplexHeader.js
+++ src/components/Game/ContentCardComplexHeader/ContentCardComplexHeader.js
@@ -4,6 +4,8 @@ import PortraitImg from 'images/portrait.png';
 import Ellipses from 'components/UI/icons/Ellipses/Ellipses';
 import RectanglesEllipses from 'components/UI/icons/RectanglesEllipses/RectanglesEllipses';
 import ContentCardReplacement from '../ContentCardReplacement/ContentCardReplacement';
+import RectText from 'components/UI/icons/Rects/RectText';
+import RectScore from 'components/UI/icons/Rects/RectScore';
 
 const ContentCardComplexHeader = ({ player, sit }) => {
   const { r1, r2, r3, outs, balls, strikes } = sit.table;
@@ -21,18 +23,8 @@ const ContentCardComplexHeader = ({ player, sit }) => {
       </div>
       <div className={cl.bottom}>
         <RectanglesEllipses r1={r1} r2={r2} r3={r3} outs={outs} />
-
-        {sit.icons.rect_text !== 'Replacement' && (
-          <div className={cl.rectText + ' ' + cl[sit.icons.rect_color]}>
-            {sit.icons.rect_text.toUpperCase()}
-          </div>
-        )}
-
-        {sit.icons.score_own !== undefined && (
-          <div className={cl.rectText + ' ' + cl[sit.icons.rect_color]}>
-            {sit.icons.score_gue + ' - ' + sit.icons.score_own}
-          </div>
-        )}
+        {sit.icons.rect_text !== 'Replacement' && <RectText icons={sit.icons} />}
+        {sit.icons.score_own !== undefined && <RectScore icons={sit.icons} />}
         <Ellipses balls={balls} strikes={strikes} />
       </div>
 
