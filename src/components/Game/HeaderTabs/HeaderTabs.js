e3218f81a757f911d84293f0ebc5dcd66333bec2
Igor Nikiforov <desola2@inbox.ru>
2021-11-26 19:45:36 +0300

    feat: change default tab at HeaderTabs

--- src/components/Game/HeaderTabs/HeaderTabs.js
+++ src/components/Game/HeaderTabs/HeaderTabs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSearchParams } from 'react-router-dom';
-import useTabs from 'hooks/useTabs'
+import useTabs from 'hooks/useTabs';
 import cl from './HeaderTabs.module.scss';
 
 const HeaderTabs = () => {
@@ -8,6 +8,8 @@ const HeaderTabs = () => {
   const tab = searchParams.get('tab');
 
   const handleTabClick = e => setSearchParams({ tab: e.target.name });
+
+
   return (
     <ul className={cl.headerTabs}>
       <li>
@@ -21,15 +23,12 @@ const HeaderTabs = () => {
         </button>
       </li>
       <li>
-        <button name='plays' onClick={handleTabClick} className={tab === 'plays' ? cl.active : ''}>
+        <button name='plays' onClick={handleTabClick} className={useTabs(tab) ? cl.active : ''}>
           Plays
         </button>
       </li>
       <li>
-        <button
-          name='videos'
-          onClick={handleTabClick}
-          className={useTabs(tab) ? cl.active : ''}>
+        <button name='videos' onClick={handleTabClick} className={tab === 'videos' ? cl.active : ''}>
           Videos
         </button>
       </li>
