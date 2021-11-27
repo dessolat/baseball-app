117dc0e5032bf7c212264a75d2430b78b0f15092
Igor Nikiforov <desola2@inbox.ru>
2021-11-27 16:56:25 +0300

    feat: add PlaysField comp
    * Add styles
    
    * Add basic info layout
    
    * Add background

--- src/components/Game/Plays/Plays.js
+++ src/components/Game/Plays/Plays.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import cl from './Plays.module.scss'
+import React from 'react';
+import cl from './Plays.module.scss';
+import PlaysField from '../PlaysField/PlaysField';
 
 const Plays = () => {
-	return (
-		<div className={cl.plays}>
-			<div className={cl.events}></div>
-			<div className={cl.speed}></div>
-			<div className={cl.spin}></div>
-			<div className={cl.field}></div>
-			<div className={cl.footer}></div>
-		</div>
-	)
-}
+  return (
+    <div className={cl.plays}>
+      <div className={cl.events}></div>
+      <div className={cl.speed}></div>
+      <div className={cl.spin}></div>
+      <PlaysField />
+      <div className={cl.footer}></div>
+    </div>
+  );
+};
 
-export default Plays
+export default Plays;
---
+++ src/components/Game/PlaysField/PlaysField.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from 'react';
+import cl from './PlaysField.module.scss';
+import gridImg from 'images/grid.png';
+
+const coords = [
+  { x: 75, y: 170 },
+  { x: 85, y: 155 },
+  { x: 97, y: 140 },
+  { x: 115, y: 131 },
+  { x: 135, y: 118 }
+];
+
+const PlaysField = () => {
+  const [count, setCount] = useState(1);
+  const [coeff, setCoeff] = useState({ x: 1, y: 1 });
+  const parent = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => setCount(prev => prev + 1), 80);
+    setCoeff({ x: parent.current.clientWidth / 671, y: parent.current.clientHeight / 330 });
+  }, []);
+
+  useEffect(() => {
+    if (count === 1) return;
+    count < coords.length && setTimeout(() => setCount(prev => prev + 1), 80);
+  }, [count]);
+
+  return (
+    <div className={cl.field} ref={parent}>
+      <img
+        className={cl.grid}
+        style={{ top: 21 * coeff.y, width: 109 * coeff.x, height: 158 * coeff.y }}
+        src={gridImg}
+        alt='grid'
+      />
+      {coords.slice(0, count).map((coord, i) => (
+        <div key={i} className={cl.ball} style={{ left: coord.x * coeff.x, top: coord.y * coeff.y }}></div>
+      ))}
+
+      <div className={cl.top}>
+        <div className={cl.speedData}>
+          <p className={cl.subHeader}>
+            <span className={cl.releaseSpeed}>Release speed</span>/ Plate point speed
+          </p>
+          <span className={cl.releaseValue}>88.8 mph</span>
+          <span className={cl.regularValue}>/ 73.7 mph</span>
+        </div>
+        <div className={cl.releaseData}>
+          <p className={cl.subHeader}>Release height</p>
+          <p className={cl.regularValue}>1.7 m</p>
+          <p className={cl.subHeader}>Release side</p>
+          <p className={cl.regularValue}>0.3 m</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaysField;
