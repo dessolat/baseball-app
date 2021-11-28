76eded9059df5521747cc9685bc39882849584ca
Igor Nikiforov <desola2@inbox.ru>
2021-11-28 20:46:19 +0300

    refactor: VideoEventsList and VideoEventsListItem

--- src/components/Game/VideoEventsList/VideoEventsList.js
+++ src/components/Game/VideoEventsList/VideoEventsList.js
@@ -1,26 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import useCurrentEvents from 'hooks/useCurrentEvents';
+import React from 'react';
 import cl from './VideoEventsList.module.scss';
 import VideoEventsListItem from './VideoEventsListItem';
-import { useSelector } from 'react-redux';
 
 const VideoEventsList = () => {
-  const [events, setEvents] = useState([]);
-  const currentCard = useSelector(state => state.game.currentCard);
-
-  useEffect(() => {
-    const newEvents = [];
-    currentCard.type !== 'Replacement'
-      ? currentCard.moments?.forEach(moment => moment.icons && newEvents.push(moment.icons))
-      : newEvents.push(currentCard.moments[0].icons);
-    setEvents(newEvents);
-  }, [currentCard]);
+	const moments = useCurrentEvents()
 
   return (
     <>
-      {events.length !== 0 && (
+      {moments.length !== 0 && (
         <ul className={cl.events}>
-          {events.map((event, i) => (
-            <VideoEventsListItem key={i} event={event} />
+          {moments.map((moment, i) => (
+            <VideoEventsListItem key={i} moment={moment} />
           ))}
         </ul>
       )}
--- src/components/Game/VideoEventsList/VideoEventsListItem.js
+++ src/components/Game/VideoEventsList/VideoEventsListItem.js
@@ -2,19 +2,19 @@ import React from 'react';
 import cl from './VideoEventsList.module.scss';
 import ReplaceEvent from 'components/UI/icons/ReplaceEvent';
 
-const VideoEventsListItem = ({ event }) => {
+const VideoEventsListItem = ({ moment }) => {
   return (
     <>
-      {event.circ_color_pitch && (
-        <li className={cl[event.circ_color_pitch]} data-before={event.batter_moment}>
-          {event.circ_text_pitch}
+      {moment.icons.circ_color_pitch && (
+        <li className={cl[moment.icons.circ_color_pitch]} data-before={moment.icons.batter_moment}>
+          {moment.icons.circ_text_pitch}
         </li>
       )}
-      {event.circ_color_play && (
+      {moment.icons.circ_color_play && (
         <li
-          className={event.circ_text_play !== 'R' ? cl[event.circ_color_play] : ''}
-          data-before={event.circ_text_pitch || event.circ_text_play === 'R' ? '' : event.batter_moment}>
-          {event.circ_text_play !== 'R' ? event.circ_text_play : <ReplaceEvent />}
+          className={moment.icons.circ_text_play !== 'R' ? cl[moment.icons.circ_color_play] : ''}
+          data-before={moment.icons.circ_text_pitch || moment.icons.circ_text_play === 'R' ? '' : moment.icons.batter_moment}>
+          {moment.icons.circ_text_play !== 'R' ? moment.icons.circ_text_play : <ReplaceEvent />}
         </li>
       )}
     </>
