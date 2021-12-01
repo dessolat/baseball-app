381f89d86444ce08b7d539d3456003e58dd57049
Igor Nikiforov <desola2@inbox.ru>
2021-12-01 18:11:17 +0300

    feat: add header team switching indication

--- src/components/Game/HeaderTeams/HeaderTeams.js
+++ src/components/Game/HeaderTeams/HeaderTeams.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import cl from './HeaderTeams.module.scss';
 
 const HeaderTeams = ({ names }) => {
+  const currentCard = useSelector(state => state.game.currentCard);
   const getShortName = name => (name.length > 8 ? name.slice(0, 7) + '…' : name);
+  const guestsClasses = [cl.guests];
+  const ownersClasses = [cl.owners];
+  currentCard.side === 'top' ? guestsClasses.push(cl.active) : ownersClasses.push(cl.active);
 
   return (
     <div className={cl.teamNames}>
-      <p className={cl.guests}>{getShortName(names[0])}</p>
-      <p className={cl.owners}>{getShortName(names[1])}</p>
+      <span className={guestsClasses.join(' ')}>{getShortName(names[0])}</span>
+      <span className={ownersClasses.join(' ')}>{getShortName(names[1])}</span>
     </div>
   );
 };
