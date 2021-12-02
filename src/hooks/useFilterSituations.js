2835e827b5d5c95da6066087a26be36d7b00fb7f
Igor Nikiforov <desola2@inbox.ru>
2021-12-02 14:22:19 +0300

    feat: create useSortFilteredSituations hook
    * Add sort priority

--- src/hooks/useFilterSituations.js
+++ src/hooks/useFilterSituations.js
@@ -1,16 +1,53 @@
 import { useMemo } from 'react';
 
-const useFilterSituations = newSituations =>
+const SORT_PRIORITY = {
+  All: 1,
+  Score: 2,
+  Single: 3,
+  Double: 4,
+  Triple: 5,
+  'Home Run': 6,
+  Strikeout: 7,
+  'Double Play': 8,
+  Stealing: 9,
+  'Caught Stealing': 10,
+  Error: 11,
+  'Wild Pitch': 12,
+  'Passed Ball': 13,
+  Walk: 14,
+  'Hit By Pitch': 15,
+  Balk: 16,
+  Groundout: 17,
+  GDP: 18,
+  Flyout: 19,
+  Pickoff: 20,
+  Sacrifice: 21,
+  Replace: 22
+};
+
+export const useFilterSituations = newSituations =>
   useMemo(() => {
-    let filteredSituations = [{ name: 'All', count: 1 }];
+    let filteredSituations = [{ name: 'All', count: 1, priority: 1 }];
     for (let newSituation of newSituations) {
       let count = newSituations.filter(sit => sit === newSituation).length;
       let index = filteredSituations.findIndex(sit => sit.name === newSituation);
       if (index !== -1) continue;
-      filteredSituations[filteredSituations.length] = { name: newSituation, count };
+      filteredSituations[filteredSituations.length] = {
+        name: newSituation,
+        count,
+        priority: SORT_PRIORITY[newSituation]
+      };
     }
 
     return filteredSituations;
   }, [newSituations]);
 
-export default useFilterSituations;
+export const useSortFilteredSituations = newSituations => {
+  const sortedSituations = useFilterSituations(newSituations);
+
+  return useMemo(() => {
+    sortedSituations.sort((a, b) => (a.priority > b.priority ? 1 : -1));
+
+    return sortedSituations;
+  }, [sortedSituations]);
+};
