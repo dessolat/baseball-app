a6c484bab30c29fa0e45762f8eba13729fe12d79
Igor Nikiforov <desola2@inbox.ru>
2021-12-02 21:49:05 +0300

    refactor: create PlaysPitch comp

---
+++ src/components/Game/PlaysPitch/PlaysPitch.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PlaysField from '../PlaysField/PlaysField';
+import PlaysSpin from '../PlaysSpin/PlaysSpin';
+import PlaysSpeed from '../PlaysSpeed/PlaysSpeed';
+
+const PlaysPitch = () => {
+	return (
+		<>
+			<PlaysSpeed />
+      <PlaysSpin />
+      <PlaysField />
+		</>
+	)
+}
+
+export default PlaysPitch
