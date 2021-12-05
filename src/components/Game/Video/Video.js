06b7a1e6c09eacbcbbfcad38faa533a00da5c216
Igor Nikiforov <desola2@inbox.ru>
2021-12-05 19:24:08 +0300

    feat: basic Video functional

--- src/components/Game/Video/Video.js
+++ src/components/Game/Video/Video.js
@@ -1,18 +1,87 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import cl from './Video.module.scss';
+import YouTube from 'react-youtube';
 import VideoEventsList from '../VideoEventsList/VideoEventsList';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentCard } from 'redux/gameReducer';
 
 const Video = () => {
+  const videoRef = useRef(null);
+  const endRef = useRef(null);
+  const intervalRef = useRef(null);
+  const momentRef = useRef(0);
+  const currentCard = useSelector(state => state.game.currentCard);
+  const filteredCards = useSelector(state => state.game.filteredCards);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (videoRef.current === null) return;
+		videoHandling()
+    // eslint-disable-next-line
+  }, [currentCard]);
+
+  const onReady = e => {
+    videoRef.current = e.target;
+    videoHandling()
+  };
+
+	const videoHandling = () => {
+		clearInterval(intervalRef.current);
+    momentRef.current = 0;
+
+    videoRef.current.seekTo(currentCard.moments[0].video.seconds_from);
+    endRef.current = currentCard.moments[0].video.seconds_to;
+
+    intervalRef.current = setInterval(() => {
+      const currentTime = videoRef.current.getCurrentTime();
+      console.log(currentTime, endRef.current);
+
+      if (currentTime >= endRef.current) {
+        momentRef.current += 1;
+
+        if (momentRef.current < currentCard.moments.length) {
+          videoRef.current.seekTo(currentCard.moments[momentRef.current].video.seconds_from);
+          endRef.current = currentCard.moments[momentRef.current].video.seconds_to;
+        } else {
+          let index = filteredCards.findIndex(
+            card => card.inning_number === currentCard.inning_number && card.who_id === currentCard.who_id
+          );
+          index++;
+          index < filteredCards.length
+            ? dispatch(setCurrentCard(filteredCards[index]))
+            : clearInterval(intervalRef.current);
+        }
+      }
+    }, 500);
+	}
+
   return (
     <div className={cl.videoWrapper + ' ' + cl.videoOne}>
-      <iframe
-        width='100%'
-        height='100%'
-        src='https://www.youtube.com/embed/gN7RdRGmBF4?autoplay=1'
-        title='YouTube video player'
-        frameBorder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowFullScreen></iframe>
+      {Object.keys(currentCard).length !== 0 ? (
+        <YouTube
+          videoId={'WCjLd7QAJq8'}
+          onReady={onReady}
+          opts={{
+            height: '100%',
+            width: '100%',
+            playerVars: {
+              autoplay: 1,
+              controls: 0,
+              modestbranding: 1
+              // loop: 1,
+              // playlist: '-GR52czEd-0'
+            }
+          }}
+        />
+      ) : (
+        <></>
+      )}
       <VideoEventsList />
     </div>
   );
