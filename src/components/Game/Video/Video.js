4d0508c970a9f81c9f828f3afe52278fe676fba2
Igor Nikiforov <desola2@inbox.ru>
2021-12-06 18:51:38 +0300

    feat: add correct playing on filter change to all

--- src/components/Game/Video/Video.js
+++ src/components/Game/Video/Video.js
@@ -3,16 +3,18 @@ import cl from './Video.module.scss';
 import YouTube from 'react-youtube';
 import VideoEventsList from '../VideoEventsList/VideoEventsList';
 import { useSelector, useDispatch } from 'react-redux';
-import { setCurrentCard } from 'redux/gameReducer';
+import { setCurrentCard, setPlaybackMode } from 'redux/gameReducer';
 
 const Video = () => {
   const videoRef = useRef(null);
   const endRef = useRef(null);
   const intervalRef = useRef(null);
   const momentRef = useRef(0);
+  const modeRef = useRef('play');
   const currentCard = useSelector(state => state.game.currentCard);
   const filteredCards = useSelector(state => state.game.filteredCards);
   const playbackMode = useSelector(state => state.game.playbackMode);
+  const situationFilter = useSelector(state => state.game.situationFilter);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -27,6 +29,50 @@ const Video = () => {
     // eslint-disable-next-line
   }, [currentCard]);
 
+  useEffect(() => {
+    modeRef.current = playbackMode;
+  }, [playbackMode]);
+
+  useEffect(() => {
+    if (situationFilter !== 'All' || !videoRef.current) return;
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      const currentTime = videoRef.current.getCurrentTime();
+
+      if (currentTime >= endRef.current) {
+        momentRef.current += 1;
+
+        if (momentRef.current < currentCard.moments.length) {
+          videoRef.current.seekTo(currentCard.moments[momentRef.current].video.seconds_from);
+          endRef.current = currentCard.moments[momentRef.current].video.seconds_to;
+        } else {
+          if (modeRef.current === 'pause') {
+            videoHandling();
+          } else {
+            let index = filteredCards.findIndex(
+              card =>
+                card.inning_number === currentCard.inning_number &&
+                card.who_id === currentCard.who_id &&
+                card.type === currentCard.type
+            );
+
+            index++;
+
+            if (index < filteredCards.length) {
+              dispatch(setCurrentCard(filteredCards[index]));
+              return;
+            }
+
+            dispatch(setPlaybackMode('pause'));
+            // clearInterval(intervalRef.current);
+          }
+        }
+      }
+    }, 500);
+    // eslint-disable-next-line
+  }, [filteredCards]);
+
   const onReady = e => {
     videoRef.current = e.target;
     videoHandling();
@@ -36,12 +82,17 @@ const Video = () => {
     clearInterval(intervalRef.current);
     momentRef.current = 0;
 
+    if (currentCard.moments.length === 0 || !currentCard.moments[0].video) {
+      videoRef.current.pauseVideo();
+      return;
+    }
+
+    videoRef.current.getPlayerState() !== 1 && videoRef.current.playVideo();
     videoRef.current.seekTo(currentCard.moments[0].video.seconds_from);
     endRef.current = currentCard.moments[0].video.seconds_to;
 
     intervalRef.current = setInterval(() => {
       const currentTime = videoRef.current.getCurrentTime();
-      console.log(currentTime, endRef.current);
 
       if (currentTime >= endRef.current) {
         momentRef.current += 1;
@@ -50,16 +101,25 @@ const Video = () => {
           videoRef.current.seekTo(currentCard.moments[momentRef.current].video.seconds_from);
           endRef.current = currentCard.moments[momentRef.current].video.seconds_to;
         } else {
-          if (playbackMode === 'pause') {
+          if (modeRef.current === 'pause') {
             videoHandling();
           } else {
             let index = filteredCards.findIndex(
-              card => card.inning_number === currentCard.inning_number && card.who_id === currentCard.who_id
+              card =>
+                card.inning_number === currentCard.inning_number &&
+                card.who_id === currentCard.who_id &&
+                card.type === currentCard.type
             );
+
             index++;
-            index < filteredCards.length
-              ? dispatch(setCurrentCard(filteredCards[index]))
-              : clearInterval(intervalRef.current);
+
+            if (index < filteredCards.length) {
+              dispatch(setCurrentCard(filteredCards[index]));
+              return;
+            }
+
+            dispatch(setPlaybackMode('pause'));
+            // clearInterval(intervalRef.current);
           }
         }
       }
@@ -78,7 +138,8 @@ const Video = () => {
             playerVars: {
               autoplay: 1,
               controls: 0,
-              modestbranding: 1
+              modestbranding: 1,
+              disablekb: 1
               // loop: 1,
               // playlist: '-GR52czEd-0'
             }
