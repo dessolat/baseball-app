1d3d7e3ed00a1bf5700f1a2483920bfe6fb55531
Igor Nikiforov <desola2@inbox.ru>
2021-12-08 20:49:24 +0300

    feat: add portrait image

--- src/components/Game/ContentCardSimple/ContentCardSimple.js
+++ src/components/Game/ContentCardSimple/ContentCardSimple.js
@@ -3,14 +3,15 @@ import cl from './ContentCardSimple.module.scss';
 import Ellipses from 'components/UI/icons/Ellipses/Ellipses';
 import RectanglesEllipses from 'components/UI/icons/RectanglesEllipses/RectanglesEllipses';
 import PortraitImg from 'images/portrait.png';
+import { useSelector } from 'react-redux';
 
-const ContentCardSimple = ({player}) => {
-	const eventsSummary = [];
+const ContentCardSimple = ({ player }) => {
+  const playersInfo = useSelector(state => state.game.playersInfo);
+  const eventsSummary = [];
   const lastMoment = player.moments.slice(-1)[0];
   const { r1, r2, r3, outs, balls, strikes } = lastMoment?.table || 0;
 
   lastMoment?.events?.forEach(event => eventsSummary.push(event.description));
-
 
   return (
     <div className={cl.classic}>
@@ -18,10 +19,18 @@ const ContentCardSimple = ({player}) => {
         <p className={cl.playerName}>{`${player.hit_order}. ${player.who}`}</p>
         <p className={cl.text}>{eventsSummary.join('.') + '.'}</p>
         <RectanglesEllipses r1={r1} r2={r2} r3={r3} outs={outs} />
-				
       </div>
       <div className={cl.portraitEllipsesWrapper}>
-        <img className={cl.portrait} src={PortraitImg} alt='Portrait' />
+        <div className={cl.portrait}>
+          <img
+            src={
+              playersInfo[player.who] && playersInfo[player.who] !== ''
+                ? `http://84.201.172.216:3030/logo/${playersInfo[player.who]}`
+                : PortraitImg
+            }
+            alt='Portrait'
+          />
+        </div>
         <Ellipses balls={balls} strikes={strikes} />
       </div>
     </div>
