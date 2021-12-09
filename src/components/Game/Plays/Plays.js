2bf5bc2965083c1f3841f7e31404d1e468a359aa
Igor Nikiforov <desola2@inbox.ru>
2021-12-09 13:47:21 +0300

    feat: add moments state

--- src/components/Game/Plays/Plays.js
+++ src/components/Game/Plays/Plays.js
@@ -6,10 +6,17 @@ import PlaysContent from '../PlaysContent/PlaysContent';
 
 const Plays = () => {
   const [footerTab, setFooterTab] = useState('pitch');
-	const classes = [cl.plays]
-	// footerTab === 'pitch' && classes.push(cl.pitch)
-	classes.push(cl.pitch)
+  const [moments, setMoments] = useState([]);
+  useEffect(() => {
+    const newMoments = [];
+    currentCard.type !== 'Replacement'
+      ? currentCard.moments?.forEach(moment => moment.icons && newMoments.push(moment))
+      : newMoments.push(currentCard.moments[0]);
+    setMoments(newMoments);
+  }, [currentCard]);
 
+  const classes = [cl.plays, cl.pitch];
+  // footerTab === 'pitch' && classes.push(cl.pitch)
   return (
     <div className={classes.join(' ')}>
       <PlaysEvents />
