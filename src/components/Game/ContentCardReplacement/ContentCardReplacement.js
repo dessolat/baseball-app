bbe961fc8956040b6fe5ce075b12aadf9df5afa8
Igor Nikiforov <desola2@inbox.ru>
2021-12-10 18:49:43 +0300

    feat: add default styles for portraits images
    * change text aligning to left side

--- src/components/Game/ContentCardReplacement/ContentCardReplacement.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacement.js
@@ -6,9 +6,22 @@ const ContentCardReplacement = ({ text = '' }) => {
   return (
     <div className={cl.replace}>
       {/* <img className={cl.portrait} src={require('images/portrait.png').default} alt='Portrait' /> */}
-      <img className={cl.portrait} src={PortraitImg} alt='Portrait' />
+      <div className={cl.portrait}>
+          <img
+						className={cl.default}
+            src={PortraitImg}
+            alt='Portrait'
+          />
+        </div>
       <p className={cl.text}>{text + '.'}</p>
-      <img className={cl.portrait} src={PortraitImg} alt='Portrait' />
+      {/* <img className={cl.portrait} src={PortraitImg} alt='Portrait' /> */}
+			<div className={cl.portrait}>
+          <img
+						className={cl.default}
+            src={PortraitImg}
+            alt='Portrait'
+          />
+        </div>
     </div>
   );
 };
