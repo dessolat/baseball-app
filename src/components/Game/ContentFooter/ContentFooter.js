c7f8a8b76fb1b1d17c65559fa5fe841186b0c1cb
Igor Nikiforov <desola2@inbox.ru>
2021-12-11 14:52:43 +0300

    feat: get player button back in all tabs

--- src/components/Game/ContentFooter/ContentFooter.js
+++ src/components/Game/ContentFooter/ContentFooter.js
@@ -2,18 +2,12 @@ import React from 'react';
 import cl from './ContentFooter.module.scss';
 import ContentPitcher from '../ContentPitcher/ContentPitcher';
 import ContentControls from '../ContentControls/ContentControls';
-import { useSearchParams } from 'react-router-dom';
 
-const ContentFooter = () => {
-	const [searchParams] = useSearchParams();
-  const tab = searchParams.get('tab');
-
-  return (
-    <div className={cl.controlsWrapper}>
-      <ContentPitcher />
-      {tab === 'videos' && <ContentControls />}
-    </div>
-  );
-};
+const ContentFooter = () => (
+  <div className={cl.controlsWrapper}>
+    <ContentPitcher />
+    <ContentControls />
+  </div>
+);
 
 export default ContentFooter;
