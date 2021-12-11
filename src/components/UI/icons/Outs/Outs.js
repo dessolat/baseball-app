a0180cf2417bb06ad7aa1ea2510ae0823a85904a
Igor Nikiforov <desola2@inbox.ru>
2021-12-11 14:34:45 +0300

    fix: remove excessive props

--- src/components/UI/icons/Outs/Outs.js
+++ src/components/UI/icons/Outs/Outs.js
@@ -5,8 +5,8 @@ import cl from './Outs.module.scss';
 const Outs = ({ outs }) => {
   return (
     <div className={cl.ellipses}>
-      <Ellipse className={cl.leftEllipse + ' ' + cl.absolute} fill={outs > 0 && '#1A4C96'} />
-      <Ellipse className={cl.rightEllipse + ' ' + cl.absolute} fill={outs > 1 && '#1A4C96'} />
+      <Ellipse fill={outs > 0 && '#1A4C96'} />
+      <Ellipse fill={outs > 1 && '#1A4C96'} />
     </div>
   );
 };
