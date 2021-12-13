7e6047cc3c86c0641c1ba65c5df3312de626a06d
Igor Nikiforov <desola2@inbox.ru>
2021-12-13 20:10:49 +0300

    feat: add currentMoment prop

--- src/components/Game/PlaysPitch/PlaysPitch.js
+++ src/components/Game/PlaysPitch/PlaysPitch.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 import PlaysField from '../PlaysField/PlaysField';
 import PlaysSpin from '../PlaysSpin/PlaysSpin';
 import PlaysSpeed from '../PlaysSpeed/PlaysSpeed';
 
-const PlaysPitch = ({currentMoment}) => {
-	return (
-		<>
-			<PlaysSpeed />
-      <PlaysSpin />
-      <PlaysField currentMoment={currentMoment}/>
-		</>
-	)
-}
+const PlaysPitch = ({ currentMoment }) => {
+  return (
+    <>
+      <PlaysSpeed />
+      <PlaysSpin currentMoment={currentMoment} />
+      <PlaysField currentMoment={currentMoment} />
+    </>
+  );
+};
 
-export default PlaysPitch
+export default PlaysPitch;
