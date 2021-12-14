354946f9b462778bbf75bf079b557e499829b4d2
Igor Nikiforov <desola2@inbox.ru>
2021-12-14 21:43:36 +0300

    feat: add PlaysHitting to PlaysContent render

--- src/components/Game/PlaysContent/PlaysContent.js
+++ src/components/Game/PlaysContent/PlaysContent.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import PlaysHitting from '../PlaysHitting/PlaysHitting';
 import PlaysPitch from '../PlaysPitch/PlaysPitch';
 
-const PlaysContent = ({ footerTab, currentMoment }) => <>{footerTab === 'pitch' ? <PlaysPitch currentMoment={currentMoment}/> : <></>}</>;
+const PlaysContent = ({ footerTab, currentMoment }) => (
+  <>{footerTab === 'pitch' ? <PlaysPitch currentMoment={currentMoment} /> : <PlaysHitting />}</>
+);
 export default PlaysContent;
