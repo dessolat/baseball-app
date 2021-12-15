66b5957b6e6d1255cae651d54ca8e7a0ea7b6e60
Igor Nikiforov <desola2@inbox.ru>
2021-12-15 19:57:22 +0300

    feat: add currentDot state to PlaysHitting comp

--- src/components/Game/PlaysHitting/PlaysHitting.js
+++ src/components/Game/PlaysHitting/PlaysHitting.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlaysBat from '../PlaysBat/PlaysBat';
 import PlaysStats from '../PlaysStats/PlaysStats';
 
-const PlaysHitting = () => (
-  <>
-    <PlaysBat />
-		<PlaysStats />
-  </>
-);
+const PlaysHitting = () => {
+  const [currentDot, setCurrentDot] = useState('');
+
+  const handleDotClick = str => () => setCurrentDot(str);
+
+  return (
+    <>
+      <PlaysBat currentDot={currentDot} handleDotClick={handleDotClick} />
+      <PlaysStats />
+    </>
+  );
+};
 
 export default PlaysHitting;
