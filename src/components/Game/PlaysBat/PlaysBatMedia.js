0e7f6e1ca5133f4c81e786fe70405a469a928b0f
Igor Nikiforov <desola2@inbox.ru>
2021-12-17 23:12:33 +0300

    feat: add currentMoment change handling

--- src/components/Game/PlaysBat/PlaysBatMedia.js
+++ src/components/Game/PlaysBat/PlaysBatMedia.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import cl from './PlaysBat.module.scss';
 // import BatterImg from 'images/batter.jpg';
+
 const oldCoords = [
   [35, 40],
   [50, 74],
@@ -36,7 +37,51 @@ for (let i = 0; i < oldCoords.length; i++) {
     coords.push([oldCoords[i-1][0] + (xDiff / 10) * j, oldCoords[i-1][1] + (yDiff / 10) * j]);
   }
 }
+
 const PlaysBatMedia = ({ currentMoment }) => {
+  const [curvePath, setCurvePath] = useState('');
+  const [frame, setFrame] = useState(0);
+  const maxFrameRef = useRef(null);
+	const timeoutRef = useRef(null)
+  // let curvePath = `M${coords[0][0]} ${coords[0][1]}`;
+  // coords.slice(1).forEach(coord => (curvePath += `L${coord[0]} ${coord[1]}`));
+  // console.log(curvePath);
+
+  useEffect(() => {
+		clearTimeout(timeoutRef.current)
+    // if (Object.keys(currentMoment).length === 0) {
+			// setCurvePath('')
+			// return}
+    maxFrameRef.current = coords.length / 2;
+    setFrame(1);
+  }, [currentMoment]);
+
+  useEffect(() => {
+		if (frame === 0) return
+    if (frame > maxFrameRef.current) {
+      maxFrameRef.current = null;
+      return;
+    }
+
+    let newCurve = '';
+    coords
+      .slice(0, frame)
+      .forEach(
+        (coord, i) => (newCurve += i === 0 ? `M${coords[0][0]} ${coords[0][1]}` : `L${coord[0]} ${coord[1]}`)
+      );
+
+    let backPath = '';
+    coords
+      .slice()
+      .reverse()
+      .slice(0, frame)
+      .forEach(coord => (backPath = `L${coord[0]} ${coord[1]}` + backPath));
+    newCurve += backPath;
+    setCurvePath(newCurve);
+
+    timeoutRef.current = setTimeout(() => setFrame(prev => prev + 1), 10);
+  }, [frame]);
+
   return (
     <div className={cl.batter}>
       <svg width='100%' height='235' viewBox='0 0 355 235' fill='none' xmlns='http://www.w3.org/2000/svg'>
