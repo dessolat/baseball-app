810925e6f47992aeea4aebe03db55db851e9951e
Igor Nikiforov <desola2@inbox.ru>
2021-12-19 16:42:04 +0300

    feat: create PlaysRunning comp placeholder

---
+++ src/components/Game/PlaysRunning/PlaysRunning.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PlaysRunningField from './PlaysRunningField';
+import PlaysRunningInfo from './PlaysRunningInfo';
+
+const PlaysRunning = () => {
+  return <></>;
+};
+
+export default PlaysRunning;
