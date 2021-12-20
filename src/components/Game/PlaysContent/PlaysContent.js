4e0540128dfca6f94ddf0374fa722f5829548fa6
Igor Nikiforov <desola2@inbox.ru>
2021-12-20 23:00:10 +0300

    feat: add PlaysRunning comp handling

--- src/components/Game/PlaysContent/PlaysContent.js
+++ src/components/Game/PlaysContent/PlaysContent.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PlaysHitting from '../PlaysHitting/PlaysHitting';
 import PlaysPitch from '../PlaysPitch/PlaysPitch';
+import PlaysRunning from '../PlaysRunning/PlaysRunning';
 
 const PlaysContent = ({ footerTab, currentMoment }) => {
   const renderComponent =
-    footerTab === 'pitch' ? <PlaysPitch currentMoment={currentMoment} /> : <PlaysHitting currentMoment={currentMoment}/>;
+    footerTab === 'pitch' ? (
+      <PlaysPitch currentMoment={currentMoment} />
+    ) : footerTab === 'hitting' ? (
+      <PlaysHitting currentMoment={currentMoment} />
+    ) : (
+      <PlaysRunning currentMoment={currentMoment} />
+    );
 
   return <>{renderComponent}</>;
 };
