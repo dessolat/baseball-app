f77d98fc1660f573de9a2d6234efae70e17c82a2
Igor Nikiforov <desola2@inbox.ru>
2021-12-21 21:23:24 +0300

    refactor: move logic from render

--- src/components/Game/FiltersSituationsList/FiltersSituationsList.js
+++ src/components/Game/FiltersSituationsList/FiltersSituationsList.js
@@ -4,13 +4,13 @@ import cl from './FiltersSituationsList.module.scss';
 import { useSortFilteredSituations } from 'hooks/useFilterSituations';
 
 const FiltersSituationsList = ({ situations }, ref) => {
-  const filteredSituations = useSortFilteredSituations(situations);
+  const filteredSituations = useSortFilteredSituations(situations).map((situation, i) => (
+    <FiltersSituationsListItem key={i} situation={situation} />
+  ));
 
   return (
     <ul ref={ref} className={cl.situationsList}>
-      {filteredSituations.map((situation, i) => (
-        <FiltersSituationsListItem key={i} situation={situation} />
-      ))}
+      {filteredSituations}
     </ul>
   );
 };
