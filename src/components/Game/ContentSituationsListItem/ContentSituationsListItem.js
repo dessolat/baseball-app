fea012429d34d76fc9d4e83bb990fec8a06d824c
Igor Nikiforov <desola2@inbox.ru>
2021-12-21 21:22:57 +0300

    refactor: move ref logic from render

--- src/components/Game/ContentSituationsListItem/ContentSituationsListItem.js
+++ src/components/Game/ContentSituationsListItem/ContentSituationsListItem.js
@@ -7,26 +7,18 @@ import ContentCardReplacement from '../ContentCardReplacement/ContentCardReplace
 const ContentSituationsListItem = ({ player, situationClick, currentCard }, ref) => {
   const classNames = [cl.listItem];
 
-  // player.type === currentCard.type &&
-  //   player.who_id === currentCard.who_id &&
-  //   player.inning_number === currentCard.inning_number &&
-  currentCard.moments && player.moments[0].inner.id === currentCard.moments[0].inner.id && classNames.push(cl.active);
+  currentCard.moments &&
+    player.moments[0].inner.id === currentCard.moments[0].inner.id &&
+    classNames.push(cl.active);
 
   //Filling situationsArr with moments where icons.rect_text exists
   const situationsArr = [];
   player.moments.forEach(moment => moment.icons?.rect_text && situationsArr.push(moment));
 
+	const itemRef = currentCard.moments && player.moments[0].inner.id === currentCard.moments[0].inner.id ? ref : null || null
   return (
     <li
-      ref={
-				currentCard.moments && player.moments[0].inner.id === currentCard.moments[0].inner.id
-        // player.who_id === currentCard.who_id &&
-        // player.inning_number === currentCard.inning_number &&
-        // player.type === currentCard.type
-				
-          ? ref
-          : null
-      }
+      ref={itemRef}
       className={classNames.join(' ')}
       onClick={() => situationClick(player)}>
       {player.type === 'Replacement' ? (
