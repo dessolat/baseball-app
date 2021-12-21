7df2f8fcb182ab6faec7fba17e9eac85226aee4b
Igor Nikiforov <desola2@inbox.ru>
2021-12-21 21:20:12 +0300

    refactor: move logic from render

--- src/components/Game/FiltersViewModes/FiltersViewModes.js
+++ src/components/Game/FiltersViewModes/FiltersViewModes.js
@@ -4,28 +4,28 @@ import { setViewMode } from 'redux/gameReducer';
 import cl from './FiltersViewModes.module.scss';
 
 const FiltersViewModes = () => {
-	const viewMode = useSelector(state => state.game.viewMode)
-	const dispatch = useDispatch()
+  const viewMode = useSelector(state => state.game.viewMode);
+  const dispatch = useDispatch();
 
-	const handleModeClick = e => {
-		dispatch(setViewMode(e.currentTarget.name))
-  };
+  const handleModeClick = e => dispatch(setViewMode(e.currentTarget.name));
+  const getClassName = mode => (viewMode === mode ? cl.active : '');
+
   return (
     <div className={cl.viewModes}>
-      <button className={viewMode === 'mode-1' ? cl.active : ''} name='mode-1' onClick={handleModeClick}>
+      <button className={getClassName('mode-1')} name='mode-1' onClick={handleModeClick}>
         <div></div>
       </button>
-      <button className={viewMode === 'mode-2' ? cl.active : ''} name='mode-2' onClick={handleModeClick}>
-        <div></div>
-        <div></div>
-      </button>
-      <button className={viewMode === 'mode-3' ? cl.active : ''} name='mode-3' onClick={handleModeClick}>
-        <div></div>
-        <div></div>
+      <button className={getClassName('mode-2')} name='mode-2' onClick={handleModeClick}>
         <div></div>
         <div></div>
       </button>
-      <button className={viewMode === 'mode-4' ? cl.active : ''} name='mode-4' onClick={handleModeClick}>
+      <button className={getClassName('mode-3')} name='mode-3' onClick={handleModeClick}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+      <button className={getClassName('mode-4')} name='mode-4' onClick={handleModeClick}>
         <div></div>
         <div></div>
         <div></div>
