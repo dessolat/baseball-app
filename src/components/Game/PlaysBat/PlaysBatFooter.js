83a611b9d36622c84735d8444b285246e9e8d3ce
Igor Nikiforov <desola2@inbox.ru>
2021-12-22 19:18:34 +0300

    feat: add slider animation
    * create getDotColor function

--- src/components/Game/PlaysBat/PlaysBatFooter.js
+++ src/components/Game/PlaysBat/PlaysBatFooter.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import cl from './PlaysBat.module.scss';
 
-const PlaysBatFooter = ({ currentDot, handleDotClick }) => {
-  const fillColors = {
-    first: currentDot === 'first' ? '#1A4C96' : '#E8E6E6',
-    second: currentDot === 'second' ? '#1A4C96' : '#E8E6E6',
-    third: currentDot === 'third' ? '#1A4C96' : '#E8E6E6',
-    fourth: currentDot === 'fourth' ? '#1A4C96' : '#E8E6E6'
-  };
+const PlaysBatFooter = ({ currentDot, currentMoment, handleDotClick }) => {
+  const dotRef = useRef(null);
+
+  useEffect(() => {
+    if (Object.keys(currentMoment).length === 0 || dotRef.current === null) return;
+    dotRef.current.classList.remove(cl.slider);
+    setTimeout(() => {
+      dotRef.current.classList.add(cl.slider);
+    }, 0);
+  }, [currentMoment]);
+
+  const getDotColor = order => (currentDot === order ? '#1A4C96' : '#E8E6E6');
+
+  const isDots = !(Object.keys(currentMoment).length === 0) && currentMoment.events[0].type !== 'replace';
 
   return (
     <div className={cl.footer}>
@@ -16,51 +23,55 @@ const PlaysBatFooter = ({ currentDot, handleDotClick }) => {
           Bat speed (mph)
         </text>
         <path d='M1 25L330 25' stroke='#1A4C96' />
-        <text x='182.5' y='12' className={cl.value}>
-          90
-        </text>
-        <circle
-          cx='190.5'
-          cy='25.38184'
-          r='5'
-          className={cl.dot}
-          onClick={handleDotClick('first')}
-          fill={fillColors.first}
-        />
-        <text x='228.5' y='12' className={cl.value}>
-          90
-        </text>
-        <circle
-          cx='236.5'
-          cy='25.38184'
-          r='5'
-          className={cl.dot}
-          onClick={handleDotClick('second')}
-          fill={fillColors.second}
-        />
-        <text x='268.5' y='12' className={cl.value}>
-          90
-        </text>
-        <circle
-          cx='277.5'
-          cy='25.38184'
-          r='5'
-          className={cl.dot}
-          onClick={handleDotClick('third')}
-          fill={fillColors.third}
-        />
-        <text x='318.7' y='12' className={cl.value}>
-          90
-        </text>
-        <circle
-          cx='326.5'
-          cy='25.5'
-          r='5'
-          className={cl.dot}
-          onClick={handleDotClick('fourth')}
-          fill={fillColors.fourth}
-        />
-        <circle cx='326.5' cy='25.5' r='7.5' className={cl.slider} fill='#4AA0F0' />
+        {isDots && (
+          <>
+            <text x='182.5' y='12' className={cl.value}>
+              90
+            </text>
+            <circle
+              cx='190.5'
+              cy='25.38184'
+              r='5'
+              className={cl.dot}
+              onClick={handleDotClick('first')}
+              fill={getDotColor('first')}
+            />
+            <text x='228.5' y='12' className={cl.value}>
+              90
+            </text>
+            <circle
+              cx='236.5'
+              cy='25.38184'
+              r='5'
+              className={cl.dot}
+              onClick={handleDotClick('second')}
+              fill={getDotColor('second')}
+            />
+            <text x='268.5' y='12' className={cl.value}>
+              90
+            </text>
+            <circle
+              cx='277.5'
+              cy='25.38184'
+              r='5'
+              className={cl.dot}
+              onClick={handleDotClick('third')}
+              fill={getDotColor('third')}
+            />
+            <text x='318.7' y='12' className={cl.value}>
+              90
+            </text>
+            <circle
+              cx='326.5'
+              cy='25.5'
+              r='5'
+              className={cl.dot}
+              onClick={handleDotClick('fourth')}
+              fill={getDotColor('fourth')}
+            />
+            <circle cx='1' cy='25.5' r='7.5' className={cl.slider} ref={dotRef} fill='#4AA0F0' />
+          </>
+        )}
       </svg>
     </div>
   );
