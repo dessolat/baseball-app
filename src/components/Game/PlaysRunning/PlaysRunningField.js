c18b0589faf4803351bbed0c61a42c9ae2cf123a
Igor Nikiforov <desola2@inbox.ru>
2021-12-22 19:40:57 +0300

    feat: change tag to svg

--- src/components/Game/PlaysRunning/PlaysRunningField.js
+++ src/components/Game/PlaysRunning/PlaysRunningField.js
@@ -2,7 +2,7 @@ import React from 'react';
 import cl from './PlaysRunning.module.scss';
 
 const PlaysRunningField = () => {
-  return <div className={cl.field}></div>;
+  return <svg className={cl.field} width='100%' height='100%' viewBox='0 0 2560 2560' fill='none'></svg>;
 };
 
 export default PlaysRunningField;
