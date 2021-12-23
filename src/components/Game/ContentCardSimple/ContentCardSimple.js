87ce4fe1d1128ff140beae0dbbb284d0f3bc00e2
Igor Nikiforov <desola2@inbox.ru>
2021-12-23 21:41:06 +0300

    feat: remove fetch on mount

--- src/components/Game/ContentCardSimple/ContentCardSimple.js
+++ src/components/Game/ContentCardSimple/ContentCardSimple.js
@@ -1,17 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import cl from './ContentCardSimple.module.scss';
 import BallsStrikes from 'components/UI/icons/BallsStrikes/BallsStrikes';
 import Bases from 'components/UI/icons/Bases/Bases';
 import PortraitImg from 'images/portrait.png';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import { setImagesData } from 'redux/gameReducer';
+import { useSelector } from 'react-redux';
 import Outs from 'components/UI/icons/Outs/Outs';
 
 const ContentCardSimple = ({ player }) => {
-  const playersInfo = useSelector(state => state.game.playersInfo);
   const imagesData = useSelector(state => state.game.imagesData);
-  const dispatch = useDispatch();
 
   const lastMoment = player.moments.slice(-1)[0];
   const { r1, r2, r3, outs, balls, strikes } = lastMoment?.table || 0;
@@ -19,27 +15,6 @@ const ContentCardSimple = ({ player }) => {
   const eventsSummary = lastMoment?.events?.reduce((sum, event) => [...sum, event.description], []) || [];
   const cardText = eventsSummary.length > 0 ? eventsSummary.join('.') + '.' : '';
   const imgClassName = !imagesData[player.who] ? cl.default : '';
-
-  useEffect(() => {
-    const fetchImage = async () => {
-      try {
-        const response = await axios.get(`http://84.201.172.216:3030/logo/${playersInfo[player.who]}`, {
-          responseType: 'arraybuffer'
-        });
-
-        dispatch(
-          setImagesData({
-            [player.who]: 'data:image/jpg;base64, ' + Buffer.from(response.data, 'binary').toString('base64')
-          })
-        );
-      } catch (err) {
-        console.log(err.message);
-      }
-    };
-
-    playersInfo[player.who] && playersInfo[player.who] !== '' && !imagesData[player.who] && fetchImage();
-		// eslint-disable-next-line
-  }, []);
 
   return (
     <div className={cl.classic}>
