211bdbb9302a17e791fbe8832f70bdc4af2158b7
Igor Nikiforov <desola2@inbox.ru>
2021-12-25 14:48:45 +0300

    refactor: move logic from render

--- src/components/UI/icons/Rects/RectScore.js
+++ src/components/UI/icons/Rects/RectScore.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import cl from './Rects.module.scss';
 
-const RectScore = ({icons}) => {
-	const classes = [cl.score, cl.rectText]
+const RectScore = ({ icons }) => {
+  const classes = [cl.score, cl.rectText];
+  const scoreText = icons.score_gue + ' - ' + icons.score_own;
 
-  return (
-    <div className={classes.join(' ')}>
-      {icons.score_gue + ' - ' + icons.score_own}
-    </div>
-  );
+  return <div className={classes.join(' ')}>{scoreText}</div>;
 };
 
 export default RectScore;
