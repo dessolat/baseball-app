43726288878dd2983557e2cb2a0c95b3f75fc3dc
Igor Nikiforov <desola2@inbox.ru>
2021-12-26 20:10:16 +0300

    feat: add wrong gameId checking to GameIdForm

--- src/components/Game/GameIdForm/GameIdForm.js
+++ src/components/Game/GameIdForm/GameIdForm.js
@@ -1,21 +1,51 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import cl from './GameIdForm.module.scss';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import RoundLoader from '../../UI/loaders/RoundLoader/RoundLoader';
 
-const GameIdForm = ({isLoading}) => {
+const GameIdForm = ({ isLoading }) => {
   const [input, setInput] = useState('');
-	const navigate = useNavigate();
+  const [isURLChecking, setIsURLChecking] = useState(false);
+  const [checkingError, setCheckingError] = useState('');
+  const navigate = useNavigate();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-		if (input === '') return
-		navigate(`/game/${input}`)
+  //Game exist checking
+  const testFetch = async id => {
+    try {
+      setIsURLChecking(true);
+      await axios.get(`http://51.250.11.151:3030/game_${id}`, {
+        timeout: 2500
+      });
+      return true;
+    } catch ({ message }) {
+      const errMessage = message[0].toUpperCase() + message.slice(1);
+      setCheckingError(errMessage);
+      setTimeout(() => setCheckingError(''), 2500);
+      return false;
+    } finally {
+      setIsURLChecking(false);
+    }
   };
 
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (input === '') return;
+
+    (await testFetch(input)) === true && navigate(`/game/${input}`);
+  };
+
+  const handleChange = e => setInput(e.target.value);
+
+  const isDisabled = isLoading || isURLChecking;
+  const errorStyles = { color: 'red', fontWeight: 600, marginLeft: 30, height: 20 };
+  const renderedBttn = isURLChecking ? <RoundLoader /> : <button disabled={isDisabled}>Change</button>;
+
   return (
-    <form className={cl.form} onSubmit={e => handleSubmit(e)}>
-      <input type='text' placeholder='id' value={input} onChange={e => setInput(e.target.value)} disabled={isLoading}/>
-      <button disabled={isLoading}>Change</button>
+    <form className={cl.form} onSubmit={handleSubmit}>
+      <input type='text' placeholder='id' value={input} onChange={handleChange} disabled={isDisabled} />
+      {renderedBttn}
+      <span style={errorStyles}>{checkingError}</span>
     </form>
   );
 };
