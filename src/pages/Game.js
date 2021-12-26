9cbb72492aac9f6437ded29cd902e58370b22cfb
Igor Nikiforov <desola2@inbox.ru>
2021-12-26 20:09:42 +0300

    feat: add wrong gameId checking

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,24 +7,39 @@ import Loader from 'components/UI/loaders/Loader/Loader';
 import ErrorLoader from 'components/UI/loaders/ErrorLoader/ErrorLoader';
 import useGameFetch from 'hooks/useGameFetch';
 import GameIdForm from 'components/Game/GameIdForm/GameIdForm';
-import { setCurrentCard, setSituationFilter } from 'redux/gameReducer';
-import { useSearchParams } from 'react-router-dom';
+import { setCurrentCard, setSituationFilter, setCurrentGameId } from 'redux/gameReducer';
+import { useSearchParams, useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Game = () => {
-	const [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const { gameId } = useParams();
+  const navigate = useNavigate();
   const tab = searchParams.get('tab');
   const innings = useSelector(state => state.game.innings);
-  const gameId = useSelector(state => state.game.gameId);
+  const currentGameId = useSelector(state => state.game.currentGameId);
   const dispatch = useDispatch();
   const [error, isLoading, cancelTokenRef, intervalRef, getFullData] = useGameFetch(
-    'http://51.250.11.151:3030/game_359'
+    `http://51.250.11.151:3030/game_${gameId}`
   );
   // const [error, isLoading, intervalRef, getFullData] = useGameFetch('http://192.168.1.3:3001/');
   // const [error, isLoading, cancelTokenRef, intervalRef, getFullData] = useGameFetch('http://localhost:3001/');
   // const [error, isLoading, intervalRef, getFullData] = useGameFetch('https://fierce-harbor-71147.herokuapp.com/');
 
   useEffect(() => {
-    dispatch(getFullData(true));
+    axios
+      .get(`http://51.250.11.151:3030/game_${gameId}`, {
+        timeout: 2500
+      })
+      .then(() => {
+        dispatch(getFullData(true));
+        dispatch(setCurrentGameId(gameId));
+      })
+      .catch(() => {
+        dispatch(setCurrentGameId(gameId));
+        navigate('/game/359');
+      });
+
     return () => {
       // eslint-disable-next-line
       clearInterval(intervalRef.current);
@@ -33,9 +48,11 @@ const Game = () => {
   }, []);
 
   useEffect(() => {
-    if (gameId === 0) return;
+    if (currentGameId === null) return;
+
     typeof cancelTokenRef.current != 'undefined' && cancelTokenRef.current.cancel(null);
     dispatch(setCurrentCard({}));
+    dispatch(setCurrentGameId(gameId));
     dispatch(setSituationFilter('All'));
     clearInterval(intervalRef.current);
     dispatch(getFullData(true, 'http://51.250.11.151:3030/game_' + gameId));
