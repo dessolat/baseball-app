cf014227c37ff9e113e600b66cc1d50897e6d578
Igor Nikiforov <desola2@inbox.ru>
2021-12-27 19:04:10 +0300

    feat: add basic functional to PlaysStats

--- src/components/Game/PlaysStats/PlaysStats.js
+++ src/components/Game/PlaysStats/PlaysStats.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import cl from './PlaysStats.module.scss';
 
-const PlaysStats = () => {
+const PlaysStats = ({ hit }) => {
+  const { init_speed_x, distance, angle, data_2d } = hit || 0;
+
+  const exitVelocityValue = init_speed_x ? init_speed_x.toFixed(1) + ' mph' : ' ';
+  const distanceValue = distance ? distance.toFixed(1) + ' m' : ' ';
+  const angleValue = angle ? angle.toFixed(0) + '°' : ' ';
+
+  let ballPath = data_2d ? `M${data_2d[0][0]} ${data_2d[0][1]}` : '';
+  data_2d && data_2d.slice(1).forEach(coords => (ballPath += `L${coords[0]} ${coords[1]}`));
+
   return (
     <div className={cl.stats}>
       <div className={cl.field}>
-        <svg width='100%' height='100%' viewBox='0 0 2560 2560' fill='none'>
+        <svg width='100%' height='100%' viewBox='0 0 2560 2560' fill='none' preserveAspectRatio="none">
+          <path
+            d={ballPath}
+            stroke='#4AA0F0'
+            strokeWidth='20'
+            strokeLinejoin='round'
+            strokeLinecap='round'></path>
         </svg>
       </div>
       <div className={cl.info}>
@@ -23,11 +38,11 @@ const PlaysStats = () => {
         </p>
         <p className={cl.row}>
           <span className={cl.title}>Exit Velocity</span>
-          <span className={cl.value}>91.0 mph</span>
+          <span className={cl.value}>{exitVelocityValue}</span>
         </p>
         <p className={cl.row}>
           <span className={cl.title}>Launch Angle</span>
-          <span className={cl.value}>41°</span>
+          <span className={cl.value}>{angleValue}</span>
         </p>
         <p className={cl.row}>
           <span className={cl.title}>Height</span>
@@ -35,7 +50,7 @@ const PlaysStats = () => {
         </p>
         <p className={cl.row}>
           <span className={cl.title}>Distance</span>
-          <span className={cl.value}>132.6 m</span>
+          <span className={cl.value}>{distanceValue}</span>
         </p>
       </div>
     </div>
