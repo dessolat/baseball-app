c7f3c116a338133e73e2d893bfda398a1777e59a
Igor Nikiforov <desola2@inbox.ru>
2021-12-28 12:37:15 +0300

    feat: move state
    * add props

--- src/components/Game/PlaysHitting/PlaysHitting.js
+++ src/components/Game/PlaysHitting/PlaysHitting.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PlaysBat from '../PlaysBat/PlaysBat';
 import PlaysStats from '../PlaysStats/PlaysStats';
 
-const PlaysHitting = ({currentMoment}) => {
-  const [currentDot, setCurrentDot] = useState('');
-
-  const handleDotClick = str => () => setCurrentDot(str);
-
+const PlaysHitting = ({ currentMoment }) => {
   return (
     <>
-      <PlaysBat currentDot={currentDot} handleDotClick={handleDotClick} currentMoment={currentMoment} />
-      <PlaysStats />
+      <PlaysBat currentMoment={currentMoment} />
+      <PlaysStats hit={currentMoment.metering?.hit} />
     </>
   );
 };
