0d03eae6dd94b78d19e100e9f8052fcf4e1c1afd
Igor Nikiforov <desola2@inbox.ru>
2021-12-28 12:30:49 +0300

    feat: add intermediatePoints rendering

--- src/components/Game/PlaysStats/PlaysStats.js
+++ src/components/Game/PlaysStats/PlaysStats.js
@@ -11,16 +11,23 @@ const PlaysStats = ({ hit }) => {
   let ballPath = data_2d ? `M${data_2d[0][0]} ${data_2d[0][1]}` : '';
   data_2d && data_2d.slice(1).forEach(coords => (ballPath += `L${coords[0]} ${coords[1]}`));
 
+  const intermediatePoints = data_2d
+    ? data_2d
+        .filter(coords => coords[3] === 1)
+        .map(coords => <circle cx={coords[0]} cy={coords[1]} r={18} fill='red'></circle>)
+    : '';
+
   return (
     <div className={cl.stats}>
       <div className={cl.field}>
-        <svg width='100%' height='100%' viewBox='0 0 2560 2560' fill='none' preserveAspectRatio="none">
+        <svg width='100%' height='100%' viewBox='0 0 2560 2560' fill='none' preserveAspectRatio='none'>
           <path
             d={ballPath}
             stroke='#4AA0F0'
             strokeWidth='20'
             strokeLinejoin='round'
             strokeLinecap='round'></path>
+          {intermediatePoints}
         </svg>
       </div>
       <div className={cl.info}>
