ff5d150bf89b185d654a613c5b90a7e77bb98e9a
Igor Nikiforov <desola2@inbox.ru>
2021-12-28 12:39:09 +0300

    refactor: move state
    * move handleDotClick func

--- src/components/Game/PlaysBat/PlaysBat.js
+++ src/components/Game/PlaysBat/PlaysBat.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cl from './PlaysBat.module.scss';
 import PlaysBatFooter from './PlaysBatFooter';
 import PlaysBatHeader from './PlaysBatHeader';
 import PlaysBatMedia from './PlaysBatMedia';
 
-const PlaysBat = ({ currentDot, handleDotClick, currentMoment }) => {
+const PlaysBat = ({ currentMoment }) => {
+  const [currentDot, setCurrentDot] = useState('');
+
+  const handleDotClick = str => () => setCurrentDot(str);
+
   return (
     <div className={cl.bat}>
       <PlaysBatHeader />
-      <PlaysBatMedia currentMoment={currentMoment}/>
+      <PlaysBatMedia currentMoment={currentMoment} />
       <PlaysBatFooter currentDot={currentDot} currentMoment={currentMoment} handleDotClick={handleDotClick} />
     </div>
   );
