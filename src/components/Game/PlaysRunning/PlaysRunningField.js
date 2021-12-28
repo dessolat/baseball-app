fc61df4eaa33dd60959db99e80d62e2c43c4d8b6
Igor Nikiforov <desola2@inbox.ru>
2021-12-28 12:36:06 +0300

    feat: add PlaysRunningField basic functional

--- src/components/Game/PlaysRunning/PlaysRunningField.js
+++ src/components/Game/PlaysRunning/PlaysRunningField.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import cl from './PlaysRunning.module.scss';
 
-const PlaysRunningField = () => {
-  return <svg className={cl.field} width='100%' height='100%' viewBox='0 0 2560 2560' fill='none'></svg>;
+const PlaysRunningField = ({ field, moments }) => {
+  // const { data_2d } = field || 0;
+
+  let ballPath = '';
+
+  moments
+    .filter(moment => moment.metering?.field?.data_2d)
+    .forEach(moment => {
+      const { data_2d } = moment.metering.field;
+
+      ballPath += `M${data_2d[0][0]} ${data_2d[0][1]}`;
+      data_2d.slice(1).forEach(coords => (ballPath += `L${coords[0]} ${coords[1]}`));
+    });
+
+  // let ballPath = data_2d ? `M${data_2d[0][0]} ${data_2d[0][1]}` : '';
+  // data_2d && data_2d.slice(1).forEach(coords => (ballPath += `L${coords[0]} ${coords[1]}`));
+
+  return (
+    <svg
+      className={cl.field}
+      width='100%'
+      height='100%'
+      viewBox='0 0 2560 2560'
+      fill='none'
+      preserveAspectRatio='none'>
+      <path
+        d={ballPath}
+        stroke='red'
+        strokeWidth='25'
+        strokeLinejoin='round'
+        strokeLinecap='round'
+        strokeDasharray='1 35'></path>
+    </svg>
+  );
 };
 
 export default PlaysRunningField;
