85f2c5aea22843ff0494f17d7e3802bb1e031cf1
Igor Nikiforov <desola2@inbox.ru>
2021-12-29 20:51:45 +0300

    feat: delete useTabs hook

--- src/hooks/useTabs.js
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react';
-
-const useTabs = tab => useMemo(() => {
-	const tabsArr = ['lineup', 'box', 'videos'];
-
-	return !tabsArr.includes(tab)
-}, [tab])
-
-export default useTabs
