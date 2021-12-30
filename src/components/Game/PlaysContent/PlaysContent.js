c08aac9f780756a9dd6696fc0f07fd54fd6acd7a
Igor Nikiforov <desola2@inbox.ru>
2021-12-30 22:03:48 +0300

    feat: add useQueryParam

--- src/components/Game/PlaysContent/PlaysContent.js
+++ src/components/Game/PlaysContent/PlaysContent.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { StringParam, useQueryParam } from 'use-query-params';
 import PlaysHitting from '../PlaysHitting/PlaysHitting';
 import PlaysPitch from '../PlaysPitch/PlaysPitch';
 import PlaysRunning from '../PlaysRunning/PlaysRunning';
 
-const PlaysContent = ({ footerTab, currentMoment, moments }) => {
+const PlaysContent = ({ currentMoment, moments }) => {
+  const [tab] = useQueryParam('ptab', StringParam);
+
   const renderComponent =
-    footerTab === 'pitch' ? (
-      <PlaysPitch currentMoment={currentMoment} />
-    ) : footerTab === 'hitting' ? (
+	tab === 'hitting' ? (
       <PlaysHitting currentMoment={currentMoment} />
-    ) : (
+    ) : tab === 'running' ? (
       <PlaysRunning currentMoment={currentMoment} moments={moments} />
+    ) : (
+      <PlaysPitch currentMoment={currentMoment} />
     );
 
   return <>{renderComponent}</>;
