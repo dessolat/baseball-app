e388eec0494a53e7af8061d5f49aa9dfea0d029a
Igor Nikiforov <desola2@inbox.ru>
2022-01-05 22:05:52 +0300

    feat: rework useSummaryRHE hook

--- src/hooks/useSummaryRHE.js
+++ src/hooks/useSummaryRHE.js
@@ -1,18 +1,22 @@
 import { useMemo } from 'react';
 
-const useSummaryRHE = (innings, inningNumber) =>
+const useSummaryRHE = innings =>
   useMemo(() => {
     const initialRHE = { bot_err: 0, top_err: 0, bot_hits: 0, bot_runs: 0, top_hits: 0, top_runs: 0 };
-    // const newInnings = inningNumber === null ? innings.slice() : innings.slice(0, inningNumber);
-    const newInnings =
-      inningNumber === null ? innings.slice() : innings.filter(inning => inning.number === inningNumber);
+    // const newInnings =
+    //   inningNumber === null ? innings.slice() : innings.filter(inning => inning.number === inningNumber);
+    const newInnings = innings.slice();
 
-    return inningNumber === null
-      ? newInnings.reduce((sum, inning) => {
-          Object.keys(sum).forEach(field => (sum[field] += inning[field] || 0));
-          return sum;
-        }, initialRHE)
-      : Object.assign(initialRHE, newInnings[0]);
-  }, [innings, inningNumber]);
+    return newInnings.reduce((sum, inning) => {
+      Object.keys(sum).forEach(field => (sum[field] += inning[field] || 0));
+      return sum;
+    }, initialRHE);
+    // return inningNumber === null
+    //   ? newInnings.reduce((sum, inning) => {
+    //       Object.keys(sum).forEach(field => (sum[field] += inning[field] || 0));
+    //       return sum;
+    //     }, initialRHE)
+    //   : Object.assign(initialRHE, newInnings[0]);
+  }, [innings]);
 
 export default useSummaryRHE;
