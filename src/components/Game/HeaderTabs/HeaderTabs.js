1cb31816f345c0df484262a23d1cbf6298b11ce1
Igor Nikiforov <desola2@inbox.ru>
2022-01-08 21:23:47 +0300

    feat: remove tab query param
    * move handleTabClick func to Game comp
    
    * rework getClass func

--- src/components/Game/HeaderTabs/HeaderTabs.js
+++ src/components/Game/HeaderTabs/HeaderTabs.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import { useQueryParam, StringParam } from 'use-query-params';
 import cl from './HeaderTabs.module.scss';
 
 const tabsArr = ['lineup', 'box', 'plays', 'videos'];
 
-const HeaderTabs = () => {
-  const [tab, setTab] = useQueryParam('tab', StringParam);
-
-  const handleTabClick = e => setTab(e.target.name);
-
-  const getClass = name => {
-    const values = ['lineup', 'box', 'videos'];
-
-    if (tab === name || (name === 'plays' && (!tab || !values.includes(tab)))) return cl.active;
-    return '';
-  };
+const HeaderTabs = ({ currentTab, handleClick }) => {
+  const getClass = name => (currentTab === name ? cl.active : '');
 
   return (
     <ul className={cl.headerTabs}>
       {tabsArr.map((item, i) => (
         <li key={i}>
-          <button name={item} onClick={handleTabClick} className={getClass(item)}>
+          <button name={item} onClick={handleClick} className={getClass(item)}>
             {item[0].toUpperCase()}
             {item.slice(1)}
           </button>
