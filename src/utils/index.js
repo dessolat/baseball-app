0713bb4a12673e996976f06ce638580fd9703a98
Igor Nikiforov <desola2@inbox.ru>
2022-01-08 20:14:53 +0300

    feat: create setSearchParam func

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,7 @@
 export const getSearchParam = param => new URL(window.location).searchParams.get(param);
+
+export const setSearchParam = (param, value) => {
+  const url = new URL(window.location);
+  url.searchParams.set(param, value);
+  window.history.pushState({}, '', url);
+};
