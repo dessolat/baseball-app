dd3ad5a9e8406b117e12876f2bfa3641abead15c
Igor Nikiforov <desola2@inbox.ru>
2022-01-09 19:03:38 +0300

    feat: add currentTab & handleClick to props
    * remove excessive comments

--- src/components/Game/PlaysFooter/PlaysFooter.js
+++ src/components/Game/PlaysFooter/PlaysFooter.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { StringParam, useQueryParam } from 'use-query-params';
 import cl from './PlaysFooter.module.scss';
 
-const PlaysFooter = () => {
-  const [tab, setTab] = useQueryParam('ptab', StringParam);
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // const tab = searchParams.get('ptab');
-
-  // const handleTabClick = e => setSearchParams({ ptab: e.target.name });
-  const handleClick = e => setTab(e.target.name);
-
-  const getClass = name => {
-    const values = ['pitch', 'hitting', 'running'];
-
-    if (tab === name || (name === 'pitch' && (!tab || !values.includes(tab)))) return cl.active;
-    return '';
-  };
+const PlaysFooter = ({ currentTab, handleClick }) => {
+  const getClass = name => currentTab === name ? cl.active : '';
 
   return (
     <ul className={cl.footer}>
