2292e59448cef2949ad7b29a722d1d92d0ba9939
Igor Nikiforov <desola2@inbox.ru>
2022-01-09 19:04:24 +0300

    feat: remove tab queryParam
    * add currentTab to props

--- src/components/Game/PlaysContent/PlaysContent.js
+++ src/components/Game/PlaysContent/PlaysContent.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { StringParam, useQueryParam } from 'use-query-params';
 import PlaysHitting from '../PlaysHitting/PlaysHitting';
 import PlaysPitch from '../PlaysPitch/PlaysPitch';
 import PlaysRunning from '../PlaysRunning/PlaysRunning';
 
-const PlaysContent = ({ currentMoment }) => {
-  const [tab] = useQueryParam('ptab', StringParam);
-
+const PlaysContent = ({ currentMoment, currentTab }) => {
   const renderComponent =
-	tab === 'hitting' ? (
+	currentTab === 'hitting' ? (
       <PlaysHitting currentMoment={currentMoment} />
-    ) : tab === 'running' ? (
+    ) : currentTab === 'running' ? (
       <PlaysRunning currentMoment={currentMoment} />
     ) : (
       <PlaysPitch currentMoment={currentMoment} />
