f328639c55de08992b1470c15e158e8098b3a533
Igor Nikiforov <desola2@inbox.ru>
2022-01-10 22:42:34 +0300

    feat: move logic from render
    * rewrite destructurized code

--- src/components/Game/PlaysEvents/PlaysEventsItem.js
+++ src/components/Game/PlaysEvents/PlaysEventsItem.js
@@ -7,34 +7,29 @@ const PlaysEventsItem = ({ moment, currentMoment, handleClick }) => {
   const classes = inner.id === currentMoment.inner?.id ? cl.active : '';
 
   const playClasses = [];
-  moment.icons.circ_text_play !== 'R' && playClasses.push(cl[moment.icons.circ_color_play]);
-  moment.icons.circ_text_play === 'OBR' && playClasses.push(cl.obr);
+  icons.circ_text_play !== 'R' && playClasses.push(cl[icons.circ_color_play]);
+  icons.circ_text_play === 'OBR' && playClasses.push(cl.obr);
 
+  const playDataBefore = icons.circ_text_pitch || icons.circ_text_play === 'R' ? '' : icons.batter_moment;
   return (
     <>
-      {moment.icons.circ_color_pitch && (
-        <li className={classes} onClick={() => handleClick(moment)}>
-          <div className={cl[moment.icons.circ_color_pitch]} data-before={moment.icons.batter_moment}>
-            {moment.icons.circ_text_pitch}
+      {icons.circ_color_pitch && (
+        <li className={classes} onClick={handleClick(moment)}>
+          <div className={cl[icons.circ_color_pitch]} data-before={icons.batter_moment}>
+            {icons.circ_text_pitch}
           </div>
           <div className={cl.text}>
-            <p>slider</p>({moment.pitcher.strikes_count} strike, {moment.pitcher.balls_count} ball)
+            <p>slider</p>({pitcher.strikes_count} strike, {pitcher.balls_count} ball)
           </div>
         </li>
       )}
-      {moment.icons.circ_color_play && (
-        <li className={classes} onClick={() => handleClick(moment)}>
-          <div
-            className={playClasses.join(' ')}
-            data-before={
-              moment.icons.circ_text_pitch || moment.icons.circ_text_play === 'R'
-                ? ''
-                : moment.icons.batter_moment
-            }>
-            {moment.icons.circ_text_play !== 'R' ? moment.icons.circ_text_play : <ReplaceEvent />}
+      {icons.circ_color_play && (
+        <li className={classes} onClick={handleClick(moment)}>
+          <div className={playClasses.join(' ')} data-before={playDataBefore}>
+            {icons.circ_text_play !== 'R' ? icons.circ_text_play : <ReplaceEvent />}
           </div>
           <div className={cl.text}>
-            <p>slider</p>({moment.pitcher.strikes_count} strike, {moment.pitcher.balls_count} ball)
+            <p>slider</p>({pitcher.strikes_count} strike, {pitcher.balls_count} ball)
           </div>
         </li>
       )}
