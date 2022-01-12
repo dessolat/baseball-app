87da7dd8796ccbb18cffcb4f8cba8d25bfc275f2
Igor Nikiforov <desola2@inbox.ru>
2022-01-12 22:22:43 +0300

    refactor: move runningList to PlaysRunningInfoList

--- src/components/Game/PlaysRunning/PlaysRunningInfo.js
+++ src/components/Game/PlaysRunning/PlaysRunningInfo.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import cl from './PlaysRunning.module.scss';
-import PlaysRunningInfoItem from './PlaysRunningInfoItem';
-
-const runningList = [
-  { title: 'Run to first base by Batter:', value: 'S: 5.7 m/s TB: 1.5 s' },
-  { title: 'Throwing ball to 2B by LF:', value: 'D: 20 m    HT: 1.5 s   AS: 100 %' },
-  { title: 'Run to second base by Runner 1:', value: 'S: 5.7 m/s TB: 2.0 s' },
-  { title: 'Run to second base by Batter:', value: 'S: 5.7 m/s TB: 1 s' },
-  { title: 'Throwing ball to 1B by 2B:', value: 'D: 20 m    HT: 3 s   AS: 100 %' },
-  { title: 'Run to first base by Batter:', value: 'S: 5.7 m/s TB: 1 s' }
-];
+import PlaysRunningInfoList from './PlaysRunningInfoList';
 
 const PlaysRunningInfo = () => {
   return (
     <div className={cl.info}>
       <p className={cl.subHeader}>Running</p>
-      <ul>
-        {runningList.map((item, i) => (
-          <PlaysRunningInfoItem key={i} item={item} />
-        ))}
-      </ul>
+      <PlaysRunningInfoList />
     </div>
   );
 };
