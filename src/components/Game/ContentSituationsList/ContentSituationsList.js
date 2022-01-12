85cb391fcb09fb04c2051b95bba3058fe2ad091b
Igor Nikiforov <desola2@inbox.ru>
2022-01-12 22:08:11 +0300

    feat: add keydown listener to useEffect

--- src/components/Game/ContentSituationsList/ContentSituationsList.js
+++ src/components/Game/ContentSituationsList/ContentSituationsList.js
@@ -1,12 +1,21 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useEffect } from 'react';
 import cl from './ContentSituationsList.module.scss';
 import ContentSituationsListItem from '../ContentSituationsListItem/ContentSituationsListItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPlaybackMode, setCurrentCard } from 'redux/gameReducer';
+import useArrowNavigate from 'hooks/useArrowNavigate';
 
 const ContentSituationsList = ({ cards, currentCard }, ref) => {
   const playbackMode = useSelector(state => state.game.playbackMode);
   const dispatch = useDispatch();
+  const handleKeyDown = useArrowNavigate(cards, currentCard);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [currentCard, cards, handleKeyDown]);
 
   const situationClick = player => () => {
     playbackMode !== 'pause' && dispatch(setPlaybackMode('pause'));
