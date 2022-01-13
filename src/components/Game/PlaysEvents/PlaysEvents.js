1b2d1c03b69fbe707f07499c6707807a19cc274c
Igor Nikiforov <desola2@inbox.ru>
2022-01-13 21:31:22 +0300

    feat: add total balls & strikes display

--- src/components/Game/PlaysEvents/PlaysEvents.js
+++ src/components/Game/PlaysEvents/PlaysEvents.js
@@ -4,13 +4,34 @@ import cl from './PlaysEvents.module.scss';
 import PlaysEventsItem from './PlaysEventsItem';
 
 const PlaysEvents = ({ moments, currentMoment, handleClick }) => {
+  const isTotal = moments.length > 0;
+  const { balls_count, strikes_count } = (isTotal && moments.slice(-1)[0].pitcher) || '';
+
   return (
-    <ul className={cl.events}>
-      {moments.length !== 0 &&
-        moments.map((moment, i) => (
-          <PlaysEventsItem key={i} moment={moment} currentMoment={currentMoment} handleClick={handleClick} />
-        ))}
-    </ul>
+    <div className={cl.events}>
+      <ul className={cl.list}>
+        {moments.length !== 0 &&
+          moments.map((moment, i) => (
+            <PlaysEventsItem
+              key={i}
+              moment={moment}
+              prevTable={i !== 0 ? moments[i - 1].table : { balls: 0, strikes: 0 }}
+              currentMoment={currentMoment}
+              handleClick={handleClick}
+            />
+          ))}
+      </ul>
+      <p className={cl.total}>
+        {isTotal && (
+          <>
+            Total:{' '}
+            <span>
+              {balls_count + strikes_count} ({balls_count} strike, {strikes_count} ball)
+            </span>
+          </>
+        )}
+      </p>
+    </div>
   );
 };
 
