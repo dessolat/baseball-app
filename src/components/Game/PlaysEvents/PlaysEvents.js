90de8e4f5955847a0ea6dcd2566442b3b75b1c9f
Igor Nikiforov <desola2@inbox.ru>
2022-01-14 16:50:15 +0300

    refactor: move code to PlaysEventsList comp

--- src/components/Game/PlaysEvents/PlaysEvents.js
+++ src/components/Game/PlaysEvents/PlaysEvents.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import cl from './PlaysEvents.module.scss';
-
-import PlaysEventsItem from './PlaysEventsItem';
+import PlaysEventsList from './PlaysEventsList';
 
 const PlaysEvents = ({ moments, currentMoment, handleClick }) => {
   const isTotal = moments.length > 0;
@@ -9,19 +8,8 @@ const PlaysEvents = ({ moments, currentMoment, handleClick }) => {
 
   return (
     <div className={cl.events}>
-      <ul className={cl.list}>
-        {moments.length !== 0 &&
-          moments.map((moment, i) => (
-            <PlaysEventsItem
-              key={i}
-              moment={moment}
-              prevTable={i !== 0 ? moments[i - 1].table : { balls: 0, strikes: 0 }}
-              currentMoment={currentMoment}
-              handleClick={handleClick}
-            />
-          ))}
-      </ul>
-      <p className={cl.total}>
+      <PlaysEventsList moments={moments} currentMoment={currentMoment} handleClick={handleClick}/>
+      <p className={cl.total}> 
         {isTotal && (
           <>
             Total:{' '}
--- src/components/Game/PlaysEvents/PlaysEventsList.js
+++ src/components/Game/PlaysEvents/PlaysEventsList.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import cl from './PlaysEvents.module.scss';
+import PlaysEventsItem from './PlaysEventsItem';
 
-const PlaysEventsList = () => {
-	return (
-		<div>
-			
-		</div>
-	)
-}
+const PlaysEventsList = ({ moments, currentMoment, handleClick }) => (
+  <ul className={cl.list}>
+    {moments.length !== 0 &&
+      moments.map((moment, i) => (
+        <PlaysEventsItem
+          key={i}
+          moment={moment}
+          prevTable={i !== 0 ? moments[i - 1].table : { balls: 0, strikes: 0 }}
+          currentMoment={currentMoment}
+          handleClick={handleClick}
+        />
+      ))}
+  </ul>
+);
 
-export default PlaysEventsList
+export default PlaysEventsList;
