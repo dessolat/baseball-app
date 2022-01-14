c4bd7c38c51e9a3395962fdd1fc6d6e3dcc68d29
Igor Nikiforov <desola2@inbox.ru>
2022-01-14 17:01:53 +0300

    refactor: move code to PlaysEventsTotal comp

--- src/components/Game/PlaysEvents/PlaysEvents.js
+++ src/components/Game/PlaysEvents/PlaysEvents.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import cl from './PlaysEvents.module.scss';
 import PlaysEventsList from './PlaysEventsList';
+import PlaysEventsTotal from './PlaysEventsTotal';
 
-const PlaysEvents = ({ moments, currentMoment, handleClick }) => {
-  const isTotal = moments.length > 0;
-  const { balls_count, strikes_count } = (isTotal && moments.slice(-1)[0].pitcher) || '';
-
-  return (
-    <div className={cl.events}>
-      <PlaysEventsList moments={moments} currentMoment={currentMoment} handleClick={handleClick}/>
-      <p className={cl.total}> 
-        {isTotal && (
-          <>
-            Total:{' '}
-            <span>
-              {balls_count + strikes_count} ({balls_count} strike, {strikes_count} ball)
-            </span>
-          </>
-        )}
-      </p>
-    </div>
-  );
-};
+const PlaysEvents = ({ moments, currentMoment, handleClick }) => (
+  <div className={cl.events}>
+    <PlaysEventsList moments={moments} currentMoment={currentMoment} handleClick={handleClick} />
+    <PlaysEventsTotal moments={moments} />
+  </div>
+);
 
 export default PlaysEvents;
--- src/components/Game/PlaysEvents/PlaysEventsTotal.js
+++ src/components/Game/PlaysEvents/PlaysEventsTotal.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import cl from './PlaysEvents.module.scss';
 
-const PlaysEventsTotal = () => {
-	return (
-		<div>
-			
-		</div>
-	)
-}
+const PlaysEventsTotal = ({moments}) => {
+	const isTotal = moments.length > 0;
+	const { balls_count, strikes_count } = (isTotal && moments.slice(-1)[0].pitcher) || '';
 
-export default PlaysEventsTotal
+  return (
+    <p className={cl.total}>
+      {isTotal && (
+        <>
+          Total:{' '}
+          <span>
+            {balls_count + strikes_count} ({balls_count} strike, {strikes_count} ball)
+          </span>
+        </>
+      )}
+    </p>
+  );
+};
+
+export default PlaysEventsTotal;
