d176a2c5c1bebd30b64e27fcb90e5501faee43cc
Igor Nikiforov <desola2@inbox.ru>
2022-01-15 21:57:46 +0300

    feat: add max_speed value

--- src/components/Game/PlaysBat/PlaysBatHeader.js
+++ src/components/Game/PlaysBat/PlaysBatHeader.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import cl from './PlaysBat.module.scss';
 
-const PlaysBatHeader = () => {
+const PlaysBatHeader = ({ bat }) => {
+  const { max_speed = '' } = bat || {};
+
   return (
     <div className={cl.header}>
       <div>
         <p className={cl.subHeader}>Max speed</p>
-        <p className={cl.headerValue}>70.3 mph</p>
+        <p className={cl.headerValue}>{max_speed} mph</p>
       </div>
       <div>
         <p className={cl.subHeader}>Attack Angle</p>
