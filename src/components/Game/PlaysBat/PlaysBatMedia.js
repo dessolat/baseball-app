8f168c38dfd7b346ddc3840d6f7640610f85f93a
Igor Nikiforov <desola2@inbox.ru>
2022-01-16 19:30:07 +0300

    feat: comment unused code

--- src/components/Game/PlaysBat/PlaysBatMedia.js
+++ src/components/Game/PlaysBat/PlaysBatMedia.js
@@ -1,41 +1,41 @@
 import React, { useState, useEffect, useRef } from 'react';
 import cl from './PlaysBat.module.scss';
 
-const oldCoords = [
-  [35, 40],
-  [50, 74],
-  [95, 88],
-  [140, 95],
-  [185, 97],
-  [230, 98],
-  [275, 98],
-  [320, 91],
-  [335, 75],
-  [355, 185],
-  [353, 200],
-  [290, 210],
-  [235, 215],
-  [180, 218],
-  [125, 218],
-  [70, 215],
-  [20, 213],
-  [0, 200]
-];
+// const oldCoords = [
+//   [35, 40],
+//   [50, 74],
+//   [95, 88],
+//   [140, 95],
+//   [185, 97],
+//   [230, 98],
+//   [275, 98],
+//   [320, 91],
+//   [335, 75],
+//   [355, 185],
+//   [353, 200],
+//   [290, 210],
+//   [235, 215],
+//   [180, 218],
+//   [125, 218],
+//   [70, 215],
+//   [20, 213],
+//   [0, 200]
+// ];
 
-let coords = [];
-for (let i = 0; i < oldCoords.length; i++) {
-  if (i === 0) {
-    coords.push(oldCoords[i]);
-    continue;
-  }
+// let coords = [];
+// for (let i = 0; i < oldCoords.length; i++) {
+//   if (i === 0) {
+//     coords.push(oldCoords[i]);
+//     continue;
+//   }
 
-  let xDiff = oldCoords[i][0] - oldCoords[i - 1][0];
-  let yDiff = oldCoords[i][1] - oldCoords[i - 1][1];
+//   let xDiff = oldCoords[i][0] - oldCoords[i - 1][0];
+//   let yDiff = oldCoords[i][1] - oldCoords[i - 1][1];
 
-  for (let j = 1; j <= 10; j++) {
-    coords.push([oldCoords[i - 1][0] + (xDiff / 10) * j, oldCoords[i - 1][1] + (yDiff / 10) * j]);
-  }
-}
+//   for (let j = 1; j <= 10; j++) {
+//     coords.push([oldCoords[i - 1][0] + (xDiff / 10) * j, oldCoords[i - 1][1] + (yDiff / 10) * j]);
+//   }
+// }
 
 const PlaysBatMedia = ({ currentMoment }) => {
   const [curvePath, setCurvePath] = useState('');
