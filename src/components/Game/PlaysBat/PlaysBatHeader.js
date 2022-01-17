ac360b260a0e6e963ddaa40fdbb1d8fba4e3045a
Igor Nikiforov <desola2@inbox.ru>
2022-01-17 21:12:53 +0300

    refactor: move logic from render

--- src/components/Game/PlaysBat/PlaysBatHeader.js
+++ src/components/Game/PlaysBat/PlaysBatHeader.js
@@ -2,17 +2,19 @@ import React, { memo } from 'react';
 import cl from './PlaysBat.module.scss';
 
 const PlaysBatHeader = ({ bat }) => {
-  const { max_speed = '' } = bat || {};
+  const { max_speed } = bat || {};
 
+	const maxSpeedValue = max_speed ? `${max_speed} mph` : ' '
+	const angleValue = max_speed ? `12.4 degrees` : ' '
   return (
     <div className={cl.header}>
       <div>
         <p className={cl.subHeader}>Max speed</p>
-        <p className={cl.headerValue}>{max_speed} mph</p>
+        <p className={cl.headerValue}>{maxSpeedValue}</p>
       </div>
       <div>
         <p className={cl.subHeader}>Attack Angle</p>
-        <p className={cl.headerValue}>12.4 degrees</p>
+        <p className={cl.headerValue}>{angleValue}</p>
       </div>
     </div>
   );
