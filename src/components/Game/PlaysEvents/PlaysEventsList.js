ce2948080016088e8b179e3261292f45fed69ff6
Igor Nikiforov <desola2@inbox.ru>
2022-01-19 19:10:06 +0300

    feat: add shadow class to list

--- src/components/Game/PlaysEvents/PlaysEventsList.js
+++ src/components/Game/PlaysEvents/PlaysEventsList.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import cl from './PlaysEvents.module.scss';
 import PlaysEventsItem from './PlaysEventsItem';
 
-const PlaysEventsList = ({ moments, currentMoment, handleClick }) => (
-  <ul className={cl.list}>
-    {moments.length !== 0 &&
-      moments.map((moment, i) => (
-        <PlaysEventsItem
-          key={i}
-          moment={moment}
-          currentMoment={currentMoment}
-          handleClick={handleClick}
-        />
-      ))}
-  </ul>
-);
+const PlaysEventsList = ({ moments, currentMoment, handleClick }) => {
+	const [classes, setClasses] = useState([cl.list])
+  const ref = useRef(null);
+
+  useEffect(() => {
+		if (ref.current === null) return;
+		
+		ref.current.scrollHeight > ref.current.clientHeight ? setClasses([cl.list, cl.listBottomShadow]) : setClasses([cl.list])
+  }, [moments]);
+	
+  return (
+    <ul className={classes.join(' ')} ref={ref}>
+      {moments.length !== 0 &&
+        moments.map((moment, i) => (
+          <PlaysEventsItem key={i} moment={moment} currentMoment={currentMoment} handleClick={handleClick} />
+        ))}
+    </ul>
+  );
+};
 
 export default PlaysEventsList;
