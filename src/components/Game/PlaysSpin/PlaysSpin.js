83bf7eec248293ee09c575cac99a5d55cf23bac7
Igor Nikiforov <desola2@inbox.ru>
2022-01-20 21:58:30 +0300

    refactor: move code to PlaysSpinInfo

--- src/components/Game/PlaysSpin/PlaysSpin.js
+++ src/components/Game/PlaysSpin/PlaysSpin.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import cl from './PlaysSpin.module.scss';
+import PlaysSpinInfo from './PlaysSpinInfo';
 
 const PlaysSpin = ({ pitch }) => {
-	const {strike_zone_x, strike_zone_y, offset_x, offset_y,rotation} = pitch || 0
+  const { strike_zone_x, strike_zone_y } = pitch || 0;
 
   const isDrawBall = pitch;
   const coordX = 50 + 50 * strike_zone_x;
   const coordY = 50 - 50 * strike_zone_y;
-  const trueSpinValue = rotation ? rotation.toFixed(2) + ' rpm' : ' ';
-	const verticalBreakValue = offset_y ? (offset_y * 100).toFixed(2) + ' cm' : ' ';
-	const horizontalBreakValue = offset_x ? (offset_x * 100).toFixed(2) + ' cm' : ' ';
 
   return (
     <div className={cl.spin}>
@@ -18,14 +16,7 @@ const PlaysSpin = ({ pitch }) => {
         <line y1='49.5' x2='100' y2='49.5' stroke='#ACACAC' />
         {isDrawBall && <circle cx={coordX} cy={coordY} r='7.5' fill='#2B9D6A' />}
       </svg>
-      <div>
-        <p className={cl.subHeader}>True spin</p>
-        <p className={cl.regularValue}>{trueSpinValue}</p>
-        <p className={cl.subHeader}>Vertical break</p>
-        <p className={cl.breakValue}>{verticalBreakValue}</p>
-        <p className={cl.subHeader}>Horizontal break</p>
-        <p className={cl.breakValue}>{horizontalBreakValue}</p>
-      </div>
+      <PlaysSpinInfo pitch={pitch} />
     </div>
   );
 };
--- src/components/Game/PlaysSpin/PlaysSpinInfo.js
+++ src/components/Game/PlaysSpin/PlaysSpinInfo.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import cl from './PlaysSpin.module.scss';
 
-const PlaysSpinInfo = () => {
-	return <div></div>;
+const PlaysSpinInfo = ({ pitch }) => {
+  const { offset_x, offset_y, rotation } = pitch || 0;
+
+  const trueSpinValue = rotation ? rotation.toFixed(2) + ' rpm' : ' ';
+  const verticalBreakValue = offset_y ? (offset_y * 100).toFixed(2) + ' cm' : ' ';
+  const horizontalBreakValue = offset_x ? (offset_x * 100).toFixed(2) + ' cm' : ' ';
+
+  return (
+    <div>
+      <p className={cl.subHeader}>True spin</p>
+      <p className={cl.regularValue}>{trueSpinValue}</p>
+      <p className={cl.subHeader}>Vertical break</p>
+      <p className={cl.breakValue}>{verticalBreakValue}</p>
+      <p className={cl.subHeader}>Horizontal break</p>
+      <p className={cl.breakValue}>{horizontalBreakValue}</p>
+    </div>
+  );
 };
 
 export default PlaysSpinInfo;
