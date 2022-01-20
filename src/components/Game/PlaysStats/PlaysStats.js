d68dd24800ab95419444ce780d66c48380c919d3
Igor Nikiforov <desola2@inbox.ru>
2022-01-20 21:34:27 +0300

    refactor: move code to PlaysStatsField

--- src/components/Game/PlaysStats/PlaysStats.js
+++ src/components/Game/PlaysStats/PlaysStats.js
@@ -1,35 +1,13 @@
 import React from 'react';
 import cl from './PlaysStats.module.scss';
+import PlaysStatsField from './PlaysStatsField';
 import PlaysStatsInfo from './PlaysStatsInfo';
 
-const PlaysStats = ({ hit }) => {
-  const { data_2d } = hit || 0;
-
-  let ballPath = data_2d ? `M${data_2d[0][0]} ${data_2d[0][1]}` : '';
-  data_2d && data_2d.slice(1).forEach(coords => (ballPath += `L${coords[0]} ${coords[1]}`));
-
-  const intermediatePoints = data_2d
-    ? data_2d
-        .filter(coords => coords[3] === 1)
-        .map((coords, i) => <circle key={i} cx={coords[0]} cy={coords[1]} r={18} fill='red'></circle>)
-    : '';
-
-  return (
-    <div className={cl.stats}>
-      <div className={cl.field}>
-        <svg width='100%' height='100%' viewBox='0 0 2560 2560' fill='none' preserveAspectRatio='none'>
-          <path
-            d={ballPath}
-            stroke='#4AA0F0'
-            strokeWidth='20'
-            strokeLinejoin='round'
-            strokeLinecap='round'></path>
-          {intermediatePoints}
-        </svg>
-      </div>
-      <PlaysStatsInfo hit={hit} />
-    </div>
-  );
-};
+const PlaysStats = ({ hit }) => (
+  <div className={cl.stats}>
+    <PlaysStatsField hit={hit} />
+    <PlaysStatsInfo hit={hit} />
+  </div>
+);
 
 export default PlaysStats;
