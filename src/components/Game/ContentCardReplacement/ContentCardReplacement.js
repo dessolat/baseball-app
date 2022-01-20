012c7e3b74678d0898c2f0a5757e71bfcb6ddcf8
Igor Nikiforov <desola2@inbox.ru>
2022-01-20 09:46:24 +0300

    feat: add text coloring to replacements

--- src/components/Game/ContentCardReplacement/ContentCardReplacement.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacement.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import cl from './ContentCardReplacement.module.scss';
 import PortraitImg from 'images/portrait.png';
 
 const ContentCardReplacement = ({ text = '' }) => {
+  const ref = useRef(null);
+
+  useLayoutEffect(() => {
+		if (ref.current === null) return 
+
+    ref.current.innerHTML = text + '.';
+  }, [text]);
+
   return (
     <div className={cl.replace}>
       <div className={cl.portrait}>
-          <img
-						className={cl.default}
-            src={PortraitImg}
-            alt='Portrait'
-          />
-        </div>
-      <p className={cl.text}>{text + '.'}</p>
-			<div className={cl.portrait}>
-          <img
-						className={cl.default}
-            src={PortraitImg}
-            alt='Portrait'
-          />
-        </div>
+        <img className={cl.default} src={PortraitImg} alt='Portrait' />
+      </div>
+      <p className={cl.text} ref={ref}>
+        {text + '.'}
+      </p>
+      <div className={cl.portrait}>
+        <img className={cl.default} src={PortraitImg} alt='Portrait' />
+      </div>
     </div>
   );
 };
