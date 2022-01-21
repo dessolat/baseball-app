02167985c04b53e67d6f43c125965eca2fe8d1de
Igor Nikiforov <desola2@inbox.ru>
2022-01-21 16:06:47 +0300

    feat: add inning to deps array

--- src/components/Game/PlaysSpeed/PlaysSpeed.js
+++ src/components/Game/PlaysSpeed/PlaysSpeed.js
@@ -65,7 +65,7 @@ const PlaysSpeed = ({ currentMoment }) => {
       // [['', 'speed']]
     );
     setChartData(newChartData);
-  }, [currentCard]);
+  }, [currentCard, innings]);
 
   // const options = {
   //   height: chartHeight,
