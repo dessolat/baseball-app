78e289a6558ea016675569e17fbb3e6d2d282644
Igor Nikiforov <desola2@inbox.ru>
2022-01-24 00:46:20 +0300

    feat: add top animation to VideoEventsList

--- src/components/Game/VideoEventsList/VideoEventsList.js
+++ src/components/Game/VideoEventsList/VideoEventsList.js
@@ -1,17 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import useCurrentEvents from 'hooks/useCurrentEvents';
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { setCurrentMoment } from 'redux/gameReducer';
 import cl from './VideoEventsList.module.scss';
 import VideoEventsListItem from './VideoEventsListItem';
 
 const VideoEventsList = () => {
-	const moments = useCurrentEvents()
+  const [classes, setClasses] = useState([]);
+  const activeCardList = useSelector(state => state.game.activeCardList);
+  const dispatch = useDispatch();
+  const moments = useCurrentEvents();
 
+  useEffect(() => {
+    if (classes.length === 0) {
+      const newClasses =
+        activeCardList === 'events' ? [cl.events, cl.blueTopLoad, cl.beforeBlue] : [cl.events, cl.beforeBlue];
+      setClasses(newClasses);
+
+      return;
+    }
+
+    const newClasses = [cl.events, cl.beforeBlue];
+    newClasses.push(activeCardList === 'events' ? cl.wider : cl.taller);
+    setClasses(newClasses);
+		// eslint-disable-next-line
+  }, [activeCardList]);
+
+  // const classes = [cl.events];
+  // classes.push(activeCardList === 'events' ? cl.wider : cl.taller);
+
+  const handleMomentClick = moment => () => dispatch(setCurrentMoment(moment));
   return (
     <>
       {moments.length !== 0 && (
-        <ul className={cl.events}>
+        <ul className={classes.join(' ')}>
           {moments.map((moment, i) => (
-            <VideoEventsListItem key={i} moment={moment} />
+            <VideoEventsListItem key={i} moment={moment} handleClick={handleMomentClick} />
           ))}
         </ul>
       )}
