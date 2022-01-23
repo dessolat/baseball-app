9b4efa3c9495d42e0ad3c52575bf6c5b60ca70f5
Igor Nikiforov <desola2@inbox.ru>
2022-01-24 00:48:14 +0300

    feat: add moment change to Video events on click

--- src/components/Game/VideoEventsList/VideoEventsListItem.js
+++ src/components/Game/VideoEventsList/VideoEventsListItem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import cl from './VideoEventsList.module.scss';
 import ReplaceEvent from 'components/UI/icons/ReplaceEvent';
+import { useSelector } from 'react-redux';
 
-const VideoEventsListItem = ({ moment }) => {
+const VideoEventsListItem = ({ moment, handleClick }) => {
+	const currentMoment = useSelector(state => state.game.currentMoment)
+
+	const pitchClasses = [moment.icons.circ_color_pitch && cl[moment.icons.circ_color_pitch]]
   const playClasses = [];
   moment.icons.circ_text_play !== 'R' && playClasses.push(cl[moment.icons.circ_color_play]);
   moment.icons.circ_text_play === 'OBR' && playClasses.push(cl.obr);
+
+	const wrapperClasses = []
+	moment.inner?.id === currentMoment.inner?.id && wrapperClasses.push(cl.active)
+	
 
   const playDataBefore =
     moment.icons.circ_text_pitch || moment.icons.circ_text_play === 'R' ? '' : moment.icons.batter_moment;
@@ -14,14 +22,14 @@ const VideoEventsListItem = ({ moment }) => {
   return (
     <>
       {moment.icons.circ_color_pitch && (
-        <li>
-          <div className={cl[moment.icons.circ_color_pitch]} data-before={moment.icons.batter_moment}>
+        <li className={wrapperClasses.join(' ')} onClick={handleClick(moment)}>
+          <div className={pitchClasses.join(' ')} data-before={moment.icons.batter_moment}>
             {moment.icons.circ_text_pitch}
           </div>
         </li>
       )}
       {moment.icons.circ_color_play && (
-        <li>
+        <li className={wrapperClasses.join(' ')} onClick={handleClick(moment)}>
           <div className={playClasses.join(' ')} data-before={playDataBefore}>
             {playContent}
           </div>
