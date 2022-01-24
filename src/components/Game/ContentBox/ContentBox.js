e0878a8ece90f1297e4ed34b25e4edb55cdc187f
Igor Nikiforov <desola2@inbox.ru>
2022-01-25 00:08:19 +0300

    feat: add columns to pitching table data

--- src/components/Game/ContentBox/ContentBox.js
+++ src/components/Game/ContentBox/ContentBox.js
@@ -17,9 +17,9 @@ const TABLE_DATA = {
 			{player_name:'SURNAME name',pos:'00',ab:0,h:0,'2b':0,'3b':0,hr:0,rbi:0,gdp:0,bb:0,hp:0,sh:0,sf:0,so:0,tb:0,avg:0,slg:0,obp:0,ops:0,sb:0,cs:0,'%sb':0,lob:0}
 		],
 		pitching: [
-			{player_name:'SURNAME name (W/L)',PA:0,R:0,ER:0,h:0,'2B':0,'3B':0,HR:0,BB:0,IBB:0,HP:0,SH:0,SF:0,SO:0,WP:0,ERA:0},
-			{player_name:'SURNAME name',PA:0,R:0,ER:0,h:0,'2B':0,'3B':0,HR:0,BB:0,IBB:0,HP:0,SH:0,SF:0,SO:0,WP:0,ERA:0},
-			{player_name:'SURNAME name',PA:0,R:0,ER:0,h:0,'2B':0,'3B':0,HR:0,BB:0,IBB:0,HP:0,SH:0,SF:0,SO:0,WP:0,ERA:0}
+			{player_name:'SURNAME name (W/L)',PA:0,R:0,ER:0,h:0,'2B':0,'3B':0,HR:0,BB:0,IBB:0,HP:0,SH:0,SF:0,SO:0,WP:0,ERA:0,NP:0,NS:0,NB:0},
+			{player_name:'SURNAME name',PA:0,R:0,ER:0,h:0,'2B':0,'3B':0,HR:0,BB:0,IBB:0,HP:0,SH:0,SF:0,SO:0,WP:0,ERA:0,NP:0,NS:0,NB:0},
+			{player_name:'SURNAME name',PA:0,R:0,ER:0,h:0,'2B':0,'3B':0,HR:0,BB:0,IBB:0,HP:0,SH:0,SF:0,SO:0,WP:0,ERA:0,NP:0,NS:0,NB:0}
 		],
 		fielding: [
 			{player_name:'SURNAME name (W/L)',CH:0,PO:0,A:0,E:0,DP:0,'FLD%':0},
