9b772b018c8d52d7bebc74635642506cab72b0cd
Igor Nikiforov <desola2@inbox.ru>
2022-01-24 14:05:10 +0300

    feat: add moments changing by arrows

--- src/hooks/useArrowNavigate.js
+++ src/hooks/useArrowNavigate.js
@@ -1,9 +1,11 @@
-import { useDispatch } from 'react-redux';
-import { setActiveCardList, setCurrentCard } from 'redux/gameReducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveCardList, setCurrentCard, setCurrentMoment } from 'redux/gameReducer';
 
 const KEYS = ['ArrowDown', 'ArrowUp', 'ArrowLeft', 'ArrowRight'];
 
 const useArrowNavigate = (cards, currentCard) => {
+  const activeCardList = useSelector(state => state.game.activeCardList);
+  const currentMoment = useSelector(state => state.game.currentMoment);
   const dispatch = useDispatch();
 
   const handleKeyDown = e => {
@@ -11,9 +13,19 @@ const useArrowNavigate = (cards, currentCard) => {
     e.preventDefault();
 
     const cardIndex = cards.findIndex(card => card.moments[0].inner.id === currentCard.moments[0].inner.id);
+    const newMoments = [];
+    currentCard.type !== 'Replacement'
+      ? currentCard.moments?.forEach(moment => moment.icons && newMoments.push(moment))
+      : newMoments.push(currentCard.moments[0]);
+    const momentIndex = newMoments?.findIndex(moment => moment.inner?.id === currentMoment?.inner?.id);
 
     switch (e.key) {
       case 'ArrowUp':
+        if (activeCardList === 'events') {
+          if (momentIndex === 0 || momentIndex === -1) break;
+          dispatch(setCurrentMoment(currentCard.moments[momentIndex - 1]));
+          break;
+        }
         if (cardIndex === 0) break;
         if (e.shiftKey) {
           const prevCard = cards
@@ -27,6 +39,11 @@ const useArrowNavigate = (cards, currentCard) => {
         dispatch(setCurrentCard(cards[cardIndex - 1]));
         break;
       case 'ArrowDown':
+        if (activeCardList === 'events') {
+          if (momentIndex >= currentCard.moments.length - 1 || momentIndex === -1) break;
+          dispatch(setCurrentMoment(currentCard.moments[momentIndex + 1]));
+          break;
+        }
         if (cardIndex >= cards.length - 1) break;
         if (e.shiftKey) {
           const nextCard = cards.slice(cardIndex + 1).find(card => card.who_id === currentCard.who_id);
@@ -37,10 +54,10 @@ const useArrowNavigate = (cards, currentCard) => {
         dispatch(setCurrentCard(cards[cardIndex + 1]));
         break;
       case 'ArrowLeft':
-				dispatch(setActiveCardList('cards'))
+        dispatch(setActiveCardList('cards'));
         break;
-				case 'ArrowRight':
-				dispatch(setActiveCardList('events'))
+      case 'ArrowRight':
+        dispatch(setActiveCardList('events'));
         break;
       default:
         break;
