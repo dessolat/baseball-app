3efc122aac10c64485c1ec2aa38f6b8bf57641ff
Igor Nikiforov <desola2@inbox.ru>
2022-01-26 23:02:43 +0300

    feat: remove lineup and box from renderTab
    * add isVideo handling

--- src/components/Game/ContentGraphics/ContentGraphics.js
+++ src/components/Game/ContentGraphics/ContentGraphics.js
@@ -2,15 +2,17 @@ import React from 'react';
 import cl from './ContentGraphics.module.scss';
 import Videos from '../Videos/Videos';
 import Plays from '../Plays/Plays';
+import { useSelector } from 'react-redux';
 
 const ContentGraphics = ({ currentTab }) => {
+	const innings = useSelector(state => state.game.innings)
+
+	const isVideo = innings[0]['top/guests'][0].moments[0].video !== null
+
   const renderTab = () => {
     switch (currentTab) {
-      case 'lineup':
-      case 'box':
-        return <></>;
       case 'videos':
-        return <Videos />;
+        return isVideo ? <Videos /> : <Plays />;
       default:
         return <Plays />;
     }
