48514705985ef707440cc0270c292b6feda018b7
Igor Nikiforov <desola2@inbox.ru>
2022-01-27 13:31:52 +0300

    feat: remove videos tab in games w/o video

--- src/components/Game/HeaderTabs/HeaderTabs.js
+++ src/components/Game/HeaderTabs/HeaderTabs.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import cl from './HeaderTabs.module.scss';
 
-const tabsArr = ['box', 'plays', 'videos'];
 
 const HeaderTabs = ({ currentTab, handleClick }) => {
-  const getClass = name => (currentTab === name ? cl.active : '');
+	const innings = useSelector(state => state.game.innings)
 
+	const isVideo = innings[0]['top/guests'][0].moments[0].video !== null
+
+	const tabsArr = isVideo ? ['box', 'plays', 'videos'] : ['box', 'plays'];
+
+	const getClass = name => (currentTab === name ? cl.active : '');
   return (
     <ul className={cl.headerTabs}>
       {tabsArr.map((item, i) => (
