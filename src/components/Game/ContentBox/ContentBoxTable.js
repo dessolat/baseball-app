c830f60d4ce1d429498328e44d8d4381a4fd2be8
Igor Nikiforov <desola2@inbox.ru>
2022-01-29 16:30:58 +0300

    feat: add footer sum & replacements

--- src/components/Game/ContentBox/ContentBoxTable.js
+++ src/components/Game/ContentBox/ContentBoxTable.js
@@ -69,6 +69,8 @@ const ContentBoxTable = ({ tableData, tableClass, tableName, footerOffset, toFix
           .filter(player => player.is_catcher)
           .reduce((sum, cur) => sum + Number(cur.content['running'][field]), 0)
       : tableData.reduce((sum, cur) => sum + Number(cur.content[table][field]), 0);
+
+	let rowDelta = 0
   return (
     <table className={cl.table + ' ' + tableClass}>
       <thead>
@@ -94,27 +96,32 @@ const ContentBoxTable = ({ tableData, tableClass, tableName, footerOffset, toFix
               ? player.is_catcher
               : true
           )
-          .map((player, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{player.content.player_name}</td>
-              {TABLES_INFO[tableName].headers.map((title, i) => (
-                <td key={i} style={toFixList.includes(title) ? { width: '3rem' } : null}>
-                  {title === 'POS'
-                    ? player.content.position[0]
-                    : ['SB', 'CS', 'SB_pr', 'LOB'].includes(title)
-                    ? player.content.running[title]
-                    : title === 'PB'
-                    ? '--' :
-										toFixList.includes(title) ? Number(player.content[tableName][title]).toFixed(3)
-                    : player.content[tableName][title]}
-                </td>
-              ))}
-              {/* {Object.entries(player).map((entry, j) => (
+          .map((player, i) => {
+						if (player.is_substituted) rowDelta++
+						
+            return (
+              <tr key={i}>
+                <td>{player.is_substituted ? ' ' : i + 1 - rowDelta}</td>
+                <td style={player.is_substituted ? {paddingLeft: '2.5rem'} : null}>{player.content.player_name}</td>
+                {TABLES_INFO[tableName].headers.map((title, i) => (
+                  <td key={i} style={toFixList.includes(title) ? { width: '3rem' } : null}>
+                    {title === 'POS'
+                      ? player.content.position[0]
+                      : ['SB', 'CS', 'SB_pr', 'LOB'].includes(title)
+                      ? player.content.running[title]
+                      : title === 'PB'
+                      ? '--'
+                      : toFixList.includes(title)
+                      ? Number(player.content[tableName][title]).toFixed(3)
+                      : player.content[tableName][title]}
+                  </td>
+                ))}
+                {/* {Object.entries(player).map((entry, j) => (
               <td key={j}>{toFixList.includes(entry[0]) ? entry[1].toFixed(3) : entry[1]}</td>
             ))} */}
-            </tr>
-          ))}
+              </tr>
+            );
+          })}
         {/* {tableData.map((player, i) => (
           <tr key={i}>
             <td>{i + 1}</td>
@@ -125,6 +132,46 @@ const ContentBoxTable = ({ tableData, tableClass, tableName, footerOffset, toFix
         ))} */}
       </tbody>
       <tfoot>
+        <tr
+          style={
+            tableData.filter(player =>
+              tableName === 'pitching'
+                ? player.is_pitcher
+                : tableName === 'catching' || tableName === 'fielding'
+                ? player.is_catcher
+                : true
+            ).length % 2
+              ? { backgroundColor: '#eaeaea' }
+              : {}
+          }>
+          <td></td>
+          <td>TOTALS</td>
+          {/* {footerOffset > 1 && <td></td>} */}
+          {TABLES_INFO[tableName].headers.map((title, i) => (
+            <td key={i}>
+              {title === 'PB'
+                ? '--'
+                : title === 'POS'
+                ? ' '
+                : title === 'SB_pr'
+                ? getFieldSum('running', title).toFixed(3)
+                : toFixList.includes(title)
+                ? getFieldSum(tableName, title).toFixed(3)
+                : getFieldSum(
+                    ['SB', 'CS', 'LOB'].includes(title) && tableName !== 'catching' ? 'running' : tableName,
+                    title
+                  )}
+            </td>
+          ))}
+
+          {/* {Object.entries(tableData[0])
+            .slice(footerOffset)
+            .map((entry, j) => (
+              <td key={j} style={toFixList.includes(entry[0]) ? { width: '3rem' } : null}>
+                {toFixList.includes(entry[0]) ? entry[1].toFixed(3) : entry[1]}
+              </td>
+            ))} */}
+        </tr>
         {/* <tr style={tableData.length % 2 ? { backgroundColor: '#eaeaea' } : {}}>
           <td></td>
           <td>TOTALS</td>
