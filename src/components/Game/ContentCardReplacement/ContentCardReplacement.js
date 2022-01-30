2a182af6e395e64213b70eb8d4e9acffe371c8cc
Igor Nikiforov <desola2@inbox.ru>
2022-01-30 21:05:01 +0300

    feat: rework replacement card functional

--- src/components/Game/ContentCardReplacement/ContentCardReplacement.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacement.js
@@ -5,23 +5,71 @@ import PortraitImg from 'images/portrait.png';
 const ContentCardReplacement = ({ events }) => {
   const ref = useRef(null);
 
-  useLayoutEffect(() => {
-		if (ref.current === null) return 
+  // useLayoutEffect(() => {
+  // 	if (ref.current === null) return
 
-    ref.current.innerHTML = text + '.';
-  }, [text]);
+  //   ref.current.innerHTML = text + '.';
+  // }, [text]);
+  const getShortName = name => (name.length > 8 ? name.slice(0, 17) + '…' : name);
 
+  let teamName;
   return (
-    <div className={cl.replace}>
-      <div className={cl.portrait}>
-        <img className={cl.default} src={PortraitImg} alt='Portrait' />
-      </div>
-      <p className={cl.text} ref={ref}>
-        {text + '.'}
-      </p>
-      <div className={cl.portrait}>
-        <img className={cl.default} src={PortraitImg} alt='Portrait' />
-      </div>
+    <div>
+      {events.map((event, i) => {
+        if (event.team !== teamName) {
+          teamName = event.team;
+
+          return (
+            <>
+              <p className={cl.title}>{getShortName(event.team)} replacements:</p>
+              <div className={cl.replace} key={i}>
+                <div className={cl.portrait}>
+                  <img className={cl.default} src={PortraitImg} alt='Portrait' />
+                </div>
+                <p className={cl.text}>{event.description}</p>
+                <div className={cl.portrait}>
+                  <img className={cl.default} src={PortraitImg} alt='Portrait' />
+                </div>
+              </div>
+            </>
+          );
+        }
+
+				return <div className={cl.replace} key={i}>
+				<div className={cl.portrait}>
+					<img className={cl.default} src={PortraitImg} alt='Portrait' />
+				</div>
+				<p className={cl.text}>{event.description}</p>
+				<div className={cl.portrait}>
+					<img className={cl.default} src={PortraitImg} alt='Portrait' />
+				</div>
+			</div>
+
+        // return i === 0 || event.team !== teamName ? (
+        //   <>
+        //     <p className={cl.title}>{getShortName(event.team)} replacements:</p>
+        //     <div className={cl.replace} key={i}>
+        //       <div className={cl.portrait}>
+        //         <img className={cl.default} src={PortraitImg} alt='Portrait' />
+        //       </div>
+        //       <p className={cl.text}>{event.description}</p>
+        //       <div className={cl.portrait}>
+        //         <img className={cl.default} src={PortraitImg} alt='Portrait' />
+        //       </div>
+        //     </div>
+        //   </>
+        // ) : (
+        //   <div className={cl.replace} key={i}>
+        //     <div className={cl.portrait}>
+        //       <img className={cl.default} src={PortraitImg} alt='Portrait' />
+        //     </div>
+        //     <p className={cl.text}>{event.description}</p>
+        //     <div className={cl.portrait}>
+        //       <img className={cl.default} src={PortraitImg} alt='Portrait' />
+        //     </div>
+        //   </div>
+        // );
+      })}
     </div>
   );
 };
