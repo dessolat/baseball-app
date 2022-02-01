9f84e756e2e34a24eea22100e6aab3c3bba725b1
Igor Nikiforov <desola2@inbox.ru>
2022-02-01 14:38:15 +0300

    feat: add portraits to replacements

--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import cl from './ContentCardReplacement.module.scss';
 import PortraitImg from 'images/portrait.png';
+import { useSelector } from 'react-redux';
 
 const ContentCardReplacementItem = ({ event }) => {
+  const imagesData = useSelector(state => state.game.imagesData);
+
   return (
     <div className={cl.replace}>
       <div className={cl.portrait}>
-        <img className={cl.default} src={PortraitImg} alt='Portrait' />
+        <img
+          className={!imagesData[event.new_player] ? cl.default : ''}
+          src={imagesData[event.new_player] || PortraitImg}
+          alt='Portrait'
+        />
       </div>
       <p className={cl.text}>{event.description}</p>
-      <div className={cl.portrait}>
-        <img className={cl.default} src={PortraitImg} alt='Portrait' />
+      <div className={cl.portrait} style={event.old_player === null ? {display: 'none'} : null}>
+        <img
+          className={!imagesData[event.old_player] ? cl.default : ''}
+          src={imagesData[event.old_player] || PortraitImg}
+          alt='Portrait'
+        />
       </div>
     </div>
   );
