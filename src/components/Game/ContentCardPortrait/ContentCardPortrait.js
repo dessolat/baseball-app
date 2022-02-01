421247a6df1ab990d1ccd8dbe0c8a20002b77612
Igor Nikiforov <desola2@inbox.ru>
2022-02-01 23:47:50 +0300

    feat: move code

--- src/components/Game/ContentCardPortrait/ContentCardPortrait.js
+++ src/components/Game/ContentCardPortrait/ContentCardPortrait.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const ContentCardPortrait = () => {
-	return <div></div>;
+const ContentCardPortrait = ({ className, src, cl, ...props }) => {
+  return (
+    <div className={cl.portrait} {...props}>
+      <img className={className} src={src} alt='Portrait' />
+    </div>
+  );
 };
 
 export default ContentCardPortrait;
--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem.js
@@ -2,27 +2,26 @@ import React from 'react';
 import cl from './ContentCardReplacement.module.scss';
 import PortraitImg from 'images/portrait.png';
 import { useSelector } from 'react-redux';
+import ContentCardPortrait from '../ContentCardPortrait/ContentCardPortrait';
 
 const ContentCardReplacementItem = ({ event }) => {
   const imagesData = useSelector(state => state.game.imagesData);
 
+  const leftImgClass = !imagesData[event.new_player] ? cl.default : '';
+  const leftImgSrc = imagesData[event.new_player] || PortraitImg;
+  const rightImgClass = !imagesData[event.old_player] ? cl.default : '';
+  const rightImgSrc = imagesData[event.old_player] || PortraitImg;
+
   return (
     <div className={cl.replace}>
-      <div className={cl.portrait}>
-        <img
-          className={!imagesData[event.new_player] ? cl.default : ''}
-          src={imagesData[event.new_player] || PortraitImg}
-          alt='Portrait'
-        />
-      </div>
+      <ContentCardPortrait className={leftImgClass} src={leftImgSrc} cl={cl} />
       <p className={cl.text}>{event.description}</p>
-      <div className={cl.portrait} style={event.old_player === null ? {display: 'none'} : null}>
-        <img
-          className={!imagesData[event.old_player] ? cl.default : ''}
-          src={imagesData[event.old_player] || PortraitImg}
-          alt='Portrait'
-        />
-      </div>
+      <ContentCardPortrait
+        className={rightImgClass}
+        src={rightImgSrc}
+        cl={cl}
+        style={event.old_player === null ? { display: 'none' } : null}
+      />
     </div>
   );
 };
