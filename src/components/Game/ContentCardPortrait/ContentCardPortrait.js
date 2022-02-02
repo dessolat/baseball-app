f30757639e74f26e912846377ba90512d68f357d
Igor Nikiforov <desola2@inbox.ru>
2022-02-02 19:21:55 +0300

    refactor: minify comp

--- src/components/Game/ContentCardPortrait/ContentCardPortrait.js
+++ src/components/Game/ContentCardPortrait/ContentCardPortrait.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const ContentCardPortrait = ({ className, src, cl, ...props }) => {
-  return (
-    <div className={cl.portrait} {...props}>
-      <img className={className} src={src} alt='Portrait' />
-    </div>
-  );
-};
+const ContentCardPortrait = ({ className, src, cl, ...props }) => (
+  <div className={cl.portrait} {...props}>
+    <img className={className} src={src} alt='Portrait' />
+  </div>
+);
 
 export default ContentCardPortrait;
