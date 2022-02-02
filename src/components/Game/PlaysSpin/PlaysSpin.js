b8e96f908e47651f93525346a0e374568300084d
Igor Nikiforov <desola2@inbox.ru>
2022-02-02 19:03:09 +0300

    refactor: move code

--- src/components/Game/PlaysSpin/PlaysSpin.js
+++ src/components/Game/PlaysSpin/PlaysSpin.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import cl from './PlaysSpin.module.scss';
+import PlaysSpinChart from './PlaysSpinChart';
 import PlaysSpinInfo from './PlaysSpinInfo';
 
-const PlaysSpin = ({ pitch }) => {
-  const { offset_x, offset_y } = pitch || 0;
-
-  const isDrawBall = pitch;
-  const coordX = 50 + 50 * offset_x;
-  const coordY = 50 - 50 * offset_y;
-
-  return (
-    <div className={cl.spin}>
-      <svg width='100' height='100' viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <line x1='49.5' y1='100' x2='49.5' stroke='#ACACAC' />
-        <line y1='49.5' x2='100' y2='49.5' stroke='#ACACAC' />
-        {isDrawBall && <circle cx={coordX} cy={coordY} r='7.5' fill='#2B9D6A' />}
-      </svg>
-      <PlaysSpinInfo pitch={pitch} />
-    </div>
-  );
-};
+const PlaysSpin = ({ pitch }) => (
+  <div className={cl.spin}>
+    <PlaysSpinChart pitch={pitch} />
+    <PlaysSpinInfo pitch={pitch} />
+  </div>
+);
 
 export default PlaysSpin;
--- src/components/Game/PlaysSpin/PlaysSpinChart.js
+++ src/components/Game/PlaysSpin/PlaysSpinChart.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const PlaysSpinChart = () => {
-	return <div></div>;
+const PlaysSpinChart = ({ pitch }) => {
+  const { offset_x, offset_y } = pitch || 0;
+
+  const isDrawBall = pitch;
+  const coordX = 50 + 50 * offset_x;
+  const coordY = 50 - 50 * offset_y;
+  return (
+    <svg width='100' height='100' viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <line x1='49.5' y1='100' x2='49.5' stroke='#ACACAC' />
+      <line y1='49.5' x2='100' y2='49.5' stroke='#ACACAC' />
+      {isDrawBall && <circle cx={coordX} cy={coordY} r='7.5' fill='#2B9D6A' />}
+    </svg>
+  );
 };
 
 export default PlaysSpinChart;
