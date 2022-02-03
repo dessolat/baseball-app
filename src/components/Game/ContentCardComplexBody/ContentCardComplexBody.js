823e667289aff4fc3f9d79f36f0d7d12a294ebc9
Igor Nikiforov <desola2@inbox.ru>
2022-02-03 20:42:34 +0300

    refactor: remove old comment

--- src/components/Game/ContentCardComplexBody/ContentCardComplexBody.js
+++ src/components/Game/ContentCardComplexBody/ContentCardComplexBody.js
@@ -19,7 +19,6 @@ const ContentCardComplexBody = ({ sit }) => {
 
   useLayoutEffect(() => {
     if (ref.current === null) return;
-    // if (sit.icons.rect_text === 'Replacement') return;
     ref.current.innerHTML = eventsSummary.join('.') + '.';
   }, [eventsSummary, sit.icons.rect_text]);
 
