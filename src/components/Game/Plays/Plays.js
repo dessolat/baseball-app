87571dfdcbd6c47133d80ac4f27d5ffed31a5325
Igor Nikiforov <desola2@inbox.ru>
2022-02-03 20:45:08 +0300

    feat: add isVideo handling

--- src/components/Game/Plays/Plays.js
+++ src/components/Game/Plays/Plays.js
@@ -7,6 +7,8 @@ import { useSelector } from 'react-redux';
 import { getSearchParam, setSearchParam } from 'utils';
 
 const Plays = ({ isVideo }) => {
+  const ptab = getSearchParam('ptab');
+  const defaultPtab = ['pitch', 'hitting', 'running'].includes(ptab) ? ptab : 'pitch';
   const [currentTab, setCurrentTab] = useState(defaultPtab);
   const [moments, setMoments] = useState([]);
   const currentCard = useSelector(state => state.game.currentCard);
@@ -17,11 +19,19 @@ const Plays = ({ isVideo }) => {
       ? currentCard.moments?.forEach(moment => moment.icons && newMoments.push(moment))
       : newMoments.push(currentCard.moments[0]);
     setMoments(newMoments);
-		// eslint-disable-next-line
+    // eslint-disable-next-line
   }, [currentCard]);
 
   const classes = [cl.plays];
-  classes.push(currentTab === 'hitting' ? cl.hitting : currentTab === 'running' ? cl.running : cl.pitch);
+  classes.push(
+    !isVideo
+      ? cl.playsNoVideo
+      : currentTab === 'hitting'
+      ? cl.hitting
+      : currentTab === 'running'
+      ? cl.running
+      : cl.pitch
+  );
 
   const handleTabClick = e => {
     setSearchParam('ptab', e.target.name);
@@ -31,8 +41,12 @@ const Plays = ({ isVideo }) => {
   return (
     <div className={classes.join(' ')}>
       <PlaysEvents moments={moments} />
-      <PlaysContent moments={moments} currentTab={currentTab}/>
-      <PlaysFooter currentTab={currentTab} handleClick={handleTabClick} />
+      {isVideo && (
+        <>
+          <PlaysContent moments={moments} currentTab={currentTab} />
+          <PlaysFooter currentTab={currentTab} handleClick={handleTabClick} />
+        </>
+      )}
     </div>
   );
 };
