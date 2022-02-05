974517af695369010e548020438d221f7ccc8ade
Igor Nikiforov <desola2@inbox.ru>
2022-02-05 22:36:02 +0300

    refactor: move code from nav

--- src/pages/Games/Games.js
+++ src/pages/Games/Games.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import cl from './Games.module.scss';
+import { Link } from 'react-router-dom';
 
 const Games = () => {
   return (
     <section className={cl.games}>
-      
+      <h1>Games page</h1>
+			<Link to='/game/279'>279</Link>
+			<Link to='/game/280'>280</Link>
+			<Link to='/game/359'>359</Link>
+			<Link to='/game/380'>380</Link>
+			<Link to='/game/383'>383</Link>
     </section>
   );
 };
