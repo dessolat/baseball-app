4aff91cbad645f8a160e950929ff72ef398a417f
Igor Nikiforov <desola2@inbox.ru>
2022-02-05 22:35:16 +0300

    feat: create Games page placeholder

---
+++ src/pages/Games/Games.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './Games.module.scss';
+
+const Games = () => {
+  return (
+    <section className={cl.games}>
+      
+    </section>
+  );
+};
+
+export default Games;
