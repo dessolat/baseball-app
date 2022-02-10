dbb4d90df6310d3921d20c4b4379d8918adfb5b5
Igor Nikiforov <desola2@inbox.ru>
2022-02-10 12:26:19 +0300

    feat: create HeaderWrapper comp placeholder

---
+++ src/components/HeaderWrapper/HeaderWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './HeaderWrapper.module.scss';
+
+const HeaderWrapper = ({children, ...props}) => {
+	return (
+		<div></div>
+	)
+}
+
+export default HeaderWrapper
