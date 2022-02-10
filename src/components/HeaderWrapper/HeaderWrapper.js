729cecc906493e74ea5284ca95c375cf32b02f6e
Igor Nikiforov <desola2@inbox.ru>
2022-02-10 12:30:46 +0300

    feat: create HeaderWrapper comp

--- src/components/HeaderWrapper/HeaderWrapper.js
+++ src/components/HeaderWrapper/HeaderWrapper.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
 import cl from './HeaderWrapper.module.scss';
 
-const HeaderWrapper = ({children, ...props}) => {
-	return (
-		<div></div>
-	)
-}
+const HeaderWrapper = ({ children, ...props }) => {
+  return (
+    <header className={cl.header} {...props}>
+      <div className='container'>{children}</div>
+    </header>
+  );
+};
 
-export default HeaderWrapper
+export default HeaderWrapper;
