db402bfb724584edfb619b01b2bb610fa5ddf626
Igor Nikiforov <desola2@inbox.ru>
2022-02-11 22:25:56 +0300

    feat: minor years layout change

--- src/components/Games/HeaderSelections/HeaderSelections.js
+++ src/components/Games/HeaderSelections/HeaderSelections.js
@@ -5,11 +5,17 @@ const HeaderSelections = () => {
   return (
     <div className={cl.selections}>
       <ul className={cl.years}>
-        <li className={cl.active}>2021</li>
-        <li>2022</li>
+        <li>
+          2021 
+          <svg width='8' height='7' viewBox='0 0 8 7' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path
+              d='M0.904896 0.455675L0.00390625 1.5116L3.8379 6.00488L7.6719 1.5116L6.77091 0.455675L3.8379 3.88555L0.904896 0.455675Z'
+              fill='#D1D1D1'
+            />
+          </svg>
+        </li>
       </ul>
       <ul className={cl.types}>
-        <li>All</li>
         <li className={cl.active}>Baseball</li>
         <li>Softball</li>
       </ul>
