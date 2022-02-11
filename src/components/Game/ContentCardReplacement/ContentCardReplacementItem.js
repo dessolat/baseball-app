d9cbad3eb1a521fdd9fb7b627336192e00b6db04
Igor Nikiforov <desola2@inbox.ru>
2022-02-11 21:57:28 +0300

    feat: minor layout change

--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem.js
@@ -4,18 +4,24 @@ import PortraitImg from 'images/portrait.png';
 import { useSelector } from 'react-redux';
 import ContentCardPortrait from '../ContentCardPortrait/ContentCardPortrait';
 
-const ContentCardReplacementItem = ({ event }) => {
+const ContentCardReplacementItem = ({ event, header = null }) => {
   const imagesData = useSelector(state => state.game.imagesData);
+  const isVideo = useSelector(state => state.game.isVideo);
 
   const leftImgClass = !imagesData[event.new_player] ? cl.default : '';
   const leftImgSrc = imagesData[event.new_player] || PortraitImg;
   const rightImgClass = !imagesData[event.old_player] ? cl.default : '';
   const rightImgSrc = imagesData[event.old_player] || PortraitImg;
 
+  const replaceClasses = [cl.replace];
+  !isVideo && replaceClasses.push(cl.noVideo);
   return (
-    <div className={cl.replace}>
+    <div className={replaceClasses.join(' ')}>
       <ContentCardPortrait className={leftImgClass} src={leftImgSrc} cl={cl} />
-      <p className={cl.text}>{event.description}</p>
+      <div className={cl.textBlock}>
+				{header !== null && <p className={cl.textBold}>{header}</p>}
+        <p className={cl.text}>{event.description}</p>
+      </div>
       <ContentCardPortrait
         className={rightImgClass}
         src={rightImgSrc}
