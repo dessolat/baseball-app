f41c8e67814808729e18eea3ebfb18c17112a42a
Igor Nikiforov <desola2@inbox.ru>
2022-02-11 21:55:26 +0300

    feat: add scrolling

--- src/components/Games/Header/Header.js
+++ src/components/Games/Header/Header.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import cl from './Header.module.scss';
 import HeaderWrapper from 'components/HeaderWrapper/HeaderWrapper';
 import Arrow from 'components/UI/buttons/Arrow/Arrow';
 import LeagueImage from 'images/league_image.png';
 
 const Header = () => {
+  const [currentScroll, setCurrentScroll] = useState(0);
+  const [isLeftScroll, setIsLeftScroll] = useState(false);
+  const [isRightScroll, setIsRightScroll] = useState(true);
+
+  const leaguesRef = useRef();
+
+  useEffect(() => {
+    setIsLeftScroll(currentScroll <= 0 ? false : true);
+    setIsRightScroll(currentScroll + leaguesRef.current.clientWidth < leaguesRef.current.scrollWidth);
+  }, [currentScroll]);
+
+  const scrollLeagues = e => {
+    if (e.currentTarget.name === 'scroll-left') {
+      const scrollValue = leaguesRef.current.scrollLeft - 248;
+      leaguesRef.current.scrollLeft -= 248;
+      setCurrentScroll(scrollValue);
+      return;
+    }
+    const scrollValue = leaguesRef.current.scrollLeft + 248;
+    leaguesRef.current.scrollLeft += 248;
+    setCurrentScroll(scrollValue);
+  };
   return (
     <HeaderWrapper>
       <div className={cl.headerContent}>
@@ -20,30 +42,21 @@ const Header = () => {
           </ul>
         </div>
         <div className={cl.leaguesWrapper}>
-          <Arrow />
-          <ul className={cl.leagues}>
-            <li className={cl.league + ' ' + cl.active}>
-              <img src={LeagueImage} alt='league-image' />
-							<p>Russian championship</p>
-            </li>
-            <li className={cl.league}>
-              <img src={LeagueImage} alt='league-image' />
-							<p>Russian championship</p>
-            </li>
-            <li className={cl.league}>
-              <img src={LeagueImage} alt='league-image' />
-							<p>Russian championship</p>
-            </li>
-            <li className={cl.league}>
-              <img src={LeagueImage} alt='league-image' />
-							<p>Russian championship</p>
-            </li>
-            <li className={cl.league}>
-              <img src={LeagueImage} alt='league-image' />
-							<p>Russian championship</p>
-            </li>
+          <Arrow onClick={scrollLeagues} style={!isLeftScroll ? { visibility: 'hidden' } : null} />
+          <ul className={cl.leagues} ref={leaguesRef}>
+            <li className={cl.league + ' ' + cl.active}>All</li>
+            <li className={cl.league}>Russian championship</li>
+            <li className={cl.league}>Russian championship</li>
+            <li className={cl.league}>Russian championship</li>
+            <li className={cl.league}>Russian championship</li>
+            <li className={cl.league}>Russian championship</li>
+            <li className={cl.league}>Russian championship</li>
           </ul>
-          <Arrow direction='right' />
+          <Arrow
+            direction='right'
+            onClick={scrollLeagues}
+            style={!isRightScroll ? { visibility: 'hidden' } : null}
+          />
         </div>
       </div>
     </HeaderWrapper>
