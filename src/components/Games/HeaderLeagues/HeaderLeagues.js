8485add9d533130b0259ab65d27f1e3a260b23fb
Igor Nikiforov <desola2@inbox.ru>
2022-02-11 22:28:03 +0300

    feat: create HeaderLeagues placeholder comp

---
+++ src/components/Games/HeaderLeagues/HeaderLeagues.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './HeaderLeagues.module.scss';
+
+const HeaderLeagues = () => {
+	return (
+		<div>HeaderLeagues</div>
+	)
+}
+
+export default HeaderLeagues
