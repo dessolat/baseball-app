1a0422fbac09f05611be0652eb7d473d02896ee2
Igor Nikiforov <desola2@inbox.ru>
2022-02-11 21:52:24 +0300

    feat: add TABLES_DATA temp

--- src/pages/Games/Games.js
+++ src/pages/Games/Games.js
@@ -2,6 +2,20 @@ import Header from 'components/Games/Header/Header';
 import React from 'react';
 import cl from './Games.module.scss';
 
+const TABLES_DATA = {
+  summary: {
+    headers: [],
+    rows: []
+  },
+  wins_losses: {
+    headers: [],
+    rows: []
+  },
+  batting: {
+    headers: [],
+    rows: []
+  }
+};
 const Games = () => {
   return (
     <Header />
