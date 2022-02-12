2c76a7922243155b3ce43131b7649d7fab7f572d
Igor Nikiforov <desola2@inbox.ru>
2022-02-12 17:25:33 +0300

    feat: add gamesReducer to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { gameReducer } from './gameReducer';
+import { gamesReducer } from './gamesReducer';
 
-const rootReducer = combineReducers({ game: gameReducer });
+const rootReducer = combineReducers({ game: gameReducer, games: gamesReducer });
 
 export default rootReducer
