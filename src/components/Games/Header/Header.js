6865bc69f15814989a99000ba492ca2eb294359b
Igor Nikiforov <desola2@inbox.ru>
2022-02-12 17:26:46 +0300

    refactor: move code

--- src/components/Games/Header/Header.js
+++ src/components/Games/Header/Header.js
@@ -1,59 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import cl from './Header.module.scss';
 import HeaderWrapper from 'components/HeaderWrapper/HeaderWrapper';
-import Arrow from 'components/UI/buttons/Arrow/Arrow';
-import LeagueImage from 'images/league_image.png';
 import HeaderSelections from '../HeaderSelections/HeaderSelections';
+import HeaderLeagues from '../HeaderLeagues/HeaderLeagues';
 
-const Header = () => {
-  const [currentScroll, setCurrentScroll] = useState(0);
-  const [isLeftScroll, setIsLeftScroll] = useState(false);
-  const [isRightScroll, setIsRightScroll] = useState(true);
-
-  const leaguesRef = useRef();
-
-  useEffect(() => {
-    setIsLeftScroll(currentScroll <= 0 ? false : true);
-    setIsRightScroll(currentScroll + leaguesRef.current.clientWidth < leaguesRef.current.scrollWidth);
-  }, [currentScroll]);
-
-  const scrollLeagues = e => {
-    if (e.currentTarget.name === 'scroll-left') {
-      const scrollValue = leaguesRef.current.scrollLeft - 248;
-      leaguesRef.current.scrollLeft -= 248;
-      setCurrentScroll(scrollValue);
-      return;
-    }
-    const scrollValue = leaguesRef.current.scrollLeft + 248;
-    leaguesRef.current.scrollLeft += 248;
-    setCurrentScroll(scrollValue);
-  };
-  return (
-    <HeaderWrapper>
-      <div className={cl.headerContent}>
-        <HeaderSelections />
-        <div className={cl.leaguesWrapper}>
-          <Arrow onClick={scrollLeagues} style={!isLeftScroll ? { visibility: 'hidden' } : null} />
-          <ul className={cl.leagues} ref={leaguesRef}>
-            <li className={cl.league}>All</li>
-            <li className={cl.league + ' ' + cl.active}>Pervenstvo Russian Unior (19 years)</li>
-            <li className={cl.league}>Russian championship</li>
-            <li className={cl.league}>Russian championship</li>
-            <li className={cl.league}>Russian championship</li>
-            <li className={cl.league}>Russian championship</li>
-            <li className={cl.league}>Russian championship</li>
-            <li className={cl.league}>Russian championship</li>
-            <li className={cl.league}>Russian championship</li>
-          </ul>
-          <Arrow
-            direction='right'
-            onClick={scrollLeagues}
-            style={!isRightScroll ? { visibility: 'hidden' } : null}
-          />
-        </div>
-      </div>
-    </HeaderWrapper>
-  );
-};
+const Header = ({ leagues }) => (
+  <HeaderWrapper>
+    <div className={cl.headerContent}>
+      <HeaderSelections />
+      <HeaderLeagues leagues={leagues} />
+    </div>
+  </HeaderWrapper>
+);
 
 export default Header;
--- src/components/Games/HeaderLeagues/HeaderLeagues.js
+++ src/components/Games/HeaderLeagues/HeaderLeagues.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import cl from './HeaderLeagues.module.scss';
+import Arrow from 'components/UI/buttons/Arrow/Arrow';
+import LeagueImage from 'images/league_image.png';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentLeague } from 'redux/gamesReducer';
 
-const HeaderLeagues = () => {
-	return (
-		<div>HeaderLeagues</div>
-	)
-}
+const HeaderLeagues = ({ leagues }) => {
+  const [currentScroll, setCurrentScroll] = useState(0);
+  const [isLeftScroll, setIsLeftScroll] = useState(false);
+  const [isRightScroll, setIsRightScroll] = useState(true);
 
-export default HeaderLeagues
+  const leaguesRef = useRef();
+
+  const currentLeague = useSelector(state => state.games.currentLeague);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLeftScroll(currentScroll <= 0 ? false : true);
+    setIsRightScroll(currentScroll + leaguesRef.current.clientWidth < leaguesRef.current.scrollWidth);
+  }, [currentScroll]);
+
+  const scrollLeagues = e => {
+    if (e.currentTarget.name === 'scroll-left') {
+      const scrollValue = leaguesRef.current.scrollLeft - 248;
+      leaguesRef.current.scrollLeft -= 248;
+      setCurrentScroll(scrollValue);
+      return;
+    }
+    const scrollValue = leaguesRef.current.scrollLeft + 248;
+    leaguesRef.current.scrollLeft += 248;
+    setCurrentScroll(scrollValue);
+  };
+  console.log(leagues);
+  return (
+    <div className={cl.leaguesWrapper}>
+      <Arrow onClick={scrollLeagues} style={!isLeftScroll ? { visibility: 'hidden' } : null} />
+      <ul className={cl.leagues} ref={leaguesRef}>
+        {leagues.map(league => {
+					console.log(league);
+          return (
+            <li
+              key={league.id}
+              className={league.id === currentLeague.id ? cl.league + ' ' + cl.active : cl.league}
+              onClick={() => dispatch(setCurrentLeague({ id: league.id, name: league.name }))}>
+              {league.name}
+            </li>
+          );
+        })}
+      </ul>
+      <Arrow
+        direction='right'
+        onClick={scrollLeagues}
+        style={!isRightScroll ? { visibility: 'hidden' } : null}
+      />
+    </div>
+  );
+};
+
+export default HeaderLeagues;
