824120c2cd03499b11b1de4bf70e634737891637
Igor Nikiforov <desola2@inbox.ru>
2022-02-12 18:08:14 +0300

    feat: add LeagueImage

--- src/components/Games/ContentTable/ContentTable.js
+++ src/components/Games/ContentTable/ContentTable.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import ContentCalendar from '../ContentCalendar/ContentCalendar';
 import cl from './ContentTable.module.scss';
+import LeagueImage from 'images/league_image.png';
 
 const ContentTable = () => {
-	const currentLeague = useSelector(state => state.games.currentLeague)
+  const currentLeague = useSelector(state => state.games.currentLeague);
 
-  return <div className={cl.wrapper}>
-		<div className={cl.header}>
-			<h2 className={cl.teamName}>{currentLeague.name}</h2>
-			<ContentCalendar />
-		</div>
-	</div>;
+  return (
+    <div className={cl.wrapper}>
+      <div className={cl.header}>
+        <div className={cl.teamWrapper}>
+          <img src={LeagueImage} />
+          <h2 className={cl.teamName}>{currentLeague.name}</h2>
+        </div>
+        <ContentCalendar />
+      </div>
+    </div>
+  );
 };
 
 export default ContentTable;
