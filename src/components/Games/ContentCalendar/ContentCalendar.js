19cd284640848ba65940ac0744c2ee617379b0ed
Igor Nikiforov <desola2@inbox.ru>
2022-02-12 17:33:49 +0300

    feat: create ContentCalendar comp basic L/O

---
+++ src/components/Games/ContentCalendar/ContentCalendar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './ContentCalendar.module.scss';
+
+const ContentCalendar = () => {
+	return (
+		<p className={cl.calendar}>Calendar</p>
+	)
+}
+
+export default ContentCalendar
--- src/components/Games/ContentTable/ContentTable.js
+++ src/components/Games/ContentTable/ContentTable.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import ContentCalendar from '../ContentCalendar/ContentCalendar';
 import cl from './ContentTable.module.scss';
 
 const ContentTable = () => {
@@ -8,7 +9,7 @@ const ContentTable = () => {
   return <div className={cl.wrapper}>
 		<div className={cl.header}>
 			<h2 className={cl.teamName}>{currentLeague.name}</h2>
-			<p className={cl.calendar}>Calendar</p>
+			<ContentCalendar />
 		</div>
 	</div>;
 };
