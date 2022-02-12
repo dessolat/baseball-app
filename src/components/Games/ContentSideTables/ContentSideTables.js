a22b74f4bcdb36671105658864caa93b56faffd7
Igor Nikiforov <desola2@inbox.ru>
2022-02-12 17:28:03 +0300

    feat: create SideTables comp

---
+++ src/components/Games/ContentSideTables/ContentSideTables.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import cl from './ContentSideTables.module.scss';
+
+const FIRST_TABLE_DATA = [
+  { id: 1, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
+  { id: 2, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
+  { id: 3, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
+  { id: 4, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
+  { id: 5, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
+  { id: 6, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
+  { id: 7, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
+  { id: 8, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' }
+];
+
+const ContentSideTables = () => {
+  return (
+    <div className={cl.side}>
+      <table className={cl.pctTable}>
+        <thead>
+          <tr>
+            <th>Team</th>
+            <th>W</th>
+            <th>L</th>
+            <th>PCT</th>
+          </tr>
+        </thead>
+        <tbody>
+          {FIRST_TABLE_DATA.map(row => (
+            <tr key={row.id}>
+              <td>{row.name}</td>
+              <td>{row.w}</td>
+              <td>{row.l}</td>
+              <td>{row.pct}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ContentSideTables;
