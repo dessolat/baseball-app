f9d0d9516168bb74bd592fe07b23b7e29d96f4b0
Igor Nikiforov <desola2@inbox.ru>
2022-02-13 15:49:44 +0300

    feat: create Dropdown comp

---
+++ src/components/UI/dropdown/GamesDropdown/Dropdown.js
@@ -0,0 +1,64 @@
+import ArrowDown from 'components/UI/icons/ArrowDown';
+import React, { useState, useEffect, useRef } from 'react';
+import cl from './Dropdown.module.scss';
+
+function listenForOutsideClicks(listening, setListening, menuRef, setIsOpen) {
+  return () => {
+    if (listening) return;
+    if (!menuRef.current) return;
+    setListening(true);
+    [`click`, `touchstart`].forEach((type) => {
+      document.addEventListener(`click`, (evt) => {
+        if (menuRef.current?.contains(evt.target)) return;
+        setIsOpen(false);
+      });
+    });
+  }
+}
+
+const Dropdown = ({ title, options, handleClick }) => {
+  // const [titleValue, setTitleValue] = useState(title);
+  const [listening, setListening] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuRef = useRef(null);
+
+  useEffect(listenForOutsideClicks(
+    listening,
+    setListening,
+    menuRef,
+    setIsOpen,
+  ));
+
+  const handleTitleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = option => () => {
+    // setTitleValue(option);
+		handleClick(option)
+		setIsOpen(false);
+  };
+
+  return (
+    <div ref={menuRef} className={cl.dropdownWrapper}>
+      <div className={cl.title} onClick={handleTitleClick}>
+        {title}
+        <div style={{position: 'absolute', right: 3, top: 0}}>
+          <ArrowDown />
+        </div>
+      </div>
+      {isOpen && (
+        <ul className={cl.list}>
+          {options.map((option, i) => (
+            <li key={i} onClick={handleOptionClick(option)}>
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
