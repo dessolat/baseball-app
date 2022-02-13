af8351c73fc53a6cb06c8b8dd00eae13eb93c99d
Igor Nikiforov <desola2@inbox.ru>
2022-02-13 15:45:24 +0300

    feat: add currentYear to gamesReducer

--- src/redux/gamesReducer.js
+++ src/redux/gamesReducer.js
@@ -1,13 +1,17 @@
 const defaultState = {
-  currentLeague: { id: 1, name: 'All' }
+  currentLeague: { id: 1, name: 'All' },
+	currentYear: new Date().getFullYear()
 };
 
 const SET_CURRENT_LEAGUE = 'SET_CURRENT_LEAGUE';
+const SET_CURRENT_YEAR = 'SET_CURRENT_YEAR';
 
 export const gamesReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_CURRENT_LEAGUE:
       return { ...state, currentLeague: action.payload };
+    case SET_CURRENT_YEAR:
+      return { ...state, currentYear: action.payload };
 
     default:
       return state;
@@ -15,3 +19,4 @@ export const gamesReducer = (state = defaultState, action) => {
 };
 
 export const setCurrentLeague = payload => ({ type: SET_CURRENT_LEAGUE, payload });
+export const setCurrentYear = payload => ({ type: SET_CURRENT_YEAR, payload });
