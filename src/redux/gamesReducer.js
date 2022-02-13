4c12a1aa8e95c619b1c93216ccf00a49bf7d2739
Igor Nikiforov <desola2@inbox.ru>
2022-02-13 16:23:07 +0300

    feat: add currentHome to gamesReducer

--- src/redux/gamesReducer.js
+++ src/redux/gamesReducer.js
@@ -1,10 +1,12 @@
 const defaultState = {
   currentLeague: { id: 1, name: 'All' },
-	currentYear: new Date().getFullYear()
+	currentYear: new Date().getFullYear(),
+	currentHome: 'All'
 };
 
 const SET_CURRENT_LEAGUE = 'SET_CURRENT_LEAGUE';
 const SET_CURRENT_YEAR = 'SET_CURRENT_YEAR';
+const SET_CURRENT_HOME = 'SET_CURRENT_HOME';
 
 export const gamesReducer = (state = defaultState, action) => {
   switch (action.type) {
@@ -12,7 +14,8 @@ export const gamesReducer = (state = defaultState, action) => {
       return { ...state, currentLeague: action.payload };
     case SET_CURRENT_YEAR:
       return { ...state, currentYear: action.payload };
-
+    case SET_CURRENT_HOME:
+      return { ...state, currentHome: action.payload };
     default:
       return state;
   }
@@ -20,3 +23,4 @@ export const gamesReducer = (state = defaultState, action) => {
 
 export const setCurrentLeague = payload => ({ type: SET_CURRENT_LEAGUE, payload });
 export const setCurrentYear = payload => ({ type: SET_CURRENT_YEAR, payload });
+export const setCurrentHome = payload => ({ type: SET_CURRENT_HOME, payload });
