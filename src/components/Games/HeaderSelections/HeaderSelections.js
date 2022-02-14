2778a4ddd157e10d2f9cfbe801e6597330e53320
Igor Nikiforov <desola2@inbox.ru>
2022-02-14 14:50:47 +0300

    feat: add dropdown to Header

--- src/components/Games/HeaderSelections/HeaderSelections.js
+++ src/components/Games/HeaderSelections/HeaderSelections.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import cl from './HeaderSelections.module.scss';
+import Dropdown from 'components/UI/dropdown/GamesDropdown/Dropdown';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentYear } from 'redux/gamesReducer';
+
+const YEARS = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014];
 
 const HeaderSelections = () => {
+  const currentYear = useSelector(state => state.games.currentYear);
+  const dispatch = useDispatch();
+
+  const handleClick = option => {
+    dispatch(setCurrentYear(option));
+  };
+
   return (
     <div className={cl.selections}>
-      <ul className={cl.years}>
-        <li>
-          2021 
-          <svg width='8' height='7' viewBox='0 0 8 7' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path
-              d='M0.904896 0.455675L0.00390625 1.5116L3.8379 6.00488L7.6719 1.5116L6.77091 0.455675L3.8379 3.88555L0.904896 0.455675Z'
-              fill='#D1D1D1'
-            />
-          </svg>
-        </li>
-      </ul>
+      <div className={cl.years}>
+        <Dropdown title={currentYear} options={YEARS} currentOption={currentYear} handleClick={handleClick}/>
+      </div>
       <ul className={cl.types}>
         <li className={cl.active}>Baseball</li>
         <li>Softball</li>
