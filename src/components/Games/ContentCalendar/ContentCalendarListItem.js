c5027d32755d4d815de7a6c78c4e87f0aa80e784
Igor Nikiforov <desola2@inbox.ru>
2022-02-15 09:09:18 +0300

    feat: create ContentCalendar comp

---
+++ src/components/Games/ContentCalendar/ContentCalendarListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import cl from './ContentCalendar.module.scss';
+
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+const ContentCalendarListItem = ({ date, handleClick, ...props }) => {
+  return (
+    <li className={cl.item} {...props} onClick={handleClick(date)}>
+      <p>{DAYS[date.getDay()]}</p>
+      <p>{`${MONTHS[date.getMonth()]} ${date.getDate()}`}</p>
+    </li>
+  );
+};
+
+export default ContentCalendarListItem;
