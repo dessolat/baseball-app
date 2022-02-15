2dc8603bcc9f8a7e6299a1cc5c96cc825066897c
Igor Nikiforov <desola2@inbox.ru>
2022-02-15 11:11:13 +0300

    feat: create ContentCalendarList comp

---
+++ src/components/Games/ContentCalendar/ContentCalendarList.js
@@ -0,0 +1,33 @@
+import React, { forwardRef } from 'react';
+import cl from './ContentCalendar.module.scss';
+import ContentCalendarListItem from './ContentCalendarListItem';
+
+const ContentCalendarList = ({ currentDate, handleClick }, ref) => {
+  const dates = [];
+  for (let i = 0; i < 13; i++) {
+    const tempDate = new Date(currentDate);
+    tempDate.setDate(tempDate.getDate() - 4 + i);
+    dates.push(tempDate);
+  }
+
+  return (
+    <div className={cl.wrapper}>
+      <ul className={cl.list} ref={ref}>
+        {dates.map((date, i) => (
+          <ContentCalendarListItem
+            key={i}
+            date={date}
+            handleClick={handleClick}
+            style={
+              currentDate.toDateString() === date.toDateString()
+                ? { fontWeight: 'bold', color: 'black' }
+                : null
+            }
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default forwardRef(ContentCalendarList);
