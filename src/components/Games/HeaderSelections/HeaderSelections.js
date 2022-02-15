513d9e3bb419d7a276ae9f3b8b8d6e2df96d76eb
Igor Nikiforov <desola2@inbox.ru>
2022-02-15 15:12:06 +0300

    feat: add year selection

--- src/components/Games/HeaderSelections/HeaderSelections.js
+++ src/components/Games/HeaderSelections/HeaderSelections.js
@@ -2,26 +2,37 @@ import React from 'react';
 import cl from './HeaderSelections.module.scss';
 import Dropdown from 'components/UI/dropdown/GamesDropdown/Dropdown';
 import { useSelector, useDispatch } from 'react-redux';
-import { setCurrentYear } from 'redux/gamesReducer';
+import { setCurrentYear, setCurrentGameType } from 'redux/gamesReducer';
 
 const YEARS = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014];
 
 const HeaderSelections = () => {
   const currentYear = useSelector(state => state.games.currentYear);
+  const currentGameType = useSelector(state => state.games.currentGameType);
   const dispatch = useDispatch();
 
   const handleClick = option => {
     dispatch(setCurrentYear(option));
   };
 
+  const getClassName = name => (name === currentGameType ? cl.active : '');
+
+  const handleYearClick = type => () => {
+    dispatch(setCurrentGameType(type));
+  };
+
   return (
     <div className={cl.selections}>
       <div className={cl.years}>
-        <Dropdown title={currentYear} options={YEARS} currentOption={currentYear} handleClick={handleClick}/>
+        <Dropdown title={currentYear} options={YEARS} currentOption={currentYear} handleClick={handleClick} />
       </div>
       <ul className={cl.types}>
-        <li className={cl.active}>Baseball</li>
-        <li>Softball</li>
+        <li className={getClassName('Baseball')} onClick={handleYearClick('Baseball')}>
+          Baseball
+        </li>
+        <li className={getClassName('Softball')} onClick={handleYearClick('Softball')}>
+          Softball
+        </li>
       </ul>
     </div>
   );
