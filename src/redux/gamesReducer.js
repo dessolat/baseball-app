7724166d403fd904d88644d3d9c0787dd4f13b72
Igor Nikiforov <desola2@inbox.ru>
2022-02-16 20:51:13 +0300

    feat: add currentDate to gamesReducer

--- src/redux/gamesReducer.js
+++ src/redux/gamesReducer.js
@@ -1,6 +1,7 @@
 const defaultState = {
   currentLeague: { id: 1, name: 'All' },
 	currentYear: new Date().getFullYear(),
+	currentDate: new Date(),
 	currentGameType: 'Baseball',
 	currentStadium: 'All',
 	currentHome: 'All',
@@ -9,6 +10,7 @@ const defaultState = {
 
 const SET_CURRENT_LEAGUE = 'SET_CURRENT_LEAGUE';
 const SET_CURRENT_YEAR = 'SET_CURRENT_YEAR';
+const SET_CURRENT_DATE = 'SET_CURRENT_DATE';
 const SET_CURRENT_GAME_TYPE = 'SET_CURRENT_GAME_TYPE';
 const SET_CURRENT_STADIUM = 'SET_CURRENT_STADIUM';
 const SET_CURRENT_HOME = 'SET_CURRENT_HOME';
@@ -20,6 +22,8 @@ export const gamesReducer = (state = defaultState, action) => {
       return { ...state, currentLeague: action.payload };
     case SET_CURRENT_YEAR:
       return { ...state, currentYear: action.payload };
+    case SET_CURRENT_DATE:
+      return { ...state, currentDate: action.payload };
     case SET_CURRENT_GAME_TYPE:
       return { ...state, currentGameType: action.payload };
     case SET_CURRENT_STADIUM:
@@ -35,6 +39,7 @@ export const gamesReducer = (state = defaultState, action) => {
 
 export const setCurrentLeague = payload => ({ type: SET_CURRENT_LEAGUE, payload });
 export const setCurrentYear = payload => ({ type: SET_CURRENT_YEAR, payload });
+export const setCurrentDate = payload => ({ type: SET_CURRENT_DATE, payload });
 export const setCurrentGameType = payload => ({ type: SET_CURRENT_GAME_TYPE, payload });
 export const setCurrentStadium = payload => ({ type: SET_CURRENT_STADIUM, payload });
 export const setCurrentHome = payload => ({ type: SET_CURRENT_HOME, payload });
