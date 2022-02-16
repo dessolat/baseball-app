7aa99843760ab010c9e3e804d7343b02d59a50de
Igor Nikiforov <desola2@inbox.ru>
2022-02-16 20:34:15 +0300

    feat: add wrapper

--- src/components/Games/ContentTable/ContentTableHeader.js
+++ src/components/Games/ContentTable/ContentTableHeader.js
@@ -3,18 +3,21 @@ import cl from './ContentTable.module.scss';
 import ContentCalendar from '../ContentCalendar/ContentCalendar';
 import LeagueImage from 'images/league_image.png';
 import { useSelector } from 'react-redux';
-
+import ContentGridCalendar from '../ContentGridCalendar/ContentGridCalendar';
 
 const ContentTableHeader = () => {
-	const currentLeague = useSelector(state => state.games.currentLeague);
-  
-	return (
+  const currentLeague = useSelector(state => state.games.currentLeague);
+
+  return (
     <div className={cl.header}>
       <div className={cl.teamWrapper}>
-        <img src={LeagueImage} alt='league-img'/>
+        <img src={LeagueImage} alt='league-img' />
         <h2 className={cl.teamName}>{currentLeague.name}</h2>
       </div>
-      <ContentCalendar />
+      <div className={cl.calendarsWrapper}>
+				<ContentGridCalendar />
+        <ContentCalendar />
+      </div>
     </div>
   );
 };
