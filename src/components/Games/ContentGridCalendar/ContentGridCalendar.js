9823e5046a1256cace3349e8f005fc8e55587755
Igor Nikiforov <desola2@inbox.ru>
2022-02-17 09:57:11 +0300

    feat: add minDetail to Calendar

--- src/components/Games/ContentGridCalendar/ContentGridCalendar.js
+++ src/components/Games/ContentGridCalendar/ContentGridCalendar.js
@@ -1,21 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import CalendarImage from 'images/calendar.png';
 import Calendar from 'react-calendar';
 import './style.scss';
 
-const ContentGridCalendar = ({ onChange }) => {
+function listenForOutsideClicks(listening, setListening, menuRef, setIsVisible) {
+  return () => {
+    if (listening) return;
+    if (!menuRef.current) return;
+    setListening(true);
+    [`click`, `touchstart`].forEach(type => {
+      document.addEventListener(type, evt => {
+        if (menuRef.current?.contains(evt.target)) return;
+				console.log(menuRef.current);
+				console.log(evt.target);
+				console.log('this');
+        setIsVisible(false);
+      });
+    });
+  };
+}
+
+const ContentGridCalendar = ({ value, onChange }) => {
+  const [listening, setListening] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
+
+  const menuRef = useRef(null);
+
+  useEffect(listenForOutsideClicks(listening, setListening, menuRef, setIsVisible));
 
   const toggleCalendar = () => setIsVisible(!isVisible);
 
   const changeHandler = value => {
-    onChange(value)
+    onChange(value);
     toggleCalendar();
   };
   return (
-    <div className='content-grid-calendar-wrapper'>
+    <div ref={menuRef} className='content-grid-calendar-wrapper'>
       <img src={CalendarImage} alt='calendar' onClick={toggleCalendar} />
-      {isVisible && <Calendar onChange={changeHandler} className='gamesCalendar' locale='EN-en' />}
+      {isVisible && <Calendar minDetail='month' value={value} onChange={changeHandler} className='gamesCalendar' locale='EN-en' />}
     </div>
   );
 };
