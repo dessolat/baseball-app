f20b62b60e5f7772c35f63303ddff8feaaf5734a
Igor Nikiforov <desola2@inbox.ru>
2022-02-18 10:46:47 +0300

    feat: create HeaderLeaguesListItem comp

---
+++ src/components/Games/HeaderLeagues/HeaderLeaguesListItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import cl from './HeaderLeagues.module.scss';
+
+const HeaderLeaguesListItem = ({ league, handleClick, currentLeague }) => {
+  return (
+    <li
+      className={league.id === currentLeague.id ? cl.league + ' ' + cl.active : cl.league}
+      onClick={handleClick(league)}>
+      {league.name}
+    </li>
+  );
+};
+
+export default HeaderLeaguesListItem;
