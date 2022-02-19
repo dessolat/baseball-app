3a64f5fe55d73fd1faf344b4fe1dd7905740aba8
Igor Nikiforov <desola2@inbox.ru>
2022-02-19 21:09:58 +0300

    feat: create ContentCalendars comp placeholder

---
+++ src/components/Games/ContentTable/ContentCalendars.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './ContentTable.module.scss';
+
+const ContentCalendars = () => {
+	return (
+		<div>ContentCalendars</div>
+	)
+}
+
+export default ContentCalendars
