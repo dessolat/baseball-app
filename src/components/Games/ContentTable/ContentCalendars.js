bed99046720ca08a0744c48b5bf94d0066b814c0
Igor Nikiforov <desola2@inbox.ru>
2022-02-19 21:16:04 +0300

    refactor: move code

--- src/components/Games/ContentTable/ContentCalendars.js
+++ src/components/Games/ContentTable/ContentCalendars.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
 import cl from './ContentTable.module.scss';
+import ContentGridCalendar from '../ContentGridCalendar/ContentGridCalendar';
+import ContentCalendar from '../ContentCalendar/ContentCalendar';
+import { setCurrentDate } from 'redux/gamesReducer';
+import { useSelector, useDispatch } from 'react-redux';
 
 const ContentCalendars = () => {
-	return (
-		<div>ContentCalendars</div>
-	)
-}
+  const currentDate = useSelector(state => state.games.currentDate);
+  const dispatch = useDispatch();
 
-export default ContentCalendars
+  const calendarChangeHandler = value => dispatch(setCurrentDate(value));
+  return (
+    <div className={cl.calendarsWrapper}>
+      <ContentGridCalendar value={currentDate} onChange={calendarChangeHandler} />
+      <ContentCalendar onChange={calendarChangeHandler} />
+    </div>
+  );
+};
+
+export default ContentCalendars;
--- src/components/Games/ContentTable/ContentTableHeader.js
+++ src/components/Games/ContentTable/ContentTableHeader.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import cl from './ContentTable.module.scss';
-import ContentCalendar from '../ContentCalendar/ContentCalendar';
 import LeagueImage from 'images/league_image.png';
-import { useSelector, useDispatch } from 'react-redux';
-import ContentGridCalendar from '../ContentGridCalendar/ContentGridCalendar';
-import { setCurrentDate } from 'redux/gamesReducer';
+import { useSelector } from 'react-redux';
+import ContentCalendars from './ContentCalendars';
 
 const ContentTableHeader = () => {
   const currentLeague = useSelector(state => state.games.currentLeague);
-	const currentDate = useSelector(state => state.games.currentDate);
-	const dispatch = useDispatch()
 
-	const calendarChangeHandler = value => dispatch(setCurrentDate(value))
   return (
     <div className={cl.header}>
       <div className={cl.teamWrapper}>
         <img src={LeagueImage} alt='league-img' />
         <h2 className={cl.teamName}>{currentLeague.name}</h2>
       </div>
-      <div className={cl.calendarsWrapper}>
-				<ContentGridCalendar value={currentDate} onChange={calendarChangeHandler} />
-        <ContentCalendar onChange={calendarChangeHandler}/>
-      </div>
+      <ContentCalendars />
     </div>
   );
 };
