3d90cebf3cd9a9ed6028c5a7f7e00b3a2f6f05ce
Igor Nikiforov <desola2@inbox.ru>
2022-02-21 14:01:27 +0300

    feat: add leaguesImages to gamesReducer

--- src/redux/gamesReducer.js
+++ src/redux/gamesReducer.js
@@ -1,11 +1,12 @@
 const defaultState = {
   currentLeague: { id: -1, name: 'All' },
-	currentYear: new Date().getFullYear(),
-	currentDate: new Date(),
-	currentGameType: 'Baseball',
-	currentStadium: 'All',
-	currentHome: 'All',
-	currentGuests: 'All'
+  currentYear: new Date().getFullYear(),
+  currentDate: new Date(),
+  currentGameType: 'Baseball',
+  currentStadium: 'All',
+  currentHome: 'All',
+  currentGuests: 'All',
+  leaguesImages: {}
 };
 
 const SET_CURRENT_LEAGUE = 'SET_CURRENT_LEAGUE';
@@ -15,7 +16,9 @@ const SET_CURRENT_GAME_TYPE = 'SET_CURRENT_GAME_TYPE';
 const SET_CURRENT_STADIUM = 'SET_CURRENT_STADIUM';
 const SET_CURRENT_HOME = 'SET_CURRENT_HOME';
 const SET_CURRENT_GUESTS = 'SET_CURRENT_GUESTS';
- 
+const ADD_LEAGUE_IMAGE = 'ADD_LEAGUE_IMAGE';
+const RESET_TABLE_FILTERS = 'RESET_TABLE_FILTERS';
+
 export const gamesReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_CURRENT_LEAGUE:
@@ -32,6 +35,10 @@ export const gamesReducer = (state = defaultState, action) => {
       return { ...state, currentHome: action.payload };
     case SET_CURRENT_GUESTS:
       return { ...state, currentGuests: action.payload };
+    case ADD_LEAGUE_IMAGE:
+      return { ...state, leaguesImages: { ...state.leaguesImages, ...action.payload } };
+    case RESET_TABLE_FILTERS:
+      return { ...state, currentStadium: 'All', currentHome: 'All', currentGuests: 'All' };
     default:
       return state;
   }
@@ -44,3 +51,5 @@ export const setCurrentGameType = payload => ({ type: SET_CURRENT_GAME_TYPE, pay
 export const setCurrentStadium = payload => ({ type: SET_CURRENT_STADIUM, payload });
 export const setCurrentHome = payload => ({ type: SET_CURRENT_HOME, payload });
 export const setCurrentGuests = payload => ({ type: SET_CURRENT_GUESTS, payload });
+export const addLeagueImage = payload => ({ type: ADD_LEAGUE_IMAGE, payload });
+export const resetTableFilters = () => ({ type: RESET_TABLE_FILTERS });
