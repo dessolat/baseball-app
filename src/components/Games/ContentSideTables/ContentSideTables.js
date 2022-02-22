8500e9a07864c90bb235b1cfbfcb80b3f26eb1bd
Igor Nikiforov <desola2@inbox.ru>
2022-02-22 09:10:40 +0300

    feat: remove first_table_data

--- src/components/Games/ContentSideTables/ContentSideTables.js
+++ src/components/Games/ContentSideTables/ContentSideTables.js
@@ -3,17 +3,6 @@ import cl from './ContentSideTables.module.scss';
 import ArrowDown from 'components/UI/icons/ArrowDown';
 import { useSelector } from 'react-redux';
 
-const FIRST_TABLE_DATA = [
-  { id: 1, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
-  { id: 2, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
-  { id: 3, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
-  { id: 4, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
-  { id: 5, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
-  { id: 6, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
-  { id: 7, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' },
-  { id: 8, name: 'RusStar Unost Moscow', w: '00', l: '00', pct: '0000' }
-];
-
 const SECOND_TABLE_DATA = [
   { id: 1, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
   { id: 2, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
