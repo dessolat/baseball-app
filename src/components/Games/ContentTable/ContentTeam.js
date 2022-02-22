6d6a7295087a50100d122db1bd1cf41858a0f2de
Igor Nikiforov <desola2@inbox.ru>
2022-02-22 09:05:22 +0300

    feat: add league image

--- src/components/Games/ContentTable/ContentTeam.js
+++ src/components/Games/ContentTable/ContentTeam.js
@@ -5,10 +5,11 @@ import { useSelector } from 'react-redux';
 
 const ContentTeam = () => {
   const currentLeague = useSelector(state => state.games.currentLeague);
+	const leaguesImages = useSelector(state => state.games.leaguesImages);
 
   return (
     <div className={cl.teamWrapper}>
-      <img src={LeagueImage} alt='league-img' />
+      <img src={leaguesImages[currentLeague.id] || LeagueImage} alt='league-img' />
       <h2 className={cl.teamName}>{currentLeague.name}</h2>
     </div>
   );
