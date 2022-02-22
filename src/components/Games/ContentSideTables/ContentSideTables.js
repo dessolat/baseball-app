fb24067acdd1828cd3c1451a433112b87c7d5993
Igor Nikiforov <desola2@inbox.ru>
2022-02-22 09:41:10 +0300

    feat: create SwitchTable comp
    * move code

--- src/components/Games/ContentSideTables/ContentSideTables.js
+++ src/components/Games/ContentSideTables/ContentSideTables.js
@@ -1,73 +1,20 @@
 import React from 'react';
 import cl from './ContentSideTables.module.scss';
-import ArrowDown from 'components/UI/icons/ArrowDown';
 import { useSelector } from 'react-redux';
-
-const SECOND_TABLE_DATA = [
-  { id: 1, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
-  { id: 2, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
-  { id: 3, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
-  { id: 4, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
-  { id: 5, name: 'SURNAME Name', team: 'RusStar', avr: '0000' }
-];
+import PctTable from './PctTable';
+import SwitchTable from './SwitchTable';
 
 const ContentSideTables = () => {
   const currentLeague = useSelector(state => state.games.currentLeague);
 
-  const getShortName = name => (name.length > 23 ? name.slice(0, 22) + '…' : name);
-
   const isTables = currentLeague.id !== -1;
   return (
     <>
-      <div className={cl.side} style={!isTables ? {height: '70vh'} : null}>
+      <div className={cl.side} style={!isTables ? { height: '70vh' } : null}>
         {isTables && (
           <>
-            <table className={cl.pctTable}>
-              <thead>
-                <tr>
-                  <th>Team</th>
-                  <th>W</th>
-                  <th>L</th>
-                  <th>PCT</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentLeague.teams.map(team => (
-                  <tr key={team.id}>
-                    <td>{getShortName(team.name)}</td>
-                    <td>{team.wins}</td>
-                    <td>{team.loses}</td>
-                    <td>0000</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <p className={cl.drop}>
-              Batting <ArrowDown />
-            </p>
-            <table className={cl.switchTable}>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Team</th>
-                  <th>
-                    <div className={cl.avr}>
-                      AVR 
-                      <ArrowDown />
-                    </div>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {SECOND_TABLE_DATA.map(row => (
-                  <tr key={row.id}>
-                    <td>{row.name}</td>
-                    <td>{row.team}</td>
-                    <td>{row.avr}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <PctTable currentLeague={currentLeague} />
+            <SwitchTable />
           </>
         )}
       </div>
---
+++ src/components/Games/ContentSideTables/SwitchTable.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import cl from './ContentSideTables.module.scss';
+import ArrowDown from 'components/UI/icons/ArrowDown';
+
+const SECOND_TABLE_DATA = [
+  { id: 1, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
+  { id: 2, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
+  { id: 3, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
+  { id: 4, name: 'SURNAME Name', team: 'RusStar', avr: '0000' },
+  { id: 5, name: 'SURNAME Name', team: 'RusStar', avr: '0000' }
+];
+
+const SwitchTable = () => {
+  return (
+    <>
+      <p className={cl.drop}>
+        Batting <ArrowDown />
+      </p>
+      <table className={cl.switchTable}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Team</th>
+            <th>
+              <div className={cl.avr}>
+                AVR 
+                <ArrowDown />
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {SECOND_TABLE_DATA.map(row => (
+            <tr key={row.id}>
+              <td>{row.name}</td>
+              <td>{row.team}</td>
+              <td>{row.avr}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default SwitchTable;
