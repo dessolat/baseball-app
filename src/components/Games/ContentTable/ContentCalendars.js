535b38b8ad8f41882df0815d40c8c34ced5b298f
Igor Nikiforov <desola2@inbox.ru>
2022-02-24 14:12:01 +0300

    fix: calendar onChange timezone

--- src/components/Games/ContentTable/ContentCalendars.js
+++ src/components/Games/ContentTable/ContentCalendars.js
@@ -9,7 +9,12 @@ const ContentCalendars = () => {
   const currentDate = useSelector(state => state.games.currentDate);
   const dispatch = useDispatch();
 
-  const calendarChangeHandler = value => dispatch(setCurrentDate(value));
+  const calendarChangeHandler = value => {
+		let newDate = value
+		newDate.setHours(0, newDate.getTimezoneOffset()*-1, 0, 0)
+		
+    dispatch(setCurrentDate(newDate));
+  };
   return (
     <div className={cl.calendarsWrapper}>
       <ContentGridCalendar value={currentDate} onChange={calendarChangeHandler} />
