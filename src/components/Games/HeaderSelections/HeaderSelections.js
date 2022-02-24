42b8b6240d06549525c6c82e8dc44a91ccf345a2
Igor Nikiforov <desola2@inbox.ru>
2022-02-24 14:16:43 +0300

    feat: add currentDate change on year change

--- src/components/Games/HeaderSelections/HeaderSelections.js
+++ src/components/Games/HeaderSelections/HeaderSelections.js
@@ -2,7 +2,7 @@ import React from 'react';
 import cl from './HeaderSelections.module.scss';
 import Dropdown from 'components/UI/dropdown/GamesDropdown/Dropdown';
 import { useSelector, useDispatch } from 'react-redux';
-import { setCurrentYear, setCurrentGameType } from 'redux/gamesReducer';
+import { setCurrentYear, setCurrentGameType, setCurrentDate } from 'redux/gamesReducer';
 
 const YEARS = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014];
 
@@ -12,6 +12,10 @@ const HeaderSelections = () => {
   const dispatch = useDispatch();
 
   const handleClick = option => {
+    const tempDate = new Date(option, 0, 1);
+    tempDate.setHours(0, tempDate.getTimezoneOffset() * -1, 0, 0);
+
+    dispatch(setCurrentDate(tempDate));
     dispatch(setCurrentYear(option));
   };
 
