bfe1189977d69b1a75817487b45188313fb0b311
Igor Nikiforov <desola2@inbox.ru>
2022-02-26 17:26:59 +0300

    feat: create ContentPlayersTable placeholder comp

---
+++ src/components/TeamGames/ContentPlayersTable/ContentPlayersTable.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import cl from './ContentPlayersTable.module.scss';
+
+const ContentPlayersTable = () => {
+  return <div className={cl.wrapper}>Players table</div>;
+};
+
+export default ContentPlayersTable;
