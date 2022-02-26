91b55b6294aaa13867e92a50b4418bd801997884
Igor Nikiforov <desola2@inbox.ru>
2022-02-26 17:27:37 +0300

    feat: create ContentGamesTable placeholder comp

---
+++ src/components/TeamGames/ContentGamesTable/ContentGamesTable.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './ContentGamesTable.module.scss';
+
+const ContentGamesTable = () => {
+	return (
+		<div className={cl.wrapper}>ContentGamesTable</div>
+	)
+}
+
+export default ContentGamesTable
