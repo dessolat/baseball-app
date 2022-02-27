2dc8df5abccaf7d3e7f60742f0581b7e015ec3ef
Igor Nikiforov <desola2@inbox.ru>
2022-02-27 13:33:29 +0300

    feat: add main functional to TeamGames page

--- src/pages/TeamGames.js
+++ src/pages/TeamGames.js
@@ -1,11 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ErrorLoader from 'components/UI/loaders/ErrorLoader/ErrorLoader';
+import Header from 'components/TeamGames/Header/Header';
+import Content from 'components/TeamGames/Content/Content';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { setCurrentLeague, setGamesAndLeagues } from 'redux/gamesReducer';
+import axios from 'axios';
 
 const TeamGames = () => {
+	const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
+	const cancelTokenRef = useRef();
+	const firstMountRef = useRef(true);
+
 	const games = useSelector(state => state.games.games);
+	const currentYear = useSelector(state => state.games.currentYear);
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+    const fetchGamesData = async () => {
+      cancelTokenRef.current = axios.CancelToken.source();
+
+      try {
+        setIsLoading(true);
+        const response = await axios.get(`http://51.250.11.151:3030/main/year-${currentYear}`, {
+          cancelToken: cancelTokenRef.current.token,
+          timeout: 5000
+        });
+        console.log(response.data);
+        setError('');
+        dispatch(setGamesAndLeagues(response.data));
+
+        if (games === null) {
+          console.log('dispatched after fetching');
+          dispatch(setCurrentLeague({ id: -1, name: 'All' }));
+        }
+      } catch (err) {
+        if (err.message === null) return;
+        console.log(err.message);
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchGamesData();
+
+    return () => {
+      cancelTokenRef.current.cancel(null);
+    };
+  }, [currentYear]);
+
+	useEffect(() => {
+    if (firstMountRef.current === true) {
+      firstMountRef.current = false;
+      return;
+    }
+
+    dispatch(setCurrentLeague({ id: -1, name: 'All' }));
+  }, [currentYear]);
   return (
     <>
       {/* {isLoading ? (
@@ -17,8 +70,8 @@ const TeamGames = () => {
         <></>
       ) : (
         <>
-          {/* <Header /> */}
-          {/* <Content games={games} /> */}
+          <Header />
+          <Content games={games} />
         </>
       )}
     </>
