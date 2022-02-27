403f5596b7dbc154490851a332dbf23010852ffd
Igor Nikiforov <desola2@inbox.ru>
2022-02-27 13:29:01 +0300

    feat: create HeaderLeaguesList comp

---
+++ src/components/TeamGames/HeaderLeagues/HeaderLeaguesList.js
@@ -0,0 +1,27 @@
+import React, { forwardRef } from 'react';
+import cl from './HeaderLeagues.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentLeague } from 'redux/gamesReducer';
+import HeaderLeaguesListItem from './HeaderLeaguesListItem';
+
+const HeaderLeaguesList = ({ leagues }, ref) => {
+  const currentLeague = useSelector(state => state.games.currentLeague);
+  const dispatch = useDispatch();
+
+  const handleLeagueClick = league => () => dispatch(setCurrentLeague(league));
+
+  return (
+    <ul className={cl.leagues} ref={ref}>
+      {leagues.map(league => (
+        <HeaderLeaguesListItem
+          key={league.id}
+          league={league}
+          handleClick={handleLeagueClick}
+          currentLeague={currentLeague}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default forwardRef(HeaderLeaguesList);
