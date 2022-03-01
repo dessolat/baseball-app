0d65dd397995387d2659e056d28274e0d628adb5
Igor Nikiforov <desola2@inbox.ru>
2022-03-01 10:53:47 +0300

    feat: create Content comp

---
+++ src/components/Stats/Content/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import cl from './Content.module.scss';
+import { useParams } from 'react-router-dom';
+import ContentTeamTable from '../ContentTeamTable/ContentTeamTable';
+import ContentPlayerTable from '../ContentPlayerTable/ContentPlayerTable';
+
+const Content = ({ games }) => {
+  const { statsType } = useParams();
+
+  return (
+    <section>
+      <div className='container'>
+        <div className={cl.content}>
+          {statsType !== 'player' ? <ContentTeamTable /> : <ContentPlayerTable />}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Content;
