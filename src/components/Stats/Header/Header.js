278d75bbf66efb37e5c930c84d534fe02434c9a0
Igor Nikiforov <desola2@inbox.ru>
2022-03-01 10:50:56 +0300

    create Header comp

---
+++ src/components/Stats/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import cl from './Header.module.scss';
+import HeaderWrapper from 'components/HeaderWrapper/HeaderWrapper';
+import HeaderSelections from '../HeaderSelections/HeaderSelections';
+import HeaderLeagues from '../HeaderLeagues/HeaderLeagues';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <div className={cl.headerContent}>
+        <HeaderSelections />
+        <HeaderLeagues />
+      </div>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
