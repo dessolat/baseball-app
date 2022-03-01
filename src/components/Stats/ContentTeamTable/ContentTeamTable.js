c147606677a7a1b8e43750ae68a4cb2ea91bf62b
Igor Nikiforov <desola2@inbox.ru>
2022-03-01 10:54:31 +0300

    feat: create ContentTeamTable comp

---
+++ src/components/Stats/ContentTeamTable/ContentTeamTable.js
@@ -0,0 +1,263 @@
+import React from 'react';
+import cl from './ContentTeamTable.module.scss';
+import { Link } from 'react-router-dom';
+
+const TABLE_DATA = [
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  },
+  {
+    team_name: 'Name Team',
+    ab: 0,
+    h: 0,
+    '2b': 0,
+    '3b': 0,
+    hr: 0,
+    rbi: 0,
+    gdp: 0,
+    bb: 0,
+    hp: 0,
+    sh: 0,
+    sf: 0,
+    so: 0,
+    tb: 0,
+    avg: 0.0,
+    slg: 0.0,
+    obp: 0.0,
+    ops: 0.0
+  }
+];
+
+const ContentTeamTable = () => {
+  return (
+    <div className={cl.wrapper}>
+      <div className={cl.table}>
+        <div className={cl.tableHeader}>
+          <div>Team</div>
+          <div>AB</div>
+          <div>H</div>
+          <div>2B</div>
+          <div>3B</div>
+          <div>HR</div>
+          <div>RBI</div>
+          <div>GDP</div>
+          <div>BB</div>
+          <div>HP</div>
+          <div>SH</div>
+          <div>SF</div>
+          <div>SO</div>
+          <div>TB</div>
+          <div>AVG</div>
+          <div>SLG</div>
+          <div>OBP</div>
+          <div>OPS</div>
+        </div>
+        <ul className={cl.rows}>
+          {TABLE_DATA.map((row, index) => (
+            <li key={index} className={cl.tableRow}>
+              <div>
+                <Link to={`/games/team/${row.team_name}`}> {row.team_name}</Link>
+              </div>
+              <div>{row.ab}</div>
+              <div>{row.h}</div>
+              <div>{row['2b']}</div>
+              <div>{row['3b']}</div>
+              <div>{row.hr}</div>
+              <div>{row.rbi}</div>
+              <div>{row.gdp}</div>
+              <div>{row.bb}</div>
+              <div>{row.hp}</div>
+              <div>{row.sh}</div>
+              <div>{row.sf}</div>
+              <div>{row.so}</div>
+              <div>{row.tb}</div>
+              <div>{row.avg.toFixed(3)}</div>
+              <div>{row.slg.toFixed(3)}</div>
+              <div>{row.obp.toFixed(3)}</div>
+              <div>{row.ops.toFixed(3)}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ContentTeamTable;
