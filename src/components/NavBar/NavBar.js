e08fedb71b455a4548061e6791a938b582c1b461
Igor Nikiforov <desola2@inbox.ru>
2022-03-04 22:17:13 +0300

    feat: add useLocation hook

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import cl from './NavBar.module.scss'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import cl from './NavBar.module.scss';
+
+const ROUTES = {
+  games: '/games',
+  stats: '/stats'
+};
 
 const NavBar = () => {
-	return (
-		<nav className={cl.navbar}>
-			<Link to='/games'>Schedule</Link>
-			<Link to='/stats/player'>Stats</Link>
-		</nav>
-	)
-}
+  const location = useLocation();
 
-export default NavBar
+  const getClass = name => (location.pathname.slice(0, ROUTES[name].length) === ROUTES[name] ? cl.active : '');
+  return (
+    <nav className={cl.navbar}>
+      <Link to='/games' className={getClass('games')}>Schedule</Link>
+      <Link to='/stats/player' className={getClass('stats')}>Stats</Link>
+    </nav>
+  );
+};
+
+export default NavBar;
