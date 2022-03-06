4a0716958dbb492af217600b9d92c68e24826381
Igor Nikiforov <desola2@inbox.ru>
2022-03-06 22:30:22 +0300

    feat: add PlayerStats page basic functional

--- src/pages/PlayerStats.js
+++ src/pages/PlayerStats.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import ErrorLoader from 'components/UI/loaders/ErrorLoader/ErrorLoader';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { setCurrentLeague, setGamesAndLeagues } from 'redux/gamesReducer';
+import axios from 'axios';
+import Header from 'components/PlayerStats/Header/Header';
+// import Content from 'components/Stats/Content/Content';
 
 const PlayerStats = () => {
-	return (
-		<div>PlayerStats</div>
-	)
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
-export default PlayerStats
+  const cancelTokenRef = useRef();
+  const firstMountRef = useRef(true);
+
+  const games = useSelector(state => state.games.games);
+  const currentYear = useSelector(state => state.games.currentYear);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchGamesData = async () => {
+      cancelTokenRef.current = axios.CancelToken.source();
+
+      try {
+        setIsLoading(true);
+        const response = await axios.get(`http://51.250.11.151:3030/main/year-${currentYear}`, {
+          cancelToken: cancelTokenRef.current.token,
+          timeout: 5000
+        });
+        console.log(response.data);
+        setError('');
+        dispatch(setGamesAndLeagues(response.data));
+
+        if (games === null) {
+          console.log('dispatched after fetching');
+          dispatch(setCurrentLeague({ id: -1, name: 'All' }));
+        }
+      } catch (err) {
+        if (err.message === null) return;
+        console.log(err.message);
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchGamesData();
+
+    if (firstMountRef.current === true) {
+      firstMountRef.current = false;
+      return;
+    }
+
+    dispatch(setCurrentLeague({ id: -1, name: 'All' }));
+
+    return () => {
+      cancelTokenRef.current.cancel(null);
+    };
+  }, [currentYear]);
+
+  return (
+    <>
+      {/* {isLoading ? (
+        <Loader />
+      ) :  */}
+      {error && games === null ? (
+        <ErrorLoader error={error} />
+      ) : games === null ? (
+        <></>
+      ) : (
+        <>
+          <Header />
+          {/* <Content games={games} /> */}
+        </>
+      )}
+    </>
+  );
+};
+
+export default PlayerStats;
