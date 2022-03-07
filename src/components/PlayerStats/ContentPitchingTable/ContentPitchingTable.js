8c77f704be489cb20ebf7db86083aec0b1520f82
Igor Nikiforov <desola2@inbox.ru>
2022-03-07 18:17:44 +0300

    feat: create ContentPitchingTable comp placeholder

---
+++ src/components/PlayerStats/ContentPitchingTable/ContentPitchingTable.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import cl from './ContentPitchingTable.module.scss';
+
+const ContentPitchingTable = () => {
+  return <div>ContentPitchingTable</div>;
+};
+
+export default ContentPitchingTable;
