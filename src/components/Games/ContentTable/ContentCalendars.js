c62cb394b44e5ef2319e93349041de27eb50a845
Igor Nikiforov <desola2@inbox.ru>
2022-03-08 20:51:29 +0300

    feat: add calendarScroll state
    * add gridCalendarChangeHandler func
    
    * add calendarScroll to props

--- src/components/Games/ContentTable/ContentCalendars.js
+++ src/components/Games/ContentTable/ContentCalendars.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cl from './ContentTable.module.scss';
 import ContentGridCalendar from '../ContentGridCalendar/ContentGridCalendar';
 import ContentCalendar from '../ContentCalendar/ContentCalendar';
@@ -6,19 +6,35 @@ import { setCurrentDate } from 'redux/gamesReducer';
 import { useSelector, useDispatch } from 'react-redux';
 
 const ContentCalendars = () => {
+  const [calendarScroll, setCalendarScroll] = useState({ isScroll: false, amount: 0 });
+
   const currentDate = useSelector(state => state.games.currentDate);
   const dispatch = useDispatch();
 
   const calendarChangeHandler = value => {
-		let newDate = value
-		newDate.setHours(0, newDate.getTimezoneOffset()*-1, 0, 0)
-		
+    let newDate = value;
+    newDate.setHours(0, newDate.getTimezoneOffset() * -1, 0, 0);
+
     dispatch(setCurrentDate(newDate));
   };
+
+  const gridCalendarChangeHandler = value => {
+    let newDate = value;
+    newDate.setHours(0, newDate.getTimezoneOffset() * -1, 0, 0);
+
+    const daysDelta = (newDate - currentDate) / 1000 / 60 / 60 / 24;
+    
+    setCalendarScroll({ isScroll: true, amount: daysDelta < -3 ? -4 : daysDelta > 3 ? 4 : daysDelta });
+    setTimeout(() => {
+      dispatch(setCurrentDate(newDate));
+      setCalendarScroll({ isScroll: false, amount: 0 });
+    }, 250);
+  };
+
   return (
     <div className={cl.calendarsWrapper}>
-      <ContentGridCalendar value={currentDate} onChange={calendarChangeHandler} />
-      <ContentCalendar onChange={calendarChangeHandler} />
+      <ContentGridCalendar value={currentDate} onChange={gridCalendarChangeHandler} />
+      <ContentCalendar onChange={calendarChangeHandler} calendarScroll={calendarScroll} />
     </div>
   );
 };
