4ba982326bc10f111442ee57aa7ae5f8fc294fc7
Igor Nikiforov <desola2@inbox.ru>
2022-03-11 22:42:51 +0300

    refactor: move code

--- src/components/Game/ContentBox/ContentBoxTable.js
+++ src/components/Game/ContentBox/ContentBoxTable.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import cl from './ContentBoxTable.module.scss';
+import ContentBoxTableBody from './ContentBoxTableBody';
 import ContentBoxTableHeader from './ContentBoxTableHeader';
 
 const TABLES_INFO = {
@@ -75,48 +76,15 @@ const ContentBoxTable = ({ tableData, tableClass, tableName, toFixList = [] }) =
           .reduce((sum, cur) => sum + Number(cur.content['running'][field]), 0)
       : tableData.reduce((sum, cur) => sum + Number(cur.content[table][field]), 0);
 
-  let rowDelta = 0;
   return (
     <table className={cl.table + ' ' + tableClass}>
       <ContentBoxTableHeader TABLES_INFO={TABLES_INFO} tableName={tableName} />
-      <tbody>
-        {tableData
-          .filter(player =>
-            tableName === 'pitching' ? player.is_pitcher : tableName === 'catching' ? player.is_catcher : true
-          )
-          .map((player, i) => {
-            if (player.is_substituted && tableName === 'batting') rowDelta++;
-
-            return (
-              <tr key={i}>
-                <td>
-                  {(player.is_substituted && tableName === 'batting') || tableName !== 'batting'
-                    ? ' '
-                    : i + 1 - rowDelta}
-                </td>
-                <td
-                  style={player.is_substituted && tableName === 'batting' ? { paddingLeft: '2.5rem' } : null}>
-                  {player.content.player_name}
-                </td>
-                {TABLES_INFO[tableName].headers.map((title, i) => (
-                  <td key={i} style={toFixList.includes(title) ? { width: '3rem' } : null}>
-                    {title === 'POS'
-                      ? player.content.position.join('/')
-                      : ['SB', 'CS', 'SB_pr', 'LOB'].includes(title)
-                      ? player.content.running[title]
-                      : ['CH', 'PO', 'A', 'E', 'DP', 'FLD'].includes(title)
-                      ? player.content.fielding[title]
-                      : title === 'PB'
-                      ? '--'
-                      : toFixList.includes(title)
-                      ? Number(player.content[tableName][title]).toFixed(3)
-                      : player.content[tableName][title]}
-                  </td>
-                ))}
-              </tr>
-            );
-          })}
-      </tbody>
+      <ContentBoxTableBody
+        TABLES_INFO={TABLES_INFO}
+        tableName={tableName}
+        tableData={tableData}
+        toFixList={toFixList}
+      />
       <tfoot>
         <tr
           style={
--- src/components/Game/ContentBox/ContentBoxTableBody.js
+++ src/components/Game/ContentBox/ContentBoxTableBody.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
 
-const ContentBoxTableBody = () => {
-	return (
-		<div>ContentBoxTableBody</div>
-	)
-}
+const ContentBoxTableBody = ({ TABLES_INFO, tableName, tableData, toFixList }) => {
+  let rowDelta = 0;
 
-export default ContentBoxTableBody
+  return (
+    <tbody>
+      {tableData
+        .filter(player =>
+          tableName === 'pitching' ? player.is_pitcher : tableName === 'catching' ? player.is_catcher : true
+        )
+        .map((player, i) => {
+          if (player.is_substituted && tableName === 'batting') rowDelta++;
+
+          return (
+            <tr key={i}>
+              <td>
+                {(player.is_substituted && tableName === 'batting') || tableName !== 'batting'
+                  ? ' '
+                  : i + 1 - rowDelta}
+              </td>
+              <td style={player.is_substituted && tableName === 'batting' ? { paddingLeft: '2.5rem' } : null}>
+                {player.content.player_name}
+              </td>
+              {TABLES_INFO[tableName].headers.map((title, i) => (
+                <td key={i} style={toFixList.includes(title) ? { width: '3rem' } : null}>
+                  {title === 'POS'
+                    ? player.content.position.join('/')
+                    : ['SB', 'CS', 'SB_pr', 'LOB'].includes(title)
+                    ? player.content.running[title]
+                    : ['CH', 'PO', 'A', 'E', 'DP', 'FLD'].includes(title)
+                    ? player.content.fielding[title]
+                    : title === 'PB'
+                    ? '--'
+                    : toFixList.includes(title)
+                    ? Number(player.content[tableName][title]).toFixed(3)
+                    : player.content[tableName][title]}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+    </tbody>
+  );
+};
+
+export default ContentBoxTableBody;
