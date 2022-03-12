1a54934037257d791fab265dff1e4b4c338fc5ab
Igor Nikiforov <desola2@inbox.ru>
2022-03-12 17:51:36 +0300

    feat: add teamGamesReducer to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,7 +2,13 @@ import { combineReducers } from 'redux';
 import { gameReducer } from './gameReducer';
 import { gamesReducer } from './gamesReducer';
 import { sharedReducer } from './sharedReducer';
+import { teamGamesReducer } from './teamGamesReducer';
 
-const rootReducer = combineReducers({ game: gameReducer, games: gamesReducer, shared: sharedReducer });
+const rootReducer = combineReducers({
+  game: gameReducer,
+  games: gamesReducer,
+  teamGames: teamGamesReducer,
+  shared: sharedReducer
+});
 
-export default rootReducer
+export default rootReducer;
