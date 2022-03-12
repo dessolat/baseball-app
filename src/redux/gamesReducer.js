c2b9d138b000b4bd520da17a129574a5f2525390
Igor Nikiforov <desola2@inbox.ru>
2022-03-12 17:03:29 +0300

    refactor: remove unused code

--- src/redux/gamesReducer.js
+++ src/redux/gamesReducer.js
@@ -1,26 +1,18 @@
 const defaultState = {
 	games: null,
 	leagues: null,
-  currentLeague: { id: -1, name: 'All' },
-  currentYear: new Date().getFullYear(),
-  currentDate: new Date(),
   currentGameType: 'Baseball',
   currentStadium: 'All',
   currentHome: 'All',
   currentGuests: 'All',
-	currentLeaguesScroll: 0,
   leaguesImages: {}
 };
 
 const SET_GAMES_AND_LEAGUES = 'SET_GAMES_AND_LEAGUES';
-const SET_CURRENT_LEAGUE = 'SET_CURRENT_LEAGUE';
-const SET_CURRENT_YEAR = 'SET_CURRENT_YEAR';
-const SET_CURRENT_DATE = 'SET_CURRENT_DATE';
 const SET_CURRENT_GAME_TYPE = 'SET_CURRENT_GAME_TYPE';
 const SET_CURRENT_STADIUM = 'SET_CURRENT_STADIUM';
 const SET_CURRENT_HOME = 'SET_CURRENT_HOME';
 const SET_CURRENT_GUESTS = 'SET_CURRENT_GUESTS';
-const SET_CURRENT_LEAGUES_SCROLL = 'SET_CURRENT_LEAGUES_SCROLL';
 const ADD_LEAGUE_IMAGE = 'ADD_LEAGUE_IMAGE';
 const RESET_TABLE_FILTERS = 'RESET_TABLE_FILTERS';
 
@@ -28,12 +20,6 @@ export const gamesReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_GAMES_AND_LEAGUES:
       return { ...state, games: action.payload.games, leagues: action.payload.leagues };
-    case SET_CURRENT_LEAGUE:
-      return { ...state, currentLeague: action.payload };
-    case SET_CURRENT_YEAR:
-      return { ...state, currentYear: action.payload };
-    case SET_CURRENT_DATE:
-      return { ...state, currentDate: action.payload };
     case SET_CURRENT_GAME_TYPE:
       return { ...state, currentGameType: action.payload };
     case SET_CURRENT_STADIUM:
@@ -42,8 +28,6 @@ export const gamesReducer = (state = defaultState, action) => {
       return { ...state, currentHome: action.payload };
     case SET_CURRENT_GUESTS:
       return { ...state, currentGuests: action.payload };
-    case SET_CURRENT_LEAGUES_SCROLL:
-      return { ...state, currentLeaguesScroll: action.payload };
     case ADD_LEAGUE_IMAGE:
       return { ...state, leaguesImages: { ...state.leaguesImages, ...action.payload } };
     case RESET_TABLE_FILTERS:
@@ -54,13 +38,9 @@ export const gamesReducer = (state = defaultState, action) => {
 };
 
 export const setGamesAndLeagues = payload => ({ type: SET_GAMES_AND_LEAGUES, payload });
-export const setCurrentLeague = payload => ({ type: SET_CURRENT_LEAGUE, payload });
-export const setCurrentYear = payload => ({ type: SET_CURRENT_YEAR, payload });
-export const setCurrentDate = payload => ({ type: SET_CURRENT_DATE, payload });
 export const setCurrentGameType = payload => ({ type: SET_CURRENT_GAME_TYPE, payload });
 export const setCurrentStadium = payload => ({ type: SET_CURRENT_STADIUM, payload });
 export const setCurrentHome = payload => ({ type: SET_CURRENT_HOME, payload });
 export const setCurrentGuests = payload => ({ type: SET_CURRENT_GUESTS, payload });
-export const setCurrentLeaguesScroll = payload => ({ type: SET_CURRENT_LEAGUES_SCROLL, payload });
 export const addLeagueImage = payload => ({ type: ADD_LEAGUE_IMAGE, payload });
 export const resetTableFilters = () => ({ type: RESET_TABLE_FILTERS });
