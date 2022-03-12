28ec0c71f85204ee2c212e23f85299590533d13a
Igor Nikiforov <desola2@inbox.ru>
2022-03-12 17:03:03 +0300

    feat: add sharedReducer to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import { gameReducer } from './gameReducer';
 import { gamesReducer } from './gamesReducer';
+import { sharedReducer } from './sharedReducer';
 
-const rootReducer = combineReducers({ game: gameReducer, games: gamesReducer });
+const rootReducer = combineReducers({ game: gameReducer, games: gamesReducer, shared: sharedReducer });
 
 export default rootReducer
