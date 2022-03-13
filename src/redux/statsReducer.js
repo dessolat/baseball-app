45c040fbe18f9e63ce226a202f2437e502bdb798
Igor Nikiforov <desola2@inbox.ru>
2022-03-13 13:23:12 +0300

    feat: create statsReducer

---
+++ src/redux/statsReducer.js
@@ -0,0 +1,22 @@
+const defaultState = {
+  statsData: [],
+	tableMode: 'batting'
+};
+
+const SET_STATS_DATA = 'SET_STATS_DATA';
+const SET_TABLE_MODE = 'SET_TABLE_MODE';
+
+export const statsReducer = (state = defaultState, action) => {
+  switch (action.type) {
+
+    case SET_STATS_DATA:
+      return { ...state, statsData: action.payload };
+    case SET_TABLE_MODE:
+      return { ...state, tableMode: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const setStatsData = payload => ({ type: SET_STATS_DATA, payload });
+export const setTableMode = payload => ({ type: SET_TABLE_MODE, payload });
