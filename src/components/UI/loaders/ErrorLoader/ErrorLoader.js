8660bdd1857e4a901fdfa46c73a4c579eb188baa
Igor Nikiforov <desola2@inbox.ru>
2022-03-13 13:23:52 +0300

    feat: add styles props to loaders

--- src/components/UI/loaders/ErrorLoader/ErrorLoader.js
+++ src/components/UI/loaders/ErrorLoader/ErrorLoader.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import cl from './ErrorLoader.module.scss';
 
-const ErrorLoader = ({ error }) => {
-	console.log(error)
-  return <p className={cl.error}>{error}</p>;
+const ErrorLoader = ({ error, styles = null }) => {
+  console.log(error);
+  return (
+    <p className={cl.error} style={styles}>
+      {error}
+    </p>
+  );
 };
 
 export default ErrorLoader;
--- src/components/UI/loaders/Loader/Loader.js
+++ src/components/UI/loaders/Loader/Loader.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import cl from './Loader.module.scss';
 
-const Loader = () => {
+const Loader = ({ styles = null }) => {
   return (
-    <div className={cl.loader}>
+    <div className={cl.loader} style={styles}>
       <div></div>
       <div></div>
       <div></div>
