6289755c73544af4f0aa7d1417580fcbfe8c685a
Igor Nikiforov <desola2@inbox.ru>
2022-03-13 13:25:53 +0300

    feat: add loaders
    * remove old players_data
    
    * move fetching from TeamGames comp
    
    * make main functional

--- src/components/TeamGames/ContentPlayersTable/ContentPlayersTable.js
+++ src/components/TeamGames/ContentPlayersTable/ContentPlayersTable.js
@@ -1,60 +1,112 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 import cl from './ContentPlayersTable.module.scss';
-
-const PLAYERS_DATA = [
-  { surname: 'SURNAME', name: 'name', pos: 'P', bt: 'L/L', ht: 0, wt: 0, yob: 1996 },
-  { surname: 'SURNAME', name: 'name', pos: 'P', bt: 'R/R', ht: 0, wt: 0, yob: 1993 },
-  { surname: 'SURNAME', name: 'name', pos: 'OF', bt: 'R/R', ht: 0, wt: 0, yob: 1991 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1997 },
-  { surname: 'SURNAME', name: 'name', pos: 'P', bt: 'R/R', ht: 0, wt: 0, yob: 1994 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1987 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 },
-  { surname: 'SURNAME', name: 'name', pos: 'IF', bt: 'R/R', ht: 0, wt: 0, yob: 1990 }
-];
+import axios from 'axios';
+import { setTeamData } from 'redux/teamGamesReducer';
+import ErrorLoader from 'components/UI/loaders/ErrorLoader/ErrorLoader';
+import Loader from 'components/UI/loaders/Loader/Loader';
 
 const ContentPlayersTable = () => {
+  const teamData = useSelector(state => state.teamGames.teamData);
+  const currentYear = useSelector(state => state.shared.currentYear);
+  const currentLeague = useSelector(state => state.shared.currentLeague);
+  const dispatch = useDispatch();
+
+  const [isTeamLoading, setIsTeamLoading] = useState(false);
+  const [error, setError] = useState('');
+  const { teamName } = useParams();
+  const cancelTeamTokenRef = useRef();
+
+  useEffect(() => {
+    const fetchGamesData = async () => {
+      cancelTeamTokenRef.current = axios.CancelToken.source();
+
+      try {
+        setIsTeamLoading(true);
+        const response = await axios.get(`http://51.250.11.151:3030/team/${teamName}`, {
+          cancelToken: cancelTeamTokenRef.current.token,
+          timeout: 5000
+        });
+        console.log(response.data);
+        setError('');
+        dispatch(setTeamData(response.data));
+      } catch (err) {
+        if (err.message === null) return;
+        console.log(err.message);
+        setError(err.message);
+      } finally {
+        setIsTeamLoading(false);
+      }
+    };
+    fetchGamesData();
+
+    return () => {
+      cancelTeamTokenRef.current.cancel(null);
+    };
+    // eslint-disable-next-line
+  }, [teamName]);
+
+  const PLAYERS_DATA =
+    currentLeague.id !== -1
+      ? teamData.find(item => item.league.id === currentLeague.id)?.roster
+      : teamData.reduce((sum, cur) => {
+          if (cur.league.year === currentYear) {
+            cur.roster.forEach(
+              curPlayer =>
+                !sum.find(
+                  sumPlayer => sumPlayer.surname === curPlayer.surname && sumPlayer.name === curPlayer.name
+                ) && sum.push(curPlayer)
+            );
+          }
+          return sum;
+        }, []);
+
   return (
     <div className={cl.wrapper}>
-      <div className={cl.table}>
-        <div className={cl.tableHeader}>
-          <div>#</div>
-          <div>Player</div>
-          <div>POS</div>
-          <div>B/T</div>
-          <div>HT</div>
-          <div>WT</div>
-          <div>YOB</div>
+      {error ? (
+        <ErrorLoader error={error} styles={{ marginTop: 0 }} />
+      ) : isTeamLoading ? (
+        <div style={{ height: '100px', display: 'grid', placeItems: 'center' }}>
+          <Loader styles={{margin: 0}} />
         </div>
-        <ul className={cl.rows}>
-          {PLAYERS_DATA.map((player, index) => (
-            <li key={index} className={cl.tableRow}>
-              <div>{index + 1}</div>
-              <div className={cl.underlineHover}>
-                <Link to={`/stats/player/${player.surname} ${player.name}`}>
-                  {player.surname + ' ' + player.name}
-                </Link>
-              </div>
-              <div>{player.pos}</div>
-              <div>{player.bt}</div>
-              <div>{player.ht}</div>
-              <div>{player.wt}</div>
-              <div>{player.yob}</div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={cl.linkWrapper}>
-        <Link to='/stats/player'>Go to Player Stat</Link>
-      </div>
+      ) : (
+        <>
+          <div className={cl.table}>
+            <div className={cl.tableHeader}>
+              <div>#</div>
+              <div>Player</div>
+              <div>POS</div>
+              <div>B/T</div>
+              <div>HT</div>
+              <div>WT</div>
+              <div>YOB</div>
+            </div>
+            <ul className={cl.rows}>
+              {PLAYERS_DATA.map((player, index) => (
+                <li key={index} className={cl.tableRow}>
+                  <div>{index + 1}</div>
+                  <div className={cl.underlineHover}>
+                    <Link to={`/stats/player/${player.name}/${player.surname}`}>
+                      {player.name + ' ' + player.surname}
+                    </Link>
+                  </div>
+                  <div>{player.pos ?? '—'}</div>
+                  <div>
+                    {player.bat_hand}/{player.throw_hand}
+                  </div>
+                  <div>{player.height}</div>
+                  <div>{player.weight}</div>
+                  <div>{player.yob}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={cl.linkWrapper}>
+            <Link to='/stats/player'>Go to Player Stat</Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/pages/TeamGames.js
+++ src/pages/TeamGames.js
@@ -7,52 +7,20 @@ import { useDispatch } from 'react-redux';
 import { setGamesAndLeagues } from 'redux/gamesReducer';
 import { setCurrentLeague } from 'redux/sharedReducer';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { setTeamData } from 'redux/teamGamesReducer';
 
 const TeamGames = () => {
   // eslint-disable-next-line
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const { teamName } = useParams();
 
   const cancelGamesTokenRef = useRef();
-  const cancelTeamTokenRef = useRef();
   const firstMountRef = useRef(true);
 
   const games = useSelector(state => state.games.games);
   const currentYear = useSelector(state => state.shared.currentYear);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchGamesData = async () => {
-      cancelTeamTokenRef.current = axios.CancelToken.source();
-
-      try {
-        setIsLoading(true);
-        const response = await axios.get(`http://51.250.11.151:3030/team/${teamName}`, {
-          cancelToken: cancelTeamTokenRef.current.token,
-          timeout: 5000
-        });
-        console.log(response.data);
-        setError('');
-        dispatch(setTeamData(response.data));
-      } catch (err) {
-        if (err.message === null) return;
-        console.log(err.message);
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchGamesData();
-
-    return () => {
-      cancelTeamTokenRef.current.cancel(null);
-    };
-    // eslint-disable-next-line
-  }, []);
 
   useEffect(() => {
     const fetchGamesData = async () => {
