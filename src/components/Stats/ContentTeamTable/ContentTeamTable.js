908abd3dbc04ad33a4ef4b38899d740e6d3b37b1
Igor Nikiforov <desola2@inbox.ru>
2022-03-14 21:23:03 +0300

    feat: add main functional

--- src/components/Stats/ContentTeamTable/ContentTeamTable.js
+++ src/components/Stats/ContentTeamTable/ContentTeamTable.js
@@ -1,259 +1,188 @@
 import React from 'react';
 import cl from './ContentTeamTable.module.scss';
 import { Link } from 'react-router-dom';
-
-const TABLE_DATA = [
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  },
-  {
-    team_name: 'Name Team',
-    ab: 0,
-    h: 0,
-    '2b': 0,
-    '3b': 0,
-    hr: 0,
-    rbi: 0,
-    gdp: 0,
-    bb: 0,
-    hp: 0,
-    sh: 0,
-    sf: 0,
-    so: 0,
-    tb: 0,
-    avg: 0.0,
-    slg: 0.0,
-    obp: 0.0,
-    ops: 0.0
-  }
-];
+import { useSelector } from 'react-redux';
 
 const ContentTeamTable = () => {
+  const tableMode = useSelector(state => state.stats.tableMode);
+  const statsData = useSelector(state => state.stats.statsData);
+  const currentLeague = useSelector(state => state.shared.currentLeague);
+
+  const tableHeaders =
+    tableMode === 'Batting' ? (
+      <>
+        <div>AB</div>
+        <div>H</div>
+        <div>1B</div>
+        <div>2B</div>
+        <div>3B</div>
+        <div>HR</div>
+        <div>RBI</div>
+        <div>GDP</div>
+        <div>BB</div>
+        <div>HP</div>
+        <div>SH</div>
+        <div>SF</div>
+        <div>SO</div>
+        <div>TB</div>
+        <div className={cl.wider}>AVG</div>
+        <div className={cl.wider}>SLG</div>
+        <div className={cl.wider}>OBP</div>
+        <div className={cl.wider}>OPS</div>
+      </>
+    ) : tableMode === 'Fielding' ? (
+      <>
+        <div>A</div>
+        <div>CH</div>
+        <div>DP</div>
+        <div>E</div>
+        <div className={cl.wider}>FLD</div>
+        <div>PO</div>
+      </>
+    ) : tableMode === 'Pitching' ? (
+      <>
+        <div>2B</div>
+        <div>3B</div>
+        <div>BB</div>
+        <div>BK</div>
+        <div>ER</div>
+        <div className={cl.wider}>ERA</div>
+        <div>H</div>
+        <div>HP</div>
+        <div>HR</div>
+        <div>IBB</div>
+        <div>IP</div>
+        <div>NB</div>
+        <div>NP</div>
+        <div>NS</div>
+        <div>PA</div>
+        <div>R</div>
+        <div>SF</div>
+        <div>SH</div>
+        <div>SO</div>
+        <div>WP</div>
+      </>
+    ) : (
+      <>
+        <div>CS</div>
+        <div>LOB</div>
+        <div>R</div>
+        <div>SB</div>
+        <div className={cl.wider}>SB_pr</div>
+      </>
+    );
+
+  const tableRows = row =>
+    tableMode === 'Batting' ? (
+      <>
+        <div>{row.AB}</div>
+        <div>{row.H}</div>
+        <div>{row['1B']}</div>
+        <div>{row['2B']}</div>
+        <div>{row['3B']}</div>
+        <div>{row.HR}</div>
+        <div>{row.RBI}</div>
+        <div>{row.GDP}</div>
+        <div>{row.BB}</div>
+        <div>{row.HP}</div>
+        <div>{row.SH}</div>
+        <div>{row.SF}</div>
+        <div>{row.SO}</div>
+        <div>{row.TB}</div>
+        <div className={cl.wider}>{Number(row.AVG).toFixed(3)}</div>
+        <div className={cl.wider}>{Number(row.SLG).toFixed(3)}</div>
+        <div className={cl.wider}>{Number(row.OBP).toFixed(3)}</div>
+        <div className={cl.wider}>{Number(row.OPS).toFixed(3)}</div>
+      </>
+    ) : tableMode === 'Fielding' ? (
+      <>
+        <div>{row.A}</div>
+        <div>{row.CH}</div>
+        <div>{row.DP}</div>
+        <div>{row.E}</div>
+        <div className={cl.wider}>{Number(row.FLD).toFixed(3)}</div>
+        <div>{row.PO}</div>
+      </>
+    ) : tableMode === 'Pitching' ? (
+      <>
+        <div>{row['2B']}</div>
+        <div>{row['3B']}</div>
+        <div>{row.BB}</div>
+        <div>{row.BK}</div>
+        <div>{row.ER}</div>
+        <div className={cl.wider}>{Number(row.ERA).toFixed(2)}</div>
+        <div>{row.H}</div>
+        <div>{row.HP}</div>
+        <div>{row.HR}</div>
+        <div>{row.IBB}</div>
+        <div>{row.IP}</div>
+        <div>{row.NB}</div>
+        <div>{row.NP}</div>
+        <div>{row.NS}</div>
+        <div>{row.PA}</div>
+        <div>{row.R}</div>
+        <div>{row.SF}</div>
+        <div>{row.SH}</div>
+        <div>{row.SO}</div>
+        <div>{row.WP}</div>
+      </>
+    ) : (
+      <>
+        <div>{row.CS}</div>
+        <div>{row.LOB}</div>
+        <div>{row.R}</div>
+        <div>{row.SB}</div>
+        <div className={cl.wider}>{Number(row.SB_pr).toFixed(3)}</div>
+      </>
+    );
+  console.log(statsData);
+
+  const getObjectsSum = (obj1, obj2) => {
+    const result = {};
+
+    for (let key in obj1) {
+      result[key] = key === 'name' ? obj1[key] : +obj1[key] + +obj2[key];
+    }
+
+    return result;
+  };
+
+  const filteredStatsData =
+    currentLeague.id !== -1
+      ? statsData.find(item => item.title === currentLeague.name).teams[tableMode.toLowerCase()]
+      : statsData.reduce((sum, league) => {
+          league.teams[tableMode.toLowerCase()].forEach(team => {
+            const teamIndex = sum.findIndex(sumTeam => sumTeam.name === team.name);
+						
+						if (teamIndex !== -1) {
+							sum[teamIndex] = getObjectsSum(sum[teamIndex], team)
+						} else {
+							sum.push(team)
+						}
+
+          });
+
+          return sum;
+        }, []) || [];
+console.log(filteredStatsData);
   return (
     <div className={cl.wrapper}>
       <div>
         <div className={cl.tableHeader}>
           <div>Team</div>
-          <div>AB</div>
-          <div>H</div>
-          <div>2B</div>
-          <div>3B</div>
-          <div>HR</div>
-          <div>RBI</div>
-          <div>GDP</div>
-          <div>BB</div>
-          <div>HP</div>
-          <div>SH</div>
-          <div>SF</div>
-          <div>SO</div>
-          <div>TB</div>
-          <div className={cl.wider}>AVG</div>
-          <div className={cl.wider}>SLG</div>
-          <div className={cl.wider}>OBP</div>
-          <div className={cl.wider}>OPS</div>
+          {tableHeaders}
         </div>
         <ul className={cl.rows}>
-          {TABLE_DATA.map((row, index) => (
-            <li key={index} className={cl.tableRow}>
-              <div>
-                <Link to={`/games/team/${row.team_name}`}> {row.team_name}</Link>
-              </div>
-              <div>{row.ab}</div>
-              <div>{row.h}</div>
-              <div>{row['2b']}</div>
-              <div>{row['3b']}</div>
-              <div>{row.hr}</div>
-              <div>{row.rbi}</div>
-              <div>{row.gdp}</div>
-              <div>{row.bb}</div>
-              <div>{row.hp}</div>
-              <div>{row.sh}</div>
-              <div>{row.sf}</div>
-              <div>{row.so}</div>
-              <div>{row.tb}</div>
-              <div className={cl.wider}>{row.avg.toFixed(3)}</div>
-              <div className={cl.wider}>{row.slg.toFixed(3)}</div>
-              <div className={cl.wider}>{row.obp.toFixed(3)}</div>
-              <div className={cl.wider}>{row.ops.toFixed(3)}</div>
-            </li>
-          ))}
+          {filteredStatsData.map((row, index) => {
+            return (
+              <li key={index} className={cl.tableRow}>
+                <div>
+                  <Link to={`/games/team/${row.name}`}> {row.name}</Link>
+                </div>
+                {tableRows(row)}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
