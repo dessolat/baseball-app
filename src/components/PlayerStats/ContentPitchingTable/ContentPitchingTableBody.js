f0b3638a1cc77483cc8c9069ed2f72f6440fe04f
Igor Nikiforov <desola2@inbox.ru>
2022-03-15 14:58:27 +0300

    feat: add basic functional

--- src/components/PlayerStats/ContentPitchingTable/ContentPitchingTableBody.js
+++ src/components/PlayerStats/ContentPitchingTable/ContentPitchingTableBody.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import cl from './ContentPitchingTable.module.scss';
 
-const ContentPitchingTableBody = ({ playerYears, TABLE_DATA, currentLeague }) => {
+const ContentPitchingTableBody = ({ playerYears, leagues, currentLeague }) => {
   return (
     <ul className={cl.rows}>
-      {TABLE_DATA.pitching.map((row, index) => (
+      {leagues.map((row, index) => (
         <li key={index} className={cl.tableRow}>
           {playerYears === 'All years' && <div className={cl.year}>{row.year}</div>}
-          {currentLeague.id === -1 && <div className={cl.league}>{row.league}</div>}
-          {currentLeague.id !== -1 && <div className={cl.game}>{row.game}</div>}
-          <div>{row.pa}</div>
-          <div>{row.r}</div>
-          <div>{row.er}</div>
-          <div>{row.h}</div>
-          <div>{row['2b']}</div>
-          <div>{row['3b']}</div>
-          <div>{row.hr}</div>
-          <div>{row.bb}</div>
-          <div>{row.ibb}</div>
-          <div>{row.hp}</div>
-          <div>{row.sh}</div>
-          <div>{row.sf}</div>
-          <div>{row.so}</div>
-          <div>{row.wp}</div>
-          <div className={cl.wider}>{row.era.toFixed(3)}</div>
-          <div>{row.np}</div>
-          <div>{row.ns}</div>
-          <div>{row.nb}</div>
+          {currentLeague.id === -1 && <div className={cl.league}>{row.title}</div>}
+          {currentLeague.id !== -1 && <div className={cl.game}>01 sep, Name team - Name Team</div>}
+          <div>{row.teams[0].pitching.PA}</div>
+          <div>{row.teams[0].pitching.R}</div>
+          <div>{row.teams[0].pitching.ER}</div>
+          <div>{row.teams[0].pitching.H}</div>
+          <div>{row.teams[0].pitching['2B']}</div>
+          <div>{row.teams[0].pitching['3B']}</div>
+          <div>{row.teams[0].pitching.HR}</div>
+          <div>{row.teams[0].pitching.BB}</div>
+          <div>{row.teams[0].pitching.IBB}</div>
+          <div>{row.teams[0].pitching.HP}</div>
+          <div>{row.teams[0].pitching.SH}</div>
+          <div>{row.teams[0].pitching.SF}</div>
+          <div>{row.teams[0].pitching.SO}</div>
+          <div>{row.teams[0].pitching.WP}</div>
+          <div className={cl.wider}>{Number(row.teams[0].pitching.ERA).toFixed(3)}</div>
+          <div>{row.teams[0].pitching.NP}</div>
+          <div>{row.teams[0].pitching.NS}</div>
+          <div>{row.teams[0].pitching.NB}</div>
         </li>
       ))}
     </ul>
