f6d57fe26ea51bd3d7d990484e633b8fe54177e4
Igor Nikiforov <desola2@inbox.ru>
2022-03-16 23:00:49 +0300

    refactor: move playerStats fetching

--- src/pages/PlayerStats.js
+++ src/pages/PlayerStats.js
@@ -1,80 +1,116 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ErrorLoader from 'components/UI/loaders/ErrorLoader/ErrorLoader';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { setGamesAndLeagues } from 'redux/gamesReducer';
+import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentLeague } from 'redux/sharedReducer';
+import { setPlayerStatsData } from 'redux/playerStatsReducer';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Header from 'components/PlayerStats/Header/Header';
 import Content from 'components/PlayerStats/Content/Content';
+import Loader from 'components/UI/loaders/Loader/Loader';
 
 const PlayerStats = () => {
-	// eslint-disable-next-line
-  const [isLoading, setIsLoading] = useState(false);
+  // eslint-disable-next-line
+  const [isStatsLoading, setIsStatsLoading] = useState(false);
   const [error, setError] = useState('');
   const currentYear = useSelector(state => state.shared.currentYear);
-	const [playerYears, setPlayerYears] = useState(currentYear);
+  const [playerYears, setPlayerYears] = useState(currentYear);
 
   const cancelTokenRef = useRef();
-  const firstMountRef = useRef(true);
+  // const firstMountRef = useRef(true);
 
-  const games = useSelector(state => state.games.games);
+  const { playerName, playerSurname } = useParams();
+
+  const playerStatsData = useSelector(state => state.playerStats.playerStatsData);
+  // const games = useSelector(state => state.games.games);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchGamesData = async () => {
+    const fetchStats = async () => {
       cancelTokenRef.current = axios.CancelToken.source();
 
       try {
-        setIsLoading(true);
-        const response = await axios.get(`http://51.250.11.151:3030/main/year-${currentYear}`, {
-          cancelToken: cancelTokenRef.current.token,
-          timeout: 5000
-        });
+        setIsStatsLoading(true);
+        const response = await axios.get(
+          `http://51.250.11.151:3030/player?surname=${playerSurname}&name=${playerName}`,
+          {
+            cancelToken: cancelTokenRef.current.token,
+            timeout: 5000
+          }
+        );
         console.log(response.data);
         setError('');
-        dispatch(setGamesAndLeagues(response.data));
-
-        if (games === null) {
-          console.log('dispatched after fetching');
-          dispatch(setCurrentLeague({ id: -1, name: 'All' }));
-        }
+        dispatch(setPlayerStatsData(response.data));
       } catch (err) {
         if (err.message === null) return;
         console.log(err.message);
         setError(err.message);
       } finally {
-        setIsLoading(false);
+        setIsStatsLoading(false);
       }
     };
-    fetchGamesData();
-
-    if (firstMountRef.current === true) {
-      firstMountRef.current = false;
-      return;
-    }
+    fetchStats();
 
     dispatch(setCurrentLeague({ id: -1, name: 'All' }));
 
     return () => {
       cancelTokenRef.current.cancel(null);
     };
-		// eslint-disable-next-line
-  }, [currentYear]);
+    // eslint-disable-next-line
+  }, [playerName, playerSurname]);
+  // useEffect(() => {
+  //   const fetchGamesData = async () => {
+  //     cancelTokenRef.current = axios.CancelToken.source();
+
+  //     try {
+  //       setIsLoading(true);
+  //       const response = await axios.get(`http://51.250.11.151:3030/main/year-${currentYear}`, {
+  //         cancelToken: cancelTokenRef.current.token,
+  //         timeout: 5000
+  //       });
+  //       console.log(response.data);
+  //       setError('');
+  //       dispatch(setGamesAndLeagues(response.data));
+
+  //       if (games === null) {
+  //         console.log('dispatched after fetching');
+  //         dispatch(setCurrentLeague({ id: -1, name: 'All' }));
+  //       }
+  //     } catch (err) {
+  //       if (err.message === null) return;
+  //       console.log(err.message);
+  //       setError(err.message);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
+  //   fetchGamesData();
+
+  //   if (firstMountRef.current === true) {
+  //     firstMountRef.current = false;
+  //     return;
+  //   }
+
+  //   dispatch(setCurrentLeague({ id: -1, name: 'All' }));
+
+  //   return () => {
+  //     cancelTokenRef.current.cancel(null);
+  //   };
+  // 	// eslint-disable-next-line
+  // }, [currentYear]);
 
   return (
     <>
-      {/* {isLoading ? (
+      {isStatsLoading ? (
         <Loader />
-      ) :  */}
-      {error && games === null ? (
+      ) : error !== '' ? (
         <ErrorLoader error={error} />
-      ) : games === null ? (
+      ) : Object.keys(playerStatsData).length === 0 ? (
         <></>
       ) : (
         <>
-          <Header playerYears={playerYears} setPlayerYears={setPlayerYears}/>
-          <Content playerYears={playerYears}/>
+          <Header playerYears={playerYears} setPlayerYears={setPlayerYears} />
+          <Content playerYears={playerYears} />
         </>
       )}
     </>
