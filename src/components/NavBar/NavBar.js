23d424fc19907ff3f9c0dc4049ebca7aeae8b868
Igor Nikiforov <desola2@inbox.ru>
2022-03-16 22:56:01 +0300

    fix: current page indication

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,18 +3,22 @@ import { Link, useLocation } from 'react-router-dom';
 import cl from './NavBar.module.scss';
 
 const ROUTES = {
-  games: '/games',
-  stats: '/stats'
+  games: ['/games', '/games/'],
+  stats: ['/stats/player', '/stats/player/', '/stats/team', '/stats/team/']
 };
 
 const NavBar = () => {
   const location = useLocation();
-
-  const getClass = name => (location.pathname.slice(0, ROUTES[name].length) === ROUTES[name] ? cl.active : '');
+  console.log(location);
+  const getClass = name => (ROUTES[name].includes(location.pathname) ? cl.active : '');
   return (
     <nav className={cl.navbar}>
-      <Link to='/games' className={getClass('games')}>Schedule</Link>
-      <Link to='/stats/player' className={getClass('stats')}>Stats</Link>
+      <Link to='/games' className={getClass('games')}>
+        Schedule
+      </Link>
+      <Link to='/stats/player' className={getClass('stats')}>
+        Stats
+      </Link>
     </nav>
   );
 };
