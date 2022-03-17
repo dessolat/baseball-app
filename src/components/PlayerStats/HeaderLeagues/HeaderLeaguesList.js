228c119fd80dd473216936def25b657d3f04d631
Igor Nikiforov <desola2@inbox.ru>
2022-03-17 12:04:24 +0300

    feat: add leagues disablling
    * fix duplicate key issue

--- src/components/PlayerStats/HeaderLeagues/HeaderLeaguesList.js
+++ src/components/PlayerStats/HeaderLeagues/HeaderLeaguesList.js
@@ -4,17 +4,22 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentLeague } from 'redux/sharedReducer';
 import HeaderLeaguesListItem from './HeaderLeaguesListItem';
 
-const HeaderLeaguesList = ({ leagues }, ref) => {
+const HeaderLeaguesList = ({ leagues, playerYears }, ref) => {
   const currentLeague = useSelector(state => state.shared.currentLeague);
   const dispatch = useDispatch();
 
-  const handleLeagueClick = league => () => dispatch(setCurrentLeague(league));
+  const handleLeagueClick = league => () => {
+    if (playerYears === 'All years') return;
+    dispatch(setCurrentLeague(league));
+  };
 
+  const classes = [cl.leagues];
+  playerYears === 'All years' && classes.push(cl.disabled);
   return (
-    <ul className={cl.leagues} ref={ref}>
-      {leagues.map(league => (
+    <ul className={classes.join(' ')} ref={ref}>
+      {leagues.map((league, index) => (
         <HeaderLeaguesListItem
-          key={league.id}
+          key={index}
           league={league}
           handleClick={handleLeagueClick}
           currentLeague={currentLeague}
