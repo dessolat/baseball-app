79d28535901fe108892282f3f2ab6fbe6ea30a44
Igor Nikiforov <desola2@inbox.ru>
2022-03-18 10:04:59 +0300

    feat: add functional

--- src/components/PlayerStats/ContentBattingTable/ContentBattingTable.js
+++ src/components/PlayerStats/ContentBattingTable/ContentBattingTable.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import cl from './ContentBattingTable.module.scss';
 
-const ContentBattingTable = ({ leagues = [], playerYears }) => {
+const ContentBattingTable = ({ filteredLeagues = [], filteredLeague, playerYears }) => {
   const currentLeague = useSelector(state => state.shared.currentLeague);
+  const currentTeam = useSelector(state => state.playerStats.playerCurrentTeam);
 
   return (
     <div className={cl.wrapper}>
@@ -43,42 +44,83 @@ const ContentBattingTable = ({ leagues = [], playerYears }) => {
           <div>LOB</div>
         </div>
         <ul className={cl.rows}>
-          {leagues.map((row, index) => (
-            <li key={index} className={cl.tableRow}>
-              {playerYears === 'All years' && <div className={cl.year}>{row.year}</div>}
-              {currentLeague.id === -1 && <div className={cl.league}>{row.title}</div>}
-              {currentLeague.id !== -1 && <div className={cl.game}>01 sep, Name team - Name Team</div>}
-              <div>{row.teams[0].batting.AB}</div>
-              <div>{row.teams[0].batting.H}</div>
-              <div>{row.teams[0].batting['1B']}</div>
-              <div>{row.teams[0].batting['2B']}</div>
-              <div>{row.teams[0].batting['3B']}</div>
-              <div>{row.teams[0].batting.HR}</div>
-              <div>{row.teams[0].batting.RBI}</div>
-              <div>{row.teams[0].batting.GDP}</div>
-              <div>{row.teams[0].batting.BB}</div>
-              <div>{row.teams[0].batting.HP}</div>
-              <div>{row.teams[0].batting.SH}</div>
-              <div>{row.teams[0].batting.SF}</div>
-              <div>{row.teams[0].batting.SO}</div>
-              <div>{row.teams[0].batting.TB}</div>
-              <div className={cl.wider}>{Number(row.teams[0].batting.AVG).toFixed(3)}</div>
-              <div className={cl.wider}>{Number(row.teams[0].batting.SLG).toFixed(3)}</div>
-              <div className={cl.wider}>{Number(row.teams[0].batting.OBP).toFixed(3)}</div>
-              <div className={cl.wider}>{Number(row.teams[0].batting.OPS).toFixed(3)}</div>
-              <div>{row.teams[0].fielding.CH}</div>
-              <div>{row.teams[0].fielding.PO}</div>
-              <div>{row.teams[0].fielding.A}</div>
-              <div>{row.teams[0].fielding.E}</div>
-              <div>{row.teams[0].fielding.DP}</div>
-              <div className={cl.wider}>{row.teams[0].fielding.FLD}</div>
-              <div>{row.teams[0].running.R}</div>
-              <div>{row.teams[0].running.SB}</div>
-              <div>{row.teams[0].running.CS}</div>
-              <div className={cl.wider}>{row.teams[0].running.SB_pr}</div>
-              <div>{row.teams[0].running.LOB}</div>
-            </li>
-          ))}
+          {currentLeague.id === -1
+            ? filteredLeagues.map((row, index) => {
+                const team = row.teams.find(team => team.name === currentTeam);
+                return (
+                  <li key={index} className={cl.tableRow}>
+                    {playerYears === 'All years' && <div className={cl.year}>{row.year}</div>}
+                    <div className={cl.league}>{row.title}</div>
+                    <div>{team.batting.AB}</div>
+                    <div>{team.batting.H}</div>
+                    <div>{team.batting['1B']}</div>
+                    <div>{team.batting['2B']}</div>
+                    <div>{team.batting['3B']}</div>
+                    <div>{team.batting.HR}</div>
+                    <div>{team.batting.RBI}</div>
+                    <div>{team.batting.GDP}</div>
+                    <div>{team.batting.BB}</div>
+                    <div>{team.batting.HP}</div>
+                    <div>{team.batting.SH}</div>
+                    <div>{team.batting.SF}</div>
+                    <div>{team.batting.SO}</div>
+                    <div>{team.batting.TB}</div>
+                    <div className={cl.wider}>{Number(team.batting.AVG).toFixed(3)}</div>
+                    <div className={cl.wider}>{Number(team.batting.SLG).toFixed(3)}</div>
+                    <div className={cl.wider}>{Number(team.batting.OBP).toFixed(3)}</div>
+                    <div className={cl.wider}>{Number(team.batting.OPS).toFixed(3)}</div>
+                    <div>{team.fielding.CH}</div>
+                    <div>{team.fielding.PO}</div>
+                    <div>{team.fielding.A}</div>
+                    <div>{team.fielding.E}</div>
+                    <div>{team.fielding.DP}</div>
+                    <div className={cl.wider}>{Number(team.fielding.FLD).toFixed(3)}</div>
+                    <div>{team.running.R}</div>
+                    <div>{team.running.SB}</div>
+                    <div>{team.running.CS}</div>
+                    <div className={cl.wider}>{team.running.SB_pr}</div>
+                    <div>{team.running.LOB}</div>
+                  </li>
+                );
+              })
+            : filteredLeague?.batting?.games_batting.map((row, index) => (
+                <li key={index} className={cl.tableRow}>
+                  <div className={cl.game}>
+                    {row.date.slice(8, 10)} sep, {row.home_team.name} - {row.visit_team.name}
+                  </div>
+                  <div>{row.AB}</div>
+                  <div>{row.H}</div>
+                  <div>{row['1B']}</div>
+                  <div>{row['2B']}</div>
+                  <div>{row['3B']}</div>
+                  <div>{row.HR}</div>
+                  <div>{row.RBI}</div>
+                  <div>{row.GDP}</div>
+                  <div>{row.BB}</div>
+                  <div>{row.HP}</div>
+                  <div>{row.SH}</div>
+                  <div>{row.SF}</div>
+                  <div>{row.SO}</div>
+                  <div>{row.TB}</div>
+                  <div className={cl.wider}>{Number(row.AVG).toFixed(3)}</div>
+                  <div className={cl.wider}>{Number(row.SLG).toFixed(3)}</div>
+                  <div className={cl.wider}>{Number(row.OBP).toFixed(3)}</div>
+                  <div className={cl.wider}>{Number(row.OPS).toFixed(3)}</div>
+                  <div>{filteredLeague.fielding.games_fielding[index].CH}</div>
+                  <div>{filteredLeague.fielding.games_fielding[index].PO}</div>
+                  <div>{filteredLeague.fielding.games_fielding[index].A}</div>
+                  <div>{filteredLeague.fielding.games_fielding[index].E}</div>
+                  <div>{filteredLeague.fielding.games_fielding[index].DP}</div>
+                  <div className={cl.wider}>
+                    {Number(filteredLeague.fielding.games_fielding[index].FLD).toFixed(3)}
+                  </div>
+                  <div>{filteredLeague.running.games_running[index].R}</div>
+                  <div>{filteredLeague.running.games_running[index].SB}</div>
+                  <div>{filteredLeague.running.games_running[index].CS}</div>
+                  <div className={cl.wider}>{filteredLeague.running.games_running[index].SB_pr}</div>
+                  <div>{filteredLeague.running.games_running[index].LOB}</div>
+                </li>
+              ))}
         </ul>
       </div>
     </div>
