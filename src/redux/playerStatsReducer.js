687f307aa67445cd93240008f948541365582a35
Igor Nikiforov <desola2@inbox.ru>
2022-03-18 09:57:27 +0300

    feat: add playerCurrentTeam

--- src/redux/playerStatsReducer.js
+++ src/redux/playerStatsReducer.js
@@ -1,16 +1,21 @@
 const defaultState = {
-  playerStatsData: {}
+  playerStatsData: {},
+	playerCurrentTeam: ''
 };
 
 const SET_PLAYER_STATS_DATA = 'SET_PLAYER_STATS_DATA';
+const SET_PLAYER_CURRENT_TEAM = 'SET_PLAYER_CURRENT_TEAM';
 
 export const playerStatsReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_PLAYER_STATS_DATA:
       return { ...state, playerStatsData: action.payload };
+    case SET_PLAYER_CURRENT_TEAM:
+      return { ...state, playerCurrentTeam: action.payload };
     default:
       return state;
   }
 };
 
 export const setPlayerStatsData = payload => ({ type: SET_PLAYER_STATS_DATA, payload });
+export const setPlayerCurrentTeam = payload => ({ type: SET_PLAYER_CURRENT_TEAM, payload });
