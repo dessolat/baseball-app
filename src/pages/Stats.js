2ff3143e672a645ed034b7274a3f8ed66430a3ed
Igor Nikiforov <desola2@inbox.ru>
2022-03-20 17:52:37 +0300

    feat: move stats fetching to root page

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -2,78 +2,110 @@ import React, { useState, useEffect, useRef } from 'react';
 import ErrorLoader from 'components/UI/loaders/ErrorLoader/ErrorLoader';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { setGamesAndLeagues } from 'redux/gamesReducer';
 import { setCurrentLeague } from 'redux/sharedReducer';
 import axios from 'axios';
 import Header from 'components/Stats/Header/Header';
 import Content from 'components/Stats/Content/Content';
+import { setStatsData } from 'redux/statsReducer';
+import Loader from 'components/UI/loaders/Loader/Loader';
 
 const Stats = () => {
-	// eslint-disable-next-line
-  const [isLoading, setIsLoading] = useState(false);
+  const [isStatsLoading, setIsStatsLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const cancelTokenRef = useRef();
   const firstMountRef = useRef(true);
+  const cancelStatsTokenRef = useRef();
 
-  const games = useSelector(state => state.games.games);
+  const statsData = useSelector(state => state.stats.statsData);
   const currentYear = useSelector(state => state.shared.currentYear);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchGamesData = async () => {
-      cancelTokenRef.current = axios.CancelToken.source();
+    const fetchStats = async () => {
+      cancelStatsTokenRef.current = axios.CancelToken.source();
 
       try {
-        setIsLoading(true);
-        const response = await axios.get(`http://51.250.11.151:3030/main/year-${currentYear}`, {
-          cancelToken: cancelTokenRef.current.token,
+        setIsStatsLoading(true);
+        const response = await axios.get(`http://51.250.11.151:3030/stats/year-${currentYear}`, {
+          cancelToken: cancelStatsTokenRef.current.token,
           timeout: 5000
         });
         console.log(response.data);
         setError('');
-        dispatch(setGamesAndLeagues(response.data));
-
-        if (games === null) {
-          console.log('dispatched after fetching');
-          dispatch(setCurrentLeague({ id: -1, name: 'All' }));
-        }
+        dispatch(setStatsData(response.data));
       } catch (err) {
         if (err.message === null) return;
         console.log(err.message);
         setError(err.message);
       } finally {
-        setIsLoading(false);
+        setIsStatsLoading(false);
       }
     };
-    fetchGamesData();
+    fetchStats();
 
-    if (firstMountRef.current === true) {
-      firstMountRef.current = false;
-      return;
-    }
+    // if (firstMountRef.current === true) {
+    //   firstMountRef.current = false;
+    //   return;
+    // }
 
     dispatch(setCurrentLeague({ id: -1, name: 'All' }));
 
     return () => {
-      cancelTokenRef.current.cancel(null);
+      cancelStatsTokenRef.current.cancel(null);
     };
-		// eslint-disable-next-line
+    // eslint-disable-next-line
   }, [currentYear]);
+  // useEffect(() => {
+  //   const fetchGamesData = async () => {
+  //     cancelTokenRef.current = axios.CancelToken.source();
+
+  //     try {
+  //       setIsLoading(true);
+  //       const response = await axios.get(`http://51.250.11.151:3030/main/year-${currentYear}`, {
+  //         cancelToken: cancelTokenRef.current.token,
+  //         timeout: 5000
+  //       });
+  //       console.log(response.data);
+  //       setError('');
+  //       dispatch(setGamesAndLeagues(response.data));
+
+  //       if (games === null) {
+  //         console.log('dispatched after fetching');
+  //         dispatch(setCurrentLeague({ id: -1, name: 'All' }));
+  //       }
+  //     } catch (err) {
+  //       if (err.message === null) return;
+  //       console.log(err.message);
+  //       setError(err.message);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
+  //   fetchGamesData();
+
+  //   if (firstMountRef.current === true) {
+  //     firstMountRef.current = false;
+  //     return;
+  //   }
+
+  //   dispatch(setCurrentLeague({ id: -1, name: 'All' }));
+
+  //   return () => {
+  //     cancelTokenRef.current.cancel(null);
+  //   };
+  // 	// eslint-disable-next-line
+  // }, [currentYear]);
 
   return (
     <>
-      {/* {isLoading ? (
-        <Loader />
-      ) :  */}
-      {error && games === null ? (
+      {error !== '' ? (
         <ErrorLoader error={error} />
-      ) : games === null ? (
-        <></>
+      ) : isStatsLoading && statsData.length === 0 ? (
+        <Loader />
       ) : (
         <>
           <Header />
-          <Content games={games} />
+          <Content />
         </>
       )}
     </>
