fc7a7cd3bb272daf300934d8c79e3cda765140e0
Igor Nikiforov <desola2@inbox.ru>
2022-03-26 13:46:04 +0300

    feat: import ActiveBodyCell comp

--- src/components/PlayerStats/ContentPitchingTable/ContentPitchingTableBody.js
+++ src/components/PlayerStats/ContentPitchingTable/ContentPitchingTableBody.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import cl from './ContentPitchingTable.module.scss';
+import ActiveBodyCell from 'components/UI/ActiveBodyCell/ActiveBodyCell';
 
 const ContentPitchingTableBody = ({
   filteredLeagues = [],
@@ -43,52 +44,54 @@ const ContentPitchingTableBody = ({
               <li key={index} className={cl.tableRow}>
                 {playerYears === 'All years' && <div className={cl.year}>{row.year}</div>}
                 <div className={cl.league}>{row.title}</div>
-                <div>{team.pitching.PA}</div>
-                <div>{team.pitching.R}</div>
-                <div>{team.pitching.ER}</div>
-                <div>{team.pitching.H}</div>
-                <div>{team.pitching['2B']}</div>
-                <div>{team.pitching['3B']}</div>
-                <div>{team.pitching.HR}</div>
-                <div>{team.pitching.BB}</div>
-                <div>{team.pitching.IBB}</div>
-                <div>{team.pitching.HP}</div>
-                <div>{team.pitching.SH}</div>
-                <div>{team.pitching.SF}</div>
-                <div>{team.pitching.SO}</div>
-                <div>{team.pitching.WP}</div>
-                <div className={cl.wider}>{Number(team.pitching.ERA).toFixed(3)}</div>
-                <div>{team.pitching.NP}</div>
-                <div>{team.pitching.NS}</div>
-                <div>{team.pitching.NB}</div>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>PA</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>R</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>ER</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>H</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>2B</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>3B</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>HR</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>BB</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>IBB</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>HP</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>SH</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>SF</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>SO</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>WP</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching} fixed={3} addedClass={cl.wider}>ERA</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>NP</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>NS</ActiveBodyCell>
+                <ActiveBodyCell sortField={sortField} row={team.pitching}>NB</ActiveBodyCell>
               </li>
             );
           })
         : //Selected league
-				sortedLeagueGames.map((row, index) => (
+          sortedLeagueGames.map((row, index) => (
             <li key={index} className={cl.tableRow}>
               <div className={cl.game}>
-                {row.date.slice(8, 10)} {MONTHS[+row.date.slice(5, 7) - 1]}, {row.home_team.name} -{' '}
-                {row.visit_team.name}
+                {row.date.slice(8, 10)} {MONTHS[+row.date.slice(5, 7) - 1]},
+                <span className={cl.teams}>
+                   {row.home_team.name} - {row.visit_team.name}
+                </span>
               </div>
-              <div>{row.PA}</div>
-              <div>{row.R}</div>
-              <div>{row.ER}</div>
-              <div>{row.H}</div>
-              <div>{row['2B']}</div>
-              <div>{row['3B']}</div>
-              <div>{row.HR}</div>
-              <div>{row.BB}</div>
-              <div>{row.IBB}</div>
-              <div>{row.HP}</div>
-              <div>{row.SH}</div>
-              <div>{row.SF}</div>
-              <div>{row.SO}</div>
-              <div>{row.WP}</div>
-              <div className={cl.wider}>{Number(row.ERA).toFixed(3)}</div>
-              <div>{row.NP}</div>
-              <div>{row.NS}</div>
-              <div>{row.NB}</div>
+              <ActiveBodyCell sortField={sortField} row={row}>PA</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>R</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>ER</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>H</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>2B</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>3B</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>HR</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>BB</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>IBB</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>HP</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>SH</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>SF</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>SO</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>WP</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row} fixed={3} addedClass={cl.wider}>ERA</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>NP</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>NS</ActiveBodyCell>
+              <ActiveBodyCell sortField={sortField} row={row}>NB</ActiveBodyCell>
             </li>
           ))}
     </ul>
