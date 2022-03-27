5a88e225c742ddf57414d431aa9952a716f17153
Igor Nikiforov <desola2@inbox.ru>
2022-03-27 13:30:34 +0300

    feat: create ContentTableModeLinks comp p/holder

---
+++ src/components/Games/ContentTable/ContentTableModeLinks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './ContentTable.module.scss';
+
+const ContentTableModeLinks = () => {
+	return (
+		<div className={cl.tableModes}>ContentTableModeLinks</div>
+	)
+}
+
+export default ContentTableModeLinks
