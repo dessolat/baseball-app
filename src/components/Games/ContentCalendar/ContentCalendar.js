187c821d0bf53cace3a74da1f48d84132ab25508
Igor Nikiforov <desola2@inbox.ru>
2022-03-27 14:18:54 +0300

    feat: import isMobile

--- src/components/Games/ContentCalendar/ContentCalendar.js
+++ src/components/Games/ContentCalendar/ContentCalendar.js
@@ -1,5 +1,5 @@
 import Arrow from 'components/UI/buttons/Arrow/Arrow';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect,useLayoutEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import cl from './ContentCalendar.module.scss';
 import ContentCalendarList from './ContentCalendarList';
@@ -9,31 +9,34 @@ const ContentCalendar = ({ onChange, calendarScroll }) => {
   const timeoutRef = useRef(null);
 
   const currentDate = useSelector(state => state.shared.currentDate);
+  const isMobile = useSelector(state => state.shared.isMobile);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     ref.current.style.transition = 'none';
-    ref.current.style.transform = `translate(0px)`;
+
+    ref.current.style.transform = `translate(${isMobile ? 43 : 0}px)`;
 
     setTimeout(() => {
-      ref.current.style.transition = 'all .4s';
+      ref.current.style.transition = 'all .25s';
       timeoutRef.current = null;
-    }, 100);
+    }, 30);
   }, [currentDate]);
 
-	useEffect(() => {
-		if (!calendarScroll.isScroll) return
+  useEffect(() => {
+    if (!calendarScroll.isScroll) return;
 
-		ref.current.style.transform = `translate(${0 - 60.5 * calendarScroll.amount}px)`;
-	}, [calendarScroll])
-	
+    ref.current.style.transform = `translate(${
+      (isMobile ? 43 : 0) - (isMobile ? 43 : 60.5) * calendarScroll.amount
+    }px)`;
+  }, [calendarScroll]);
 
   const handleDateClick = date => () => {
     if (timeoutRef.current !== null) return;
     const daysDelta = (date - currentDate) / 1000 / 60 / 60 / 24;
-    ref.current.style.transform = `translate(${0 - 60.5 * daysDelta}px)`;
+    ref.current.style.transform = `translate(${(isMobile ? 43 : 0) - (isMobile ? 43 : 60.5) * daysDelta}px)`;
     timeoutRef.current = setTimeout(() => {
       onChange(date);
-    }, 250);
+    }, 350); //250
   };
 
   const handleArrowClick = dir => () => {
@@ -43,11 +46,13 @@ const ContentCalendar = ({ onChange, calendarScroll }) => {
   };
   return (
     <div className={cl.calendar}>
-      {/* <button onClick={handleArrowClick('left')}>Left</button> */}
-      <Arrow onClick={handleArrowClick('left')} style={{ marginRight: '.3rem' }} />
+      <Arrow onClick={handleArrowClick('left')} style={{ marginRight: isMobile ? 0 : '.3rem' }} />
       <ContentCalendarList currentDate={currentDate} handleClick={handleDateClick} ref={ref} />
-      <Arrow direction='right' onClick={handleArrowClick('right')} style={{ marginLeft: '.5rem' }} />
-      {/* <button onClick={handleArrowClick('right')}>Right</button> */}
+      <Arrow
+        direction='right'
+        onClick={handleArrowClick('right')}
+        style={{ marginLeft: isMobile ? 0 : '.5rem' }}
+      />
     </div>
   );
 };
