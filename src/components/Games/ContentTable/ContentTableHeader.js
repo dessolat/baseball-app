b87f2373559506835e71f70c5ccd27cd3070e432
Igor Nikiforov <desola2@inbox.ru>
2022-03-27 14:19:16 +0300

    feat: import ContentTableModeLinks comp

--- src/components/Games/ContentTable/ContentTableHeader.js
+++ src/components/Games/ContentTable/ContentTableHeader.js
@@ -2,12 +2,17 @@ import React from 'react';
 import cl from './ContentTable.module.scss';
 import ContentCalendars from './ContentCalendars';
 import ContentTeam from './ContentTeam';
+import ContentTableModeLinks from './ContentTableModeLinks';
 
-const ContentTableHeader = () => (
-  <div className={cl.header}>
-    <ContentTeam />
-    <ContentCalendars />
-  </div>
-);
+const ContentTableHeader = () => {
+	
+  return (
+    <div className={cl.header}>
+      <ContentTeam />
+      <ContentCalendars />
+			<ContentTableModeLinks />
+    </div>
+  );
+};
 
 export default ContentTableHeader;
