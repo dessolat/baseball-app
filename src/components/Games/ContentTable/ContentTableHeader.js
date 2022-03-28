dbcbeb618351dac32b4f4c60cf4f807b893ab463
Igor Nikiforov <desola2@inbox.ru>
2022-03-28 22:12:14 +0300

    feat: title on mobileTableMode change

--- src/components/Games/ContentTable/ContentTableHeader.js
+++ src/components/Games/ContentTable/ContentTableHeader.js
@@ -3,14 +3,21 @@ import cl from './ContentTable.module.scss';
 import ContentCalendars from './ContentCalendars';
 import ContentTeam from './ContentTeam';
 import ContentTableModeLinks from './ContentTableModeLinks';
+import { useSelector } from 'react-redux';
 
 const ContentTableHeader = () => {
-	
+  const isMobile = useSelector(state => state.shared.isMobile);
+  const currentLeague = useSelector(state => state.shared.currentLeague);
+  const mobileTableMode = useSelector(state => state.games.mobileTableMode);
+
   return (
     <div className={cl.header}>
       <ContentTeam />
-      <ContentCalendars />
-			<ContentTableModeLinks />
+      {(!isMobile || (isMobile && mobileTableMode === 'Calendar')) && <ContentCalendars />}
+      {mobileTableMode !== 'Calendar' && (
+        <div className={cl.modeTitle}>{mobileTableMode === 'Team tablo' ? 'Team tablo' : 'Leaders'}</div>
+      )}
+      {currentLeague.id !== -1 && <ContentTableModeLinks />}
     </div>
   );
 };
