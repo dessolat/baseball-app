318e18a251a1907c9a52bb436bada94e622ad98a
Igor Nikiforov <desola2@inbox.ru>
2022-03-28 22:05:13 +0300

    feat: add handleModeClick func

--- src/components/Games/ContentTable/ContentTableModeLinks.js
+++ src/components/Games/ContentTable/ContentTableModeLinks.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { setMobileTableMode } from 'redux/gamesReducer';
 import cl from './ContentTable.module.scss';
 
 const ContentTableModeLinks = () => {
+	const isMobile = useSelector(state => state.shared.isMobile)
+	const mobileTableMode = useSelector(state => state.games.mobileTableMode)
+	const dispatch = useDispatch()
+
+	const handleModeClick = mode => () => dispatch(setMobileTableMode(mode))
 	return (
 		<div className={cl.tableModes}>ContentTableModeLinks</div>
 	)
