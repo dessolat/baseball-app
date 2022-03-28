648199c6c55fe2ac4be932a1679d6f34acd46a99
Igor Nikiforov <desola2@inbox.ru>
2022-03-28 22:04:14 +0300

    feat: set table mode to Calendar on All leag click

--- src/components/Games/HeaderLeagues/HeaderLeaguesList.js
+++ src/components/Games/HeaderLeagues/HeaderLeaguesList.js
@@ -3,12 +3,16 @@ import cl from './HeaderLeagues.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentLeague } from 'redux/sharedReducer';
 import HeaderLeaguesListItem from './HeaderLeaguesListItem';
+import { setMobileTableMode } from 'redux/gamesReducer';
 
 const HeaderLeaguesList = ({ leagues }, ref) => {
   const currentLeague = useSelector(state => state.shared.currentLeague);
   const dispatch = useDispatch();
 
-  const handleLeagueClick = league => () => dispatch(setCurrentLeague(league));
+  const handleLeagueClick = league => () => {
+		league.id === -1 && dispatch(setMobileTableMode('Calendar'))
+    dispatch(setCurrentLeague(league));
+  };
 
   return (
     <ul className={cl.leagues} ref={ref}>
