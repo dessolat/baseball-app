eb60e876b4afee5b8560b52bda69d94ec2325beb
Igor Nikiforov <desola2@inbox.ru>
2022-03-29 11:04:11 +0300

    feat: add isMobile & mobileTableMode check

--- src/components/Games/ContentSideTables/ContentSideTables.js
+++ src/components/Games/ContentSideTables/ContentSideTables.js
@@ -7,14 +7,15 @@ import SwitchTable from './SwitchTable';
 const ContentSideTables = () => {
   const currentLeague = useSelector(state => state.shared.currentLeague);
   const isMobile = useSelector(state => state.shared.isMobile);
+  const mobileTableMode = useSelector(state => state.games.mobileTableMode);
 
   const isTables = currentLeague.id !== -1;
   return (
     <div className={cl.side} style={!isTables ? { height: isMobile ? 'auto' : '70vh' } : null}>
       {isTables && (
         <>
-          <PctTable currentLeague={currentLeague} />
-          <SwitchTable />
+          {(!isMobile || mobileTableMode === 'Team tablo') && <PctTable currentLeague={currentLeague} />}
+          {(!isMobile || mobileTableMode === 'Leaders') && <SwitchTable />}
         </>
       )}
     </div>
