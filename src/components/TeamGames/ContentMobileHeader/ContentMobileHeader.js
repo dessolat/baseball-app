bca05da16e2c2e9ff45d5921d99843f34208da7e
Igor Nikiforov <desola2@inbox.ru>
2022-04-02 23:37:53 +0300

    feat: add links

--- src/components/TeamGames/ContentMobileHeader/ContentMobileHeader.js
+++ src/components/TeamGames/ContentMobileHeader/ContentMobileHeader.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ContentHeaderModeLinks from './ContentHeaderModeLinks';
 import cl from './ContentMobileHeader.module.scss';
 
 const ContentMobileHeader = () => {
-  return <div>ContentMobileHeader</div>;
+  return (
+    <div className={cl.mobileHeader}>
+      <ContentHeaderModeLinks />
+			<div className={cl.linkWrapper}>
+        <Link to='/stats/player'>Go to Player Stat</Link>
+      </div>
+    </div>
+  );
 };
 
 export default ContentMobileHeader;
