39b3f52586dd7fb3c46805aaa03f95e5ec35d5c3
Igor Nikiforov <desola2@inbox.ru>
2022-04-02 17:11:44 +0300

    feat: add mobileTableMode to teamGamesReducer

--- src/redux/teamGamesReducer.js
+++ src/redux/teamGamesReducer.js
@@ -1,16 +1,21 @@
 const defaultState = {
-  teamData: []
+  teamData: [],
+	mobileTableMode: 'Players'
 };
 
 const SET_TEAM_DATA = 'SET_TEAM_DATA';
+const SET_MOBILE_TEAM_GAMES_TABLE_MODE = 'SET_MOBILE_TEAM_GAMES_TABLE_MODE';
 
 export const teamGamesReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_TEAM_DATA:
       return { ...state, teamData: action.payload };
+    case SET_MOBILE_TEAM_GAMES_TABLE_MODE:
+      return { ...state, mobileTableMode: action.payload };
     default:
       return state;
   }
 };
 
 export const setTeamData = payload => ({ type: SET_TEAM_DATA, payload });
+export const setMobileTableMode = payload => ({ type: SET_MOBILE_TEAM_GAMES_TABLE_MODE, payload });
