0ddb90056ed07094198106ea8fe72c493e659c75
Igor Nikiforov <desola2@inbox.ru>
2022-04-02 23:40:03 +0300

    feat: import ContentMobileHeader comp

--- src/components/TeamGames/Content/Content.js
+++ src/components/TeamGames/Content/Content.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ContentPlayersTable from '../ContentPlayersTable/ContentPlayersTable';
 import ContentGamesTable from '../ContentGamesTable/ContentGamesTable';
 import cl from './Content.module.scss';
+import { useSelector } from 'react-redux';
+import ContentMobileHeader from '../ContentMobileHeader/ContentMobileHeader';
 
 const Content = () => {
+	const isMobile = useSelector(state => state.shared.isMobile)
+
   return (
     <section>
       <div className='container'>
         <div className={cl.content}>
+					{isMobile && <ContentMobileHeader />}
           <ContentPlayersTable />
           <ContentGamesTable />
         </div>
