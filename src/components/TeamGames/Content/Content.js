9293ce31b6f45af5ad63903e7a9083c60048bea2
Igor Nikiforov <desola2@inbox.ru>
2022-04-03 13:58:00 +0300

    feat: add mobileTableMode checking

--- src/components/TeamGames/Content/Content.js
+++ src/components/TeamGames/Content/Content.js
@@ -6,15 +6,16 @@ import { useSelector } from 'react-redux';
 import ContentMobileHeader from '../ContentMobileHeader/ContentMobileHeader';
 
 const Content = () => {
-	const isMobile = useSelector(state => state.shared.isMobile)
+  const isMobile = useSelector(state => state.shared.isMobile);
+  const mobileTableMode = useSelector(state => state.teamGames.mobileTableMode);
 
   return (
     <section>
       <div className='container'>
         <div className={cl.content}>
-					{isMobile && <ContentMobileHeader />}
-          <ContentPlayersTable />
-          <ContentGamesTable />
+          {isMobile && <ContentMobileHeader />}
+          {mobileTableMode === 'Players' && <ContentPlayersTable />}
+          {(mobileTableMode === 'Calendar' || !isMobile) && <ContentGamesTable />}
         </div>
       </div>
     </section>
