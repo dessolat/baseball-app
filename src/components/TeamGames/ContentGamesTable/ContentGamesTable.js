0e904d7070740fc2c3df63325adb9f008cee1ec1
Igor Nikiforov <desola2@inbox.ru>
2022-04-04 22:25:48 +0300

    feat: change MONTHS names to full

--- src/components/TeamGames/ContentGamesTable/ContentGamesTable.js
+++ src/components/TeamGames/ContentGamesTable/ContentGamesTable.js
@@ -5,23 +5,24 @@ import { useSelector } from 'react-redux';
 import { getShortName } from 'utils';
 
 const MONTHS = {
-  '01': 'jan',
-  '02': 'feb',
-  '03': 'mar',
-  '04': 'apr',
+  '01': 'january',
+  '02': 'february',
+  '03': 'march',
+  '04': 'april',
   '05': 'may',
-  '06': 'jun',
-  '07': 'jul',
-  '08': 'aug',
-  '09': 'sep',
-  10: 'oct',
-  11: 'nov',
-  12: 'dec'
+  '06': 'june',
+  '07': 'july',
+  '08': 'august',
+  '09': 'september',
+  10: 'october',
+  11: 'november',
+  12: 'december'
 };
 
 const ContentGamesTable = () => {
   const currentLeague = useSelector(state => state.shared.currentLeague);
   const currentYear = useSelector(state => state.shared.currentYear);
+  const isMobile = useSelector(state => state.shared.isMobile);
   const teamData = useSelector(state => state.teamGames.teamData);
 
   //Filtering teamData leagues by current year
@@ -58,7 +59,11 @@ const ContentGamesTable = () => {
         <ul className={cl.rows}>
           {sortedGamesArray.map((game, index) => (
             <li key={game.id} className={cl.tableRow}>
-              <div>{game.date.slice(8, 10) + ' ' + MONTHS[game.date.slice(5, 7)]}</div>
+              <div>
+                {game.date.slice(8, 10) +
+                  ' ' +
+                  (!isMobile ? MONTHS[game.date.slice(5, 7)].slice(0, 3) : MONTHS[game.date.slice(5, 7)])}
+              </div>
               <div className={cl.underlineHover}>
                 <Link to={`/games/team/${game.homies.name}`}> {getShortName(game.homies.name, 22)}</Link>
               </div>
@@ -75,7 +80,7 @@ const ContentGamesTable = () => {
                   {game.has_records && <Link to={`/game/${game.id}?tab=videos`}>Videos</Link>}
                 </div>
               </div>
-              <div>{game.inn !== null ? `${game.last_inn} inn` : '—'} </div>
+              <div>{game.last_inn !== null ? `${game.last_inn} inn` : '—'} </div>
             </li>
           ))}
         </ul>
