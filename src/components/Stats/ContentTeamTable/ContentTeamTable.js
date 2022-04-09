a3ed43c7eb49feb9f1c3d91123af11d1490866d5
Igor Nikiforov <desola2@inbox.ru>
2022-04-09 14:49:34 +0300

    feat: add isMobile functional template

--- src/components/Stats/ContentTeamTable/ContentTeamTable.js
+++ src/components/Stats/ContentTeamTable/ContentTeamTable.js
@@ -35,26 +35,90 @@ const ContentTeamTable = ({ getTableHeaders, getTableRows, getSortedStatsData })
         }, []) || [];
 
   return (
-    <div className={cl.wrapper}>
-      <div>
-        <div className={cl.tableHeader}>
-          <div>Team</div>
-          {getTableHeaders(sortField, sortDirection, handleFieldClick, cl)}
+    <>
+      {isMobile ? (
+        <div className={cl.mobileWrapper}>
+          <div className={cl.fullHeader}>
+            <div className={cl.leftHeader}>
+              <div>Players</div>
+              <div>POS</div>
+              <div>
+                <Dropdown
+                  title={'Team'}
+                  options={teamOptions}
+                  currentOption={currentTeam}
+                  handleClick={handleTeamClick}
+                  listStyles={{ left: '-.3rem', width: 'calc(100% + 4rem)' }}
+                  itemStyles={{ fontSize: '12px', padding: '0.2rem 0.5rem' }}
+                  shortNames={13}
+                />
+              </div>
+            </div>
+            <div className={cl.rightHeader} ref={headerScroll}>
+              {getTableHeaders(sortField, sortDirection, handleFieldClick, cl, {
+                top: '.1rem',
+                transform: 'translateX(-50%) scale(0.7)'
+              })}
+            </div>
+          </div>
+          <div className={cl.sides}>
+            <div className={cl.leftRows}>
+              {getSortedStatsData(filteredStatsData, sortField, sortDirection).map((row, index) => {
+                return (
+                  <div key={index} className={cl.tableRow}>
+                    <div>
+                      <Link to={`/stats/player/${row.name}/${row.surname}`}>
+                        {' '}
+                        <span>
+                          {row.name} {row.surname}
+                        </span>
+                      </Link>
+                    </div>
+                    <div>{index}</div>
+                    <div>
+                      <img src={TeamLogo} alt='team-logo' />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div
+              className={cl.rightRows}
+              onScroll={e => (headerScroll.current.scrollLeft = e.target.scrollLeft)}
+              ref={rowsScroll}>
+              {getSortedStatsData(filteredStatsData, sortField, sortDirection).map((row, index) => {
+                return (
+                  <div key={index} className={cl.tableRow}>
+                    {getTableRows(row, cl, sortField)}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-        <ul className={cl.rows}>
-          {getSortedStatsData(filteredStatsData, sortField, sortDirection).map((row, index) => {
-            return (
-              <li key={index} className={cl.tableRow}>
-                <div>
-                  <Link to={`/games/team/${row.name}`}> {row.name}</Link>
-                </div>
-                {getTableRows(row, cl, sortField)}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+      ) : (
+        <div className={cl.wrapper}>
+          <div>
+            <div className={cl.tableHeader}>
+              <div>Team</div>
+              {getTableHeaders(sortField, sortDirection, handleFieldClick, cl)}
+            </div>
+            <ul className={cl.rows}>
+              {getSortedStatsData(filteredStatsData, sortField, sortDirection).map((row, index) => {
+                return (
+                  <li key={index} className={cl.tableRow}>
+                    <div>
+                      <Link to={`/games/team/${row.name}`}> {row.name}</Link>
+                    </div>
+                    {getTableRows(row, cl, sortField)}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
