b556c217d3f777f076392c4a13938775c5c61be6
Igor Nikiforov <desola2@inbox.ru>
2022-04-10 18:07:11 +0300

    feat: add tableType to reducer

--- src/redux/playerStatsReducer.js
+++ src/redux/playerStatsReducer.js
@@ -1,10 +1,12 @@
 const defaultState = {
   playerStatsData: {},
-	playerCurrentTeam: ''
+	playerCurrentTeam: '',
+	tableType: 'Batting'
 };
 
 const SET_PLAYER_STATS_DATA = 'SET_PLAYER_STATS_DATA';
 const SET_PLAYER_CURRENT_TEAM = 'SET_PLAYER_CURRENT_TEAM';
+const SET_TABLE_TYPE = 'SET_TABLE_TYPE';
 
 export const playerStatsReducer = (state = defaultState, action) => {
   switch (action.type) {
@@ -12,6 +14,8 @@ export const playerStatsReducer = (state = defaultState, action) => {
       return { ...state, playerStatsData: action.payload };
     case SET_PLAYER_CURRENT_TEAM:
       return { ...state, playerCurrentTeam: action.payload };
+    case SET_TABLE_TYPE:
+      return { ...state, tableType: action.payload };
     default:
       return state;
   }
@@ -19,3 +23,4 @@ export const playerStatsReducer = (state = defaultState, action) => {
 
 export const setPlayerStatsData = payload => ({ type: SET_PLAYER_STATS_DATA, payload });
 export const setPlayerCurrentTeam = payload => ({ type: SET_PLAYER_CURRENT_TEAM, payload });
+export const setTableType = payload => ({ type: SET_TABLE_TYPE, payload });
