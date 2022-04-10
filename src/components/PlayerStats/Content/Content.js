1ebd01f48e0930312bca744511461c5a13250c64
Igor Nikiforov <desola2@inbox.ru>
2022-04-10 18:34:37 +0300

    feat: add isMobile handling

--- src/components/PlayerStats/Content/Content.js
+++ src/components/PlayerStats/Content/Content.js
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import cl from './Content.module.scss';
 import ContentBattingTable from '../ContentBattingTable/ContentBattingTable';
 import ContentPitchingTable from '../ContentPitchingTable/ContentPitchingTable';
 import Dropdown from 'components/UI/dropdown/GamesDropdown/Dropdown';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setTableType } from 'redux/playerStatsReducer';
+import ContentMobilePlayerInfo from './ContentMobilePlayerInfo';
+import ContentMobileTable from '../ContentMobileTable/ContentMobileTable';
 
 const MONTHS = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
 const Content = ({ playerYears }) => {
-  const [tableType, setTableType] = useState('Batting');
-
   const statsData = useSelector(state => state.playerStats.playerStatsData);
   const currentLeague = useSelector(state => state.shared.currentLeague);
+  const isMobile = useSelector(state => state.shared.isMobile);
   const currentTeam = useSelector(state => state.playerStats.playerCurrentTeam);
+  const tableType = useSelector(state => state.playerStats.tableType);
+  const dispatch = useDispatch();
 
   const TABLE_OPTIONS = ['Batting', 'Pitching'];
 
-  const handleTableOptionClick = option => setTableType(option);
+  const handleTableOptionClick = option => dispatch(setTableType(option));
 
   const filteredLeagues =
     playerYears === 'All years'
@@ -35,6 +39,7 @@ const Content = ({ playerYears }) => {
             <></>
           ) : (
             <>
+              {isMobile && <ContentMobilePlayerInfo statsData={statsData} />}
               <p className={cl.playerChr}>
                 {statsData.pos || '—'} | B/T: {statsData.bat_hand}/{statsData.throw_hand} | {statsData.height}{' '}
                 {statsData.weight}LBS | Age: {new Date().getFullYear() - statsData.yob}
@@ -47,7 +52,14 @@ const Content = ({ playerYears }) => {
                   handleClick={handleTableOptionClick}
                 />
               </div>
-              {tableType === 'Batting' ? (
+              {isMobile ? (
+                <ContentMobileTable
+                  filteredLeagues={filteredLeagues}
+                  filteredLeague={filteredLeague}
+                  playerYears={playerYears}
+                  MONTHS={MONTHS}
+                />
+              ) : tableType === 'Batting' ? (
                 <ContentBattingTable
                   filteredLeagues={filteredLeagues}
                   filteredLeague={filteredLeague}
