2aac01fedb59612a1ac40b58c09d9b6f59a3f0d6
Igor Nikiforov <desola2@inbox.ru>
2022-04-16 19:05:23 +0300

    feat: add playOnlineBtn class for adaptive

--- src/components/UI/buttons/PlayOnline/PlayOnline.js
+++ src/components/UI/buttons/PlayOnline/PlayOnline.js
@@ -4,7 +4,7 @@ import cl from './PlayOnline.module.scss';
 const PlayOnline = ({ className, ...props }) => {
   return (
     <button {...props} className={cl.btn + ' ' + className}>
-      <svg width='15' height='17' viewBox='0 0 15 17' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <svg width='15' height='17' viewBox='0 0 15 17' fill='none' xmlns='http://www.w3.org/2000/svg' className={cl.playOnlineBtn}>
         <path
           d='M7.40515 16.6719L0.673439 10.875H3.70153H3.95153V10.625V0.25H10.8546V3.77778V10.625V10.875H11.1046H14.3132L7.40515 16.6719Z'
           fill='#1A4C96'
