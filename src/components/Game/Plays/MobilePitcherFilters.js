0df82650577738153af63722782f6976661c1732
Igor Nikiforov <desola2@inbox.ru>
2022-04-16 17:39:06 +0300

    feat: create MobilePitcherFilters comp p/holder

---
+++ src/components/Game/Plays/MobilePitcherFilters.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import cl from './Plays.module.scss';
+
+const MobilePitcherFilters = () => {
+	return (
+		<div className={cl.mobilePitcherFilters}>
+			
+		</div>
+	)
+}
+
+export default MobilePitcherFilters
