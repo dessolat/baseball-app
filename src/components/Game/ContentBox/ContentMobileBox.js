8446c2a90eb760d73d560e42ffa3dc8ad5dfe424
Igor Nikiforov <desola2@inbox.ru>
2022-04-18 09:14:00 +0300

    feat: add MobileBoxTable comp p/holder

--- src/components/Game/ContentBox/ContentMobileBox.js
+++ src/components/Game/ContentBox/ContentMobileBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import cl from './ContentMobileBox.module.scss';
 import MobileBoxHeader from './MobileBoxHeader';
+import MobileBoxTable from './MobileBoxTable';
 
 const ContentMobileBox = () => {
   return (
     <div className={cl.mobileBox}>
       <MobileBoxHeader />
-      Mobile box
+      <MobileBoxTable />
     </div>
   );
 };
