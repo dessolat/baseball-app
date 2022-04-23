18da900464ae661c7bd46f93ca671f488d01d688
Igor Nikiforov <desola2@inbox.ru>
2022-04-23 23:30:33 +0300

    feat: add pitchState checking

--- src/components/Game/PlaysSpin/PlaysSpin.js
+++ src/components/Game/PlaysSpin/PlaysSpin.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import cl from './PlaysSpin.module.scss';
 import PlaysSpinChart from './PlaysSpinChart';
 import PlaysSpinInfo from './PlaysSpinInfo';
 
-const PlaysSpin = ({ pitch }) => (
-  <div className={cl.spin}>
-    <PlaysSpinChart pitch={pitch} />
-    <PlaysSpinInfo pitch={pitch} />
-  </div>
-);
+const PlaysSpin = ({ pitch }) => {
+	const pitchState = useSelector(state => state.game.pitchState)
+	
+  return (
+    <div className={pitchState !== 'Field' ? cl.spin : cl.spin + ' ' + cl.dnone}>
+      <PlaysSpinChart pitch={pitch} />
+      <PlaysSpinInfo pitch={pitch} />
+    </div>
+  );
+};
 
 export default PlaysSpin;
