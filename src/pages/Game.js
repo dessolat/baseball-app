991fc11e579c76d29de01b312d88e836c5d71141
Igor Nikiforov <desola2@inbox.ru>
2022-04-23 23:36:10 +0300

    feat: increase timeout from 5000 to 7000

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -29,7 +29,7 @@ const Game = () => {
   useEffect(() => {
     axios
       .get(`http://51.250.11.151:3030/game_${gameId}`, {
-        timeout: 5000
+        timeout: 7000
       })
       .then(() => {
         dispatch(getFullData(true));
