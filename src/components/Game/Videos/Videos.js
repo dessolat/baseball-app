da1bf4162d61a1a623c95fa5423cc028def48e9c
Igor Nikiforov <desola2@inbox.ru>
2022-04-23 23:35:03 +0300

    feat: import PlaysEvents comp

--- src/components/Game/Videos/Videos.js
+++ src/components/Game/Videos/Videos.js
@@ -1,12 +1,19 @@
+import useCurrentEvents from 'hooks/useCurrentEvents';
 import React from 'react';
+import PlaysEvents from '../PlaysEvents/PlaysEvents';
 import Video from '../Video/Video';
 import cl from './Videos.module.scss';
 
 const Videos = () => {
   return (
-    <div className={cl.wrapper}>
-      <Video />
-    </div>
+    <>
+      <div className={cl.wrapper}>
+        <Video />
+      </div>
+      <div className={cl.eventsWrapper}>
+        <PlaysEvents moments={useCurrentEvents()} />
+      </div>
+    </>
   );
 };
 
