95eadf37f6bbab3290fd1a9451e56fb7ae160d7a
Igor Nikiforov <desola2@inbox.ru>
2022-04-24 21:07:47 +0300

    feat: add left arrow comp

--- src/components/Game/PlaysStats/PlaysStats.js
+++ src/components/Game/PlaysStats/PlaysStats.js
@@ -1,12 +1,16 @@
+import Arrow from 'components/UI/buttons/Arrow/Arrow';
 import React from 'react';
 import cl from './PlaysStats.module.scss';
 import PlaysStatsField from './PlaysStatsField';
 import PlaysStatsInfo from './PlaysStatsInfo';
 
-const PlaysStats = ({ hit }) => (
+const PlaysStats = ({ hit, setHittingMode }) => (
   <div className={cl.stats}>
     <PlaysStatsField hit={hit} />
     <PlaysStatsInfo hit={hit} />
+    <div className={cl.arrowWrapper}>
+      <Arrow onClick={() => setHittingMode('Bat')} />
+    </div>
   </div>
 );
 
