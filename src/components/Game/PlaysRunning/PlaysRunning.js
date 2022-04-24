6c3248068b020337a943109373abae30c054b1ef
Igor Nikiforov <desola2@inbox.ru>
2022-04-24 21:03:36 +0300

    feat: add runningMode state

--- src/components/Game/PlaysRunning/PlaysRunning.js
+++ src/components/Game/PlaysRunning/PlaysRunning.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import PlaysRunningField from './PlaysRunningField';
 import PlaysRunningInfo from './PlaysRunningInfo';
 
 const PlaysRunning = () => {
-	const currentMoment = useSelector(state => state.game.currentMoment)
+  const [runningMode, setRunningMode] = useState('Field');
+
+  const currentMoment = useSelector(state => state.game.currentMoment);
   return (
     <>
       <PlaysRunningField field={currentMoment.metering?.field} />
