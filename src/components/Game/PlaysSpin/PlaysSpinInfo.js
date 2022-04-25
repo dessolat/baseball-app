c835cb9ff9eaedd0be4ded72b7787e004ded4a65
Igor Nikiforov <desola2@inbox.ru>
2022-04-25 22:56:00 +0300

    feat: divide into divs
    * add spinInfo class

--- src/components/Game/PlaysSpin/PlaysSpinInfo.js
+++ src/components/Game/PlaysSpin/PlaysSpinInfo.js
@@ -3,19 +3,25 @@ import cl from './PlaysSpin.module.scss';
 
 const PlaysSpinInfo = ({ pitch }) => {
   const { offset_x, offset_y, rotation } = pitch || {};
-	
+
   const trueSpinValue = rotation ? rotation.toFixed(2) + ' rpm' : ' ';
   const verticalBreakValue = offset_y ? (offset_y * 100).toFixed(2) + ' cm' : ' ';
   const horizontalBreakValue = offset_x ? (offset_x * 100).toFixed(2) + ' cm' : ' ';
 
   return (
-    <div>
-      <p className={cl.subHeader}>True spin</p>
-      <p className={cl.regularValue}>{trueSpinValue}</p>
-      <p className={cl.subHeader}>Vertical break</p>
-      <p className={cl.breakValue}>{verticalBreakValue}</p>
-      <p className={cl.subHeader}>Horizontal break</p>
-      <p className={cl.breakValue}>{horizontalBreakValue}</p>
+    <div className={cl.spinInfo}>
+      <div>
+        <p className={cl.subHeader}>True spin</p>
+        <p className={cl.regularValue}>{trueSpinValue}</p>
+      </div>
+      <div>
+        <p className={cl.subHeader}>Vertical break</p>
+        <p className={cl.breakValue}>{verticalBreakValue}</p>
+      </div>
+      <div>
+        <p className={cl.subHeader}>Horizontal break</p>
+        <p className={cl.breakValue}>{horizontalBreakValue}</p>
+      </div>
     </div>
   );
 };
