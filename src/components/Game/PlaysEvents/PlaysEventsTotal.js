4660bbf0a2a373128ab63de99757e3a40ef71b6b
Igor Nikiforov <desola2@inbox.ru>
2022-04-28 11:35:47 +0300

    feat: add pitcher field

--- src/components/Game/PlaysEvents/PlaysEventsTotal.js
+++ src/components/Game/PlaysEvents/PlaysEventsTotal.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import ContentPitcher from '../ContentPitcher/ContentPitcher';
 import cl from './PlaysEvents.module.scss';
 
-const PlaysEventsTotal = ({moments}) => {
-	const isTotal = moments.length > 0;
-	const { balls_count, strikes_count } = (isTotal && moments.slice(-1)[0].pitcher) || '';
+const PlaysEventsTotal = ({ moments }) => {
+  const isVideo = useSelector(state => state.game.isVideo);
+
+  const isTotal = moments.length > 0;
+  const { balls_count, strikes_count } = (isTotal && moments.slice(-1)[0].pitcher) || '';
 
   return (
     <p className={cl.total}>
       {isTotal && (
         <>
+          {!isVideo && (
+            <div className={cl.pitcherWrapper}>
+              <ContentPitcher />
+            </div>
+          )}
           Total:{' '}
           <span>
             {balls_count + strikes_count} ({balls_count} ball, {strikes_count} strike)
