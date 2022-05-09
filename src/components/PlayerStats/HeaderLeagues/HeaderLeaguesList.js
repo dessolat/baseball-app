ed3df6ccaaecf410476d0cf5fc7435dce74317e0
Igor Nikiforov <desola2@inbox.ru>
2022-05-09 19:41:20 +0300

    feat: change handleLeagueClick func

--- src/components/PlayerStats/HeaderLeagues/HeaderLeaguesList.js
+++ src/components/PlayerStats/HeaderLeagues/HeaderLeaguesList.js
@@ -3,14 +3,41 @@ import cl from './HeaderLeagues.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentLeague } from 'redux/sharedReducer';
 import HeaderLeaguesListItem from './HeaderLeaguesListItem';
+import { setPlayerCurrentTeam } from 'redux/playerStatsReducer';
 
 const HeaderLeaguesList = ({ leagues, playerYears }, ref) => {
   const currentLeague = useSelector(state => state.shared.currentLeague);
+	const statsData = useSelector(state => state.playerStats.playerStatsData);
   const dispatch = useDispatch();
 
   const handleLeagueClick = league => () => {
     if (playerYears === 'All years') return;
     dispatch(setCurrentLeague(league));
+
+		if (league.id === -1) {
+			const teamsArr = Array.from(
+				statsData.leagues
+					.filter(league => league.year === playerYears)
+					.reduce((sum, league) => {
+						league.teams.forEach(team => sum.add(team.name));
+						return sum;
+					}, new Set())
+			);
+
+			dispatch(setPlayerCurrentTeam(teamsArr.length > 1 ? 'All teams' : teamsArr[0]))
+			return
+		}
+		
+		dispatch(setPlayerCurrentTeam(league.teams.length > 1 ? 'All teams' : league.teams[0].name))
+		// const teamsArr = Array.from(
+		// 	statsData.leagues
+		// 		.filter(league => league.year === playerYears)
+		// 		.reduce((sum, league) => {
+		// 			league.teams.forEach(team => sum.add(team.name));
+		// 			return sum;
+		// 		}, new Set())
+		// );
+		// dispatch(setPlayerCurrentTeam(league.teams ? ))
 		// league.teams && dispatch(setPlayerCurrentTeam(league.teams[0].name))
   };
 
