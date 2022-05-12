8a480d7ca53d6e4e6eea785439aae6ba24ba939d
Igor Nikiforov <desola2@inbox.ru>
2022-05-12 19:12:45 +0300

    refactor: remove commented code

--- src/components/Game/ContentBox/ContentBoxTableFooter.js
+++ src/components/Game/ContentBox/ContentBoxTableFooter.js
@@ -1,15 +1,6 @@
 import React from 'react';
 
 const ContentBoxTableFooter = ({ TABLES_INFO, tableName, tableData, toFixList }) => {
-  // const getFieldSum = (table, field) =>
-  //   table === 'fielding'
-  //     ? tableData.players_stats.reduce((sum, cur) => sum + Number(cur.content.stat[table][field]), 0)
-  //     : table === 'catching'
-  //     ? tableData.players_stats
-  //         .filter(player => player.is_catcher)
-  //         .reduce((sum, cur) => sum + Number(cur.content.stat['running'][field]), 0)
-  //     : tableData.players_stats.reduce((sum, cur) => sum + Number(cur.content.stat[table][field]), 0);
-
   return (
     <tfoot>
       <tr
@@ -24,31 +15,18 @@ const ContentBoxTableFooter = ({ TABLES_INFO, tableName, tableData, toFixList })
         <td>TOTALS</td>
         {TABLES_INFO[tableName].headers.map((title, i) => (
           <td key={i}>
-            {
-						// title === 'PB'
-            //   ? '--'
-            //   : 
-							title === 'POS'
+            {title === 'POS'
               ? ' '
               : title === 'SB_pr'
-              ? // ? getFieldSum('running', title).toFixed(3)
-                tableData.total_stats.running.SB_pr
+              ? tableData.total_stats.running.SB_pr
               : title === 'FLD'
-              ? // ? getFieldSum('fielding', title).toFixed(3)
-                Number(tableData.total_stats.fielding.FLD).toFixed(3)
+              ? Number(tableData.total_stats.fielding.FLD).toFixed(3)
               : ['CH', 'PO', 'A', 'E', 'DP'].includes(title)
-              ? // ? getFieldSum('fielding', title)
-                tableData.total_stats.fielding[title]
+              ? tableData.total_stats.fielding[title]
               : toFixList.includes(title)
-              ? // ? getFieldSum(tableName, title).toFixed(3)
-                Number(tableData.total_stats[tableName][title]).toFixed(3)
-              : // : getFieldSum(
-              //     ['SB', 'CS', 'LOB'].includes(title) && tableName !== 'catching' ? 'running' : tableName,
-              //     title
-              //   )
-              // ['SB', 'CS', 'LOB'].includes(title) && tableName !== 'catching'
-              ['SB', 'CS', 'LOB', 'PB'].includes(title)
-              ? tableData.total_stats[tableName==='batting' ? 'running' : 'catching'][title]
+              ? Number(tableData.total_stats[tableName][title]).toFixed(3)
+              : ['SB', 'CS', 'LOB', 'PB'].includes(title)
+              ? tableData.total_stats[tableName === 'batting' ? 'running' : 'catching'][title]
               : tableData.total_stats[tableName][title]}
           </td>
         ))}
