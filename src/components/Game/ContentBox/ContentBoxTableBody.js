5b581a2508a5704b0ddb84874b25f5da88c93d61
Igor Nikiforov <desola2@inbox.ru>
2022-05-18 10:20:59 +0300

    feat: replace -1 values to '—'

--- src/components/Game/ContentBox/ContentBoxTableBody.js
+++ src/components/Game/ContentBox/ContentBoxTableBody.js
@@ -30,21 +30,25 @@ const ContentBoxTableBody = ({ TABLES_INFO, tableName, tableData, toFixList }) =
               <td style={player.is_substituted && tableName === 'batting' ? { paddingLeft: '2.5rem' } : null}>
                 <Link to={`/stats/player/${player.id}`}>{player.content.player_name}</Link>
               </td>
-              {TABLES_INFO[tableName].headers.map((title, i) => (
-                <td key={i} style={toFixList.includes(title) ? { width: '3rem' } : null}>
-                  {title === 'POS'
-                    ? player.content.position.join('/')
-                    : ['SB', 'CS', 'SB_pr', 'LOB', 'PB'].includes(title)
-                    ? player.content.stat[tableName === 'batting' ? 'running' : 'catching'][title]
-                    : ['CH', 'PO', 'A', 'E', 'DP', 'FLD'].includes(title)
-                    ? player.content.stat.fielding[title]
-                    : toFixList.includes(title)
-                    ? player.content.stat[tableName][title] === 'INF'
-                      ? 'INF'
-                      : Number(player.content.stat[tableName][title]).toFixed(3)
-                    : player.content.stat[tableName][title]}
-                </td>
-              ))}
+              {TABLES_INFO[tableName].headers.map((title, i) => {
+								const value = title === 'POS'
+								? player.content.position.join('/')
+								: ['SB', 'CS', 'SB_pr', 'LOB', 'PB'].includes(title)
+								? player.content.stat[tableName === 'batting' ? 'running' : 'catching'][title]
+								: ['CH', 'PO', 'A', 'E', 'DP', 'FLD'].includes(title)
+								? player.content.stat.fielding[title]
+								: toFixList.includes(title)
+								? player.content.stat[tableName][title] === 'INF'
+									? 'INF'
+									: Number(player.content.stat[tableName][title]).toFixed(3)
+								: player.content.stat[tableName][title]
+
+                return (
+                  <td key={i} style={toFixList.includes(title) ? { width: '3rem' } : null}>
+                    {value !== -1 ? value : '—'}
+                  </td>
+                );
+              })}
             </tr>
           );
         })}
