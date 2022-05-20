13a2bb8ade2c66cd57a49d18b2c064bdbef2b8e1
Igor Nikiforov <desola2@inbox.ru>
2022-05-20 13:51:03 +0300

    feat: add availableDates

--- src/components/Games/ContentCalendar/ContentCalendar.js
+++ src/components/Games/ContentCalendar/ContentCalendar.js
@@ -1,5 +1,5 @@
 import Arrow from 'components/UI/buttons/Arrow/Arrow';
-import React, { useEffect,useLayoutEffect, useRef } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import cl from './ContentCalendar.module.scss';
 import ContentCalendarList from './ContentCalendarList';
@@ -10,6 +10,12 @@ const ContentCalendar = ({ onChange, calendarScroll }) => {
 
   const currentDate = useSelector(state => state.shared.currentDate);
   const isMobile = useSelector(state => state.shared.isMobile);
+  const games = useSelector(state => state.games.games);
+  const currentLeague = useSelector(state => state.games.currentLeague);
+  const currentGameType = useSelector(state => state.shared.currentGameType);
+  const currentStadium = useSelector(state => state.games.currentStadium);
+  const currentHome = useSelector(state => state.games.currentHome);
+  const currentGuests = useSelector(state => state.games.currentGuests);
 
   useLayoutEffect(() => {
     ref.current.style.transition = 'none';
@@ -44,10 +50,34 @@ const ContentCalendar = ({ onChange, calendarScroll }) => {
     newDate.setDate(newDate.getDate() + (dir === 'right' ? 1 : -1));
     handleDateClick(newDate)();
   };
+
+  let availableDates = useMemo(() => {
+    const filteredGames = games.filter(
+      game =>
+        (currentStadium !== 'All' ? game.stadium_name === currentStadium : true) &&
+        (currentLeague.id !== -1
+          ? game.league_id === currentLeague.id
+          : currentGameType === game.game_type) &&
+        (currentHome !== 'All' ? game.owners_name === currentHome : true) &&
+        (currentGuests !== 'All' ? game.guests_name === currentGuests : true)
+    );
+
+    const sortedDates = filteredGames
+      .reduce((sum, game) => {
+        sum.push(game.date);
+        return sum;
+      }, [])
+      .sort((a, b) => (a > b ? 1 : -1));
+
+		const uniqueSortedDates = Array.from(new Set(sortedDates))
+
+    return uniqueSortedDates
+  }, [games, currentLeague, currentGameType, currentStadium, currentHome, currentGuests]);
+
   return (
     <div className={cl.calendar}>
       <Arrow onClick={handleArrowClick('left')} style={{ marginRight: isMobile ? 0 : '.3rem' }} />
-      <ContentCalendarList currentDate={currentDate} handleClick={handleDateClick} ref={ref} />
+      <ContentCalendarList currentDate={currentDate} handleClick={handleDateClick} availableDates={availableDates} ref={ref} />
       <Arrow
         direction='right'
         onClick={handleArrowClick('right')}
--- src/components/Games/ContentCalendar/ContentCalendarList.js
+++ src/components/Games/ContentCalendar/ContentCalendarList.js
@@ -2,18 +2,13 @@ import React, { forwardRef } from 'react';
 import cl from './ContentCalendar.module.scss';
 import ContentCalendarListItem from './ContentCalendarListItem';
 
-const ContentCalendarList = ({ currentDate, handleClick }, ref) => {
+const ContentCalendarList = ({ currentDate, handleClick, availableDates }, ref) => {
   const dates = [];
   for (let i = 0; i < 15; i++) {
     const tempDate = new Date(currentDate);
-		// console.log(tempDate);
-		// tempDate.setHours(0, tempDate.getHours() + tempDate.getTimezoneOffset() * -1, 0, 0);
-		// console.log(tempDate);
     tempDate.setDate(tempDate.getDate() - 6 + i);
     dates.push(tempDate);
   }
-	// console.log(dates);
-	// console.log(currentDate.toDateString());
 
   return (
     <div className={cl.wrapper}>
