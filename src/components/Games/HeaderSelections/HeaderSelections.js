fd12d5a2518220cdae99031df306f8c61b2a3811
Igor Nikiforov <desola2@inbox.ru>
2022-05-21 17:13:01 +0300

    feat: change tempDate to currentDate

--- src/components/Games/HeaderSelections/HeaderSelections.js
+++ src/components/Games/HeaderSelections/HeaderSelections.js
@@ -12,10 +12,11 @@ const HeaderSelections = () => {
   const dispatch = useDispatch();
 
   const handleClick = option => {
-    const tempDate = new Date(option, 0, 1);
-    tempDate.setHours(0, tempDate.getTimezoneOffset() * -1, 0, 0);
+    // const tempDate = new Date(option, 0, 1);
+    // tempDate.setHours(0, tempDate.getTimezoneOffset() * -1, 0, 0);
 
-    dispatch(setCurrentDate(tempDate));
+    dispatch(setCurrentDate(new Date()));
+    // dispatch(setCurrentDate(tempDate));
     dispatch(setCurrentYear(option));
   };
 
