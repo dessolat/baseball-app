948c0fe574a14d64b511cb3b5aecd84153c8f465
Igor Nikiforov <desola2@inbox.ru>
2022-05-30 21:29:41 +0300

    fix: mobile links

--- src/components/TeamGames/ContentMobileHeader/ContentMobileHeader.js
+++ src/components/TeamGames/ContentMobileHeader/ContentMobileHeader.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 import ContentHeaderModeLinks from './ContentHeaderModeLinks';
 import cl from './ContentMobileHeader.module.scss';
 
 const ContentMobileHeader = () => {
+  const { teamName } = useParams();
+
+  const mobileTableMode = useSelector(state => state.teamGames.mobileTableMode);
+
+  const statsLink =
+    mobileTableMode === 'Calendar' ? (
+      <Link to={`/stats/team`}>Go to Team Stat</Link>
+    ) : (
+      <Link to={`/stats/player?team=${teamName}`}>Go to Player Stat</Link>
+    );
   return (
     <div className={cl.mobileHeader}>
       <ContentHeaderModeLinks />
-			<div className={cl.linkWrapper}>
-        <Link to='/stats/player'>Go to Player Stat</Link>
-      </div>
+      <div className={cl.linkWrapper}>{statsLink}</div>
     </div>
   );
 };
