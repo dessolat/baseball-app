7ebf0a843855fdc4fbc24b8a5d3d0b7d4838b04f
Igor Nikiforov <desola2@inbox.ru>
2022-06-01 21:15:40 +0300

    feat: set mode param on click

--- src/components/Games/ContentTable/ContentTableModeLinks.js
+++ src/components/Games/ContentTable/ContentTableModeLinks.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setMobileTableMode } from 'redux/gamesReducer';
+import { setSearchParam } from 'utils';
 import cl from './ContentTable.module.scss';
 
 const ContentTableModeLinks = () => {
-	const mobileTableMode = useSelector(state => state.games.mobileTableMode)
-	const dispatch = useDispatch()
+  const mobileTableMode = useSelector(state => state.games.mobileTableMode);
+  const dispatch = useDispatch();
 
-	const handleModeClick = mode => () => dispatch(setMobileTableMode(mode))
-	return (
-		<div className={cl.tableModes}>
-			{mobileTableMode !== 'Team tablo' && <button onClick={handleModeClick('Team tablo')}>Team tablo</button>}
-			{mobileTableMode !== 'Calendar' && <button onClick={handleModeClick('Calendar')}>Calendar</button>}
-			{mobileTableMode !== 'Leaders' && <button onClick={handleModeClick('Leaders')}>Leaders</button>}
-		</div>
-	)
-}
+  const handleModeClick = mode => () => {
+    dispatch(setMobileTableMode(mode));
+		setSearchParam('mode', mode)
+  };
+  return (
+    <div className={cl.tableModes}>
+      {mobileTableMode !== 'Team tablo' && (
+        <button onClick={handleModeClick('Team tablo')}>Team tablo</button>
+      )}
+      {mobileTableMode !== 'Calendar' && <button onClick={handleModeClick('Calendar')}>Calendar</button>}
+      {mobileTableMode !== 'Leaders' && <button onClick={handleModeClick('Leaders')}>Leaders</button>}
+    </div>
+  );
+};
 
-export default ContentTableModeLinks
+export default ContentTableModeLinks;
