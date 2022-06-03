f9d111a4e4bbd902a4193ba789ba86b221d7514d
Igor Nikiforov <desola2@inbox.ru>
2022-06-03 19:22:19 +0300

    feat: check league id to render side tables

--- src/components/Games/Content/Content.js
+++ src/components/Games/Content/Content.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ContentSideTables from '../ContentSideTables/ContentSideTables';
 import ContentTable from '../ContentTable/ContentTable';
+import { useSelector } from 'react-redux';
 import cl from './Content.module.scss';
 
 const Content = ({ games }) => {
+  const currentLeague = useSelector(state => state.games.currentLeague);
+
   return (
     <section>
       <div className='container'>
         <div className={cl.content}>
           <ContentTable games={games} />
-          <ContentSideTables />
+          {currentLeague.id !== -1 && <ContentSideTables />}
         </div>
       </div>
     </section>
