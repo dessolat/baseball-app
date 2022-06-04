68ba2b80df6faf4a1f447adc9e534d72193ad3b7
dessolat <desola2@inbox.ru>
2022-06-04 19:15:03 +0300

    feat: merge psctTable & switchTable

--- src/components/Games/ContentSideTables/ContentSideTables.js
+++ src/components/Games/ContentSideTables/ContentSideTables.js
@@ -12,11 +12,19 @@ const ContentSideTables = () => {
   const isTables = currentLeague.id !== -1;
   return (
     // <div className={cl.side} style={{ maxHeight: '70vh' }}>
-    <div className={cl.side} style={!isTables ? { height: isMobile ? 'auto' : '70vh' } : { maxHeight: isMobile ? 'auto' : '75vh' }}>
+    <div
+      className={cl.side}
+      style={!isTables ? { height: isMobile ? 'auto' : '70vh' } : { maxHeight: isMobile ? 'auto' : '75vh' }}>
       {isTables && (
         <>
-          {(!isMobile || mobileTableMode === 'Team tablo') && <PctTable currentLeague={currentLeague} />}
-          {(!isMobile || mobileTableMode === 'Leaders') && <SwitchTable />}
+          {(!isMobile || mobileTableMode === 'Team tablo/Leader') && (
+            <>
+              <PctTable currentLeague={currentLeague} />
+              <SwitchTable />
+            </>
+          )}
+          {/* {(!isMobile || mobileTableMode === 'Team tablo') && <PctTable currentLeague={currentLeague} />}
+          {(!isMobile || mobileTableMode === 'Leaders') && <SwitchTable />} */}
         </>
       )}
     </div>
