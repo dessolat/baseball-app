b562b6780992485b4ea74d41ec3b43a48280882c
Igor Nikiforov <desola2@inbox.ru>
2022-06-05 20:02:49 +0300

    feat: add mobile handling

--- src/components/Stats/ContentPlayerTable/ContentPlayerFilterField.js
+++ src/components/Stats/ContentPlayerTable/ContentPlayerFilterField.js
@@ -1,22 +1,31 @@
 import React, { useState, useRef } from 'react';
 import cl from './ContentPlayerFilterField.module.scss';
 
-const ContentPlayerFilterField = ({setPlayerFilter}) => {
+const ContentPlayerFilterField = ({ setPlayerFilter, mobile = false }) => {
   const [tempPlayerFilter, setTempPlayerFilter] = useState('');
 
-	const filterTimeoutRef = useRef();
+  const filterTimeoutRef = useRef();
 
-	const handleFilterFieldChange = e => {
-		const value = e.target.value;
-		setTempPlayerFilter(value)
+  const handleFilterFieldChange = e => {
+    const value = e.target.value;
+    setTempPlayerFilter(value);
     clearTimeout(filterTimeoutRef.current);
 
     filterTimeoutRef.current = setTimeout(() => {
       setPlayerFilter(value);
     }, 1000);
   };
+
+	const styles = [cl.filterField]
+	mobile && styles.push(cl.mobile)
   return (
-    <input className={cl.filterField} value={tempPlayerFilter} onChange={handleFilterFieldChange}></input>
+    <input
+      className={styles.join(' ')}
+      value={tempPlayerFilter}
+      onChange={handleFilterFieldChange}
+      placeholder={'Search of player'}>
+				{/* <img src={filterIcon} /> */}
+			</input>
   );
 };
 
