7a2867f6d4996e899c4a594d292f8807b3bc2071
Igor Nikiforov <desola2@inbox.ru>
2022-06-20 09:44:08 +0300

    feat: add PlayerFilterField for mobile landscape

--- src/components/Stats/HeaderSelections/HeaderSelections.js
+++ src/components/Stats/HeaderSelections/HeaderSelections.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentYear, setCurrentDate, setCurrentGameType } from 'redux/sharedReducer';
 import { Link, useParams } from 'react-router-dom';
 import { setTableMode } from 'redux/statsReducer';
+import ContentPlayerFilterField from '../ContentPlayerTable/ContentPlayerFilterField';
 
 const YEARS = [2022, 2021, 2020];
 const TABLE_MODES = ['Batting', 'Fielding / Running', 'Pitching'];
@@ -50,6 +51,9 @@ const HeaderSelections = () => {
           />
         </div>
       </div>
+			<div className={cl.filterFieldWrapper}>
+					<ContentPlayerFilterField />
+				</div>
       <div className={cl.bottom}>
         <div className={cl.batting}>
           <Dropdown
