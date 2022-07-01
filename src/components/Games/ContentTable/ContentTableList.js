eb16c4173328397c19b1a55fc9bc05463527ee6a
Igor Nikiforov <desola2@inbox.ru>
2022-07-01 14:00:39 +0300

    refactor: create ContentTableList comp
    * move code from ContentTable comp

---
+++ src/components/Games/ContentTable/ContentTableList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { forwardRef } from 'react';
+import ContentTableListItem from './ContentTableListItem';
+
+const ContentTableList = ({ cl, filteredData }, ref) => (
+  <ul className={cl.rows}>
+    {filteredData.map((game, index, arr) => (
+      <ContentTableListItem game={game} index={index} arr={arr} cl={cl} ref={ref} />
+    ))}
+  </ul>
+);
+export default forwardRef(ContentTableList);
