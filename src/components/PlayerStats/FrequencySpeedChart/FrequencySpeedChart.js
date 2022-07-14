117481cb76c5471030543fd531476b593c9cd87b
Igor Nikiforov <desola2@inbox.ru>
2022-07-14 23:24:47 +0300

    feat: create FrequencySpeedChart empty comp

---
+++ src/components/PlayerStats/FrequencySpeedChart/FrequencySpeedChart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './FrequencySpeedChart.module.scss';
+
+const FrequencySpeedChart = () => {
+	return (
+		<div>FrequencySpeedChart</div>
+	)
+}
+
+export default FrequencySpeedChart
