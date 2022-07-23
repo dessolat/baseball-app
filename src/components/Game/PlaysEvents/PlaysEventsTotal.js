d3784e375807c1fd6ebd1955178d100c65d22a80
Igor Nikiforov <desola2@inbox.ru>
2022-07-23 18:11:53 +0300

    fix: show pitcher when moments are empty

--- src/components/Game/PlaysEvents/PlaysEventsTotal.js
+++ src/components/Game/PlaysEvents/PlaysEventsTotal.js
@@ -5,26 +5,25 @@ import cl from './PlaysEvents.module.scss';
 
 const PlaysEventsTotal = ({ moments }) => {
   const isVideo = useSelector(state => state.game.isVideo);
+  const currentCard = useSelector(state => state.game.currentCard);
 
-  const isTotal = moments.length > 0;
-  const { balls_count, strikes_count } = (isTotal && moments.slice(-1)[0].pitcher) || '';
+  // const isTotal = moments.length > 0;
+  const { balls_count, strikes_count } =
+    (moments.length > 0 ? moments.slice(-1)[0].pitcher : currentCard.moments?.slice(-1)[0].pitcher) || '';
+  // const { balls_count, strikes_count } = (isTotal && moments.slice(-1)[0].pitcher) || '';
 
   const totalStyles = !isVideo ? { borderRight: 'none' } : null;
   return (
     <div className={cl.total} style={totalStyles}>
-      {isTotal && (
-        <>
-          {!isVideo && (
-            <div className={cl.pitcherWrapper}>
-              <ContentPitcher />
-            </div>
-          )}
-          Total:{' '}
-          <span>
-            {balls_count + strikes_count} ({balls_count} ball, {strikes_count} strike)
-          </span>
-        </>
+      {!isVideo && (
+        <div className={cl.pitcherWrapper}>
+          <ContentPitcher />
+        </div>
       )}
+      Total:{' '}
+      <span>
+        {balls_count + strikes_count} ({balls_count} ball, {strikes_count} strike)
+      </span>
     </div>
   );
 };
