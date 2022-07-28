4b537090f389f62f7ddc53637ac949be08154db9
Igor Nikiforov <desola2@inbox.ru>
2022-07-28 23:10:40 +0300

    feat: add props

--- src/components/PlayerStats/ContentPitchingTable/ContentPitchingTable.js
+++ src/components/PlayerStats/ContentPitchingTable/ContentPitchingTable.js
@@ -4,13 +4,21 @@ import cl from './ContentPitchingTable.module.scss';
 import ContentPitchingTableBody from './ContentPitchingTableBody';
 import ContentPitchingTableHeader from './ContentPitchingTableHeader';
 
-const ContentPitchingTable = ({ filteredLeagues = [], filteredLeague, playerYears, MONTHS, handleLeagueClick }) => {
+const ContentPitchingTable = ({
+  filteredLeagues = [],
+  filteredLeague,
+  playerYears,
+  MONTHS,
+  handleLeagueClick,
+  getSortedTableOptions,
+  handleTableOptionClick
+}) => {
   const [sortField, setSortField] = useState('IP');
   const [sortDirection, setSortDirection] = useState('asc');
 
   const currentLeague = useSelector(state => state.games.currentLeague);
 
-	const handleFieldClick = field => () => {
+  const handleFieldClick = field => () => {
     sortField !== field ? setSortField(field) : setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
   };
   return (
@@ -21,8 +29,10 @@ const ContentPitchingTable = ({ filteredLeagues = [], filteredLeague, playerYear
           currentLeague={currentLeague}
           sortField={sortField}
           sortDirection={sortDirection}
-					handleFieldClick={handleFieldClick}
-					/>
+          handleFieldClick={handleFieldClick}
+          getSortedTableOptions={getSortedTableOptions}
+          handleTableOptionClick={handleTableOptionClick}
+        />
         <ContentPitchingTableBody
           filteredLeagues={filteredLeagues}
           filteredLeague={filteredLeague}
@@ -31,7 +41,7 @@ const ContentPitchingTable = ({ filteredLeagues = [], filteredLeague, playerYear
           sortField={sortField}
           sortDirection={sortDirection}
           MONTHS={MONTHS}
-					handleLeagueClick={handleLeagueClick}
+          handleLeagueClick={handleLeagueClick}
         />
       </div>
     </div>
