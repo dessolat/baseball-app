5fe7c642e2a87805e2a1c8a3b8c8cd945980959a
Igor Nikiforov <desola2@inbox.ru>
2022-08-01 12:37:21 +0300

    refactor: remove commented code

--- src/components/Game/PlayerFilterField/PlayerFilterField.js
+++ src/components/Game/PlayerFilterField/PlayerFilterField.js
@@ -1,37 +1,22 @@
 import React, { useState } from 'react';
 import { useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setFilteredCards, setPlayerCardFilter } from 'redux/gameReducer';
+import { useDispatch } from 'react-redux';
+import { setPlayerCardFilter } from 'redux/gameReducer';
 import cl from './PlayerFilterField.module.scss';
 
 const PlayerFilterField = () => {
   const [filterValue, setFilterValue] = useState('');
 
-  const filteredCards = useSelector(state => state.game.filteredCards);
   const dispatch = useDispatch();
 
   const filterTimeoutRef = useRef();
-  console.log(filteredCards);
+
   const changeHandler = e => {
     const value = e.target.value;
     setFilterValue(value);
     clearTimeout(filterTimeoutRef.current);
 
     filterTimeoutRef.current = setTimeout(() => {
-      // const filterArr = value.split(' ');
-
-      // const newCards =
-      //   value !== ''
-      //     ? filteredCards.filter(card => {
-      //         return filterArr.reduce((sum, word) => {
-      //           if (!(card.who.slice(0, word.length).toLowerCase() === word.toLowerCase())) {
-      //             sum = false;
-      //           }
-      //           return sum;
-      //         }, true);
-      //       })
-      //     : filteredCards;
-
       dispatch(setPlayerCardFilter(value));
     }, 400);
   };
