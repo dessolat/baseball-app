02c1e4597ef853e049112c13503339c168273ae9
Igor Nikiforov <desola2@inbox.ru>
2022-08-11 22:06:57 +0300

    feat: add fields to fieldsInfo

--- src/components/PlayerStats/ContentBattingTable/ContentBattingTable.js
+++ src/components/PlayerStats/ContentBattingTable/ContentBattingTable.js
@@ -5,35 +5,35 @@ import ContentBattingTableHeader from './ContentBattingTableHeader';
 import ContentBattingTableBody from './ContentBattingTableBody';
 
 const fieldsInfo = [
-	{ name: 'AB', type: 'batting', fixed: null, addedClass: cl.wide },
-	{ name: 'R', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: 'H', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: '2B', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: '3B', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: 'HR', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: 'RBI', type: 'batting', fixed: null, addedClass: null },
-	{ name: 'GDP', type: 'batting', fixed: null, addedClass: cl.wide },
-	{ name: 'BB', type: 'batting', fixed: null, addedClass: null },
-	{ name: 'IBB', type: 'batting', fixed: null, addedClass: null },
-	{ name: 'HP', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: 'SH', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: 'SF', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: 'SO', type: 'batting', fixed: null, addedClass: cl.tall },
-	{ name: 'TB', type: 'batting', fixed: null, addedClass: cl.wide },
-	{ name: 'AVG', type: 'batting', fixed: 3, addedClass: cl.wider },
-	{ name: 'SLG', type: 'batting', fixed: 3, addedClass: cl.wider },
-	{ name: 'OBP', type: 'batting', fixed: 3, addedClass: cl.wider },
-	{ name: 'OPS', type: 'batting', fixed: 3, addedClass: cl.wider },
-	{ name: 'SB', type: 'running', fixed: null, addedClass: cl.tall },
-	{ name: 'CS', type: 'running', fixed: null, addedClass: cl.tall },
-	{ name: 'SB_pr', type: 'running', fixed: null, addedClass: cl.wider },
-	{ name: 'LOB', type: 'running', fixed: null, addedClass: null },
-	{ name: 'CH', type: 'fielding', fixed: null, addedClass: cl.wide },
-	{ name: 'PO', type: 'fielding', fixed: null, addedClass: null },
-	{ name: 'A', type: 'fielding', fixed: null, addedClass: null },
-	{ name: 'E', type: 'fielding', fixed: null, addedClass: cl.tall },
-	{ name: 'DP', type: 'fielding', fixed: null, addedClass: cl.tall },
-	{ name: 'FLD', type: 'fielding', fixed: 3, addedClass: cl.wider }
+  { name: 'AB', type: 'batting', fixed: null, addedClass: cl.wide },
+  { name: 'R', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: 'H', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: '2B', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: '3B', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: 'HR', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: 'RBI', type: 'batting', fixed: null, addedClass: null },
+  { name: 'GDP', type: 'batting', fixed: null, addedClass: cl.wide },
+  { name: 'BB', type: 'batting', fixed: null, addedClass: null },
+  { name: 'IBB', type: 'batting', fixed: null, addedClass: null },
+  { name: 'HP', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: 'SH', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: 'SF', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: 'SO', type: 'batting', fixed: null, addedClass: cl.tall },
+  { name: 'TB', type: 'batting', fixed: null, addedClass: cl.wide },
+  { name: 'AVG', type: 'batting', fixed: 3, addedClass: cl.wider },
+  { name: 'SLG', type: 'batting', fixed: 3, addedClass: cl.wider },
+  { name: 'OBP', type: 'batting', fixed: 3, addedClass: cl.wider },
+  { name: 'OPS', type: 'batting', fixed: 3, addedClass: cl.wider },
+  { name: 'SB', type: 'running', fixed: null, addedClass: cl.tall, headerWrapped: true },
+  { name: 'CS', type: 'running', fixed: null, addedClass: cl.tall },
+  { name: 'SB_pr', type: 'running', fixed: null, addedClass: cl.wider, childField: '%SB' },
+  { name: 'LOB', type: 'running', fixed: null, addedClass: null },
+  { name: 'CH', type: 'fielding', fixed: null, addedClass: cl.wide, headerWrapped: true },
+  { name: 'PO', type: 'fielding', fixed: null, addedClass: null, headerWrapped: true },
+  { name: 'A', type: 'fielding', fixed: null, addedClass: null },
+  { name: 'E', type: 'fielding', fixed: null, addedClass: cl.tall },
+  { name: 'DP', type: 'fielding', fixed: null, addedClass: cl.tall },
+  { name: 'FLD', type: 'fielding', fixed: 3, addedClass: cl.wider, childField: 'FLD%' }
 ];
 
 const ContentBattingTable = ({
@@ -66,19 +66,19 @@ const ContentBattingTable = ({
           sortField={sortField}
           sortDirection={sortDirection}
           handleFieldClick={handleFieldClick}
-					fieldsInfo={fieldsInfo}
+          fieldsInfo={fieldsInfo}
         />
         <ContentBattingTableBody
           cl={cl}
           currentLeague={currentLeague}
           playerYears={playerYears}
           filteredLeague={filteredLeague}
-					filteredLeagues={filteredLeagues}
+          filteredLeagues={filteredLeagues}
           sortField={sortField}
-					sortDirection={sortDirection}
-					handleLeagueClick={handleLeagueClick}
-					MONTHS={MONTHS}
-					fieldsInfo={fieldsInfo}
+          sortDirection={sortDirection}
+          handleLeagueClick={handleLeagueClick}
+          MONTHS={MONTHS}
+          fieldsInfo={fieldsInfo}
         />
       </div>
     </div>
