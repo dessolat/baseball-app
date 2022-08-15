cd399706edb2315f8f0d8033a06aeef1d0e38782
Igor Nikiforov <desola2@inbox.ru>
2022-08-15 22:19:27 +0300

    rename: comp

---
+++ src/components/PlayerStats/ContentMobileTable/ContentMobileTableHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContentMobileTableHeader = () => {
+	return (
+		<div>ContentMobileTableHeader</div>
+	)
+}
+
+export default ContentMobileTableHeader
--- src/components/PlayerStats/ContentMobileTable/ContentMovileTableHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ContentMovileTableHeader = () => {
-	return (
-		<div>ContentMovileTableHeader</div>
-	)
-}
-
-export default ContentMovileTableHeader
