1fc428a4ac92ecdca777563659ce8f98b2319b97
Igor Nikiforov <desola2@inbox.ru>
2022-08-19 18:32:17 +0300

    refactor: remove commented code

--- src/components/Game/ContentBoxFooter/ContentBoxFooter.js
+++ src/components/Game/ContentBoxFooter/ContentBoxFooter.js
@@ -2,8 +2,6 @@ import React from 'react';
 import cl from './ContentBoxFooter.module.scss';
 
 const ContentBoxFooter = ({ footer }) => {
-  // const { location, stadium, weather, att, t, umpires, scorers, tc } = footer || {};
-
   const footerArr = footer.split('\n');
   const columnsCount = Math.ceil(footerArr.length / 6);
   const columnsArr = new Array(columnsCount).fill('', 0, columnsCount);
@@ -24,23 +22,6 @@ const ContentBoxFooter = ({ footer }) => {
             </div>
           );
         })}
-
-        {/* <div>
-          <p>Location: <span>{location}</span></p>
-          <p>Stadium: <span>{stadium}</span></p>
-          <p>Weather: <span>{weather}</span></p>
-          <p>Att: <span>{att}</span></p>
-          <p>Time: <span>{t}</span></p>
-          <p>HP Umpire: <span>{umpires ? umpires[0] : ' '}</span></p>
-        </div>
-        <div>
-          <p>1B Umpire: <span>{umpires ? umpires[1] : ' '}</span></p>
-          <p>Scorer: <span>{scorers ? scorers[0] : ' '}</span></p>
-          <p>Scorer: <span>{scorers ? scorers[1] : ' '}</span></p>
-          <p>Scorer: <span>{scorers ? scorers[2] : ' '}</span></p>
-          <p>TC: <span>{tc ? tc[0] : ' '}</span></p>
-          <p>TC: <span>{tc ? tc[1] : ' '}</span></p>
-        </div> */}
       </div>
     </div>
   );
