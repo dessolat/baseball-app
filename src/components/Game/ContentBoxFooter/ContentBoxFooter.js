b524eb1ed0d9de40a790ecb00f961b46bb516438
Igor Nikiforov <desola2@inbox.ru>
2022-08-19 18:31:17 +0300

    feat: change box footer behavior

--- src/components/Game/ContentBoxFooter/ContentBoxFooter.js
+++ src/components/Game/ContentBoxFooter/ContentBoxFooter.js
@@ -2,12 +2,30 @@ import React from 'react';
 import cl from './ContentBoxFooter.module.scss';
 
 const ContentBoxFooter = ({ footer }) => {
-  const { location, stadium, weather, att, t, umpires, scorers, tc } = footer || {};
+  // const { location, stadium, weather, att, t, umpires, scorers, tc } = footer || {};
+
+  const footerArr = footer.split('\n');
+  const columnsCount = Math.ceil(footerArr.length / 6);
+  const columnsArr = new Array(columnsCount).fill('', 0, columnsCount);
 
   return (
     <div className={cl.footer}>
       <div className={cl.footerContainer}>
-        <div>
+        {columnsArr.map((_, i) => {
+          const rowsArr = [];
+          for (let j = 0; j < 6; j++) {
+            i * 6 + j < footerArr.length && rowsArr.push(null);
+          }
+          return (
+            <div key={i}>
+              {rowsArr.map((_, k) => (
+                <p key={k}>{footerArr[i * 6 + k]}</p>
+              ))}
+            </div>
+          );
+        })}
+
+        {/* <div>
           <p>Location: <span>{location}</span></p>
           <p>Stadium: <span>{stadium}</span></p>
           <p>Weather: <span>{weather}</span></p>
@@ -22,7 +40,7 @@ const ContentBoxFooter = ({ footer }) => {
           <p>Scorer: <span>{scorers ? scorers[2] : ' '}</span></p>
           <p>TC: <span>{tc ? tc[0] : ' '}</span></p>
           <p>TC: <span>{tc ? tc[1] : ' '}</span></p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
