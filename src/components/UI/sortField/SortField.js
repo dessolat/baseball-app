5ac6bc70a8047c166162f0bb45a21fa3cc2909ff
Igor Nikiforov <desola2@inbox.ru>
2022-08-20 21:15:59 +0300

    refactor: remove commented code

--- src/components/UI/sortField/SortField.js
+++ src/components/UI/sortField/SortField.js
@@ -23,14 +23,6 @@ const SortField = ({
         </div>
       )}
     </div>
-
-    // <div onClick={handleClick(renamedField || children)} className={cl.cell + ' ' + addedClass}>
-    //   {sortField === (renamedField || children) && <span className={cl.invisibleArrow}>↓</span>}
-    //   {children}
-    //   <span className={cl.arrow}>
-    //     {sortField === (renamedField || children) ? (sortDirection === 'asc' ? '↓' : '↑') : ''}
-    //   </span>
-    // </div>
   );
 };
 
