61d8e7926461b832b0d09f37e4a51cfe3a3fa5bc
Igor Nikiforov <desola2@inbox.ru>
2022-08-24 21:25:09 +0300

    feat: remove excessive logging

--- src/components/UI/loaders/ErrorLoader/ErrorLoader.js
+++ src/components/UI/loaders/ErrorLoader/ErrorLoader.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import cl from './ErrorLoader.module.scss';
 
-const ErrorLoader = ({ error, styles = null }) => {
-  console.log(error);
-  return (
-    <p className={cl.error} style={styles}>
-      {error}
-    </p>
-  );
-};
+const ErrorLoader = ({ error, styles = null }) => (
+  <p className={cl.error} style={styles}>
+    {error}
+  </p>
+);
 
 export default ErrorLoader;
