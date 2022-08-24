84873195d49256c39e48a746a654eb999eb5c612
Igor Nikiforov <desola2@inbox.ru>
2022-08-24 21:26:09 +0300

    refactor: beautify code

--- src/components/UI/loaders/Loader/Loader.js
+++ src/components/UI/loaders/Loader/Loader.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import cl from './Loader.module.scss';
 
-const Loader = ({ styles = null }) => {
-  return (
-    <div className={cl.loader} style={styles}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
-};
+const Loader = ({ styles = null }) => (
+  <div className={cl.loader} style={styles}>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 
 export default Loader;
