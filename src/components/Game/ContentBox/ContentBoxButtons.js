866f448805ebcc84680b6b9ddad914db5c97c369
Igor Nikiforov <desola2@inbox.ru>
2022-08-26 22:30:53 +0300

    refactor: remove commented code

--- src/components/Game/ContentBox/ContentBoxButtons.js
+++ src/components/Game/ContentBox/ContentBoxButtons.js
@@ -7,12 +7,6 @@ const ContentBoxButtons = () => {
     <div className={cl.buttons}>
       <ContentBoxButton team={'guests'} />
       <ContentBoxButton team={'owners'} />
-      {/* <span className={getClassName('guests')} onClick={handleButtonClick('guests')}>
-        {preview && getShortName(preview.guests.name, 8)}
-      </span>
-      <span className={getClassName('owners')} onClick={handleButtonClick('owners')}>
-        {preview && getShortName(preview.owners.name, 8)}
-      </span> */}
     </div>
   );
 };
