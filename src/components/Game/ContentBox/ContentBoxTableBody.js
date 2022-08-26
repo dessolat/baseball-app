4d356e1c73c6956200eda02941ca8f165ccd3b3b
Igor Nikiforov <desola2@inbox.ru>
2022-08-26 23:03:04 +0300

    feat: move code to ContentBoxTableBodyRow

--- src/components/Game/ContentBox/ContentBoxTableBody.js
+++ src/components/Game/ContentBox/ContentBoxTableBody.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import ContentBoxTableBodyRow from './ContentBoxTableBodyRow';
 
 const ContentBoxTableBody = ({ TABLES_INFO, tableName, tableData, toFixList }) => {
   let rowDelta = 0;
@@ -8,11 +8,11 @@ const ContentBoxTableBody = ({ TABLES_INFO, tableName, tableData, toFixList }) =
 
   tableName === 'pitching' &&
     tableData.pitchers_order.forEach((orderId, i) => {
-			const player =  orderedPlayersStats.find(player => player.id === orderId)
-      
-			if (player !== undefined) {
-				player.order = i + 1;
-			}
+      const player = orderedPlayersStats.find(player => player.id === orderId);
+
+      if (player !== undefined) {
+        player.order = i + 1;
+      }
     });
 
   return (
@@ -23,37 +23,18 @@ const ContentBoxTableBody = ({ TABLES_INFO, tableName, tableData, toFixList }) =
         )
         .sort((a, b) => (tableName === 'pitching' ? (a.order > b.order ? 1 : -1) : 0))
         .map((player, i) => {
-          if (player.is_substituted && tableName === 'batting') rowDelta++;
+					if (player.is_substituted && tableName === 'batting') rowDelta++;
+					
           return (
-            <tr key={i}>
-              <td>
-                {(player.is_substituted && tableName === 'batting') || tableName !== 'batting'
-                  ? ' '
-                  : i + 1 - rowDelta}
-              </td>
-              <td style={player.is_substituted && tableName === 'batting' ? { paddingLeft: '2.5rem' } : null}>
-                <Link to={`/stats/player/${player.id}`}>{player.content.name}</Link>
-              </td>
-              {TABLES_INFO[tableName].headers.map((title, i) => {
-								const value = title === 'POS'
-								? player.content.positions.join('/')
-								: ['SB', 'CS', 'SB_pr', 'LOB', 'PB'].includes(title)
-								? player.content.stats[tableName === 'batting' ? 'running' : 'catching'][title]
-								: ['CH', 'PO', 'A', 'E', 'DP', 'FLD'].includes(title)
-								? player.content.stats.fielding[title]
-								: toFixList.includes(title)
-								? player.content.stats[tableName][title] === 'Infinity'
-									? 'INF'
-									: Number(player.content.stats[tableName][title]).toFixed(3)
-								: player.content.stats[tableName][title]
-
-                return (
-                  <td key={i} style={toFixList.includes(title) ? { width: '3rem' } : null}>
-                    {Number(value) < 0 ? '—' : value}
-                  </td>
-                );
-              })}
-            </tr>
+            <ContentBoxTableBodyRow
+              key={i}
+              player={player}
+              TABLES_INFO={TABLES_INFO}
+              toFixList={toFixList}
+              rowIndex={i}
+              tableName={tableName}
+              rowDelta={rowDelta}
+            />
           );
         })}
     </tbody>
--- src/components/Game/ContentBox/ContentBoxTableBodyRow.js
+++ src/components/Game/ContentBox/ContentBoxTableBodyRow.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ContentBoxTableBodyRow = () => {
-	return (
-		<div>ContentBoxTableBodyRow</div>
-	)
-}
+const ContentBoxTableBodyRow = ({player,TABLES_INFO,toFixList,rowIndex,tableName,rowDelta}) => {
+  return (
+    <tr>
+      <td>
+        {(player.is_substituted && tableName === 'batting') || tableName !== 'batting'
+          ? ' '
+          : rowIndex + 1 - rowDelta}
+      </td>
+      <td style={player.is_substituted && tableName === 'batting' ? { paddingLeft: '2.5rem' } : null}>
+        <Link to={`/stats/player/${player.id}`}>{player.content.name}</Link>
+      </td>
+      {TABLES_INFO[tableName].headers.map((title, i) => {
+        const value =
+          title === 'POS'
+            ? player.content.positions.join('/')
+            : ['SB', 'CS', 'SB_pr', 'LOB', 'PB'].includes(title)
+            ? player.content.stats[tableName === 'batting' ? 'running' : 'catching'][title]
+            : ['CH', 'PO', 'A', 'E', 'DP', 'FLD'].includes(title)
+            ? player.content.stats.fielding[title]
+            : toFixList.includes(title)
+            ? player.content.stats[tableName][title] === 'Infinity'
+              ? 'INF'
+              : Number(player.content.stats[tableName][title]).toFixed(3)
+            : player.content.stats[tableName][title];
 
-export default ContentBoxTableBodyRow
+        return (
+          <td key={i} style={toFixList.includes(title) ? { width: '3rem' } : null}>
+            {Number(value) < 0 ? '—' : value}
+          </td>
+        );
+      })}
+    </tr>
+  );
+};
+
+export default ContentBoxTableBodyRow;
