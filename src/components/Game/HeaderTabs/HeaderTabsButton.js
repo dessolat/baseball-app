ba0a3837ac770f9e0fc2e76cf511230f73e4d18f
Igor Nikiforov <desola2@inbox.ru>
2022-08-28 21:35:44 +0300

    feat: import capitalizeFirstLetter from utils

--- src/components/Game/HeaderTabs/HeaderTabsButton.js
+++ src/components/Game/HeaderTabs/HeaderTabsButton.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { capitalizeFirstLetter } from 'utils';
 
 const HeaderTabsButton = ({ item, handleClick, getClass }) => {
   return (
     <li>
       <button name={item} onClick={handleClick} className={getClass(item)}>
-        {item[0].toUpperCase()}
-        {item.slice(1)}
+				{capitalizeFirstLetter(item)}
+        {/* {item[0].toUpperCase()}
+        {item.slice(1)} */}
       </button>
     </li>
   );
