f917f922a450c394278f19499468cad856f58803
Igor Nikiforov <desola2@inbox.ru>
2022-08-28 21:34:48 +0300

    refactor: move code to HeaderTabsButton comp

--- src/components/Game/HeaderTabs/HeaderTabs.js
+++ src/components/Game/HeaderTabs/HeaderTabs.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import cl from './HeaderTabs.module.scss';
-
+import HeaderTabsButton from './HeaderTabsButton';
 
 const HeaderTabs = ({ currentTab, handleClick }) => {
-	const isVideo = useSelector(state => state.game.isVideo)
+  const isVideo = useSelector(state => state.game.isVideo);
 
-	const tabsArr = isVideo ? ['box', 'plays', 'videos'] : ['box', 'plays'];
+  const tabsArr = isVideo ? ['box', 'plays', 'videos'] : ['box', 'plays'];
 
-	const getClass = name => (currentTab === name || (currentTab === 'videos' && name === 'plays' && !isVideo) ? cl.active : '');
+  const getClass = name =>
+    currentTab === name || (currentTab === 'videos' && name === 'plays' && !isVideo) ? cl.active : '';
   return (
     <ul className={cl.headerTabs}>
       {tabsArr.map((item, i) => (
-        <li key={i}>
-          <button name={item} onClick={handleClick} className={getClass(item)}>
-            {item[0].toUpperCase()}
-            {item.slice(1)}
-          </button>
-        </li>
+        <HeaderTabsButton key={i} item={item} handleClick={handleClick} getClass={getClass} />
       ))}
     </ul>
   );
--- src/components/Game/HeaderTabs/HeaderTabsButton.js
+++ src/components/Game/HeaderTabs/HeaderTabsButton.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const HeaderTabsButton = () => {
-	return (
-		<div>HeaderTabsButton</div>
-	)
-}
+const HeaderTabsButton = ({ item, handleClick, getClass }) => {
+  return (
+    <li>
+      <button name={item} onClick={handleClick} className={getClass(item)}>
+        {item[0].toUpperCase()}
+        {item.slice(1)}
+      </button>
+    </li>
+  );
+};
 
-export default HeaderTabsButton
+export default HeaderTabsButton;
