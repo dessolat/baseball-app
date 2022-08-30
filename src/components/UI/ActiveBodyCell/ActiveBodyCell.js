58d3112d6f60a30efe49a8b335d30fc715aa9614
Igor Nikiforov <desola2@inbox.ru>
2022-08-30 14:04:22 +0300

    fix: INF issue in ERA column

--- src/components/UI/ActiveBodyCell/ActiveBodyCell.js
+++ src/components/UI/ActiveBodyCell/ActiveBodyCell.js
@@ -4,15 +4,15 @@ import cl from './ActiveBodyCell.module.scss';
 const ActiveBodyCell = ({ sortField, addedClass = null, row, fixed = null, noAction = false, children }) => {
   const isRow = row !== undefined;
 
-	const classes = [addedClass]
-	sortField === children && !noAction && classes.push(cl.activeCell)
+  const classes = [addedClass];
+  sortField === children && !noAction && classes.push(cl.activeCell);
   return (
     <>
       {isRow && (
         <div className={classes.join(' ')}>
           {row[children] < 0
             ? '—'
-            : row[children] === 'inf'
+            : row[children] === 'inf' || row[children] === 'INF'
             ? 'INF'
             : fixed
             ? Number(row[children]).toFixed(fixed)
