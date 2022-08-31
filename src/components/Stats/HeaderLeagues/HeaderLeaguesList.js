0e7ee6bb69a0313cc5f4b6b2a55fb93134bc63f6
Igor Nikiforov <desola2@inbox.ru>
2022-08-31 20:28:44 +0300

    refactor: remove old commented code

--- src/components/Stats/HeaderLeagues/HeaderLeaguesList.js
+++ src/components/Stats/HeaderLeagues/HeaderLeaguesList.js
@@ -7,21 +7,12 @@ import HeaderLeaguesListItem from './HeaderLeaguesListItem';
 const HeaderLeaguesList = ({ leagues }, ref) => {
   const currentLeague = useSelector(state => state.games.currentLeague);
   const currentGameType = useSelector(state => state.shared.currentGameType);
-  // const currentYear = useSelector(state => state.shared.currentYear);
-  // const allYearsLeagues = useSelector(state => state.shared.allYearsLeagues);
   const dispatch = useDispatch();
 
   const handleLeagueClick = league => () => {
     const { id, title } = league;
-    // const leagueId =
-    //   league.id === -1
-    //     ? -1
-    //     : allYearsLeagues[currentYear].find(
-    //         curLeague => curLeague.name === league.title && curLeague.game_type === currentGameType
-    //       ).id;
 
     dispatch(setCurrentLeague({ ...league, name: title, title, id }));
-    // dispatch(setCurrentLeague({ ...league, name: league.title, title: league.title, id: leagueId }));
   };
 
   return (
