0624703657af75267470c03141e0d1d1e1e182f4
Igor Nikiforov <desola2@inbox.ru>
2022-09-03 21:20:00 +0300

    feat: create RowLink comp functional

--- src/components/TeamGames/ContentGamesTable/RowLink.js
+++ src/components/TeamGames/ContentGamesTable/RowLink.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const RowLink = () => {
-	return (
-		<div>RowLink</div>
-	)
-}
+const RowLink = ({ to, id }) => {
+  return <Link to={`/game/${id}?tab=${to}`}>{to[0].toUpperCase() + to.slice(1)}</Link>;
+};
 
-export default RowLink
+export default RowLink;
