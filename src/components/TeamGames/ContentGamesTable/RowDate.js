80afc98b66c47781540186b992cb79220fea1469
Igor Nikiforov <desola2@inbox.ru>
2022-09-03 21:24:51 +0300

    feat: create RowDate comp functional

--- src/components/TeamGames/ContentGamesTable/RowDate.js
+++ src/components/TeamGames/ContentGamesTable/RowDate.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-const RowDate = () => {
-	return (
-		<div>RowDate</div>
-	)
-}
+const MONTHS = {
+  '01': 'january',
+  '02': 'february',
+  '03': 'march',
+  '04': 'april',
+  '05': 'may',
+  '06': 'june',
+  '07': 'july',
+  '08': 'august',
+  '09': 'september',
+  10: 'october',
+  11: 'november',
+  12: 'december'
+};
 
-export default RowDate
+const RowDate = ({ date }) => {
+  const isMobile = useSelector(state => state.shared.isMobile);
+
+  return (
+    <div>
+      {date.slice(8, 10) +
+        ' ' +
+        (!isMobile ? MONTHS[date.slice(5, 7)].slice(0, 3) : MONTHS[date.slice(5, 7)])}
+    </div>
+  );
+};
+
+export default RowDate;
