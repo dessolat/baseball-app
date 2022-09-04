ec7f8661a5efffa54a24da74e6503f010e307e09
Igor Nikiforov <desola2@inbox.ru>
2022-09-04 20:00:58 +0300

    refactor: move code

--- src/components/TeamGames/ContentMobileHeader/ContentHeaderModeLink.js
+++ src/components/TeamGames/ContentMobileHeader/ContentHeaderModeLink.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMobileTableMode } from 'redux/teamGamesReducer';
 
-const ContentHeaderModeLink = () => {
-	return (
-		<div>ContentHeaderModeLink</div>
-	)
-}
+const ContentHeaderModeLink = ({ title, cl }) => {
+  const mobileTableMode = useSelector(state => state.teamGames.mobileTableMode);
+  const dispatch = useDispatch();
 
-export default ContentHeaderModeLink
+  const handleModeClick = mode => () => dispatch(setMobileTableMode(mode));
+  const getClassName = title => (mobileTableMode === title ? cl.active : null);
+
+  return (
+    <button onClick={handleModeClick(title)} className={getClassName(title)}>
+      {title}
+    </button>
+  );
+};
+
+export default ContentHeaderModeLink;
--- src/components/TeamGames/ContentMobileHeader/ContentHeaderModeLinks.js
+++ src/components/TeamGames/ContentMobileHeader/ContentHeaderModeLinks.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setMobileTableMode } from 'redux/teamGamesReducer';
+import ContentHeaderModeLink from './ContentHeaderModeLink';
 import cl from './ContentMobileHeader.module.scss';
 
 const ContentHeaderModeLinks = () => {
-  const mobileTableMode = useSelector(state => state.teamGames.mobileTableMode);
-  const dispatch = useDispatch();
-
-  const handleModeClick = mode => () => dispatch(setMobileTableMode(mode));
-  const getClassName = title => mobileTableMode === title ? cl.active : null;
   return (
     <div className={cl.tableModes}>
-      <button onClick={handleModeClick('Players')} className={getClassName('Players')}>
-        Players
-      </button>
-      <button onClick={handleModeClick('Calendar')} className={getClassName('Calendar')}>
-        Calendar
-      </button>
+      <ContentHeaderModeLink title='Players' cl={cl} />
+      <ContentHeaderModeLink title='Calendar' cl={cl} />
     </div>
   );
 };
