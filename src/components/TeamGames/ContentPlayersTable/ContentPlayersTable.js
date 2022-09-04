6021d2220b71b0be64ea0ee629291f216ae80015
Igor Nikiforov <desola2@inbox.ru>
2022-09-04 20:07:33 +0300

    refactor: move code to ContentPlayersTableBody

--- src/components/TeamGames/ContentPlayersTable/ContentPlayersTable.js
+++ src/components/TeamGames/ContentPlayersTable/ContentPlayersTable.js
@@ -1,61 +1,17 @@
 import React from 'react';
 import cl from './ContentPlayersTable.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import { setPlayerCurrentTeam as setCurrentTeam } from 'redux/playerStatsReducer';
 import ContentPlayersTableHeader from './ContentPlayersTableHeader';
+import ContentPlayersTableBody from './ContentPlayersTableBody';
 
 const ContentPlayersTable = () => {
   const { teamName } = useParams();
-
-  const teamData = useSelector(state => state.teamGames.teamData);
-  const currentYear = useSelector(state => state.shared.currentYear);
-  const currentLeague = useSelector(state => state.games.currentLeague);
-
-  const dispatch = useDispatch();
-
-  const handlePlayerClick = () => {
-    dispatch(setCurrentTeam(teamName));
-  };
-
-  const PLAYERS_DATA =
-    currentLeague.id !== -1
-      ? teamData.find(item => item.league.id === currentLeague.id)?.roster || []
-      : teamData.reduce((sum, cur) => {
-          if (cur.league.year === currentYear) {
-            cur.roster.forEach(
-              curPlayer =>
-                !sum.find(
-                  sumPlayer => sumPlayer.surname === curPlayer.surname && sumPlayer.name === curPlayer.name
-                ) && sum.push(curPlayer)
-            );
-          }
-          return sum;
-        }, []);
 
   return (
     <div className={cl.wrapper}>
       <div className={cl.table}>
         <ContentPlayersTableHeader headerStyle={cl.tableHeader} />
-        <ul className={cl.rows}>
-          {PLAYERS_DATA.map((player, index) => (
-            <li key={index} className={cl.tableRow}>
-              <div>{index + 1}</div>
-              <div className={cl.underlineHover}>
-                <Link to={`/stats/player/${player.id}`} onClick={handlePlayerClick}>
-                  {player.name + ' ' + player.surname}
-                </Link>
-              </div>
-              <div>{player.pos ?? '—'}</div>
-              <div>
-                {player.bat_hand}/{player.throw_hand}
-              </div>
-              <div>{player.height}</div>
-              <div>{player.weight}</div>
-              <div>{player.yob}</div>
-            </li>
-          ))}
-        </ul>
+        <ContentPlayersTableBody cl={cl} teamName={teamName} />
       </div>
       <div className={cl.linkWrapper}>
         <Link to={`/stats/player?team=${teamName}`}>Go to Player Stat</Link>
--- src/components/TeamGames/ContentPlayersTable/ContentPlayersTableBody.js
+++ src/components/TeamGames/ContentPlayersTable/ContentPlayersTableBody.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setPlayerCurrentTeam as setCurrentTeam } from 'redux/playerStatsReducer';
 
-const ContentPlayersTableBody = () => {
-	return (
-		<div>ContentPlayersTableBody</div>
-	)
-}
+const ContentPlayersTableBody = ({ cl, teamName }) => {
+  const teamData = useSelector(state => state.teamGames.teamData);
+  const currentYear = useSelector(state => state.shared.currentYear);
+  const currentLeague = useSelector(state => state.games.currentLeague);
 
-export default ContentPlayersTableBody
+  const dispatch = useDispatch();
+
+  const handlePlayerClick = () => {
+    dispatch(setCurrentTeam(teamName));
+  };
+
+  const PLAYERS_DATA =
+    currentLeague.id !== -1
+      ? teamData.find(item => item.league.id === currentLeague.id)?.roster || []
+      : teamData.reduce((sum, cur) => {
+          if (cur.league.year === currentYear) {
+            cur.roster.forEach(
+              curPlayer =>
+                !sum.find(
+                  sumPlayer => sumPlayer.surname === curPlayer.surname && sumPlayer.name === curPlayer.name
+                ) && sum.push(curPlayer)
+            );
+          }
+          return sum;
+        }, []);
+  return (
+    <ul className={cl.rows}>
+      {PLAYERS_DATA.map((player, index) => (
+        <li key={index} className={cl.tableRow}>
+          <div>{index + 1}</div>
+          <div className={cl.underlineHover}>
+            <Link to={`/stats/player/${player.id}`} onClick={handlePlayerClick}>
+              {player.name + ' ' + player.surname}
+            </Link>
+          </div>
+          <div>{player.pos ?? '—'}</div>
+          <div>
+            {player.bat_hand}/{player.throw_hand}
+          </div>
+          <div>{player.height}</div>
+          <div>{player.weight}</div>
+          <div>{player.yob}</div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContentPlayersTableBody;
