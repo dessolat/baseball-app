d99b9e5826b41095df9aefa4ecdfca94c62522e4
Igor Nikiforov <desola2@inbox.ru>
2022-09-04 20:15:31 +0300

    refactor: move code to ContentPlayersTableRow

--- src/components/TeamGames/ContentPlayersTable/ContentPlayersTableBody.js
+++ src/components/TeamGames/ContentPlayersTable/ContentPlayersTableBody.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { setPlayerCurrentTeam as setCurrentTeam } from 'redux/playerStatsReducer';
+import { useSelector } from 'react-redux';
+import ContentPlayersTableRow from './ContentPlayersTableRow/ContentPlayersTableRow';
 
 const ContentPlayersTableBody = ({ cl }) => {
   const teamData = useSelector(state => state.teamGames.teamData);
   const currentYear = useSelector(state => state.shared.currentYear);
   const currentLeague = useSelector(state => state.games.currentLeague);
-
-  const dispatch = useDispatch();
-
-  const handlePlayerClick = () => {
-    dispatch(setCurrentTeam(teamName));
-  };
-
+ 
   const PLAYERS_DATA =
     currentLeague.id !== -1
       ? teamData.find(item => item.league.id === currentLeague.id)?.roster || []
@@ -30,22 +23,8 @@ const ContentPlayersTableBody = ({ cl }) => {
         }, []);
   return (
     <ul className={cl.rows}>
-      {PLAYERS_DATA.map((player, index) => (
-        <li key={index} className={cl.tableRow}>
-          <div>{index + 1}</div>
-          <div className={cl.underlineHover}>
-            <Link to={`/stats/player/${player.id}`} onClick={handlePlayerClick}>
-              {player.name + ' ' + player.surname}
-            </Link>
-          </div>
-          <div>{player.pos ?? '—'}</div>
-          <div>
-            {player.bat_hand}/{player.throw_hand}
-          </div>
-          <div>{player.height}</div>
-          <div>{player.weight}</div>
-          <div>{player.yob}</div>
-        </li>
+      {PLAYERS_DATA.map((player, i) => (
+        <ContentPlayersTableRow player={player} index={i} cl={cl} />
       ))}
     </ul>
   );
--- src/components/TeamGames/ContentPlayersTable/ContentPlayersTableRow/ContentPlayersTableRow.js
+++ src/components/TeamGames/ContentPlayersTable/ContentPlayersTableRow/ContentPlayersTableRow.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { setPlayerCurrentTeam as setCurrentTeam } from 'redux/playerStatsReducer';
+import { useDispatch } from 'react-redux';
 
-const ContentPlayersTableRow = () => {
-	return (
-		<div>ContentPlayersTableRow</div>
-	)
-}
+const ContentPlayersTableRow = ({ player, index, cl }) => {
+  const { teamName } = useParams();
 
-export default ContentPlayersTableRow
+	const handlePlayerClick = () => {
+    dispatch(setCurrentTeam(teamName));
+  };
+
+  const dispatch = useDispatch();
+
+  return (
+    <li key={index} className={cl.tableRow}>
+      <div>{index + 1}</div>
+      <div className={cl.underlineHover}>
+        <Link to={`/stats/player/${player.id}`} onClick={handlePlayerClick}>
+          {player.name + ' ' + player.surname}
+        </Link>
+      </div>
+      <div>{player.pos ?? '—'}</div>
+      <div>
+        {player.bat_hand}/{player.throw_hand}
+      </div>
+      <div>{player.height}</div>
+      <div>{player.weight}</div>
+      <div>{player.yob}</div>
+    </li>
+  );
+};
+
+export default ContentPlayersTableRow;
