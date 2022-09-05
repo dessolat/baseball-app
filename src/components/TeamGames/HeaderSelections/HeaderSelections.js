00868885dc6f8998d9ec48d9cc958919ce27f311
Igor Nikiforov <desola2@inbox.ru>
2022-09-05 23:07:07 +0300

    refactor: move code

--- src/components/TeamGames/HeaderSelections/HeaderSelections.js
+++ src/components/TeamGames/HeaderSelections/HeaderSelections.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import cl from './HeaderSelections.module.scss';
-import Dropdown from 'components/UI/dropdown/GamesDropdown/Dropdown';
-import { useSelector, useDispatch } from 'react-redux';
-import { setCurrentYear, setCurrentDate } from 'redux/sharedReducer';
-import { setCurrentLeague } from 'redux/gamesReducer';
 import HeaderSelectionsTeamInfo from './HeaderSelectionsTeamInfo';
+import HeaderSelectionsYears from './HeaderSelectionsYears';
 
-const YEARS = [2022, 2021, 2020];
-
-const HeaderSelections = () => {
-  const currentYear = useSelector(state => state.shared.currentYear);
-  const dispatch = useDispatch();
-
-  const handleClick = option => {
-    const tempDate = new Date(option, 0, 1);
-    tempDate.setHours(0, tempDate.getTimezoneOffset() * -1, 0, 0);
-
-    dispatch(setCurrentDate(tempDate));
-    dispatch(setCurrentYear(option));
-    dispatch(setCurrentLeague({ id: -1, title: 'All' }));
-  };
-  return (
-    <div className={cl.selections}>
-      <HeaderSelectionsTeamInfo cl={cl} />
-      <div className={cl.years}>
-        <Dropdown title={currentYear} options={YEARS} currentOption={currentYear} handleClick={handleClick} />
-      </div>
-    </div>
-  );
-};
+const HeaderSelections = () => (
+  <div className={cl.selections}>
+    <HeaderSelectionsTeamInfo cl={cl} />
+    <HeaderSelectionsYears yearsClass={cl.years} />
+  </div>
+);
 
 export default HeaderSelections;
--- src/components/TeamGames/HeaderSelections/HeaderSelectionsYears.js
+++ src/components/TeamGames/HeaderSelections/HeaderSelectionsYears.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Dropdown from 'components/UI/dropdown/GamesDropdown/Dropdown';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentYear, setCurrentDate } from 'redux/sharedReducer';
+import { setCurrentLeague } from 'redux/gamesReducer';
 
-const HeaderSelectionsYears = () => {
-	return (
-		<div>HeaderSelectionsYears</div>
-	)
-}
+const YEARS = [2022, 2021, 2020];
 
-export default HeaderSelectionsYears
+const HeaderSelectionsYears = ({yearsClass}) => {
+  const currentYear = useSelector(state => state.shared.currentYear);
+
+	const dispatch = useDispatch();
+
+  const handleClick = option => {
+    const tempDate = new Date(option, 0, 1);
+    tempDate.setHours(0, tempDate.getTimezoneOffset() * -1, 0, 0);
+
+    dispatch(setCurrentDate(tempDate));
+    dispatch(setCurrentYear(option));
+    dispatch(setCurrentLeague({ id: -1, title: 'All' }));
+  };
+
+  return (
+    <div className={yearsClass}>
+      <Dropdown title={currentYear} options={YEARS} currentOption={currentYear} handleClick={handleClick} />
+    </div>
+  );
+};
+
+export default HeaderSelectionsYears;
