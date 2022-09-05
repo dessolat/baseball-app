b6c728bb4afce95bbcf30b6f2b41e7c55387589a
Igor Nikiforov <desola2@inbox.ru>
2022-09-05 22:51:12 +0300

    feat: add leagueClasses const

--- src/components/TeamGames/HeaderLeagues/HeaderLeaguesListItem.js
+++ src/components/TeamGames/HeaderLeagues/HeaderLeaguesListItem.js
@@ -3,9 +3,13 @@ import cl from './HeaderLeagues.module.scss';
 
 const HeaderLeaguesListItem = ({ league, handleClick, currentLeague }) => {
   const leagueName = league.title !== 'All' ? league.title : 'All leagues';
+	
+	const leagueClasses = [cl.league]
+	league.id === currentLeague.id && leagueClasses.push(cl.active)
+
   return (
     <li
-      className={league.id === currentLeague.id ? cl.league + ' ' + cl.active : cl.league}
+      className={leagueClasses.join(' ')}
       onClick={handleClick(league)}>
       {leagueName}
     </li>
