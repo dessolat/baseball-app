1a38df3ad83b95ff23e065636de51547a17b9526
Igor Nikiforov <desola2@inbox.ru>
2022-09-06 21:14:56 +0300

    refactor: beautify code

--- src/components/TeamGames/HeaderSelections/HeaderSelectionsLinks.js
+++ src/components/TeamGames/HeaderSelections/HeaderSelectionsLinks.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ContentHeaderModeLinks from '../ContentMobileHeader/ContentHeaderModeLinks';
 
-const HeaderSelectionsLinks = ({ wrapperClass }) => {
-  return (
-    <div className={wrapperClass}>
-      <ContentHeaderModeLinks />
-    </div>
-  );
-};
+const HeaderSelectionsLinks = ({ wrapperClass }) => (
+  <div className={wrapperClass}>
+    <ContentHeaderModeLinks />
+  </div>
+);
 
 export default HeaderSelectionsLinks;
--- src/components/TeamGames/HeaderSelections/HeaderSelectionsTeamInfo.js
+++ src/components/TeamGames/HeaderSelections/HeaderSelectionsTeamInfo.js
@@ -3,14 +3,12 @@ import TeamLogo from 'images/team_logo.png';
 import HeaderSelectionsTeamName from './HeaderSelectionsTeamName';
 import HeaderSelectionsLinks from './HeaderSelectionsLinks';
 
-const HeaderSelectionsTeamInfo = ({ cl }) => {
-  return (
-    <div className={cl.teamInfo}>
-      <img src={TeamLogo} alt='' className={cl.teamImg} />
-      <HeaderSelectionsTeamName teamClass={cl.teamName} />
-      <HeaderSelectionsLinks wrapperClass={cl.linksWrapper} />
-    </div>
-  );
-};
+const HeaderSelectionsTeamInfo = ({ cl }) => (
+  <div className={cl.teamInfo}>
+    <img src={TeamLogo} alt='' className={cl.teamImg} />
+    <HeaderSelectionsTeamName teamClass={cl.teamName} />
+    <HeaderSelectionsLinks wrapperClass={cl.linksWrapper} />
+  </div>
+);
 
 export default HeaderSelectionsTeamInfo;
