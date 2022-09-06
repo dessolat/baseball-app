8e085303005be46c414aedc858468dd6082a3882
Igor Nikiforov <desola2@inbox.ru>
2022-09-06 21:35:50 +0300

    refactor: integrate classnames

--- src/components/UI/ActiveBodyCell/ActiveBodyCell.js
+++ src/components/UI/ActiveBodyCell/ActiveBodyCell.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import cl from './ActiveBodyCell.module.scss';
+import classNames from 'classnames';
 
 const ActiveBodyCell = ({ sortField, addedClass = null, row, fixed = null, noAction = false, children }) => {
   const isRow = row !== undefined;
 
-  const classes = [addedClass];
-  sortField === children && !noAction && classes.push(cl.activeCell);
+  const classes = classNames(addedClass, {
+    [cl.activeCell]: sortField === children && !noAction
+  });
   return (
     <>
       {isRow && (
-        <div className={classes.join(' ')}>
+        <div className={classes}>
           {row[children] < 0
             ? '—'
             : row[children] === 'inf' || row[children] === 'INF'
