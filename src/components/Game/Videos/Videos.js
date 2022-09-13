5b305a2ad98621c65dab8b05e4e61fb91059f7cf
Igor Nikiforov <desola2@inbox.ru>
2022-09-13 17:06:59 +0300

    feat: add MODE_LINKS const

--- src/components/Game/Videos/Videos.js
+++ src/components/Game/Videos/Videos.js
@@ -8,8 +8,17 @@ import { useSelector } from 'react-redux';
 
 const Videos = () => {
   const preview = useSelector(state => state.game.preview);
+  const viewMode = useSelector(state => state.game.viewMode);
+
   const { pitch_link, bat_left_link, bat_right_link, left_add, left_main, right_add, right_main } =
     preview.camera_info;
+
+  const MODE_LINKS = {
+    'mode-1': [pitch_link],
+		'mode-2': [bat_left_link, bat_right_link],
+		'mode-3': [left_add, left_main, right_add, right_main],
+		'mode-4': [left_add, left_main, right_add, right_main]
+  };
 
   const videoId1 = getYouTubeID(pitch_link);
   return (
