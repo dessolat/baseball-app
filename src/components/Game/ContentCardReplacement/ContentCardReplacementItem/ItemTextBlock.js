b59a8085685d99dd47a10f7e5d153034b767967b
Igor Nikiforov <desola2@inbox.ru>
2022-09-19 11:33:12 +0300

    fix: header align to center

--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemTextBlock.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemTextBlock.js
@@ -4,7 +4,7 @@ import ItemRow from './ItemRow';
 const ItemTextBlock = ({ cl, header, event, isVideo }) => {
   return (
     <div className={cl.textBlock}>
-      {header !== null && !isVideo && <p className={cl.textBold}>{header}</p>}
+      {header !== null && !isVideo && <p className={cl.textBold} style={{textAlign: 'center'}}>{header}</p>}
       <div className={cl.text}>
         <ItemRow event={event} rowNumber={1} cl={cl} />
         {event.rows_numbers === 2 && <ItemRow event={event} rowNumber={2} cl={cl} />}
