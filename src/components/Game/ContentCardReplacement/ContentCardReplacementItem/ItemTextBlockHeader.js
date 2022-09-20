e188981f1133d4979298f0b6d81608bef9241d86
Igor Nikiforov <desola2@inbox.ru>
2022-09-20 17:00:46 +0300

    feat: add headerClasses

--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemTextBlockHeader.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemTextBlockHeader.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const ItemTextBlockHeader = ({ cl, header }) => {
-  return (
-    <p className={cl.textBold} style={{ textAlign: 'center' }}>
-      {header}
-    </p>
-  );
+  const headerClasses = [cl.textBold, cl.textAlignCenter].join(' ');
+
+  return <p className={headerClasses}>{header}</p>;
 };
 
 export default ItemTextBlockHeader;
