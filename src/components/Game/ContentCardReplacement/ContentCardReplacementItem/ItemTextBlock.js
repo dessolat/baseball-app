4ba03912ae112915a0a0a4de15688d6eae1ce613
Igor Nikiforov <desola2@inbox.ru>
2022-09-20 17:01:43 +0300

    refactor: add isSecondRow const

--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemTextBlock.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemTextBlock.js
@@ -4,13 +4,14 @@ import ItemTextBlockHeader from './ItemTextBlockHeader';
 
 const ItemTextBlock = ({ cl, header, event, isVideo }) => {
   const isHeader = header !== null && !isVideo;
+  const isSecondRow = event.rows_numbers === 2;
 
   return (
     <div className={cl.textBlock}>
       {isHeader && <ItemTextBlockHeader cl={cl} header={header} />}
       <div className={cl.text}>
         <ItemRow event={event} rowNumber={1} cl={cl} />
-        {event.rows_numbers === 2 && <ItemRow event={event} rowNumber={2} cl={cl} />}
+        {isSecondRow && <ItemRow event={event} rowNumber={2} cl={cl} />}
       </div>
     </div>
   );
