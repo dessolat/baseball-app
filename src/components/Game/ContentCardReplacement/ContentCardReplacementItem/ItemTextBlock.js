4231e8ef0bab93499f22c71eafd056403e6c3801
Igor Nikiforov <desola2@inbox.ru>
2022-09-20 16:54:27 +0300

    refactor: add isHeader const

--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemTextBlock.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemTextBlock.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ItemRow from './ItemRow';
 
 const ItemTextBlock = ({ cl, header, event, isVideo }) => {
+	const isHeader = header !== null && !isVideo
+	
   return (
     <div className={cl.textBlock}>
-      {header !== null && !isVideo && <p className={cl.textBold} style={{textAlign: 'center'}}>{header}</p>}
+      {isHeader && <p className={cl.textBold} style={{textAlign: 'center'}}>{header}</p>}
       <div className={cl.text}>
         <ItemRow event={event} rowNumber={1} cl={cl} />
         {event.rows_numbers === 2 && <ItemRow event={event} rowNumber={2} cl={cl} />}
