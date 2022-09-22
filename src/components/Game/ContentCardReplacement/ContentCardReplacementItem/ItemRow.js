119a12051eefb6a2539a55c92d619e32953cac30
Igor Nikiforov <desola2@inbox.ru>
2022-09-22 22:38:47 +0300

    feat: move code
    * rename classes consts
    
    * add props

--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemRow.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemRow.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import classNames from 'classnames';
 import SwitchArrow from 'components/UI/dividers/SwitchArrow/SwitchArrow';
+import ItemRowSideText from './ItemRowSideText';
 
-const ItemRow = ({ event, rowNumber = 1, cl }) => {
-  const leftSideStyles = classNames(cl.leftSide, {
-    [cl.textBold]: (event.left_row2_index && rowNumber === 2) || (event.left_row1_index && rowNumber === 1)
-  });
-
-  const rightSideStyles = classNames(cl.rightSide, {
-    [cl.textBold]: (event.right_row1_index && rowNumber === 1) || (event.right_row2_index && rowNumber === 2)
-  });
-  return (
-    <div className={cl.textRow}>
-      <p className={leftSideStyles}>
-        {event[`left_row${rowNumber}_text`] || event[`left_row${rowNumber}_index`]}
-      </p>
-      <SwitchArrow />
-      <p className={rightSideStyles}>
-        {event[`right_row${rowNumber}_text`] || event[`right_row${rowNumber}_index`]}
-      </p>
-    </div>
-  );
-};
+const ItemRow = ({ event, rowNumber = 1, cl }) => (
+  <div className={cl.textRow}>
+    <ItemRowSideText event={event} cl={cl} rowNumber={rowNumber} />
+    <SwitchArrow />
+    <ItemRowSideText event={event} cl={cl} side='right' rowNumber={rowNumber} />
+  </div>
+);
 
 export default ItemRow;
--- src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemRowSideText.js
+++ src/components/Game/ContentCardReplacement/ContentCardReplacementItem/ItemRowSideText.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import classNames from 'classnames';
 
-const ItemRowSideText = () => {
-	return (
-		<div>ItemRowSideText</div>
-	)
-}
+const ItemRowSideText = ({ event, cl, side = 'left', rowNumber = 1 }) => {
+  const leftSideClasses = classNames(cl.leftSide, {
+    [cl.textBold]: (event.left_row2_index && rowNumber === 2) || (event.left_row1_index && rowNumber === 1)
+  });
 
-export default ItemRowSideText
+  const rightSideClasses = classNames(cl.rightSide, {
+    [cl.textBold]: (event.right_row1_index && rowNumber === 1) || (event.right_row2_index && rowNumber === 2)
+  });
+
+  const classes = side === 'left' ? leftSideClasses : rightSideClasses;
+  return (
+    <p className={classes}>
+      {event[`${side}_row${rowNumber}_text`] || event[`${side}_row${rowNumber}_index`]}
+    </p>
+  );
+};
+
+export default ItemRowSideText;
