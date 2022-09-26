f9a0d6572a1b1dacde04249d2b26b2e195dfdc67
Igor Nikiforov <desola2@inbox.ru>
2022-09-26 21:21:11 +0300

    refactor: move code

--- src/components/Game/ContentCardComplexHeader/ContentCardComplexHeader.js
+++ src/components/Game/ContentCardComplexHeader/ContentCardComplexHeader.js
@@ -1,19 +1,15 @@
-import React, { useLayoutEffect, useRef, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import cl from './ContentCardComplexHeader.module.scss';
 import 'scss/colorWords.scss';
-import PortraitImg from 'images/portrait.png';
 import ContentCardReplacement from '../ContentCardReplacement/ContentCardReplacement';
 import RectText from 'components/UI/icons/Rects/RectText';
 import RectScore from 'components/UI/icons/Rects/RectScore';
 import { useSelector } from 'react-redux';
-import ContentCardPortrait from '../ContentCardPortrait/ContentCardPortrait';
-import ContentCardTitle from '../ContentCardTitle/ContentCardTitle';
 import classNames from 'classnames';
 import ContentCardSigns from '../ContentCardSigns/ContentCardSigns';
+import HeaderTop from './HeaderTop';
 
 const ContentCardComplexHeader = ({ player, sit }) => {
-  const ref = useRef(null);
-  const imagesData = useSelector(state => state.game.imagesData);
   const isVideo = useSelector(state => state.game.isVideo);
 
   const eventsSummary = useMemo(
@@ -25,18 +21,9 @@ const ContentCardComplexHeader = ({ player, sit }) => {
     [sit.events]
   );
 
-  useLayoutEffect(() => {
-    ref.current.innerHTML = eventsSummary.length > 0 ? eventsSummary.join('.') + '.' : '';
-  }, [eventsSummary, sit.icons.rect_text]);
-
-  const cardText = eventsSummary.length > 0 ? eventsSummary.join('.') + '.' : '';
-
   const isRectText = sit.icons.rect_text !== 'Replacement';
   const isRectScore = sit.icons.score_own !== undefined;
 
-  const imgSrc = imagesData[player.who_id] || PortraitImg;
-
-  const imgClasses = classNames({ [cl.default]: !imagesData[player.who_id] });
   const bottomClasses = classNames(cl.bottom, {
     [cl.noVideo]: !isVideo
   });
@@ -45,20 +32,11 @@ const ContentCardComplexHeader = ({ player, sit }) => {
     eventsSummary.length === 0 && !isVideo ? { position: 'absolute', top: '1.6rem', right: 0 } : null;
   return (
     <div className={cl.header}>
-      <div className={cl.top}>
-        <ContentCardTitle player={player} />
-        <div className={cl.portraitTextWrapper}>
-          <ContentCardPortrait className={imgClasses} src={imgSrc} cl={cl} />
-          <p className={cl.text} ref={ref}>
-            {cardText}
-          </p>
-        </div>
-      </div>
-
+      <HeaderTop cl={cl} eventsSummary={eventsSummary} player={player} sit={sit} />
       <div className={bottomClasses} style={bottomStyles}>
         {isRectText && <RectText icons={sit.icons} />}
         {isRectScore && <RectScore icons={sit.icons} />}
-        <ContentCardSigns table={sit.table}/>
+        <ContentCardSigns table={sit.table} />
       </div>
 
       {!isRectText && <ContentCardReplacement events={sit.events} />}
--- src/components/Game/ContentCardComplexHeader/HeaderTop.js
+++ src/components/Game/ContentCardComplexHeader/HeaderTop.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef } from 'react';
+import ContentCardPortrait from '../ContentCardPortrait/ContentCardPortrait';
+import ContentCardTitle from '../ContentCardTitle/ContentCardTitle';
+import PortraitImg from 'images/portrait.png';
+import { useSelector } from 'react-redux';
+import classNames from 'classnames';
 
-const HeaderTop = () => {
-	return (
-		<div>HeaderTop</div>
-	)
-}
+const HeaderTop = ({ cl, eventsSummary, player, sit }) => {
+  const ref = useRef(null);
 
-export default HeaderTop
+  const imagesData = useSelector(state => state.game.imagesData);
+
+  useLayoutEffect(() => {
+    ref.current.innerHTML = eventsSummary.length > 0 ? eventsSummary.join('.') + '.' : '';
+  }, [eventsSummary, sit.icons.rect_text]);
+
+  const cardText = eventsSummary.length > 0 ? eventsSummary.join('.') + '.' : '';
+  const imgSrc = imagesData[player.who_id] || PortraitImg;
+  const imgClasses = classNames({ [cl.default]: !imagesData[player.who_id] });
+
+  return (
+    <div className={cl.top}>
+      <ContentCardTitle player={player} />
+      <div className={cl.portraitTextWrapper}>
+        <ContentCardPortrait className={imgClasses} src={imgSrc} cl={cl} />
+        <p className={cl.text} ref={ref}>
+          {cardText}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderTop;
