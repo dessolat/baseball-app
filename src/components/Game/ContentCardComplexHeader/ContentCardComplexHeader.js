1eda41ab9d38556392f8d0f4cb43867e9d38cd53
Igor Nikiforov <desola2@inbox.ru>
2022-09-26 21:26:52 +0300

    refactor: move code

--- src/components/Game/ContentCardComplexHeader/ContentCardComplexHeader.js
+++ src/components/Game/ContentCardComplexHeader/ContentCardComplexHeader.js
@@ -2,16 +2,10 @@ import React, { useMemo } from 'react';
 import cl from './ContentCardComplexHeader.module.scss';
 import 'scss/colorWords.scss';
 import ContentCardReplacement from '../ContentCardReplacement/ContentCardReplacement';
-import RectText from 'components/UI/icons/Rects/RectText';
-import RectScore from 'components/UI/icons/Rects/RectScore';
-import { useSelector } from 'react-redux';
-import classNames from 'classnames';
-import ContentCardSigns from '../ContentCardSigns/ContentCardSigns';
 import HeaderTop from './HeaderTop';
+import HeaderBottom from './HeaderBottom';
 
 const ContentCardComplexHeader = ({ player, sit }) => {
-  const isVideo = useSelector(state => state.game.isVideo);
-
   const eventsSummary = useMemo(
     () =>
       sit.icons.rect_text !== 'Replacement'
@@ -22,22 +16,11 @@ const ContentCardComplexHeader = ({ player, sit }) => {
   );
 
   const isRectText = sit.icons.rect_text !== 'Replacement';
-  const isRectScore = sit.icons.score_own !== undefined;
 
-  const bottomClasses = classNames(cl.bottom, {
-    [cl.noVideo]: !isVideo
-  });
-
-  const bottomStyles =
-    eventsSummary.length === 0 && !isVideo ? { position: 'absolute', top: '1.6rem', right: 0 } : null;
   return (
     <div className={cl.header}>
       <HeaderTop cl={cl} eventsSummary={eventsSummary} player={player} sit={sit} />
-      <div className={bottomClasses} style={bottomStyles}>
-        {isRectText && <RectText icons={sit.icons} />}
-        {isRectScore && <RectScore icons={sit.icons} />}
-        <ContentCardSigns table={sit.table} />
-      </div>
+      <HeaderBottom cl={cl} eventsSummary={eventsSummary} isRectText={isRectText} sit={sit} />
 
       {!isRectText && <ContentCardReplacement events={sit.events} />}
     </div>
--- src/components/Game/ContentCardComplexHeader/HeaderBottom.js
+++ src/components/Game/ContentCardComplexHeader/HeaderBottom.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import RectText from 'components/UI/icons/Rects/RectText';
+import RectScore from 'components/UI/icons/Rects/RectScore';
+import ContentCardSigns from '../ContentCardSigns/ContentCardSigns';
+import { useSelector } from 'react-redux';
+import classNames from 'classnames';
 
-const HeaderBottom = () => {
-	return (
-		<div>HeaderBottom</div>
-	)
-}
+const HeaderBottom = ({cl, eventsSummary, isRectText, sit}) => {
+  const isVideo = useSelector(state => state.game.isVideo);
 
-export default HeaderBottom
+	const isRectScore = sit.icons.score_own !== undefined;
+
+  const bottomClasses = classNames(cl.bottom, {
+    [cl.noVideo]: !isVideo
+  });
+
+  const bottomStyles =
+    eventsSummary.length === 0 && !isVideo ? { position: 'absolute', top: '1.6rem', right: 0 } : null;
+  return (
+    <div className={bottomClasses} style={bottomStyles}>
+      {isRectText && <RectText icons={sit.icons} />}
+      {isRectScore && <RectScore icons={sit.icons} />}
+      <ContentCardSigns table={sit.table} />
+    </div>
+  );
+};
+
+export default HeaderBottom;
