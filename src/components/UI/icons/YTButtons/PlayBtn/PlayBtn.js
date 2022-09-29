dfe95ffbeaa14252921131a377f103acc748d6e4
Igor Nikiforov <desola2@inbox.ru>
2022-09-29 21:38:06 +0300

    feat: add btn code

--- src/components/UI/icons/YTButtons/PlayBtn/PlayBtn.js
+++ src/components/UI/icons/YTButtons/PlayBtn/PlayBtn.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const PlayBtn = () => {
-	return (
-		<div>PlayBtn</div>
-	)
-}
+  return (
+    <svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
+      <use class='ytp-svg-shadow' xlink:href='#ytp-id-576'></use>
+      <path
+        class='ytp-svg-fill'
+        d='M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z'
+        id='ytp-id-576'></path>
+    </svg>
+  );
+};
 
-export default PlayBtn
+export default PlayBtn;
