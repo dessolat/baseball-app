8ace34e7a5cb64718eaaa9577abcd76f4dc24714
Igor Nikiforov <desola2@inbox.ru>
2022-09-29 21:40:14 +0300

    feat: add btn code

--- src/components/UI/icons/YTButtons/PauseBtn/PauseBtn.js
+++ src/components/UI/icons/YTButtons/PauseBtn/PauseBtn.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const PauseBtn = () => {
-	return (
-		<div>PauseBtn</div>
-	)
-}
+  return (
+    <svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
+      <use class='ytp-svg-shadow' xlink:href='#ytp-id-592'></use>
+      <path
+        class='ytp-svg-fill'
+        d='M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z'
+        id='ytp-id-592'></path>
+    </svg>
+  );
+};
 
-export default PauseBtn
+export default PauseBtn;
