05cebd59b89194f792b261b7783be9bc342f958d
Igor Nikiforov <desola2@inbox.ru>
2022-10-01 12:48:07 +0300

    feat: create VideoControls comp p/holder

---
+++ src/components/Game/Video/VideoControls/VideoControls.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './VideoControls.module.scss';
+
+const VideoControls = () => {
+	return (
+		<div>VideoControls</div>
+	)
+}
+
+export default VideoControls
