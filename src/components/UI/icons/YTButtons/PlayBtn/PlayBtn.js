c61dfa39500937fb77ecb987d8fea2085d16a885
Igor Nikiforov <desola2@inbox.ru>
2022-10-01 13:04:21 +0300

    feat: remove xlink attrib
    * add playBtn class

--- src/components/UI/icons/YTButtons/PlayBtn/PlayBtn.js
+++ src/components/UI/icons/YTButtons/PlayBtn/PlayBtn.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import cl from './PlayBtn.module.scss';
 
 const PlayBtn = () => {
   return (
-    <svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
-      <use class='ytp-svg-shadow' xlink:href='#ytp-id-576'></use>
+    <svg height='100%' version='1.1' viewBox='0 0 36 36' className={cl.playBtn}>
+      <use class='ytp-svg-shadow'></use>
       <path
         class='ytp-svg-fill'
         d='M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z'
