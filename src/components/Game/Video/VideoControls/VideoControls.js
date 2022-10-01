2032b72d234af6da1d3f2a8c4578f9fce5698602
Igor Nikiforov <desola2@inbox.ru>
2022-10-01 13:05:13 +0300

    feat: import playBtn & speedBtn comps

--- src/components/Game/Video/VideoControls/VideoControls.js
+++ src/components/Game/Video/VideoControls/VideoControls.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import cl from './VideoControls.module.scss';
+import PlayBtn from 'components/UI/icons/YTButtons/PlayBtn/PlayBtn';
+import SpeedBtn from 'components/UI/icons/YTButtons/SpeedBtn/SpeedBtn';
 
 const VideoControls = () => {
 	return (
-		<div className={cl.wrapper}>VideoControls</div>
+		<div className={cl.wrapper}>
+			<div className={cl.timeLine}>Timeline</div>
+			<div className={cl.buttons}>
+				<PlayBtn />
+				<SpeedBtn />
+			</div>
+		</div>
 	)
 }
 
