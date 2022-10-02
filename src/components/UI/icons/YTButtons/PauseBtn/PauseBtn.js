102480594884286fd227eb1dcf0956afea9a8795
Igor Nikiforov <desola2@inbox.ru>
2022-10-02 15:59:13 +0300

    feat: add button wrapper tag
    * remove xlink attribute

--- src/components/UI/icons/YTButtons/PauseBtn/PauseBtn.js
+++ src/components/UI/icons/YTButtons/PauseBtn/PauseBtn.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import cl from './PauseBtn.module.scss';
 
 const PauseBtn = () => {
   return (
-    <svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
-      <use class='ytp-svg-shadow' xlink:href='#ytp-id-592'></use>
-      <path
-        class='ytp-svg-fill'
-        d='M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z'
-        id='ytp-id-592'></path>
-    </svg>
+    <button className={cl.playBtn}>
+      <svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
+        <use class='ytp-svg-shadow'></use>
+        <path
+          class='ytp-svg-fill'
+          d='M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z'
+          id='ytp-id-592'></path>
+      </svg>
+    </button>
   );
 };
 
