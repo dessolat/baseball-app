0294ad542e1aa7d0f6c46421181f936023552aa6
Igor Nikiforov <desola2@inbox.ru>
2022-10-03 11:58:31 +0300

    fix: className issue

--- src/components/UI/icons/YTButtons/PlayBtn/PlayBtn.js
+++ src/components/UI/icons/YTButtons/PlayBtn/PlayBtn.js
@@ -5,9 +5,9 @@ const PlayBtn = () => {
   return (
     <button className={cl.playBtn}>
       <svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
-        <use class='ytp-svg-shadow'></use>
+        <use className='ytp-svg-shadow'></use>
         <path
-          class='ytp-svg-fill'
+          className='ytp-svg-fill'
           d='M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z'
           id='ytp-id-576'></path>
       </svg>
