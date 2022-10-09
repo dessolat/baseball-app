5cdd206a8dfea3f5bb81a913849fc93522be9650
Igor Nikiforov <desola2@inbox.ru>
2022-10-09 16:02:03 +0300

    feat: add videoState selector
    * add playPauseBtn const

--- src/components/Game/Video/VideoControls/VideoControls.js
+++ src/components/Game/Video/VideoControls/VideoControls.js
@@ -2,13 +2,18 @@ import React from 'react';
 import cl from './VideoControls.module.scss';
 import PlayBtn from 'components/UI/icons/YTButtons/PlayBtn/PlayBtn';
 import SpeedBtn from 'components/UI/icons/YTButtons/SpeedBtn/SpeedBtn';
+import { useSelector } from 'react-redux';
+import PauseBtn from 'components/UI/icons/YTButtons/PauseBtn/PauseBtn';
 
 const VideoControls = ({ controlsWrapper, rateChangeHandler }) => {
+  const videoState = useSelector(state => state.game.videoState);
+
+  const playPauseBtn = videoState === 1 || videoState === 3 || videoState === -1? <PauseBtn /> : <PlayBtn />;
   return (
     <div className={controlsWrapper}>
       <div className={cl.timeLine}>Timeline</div>
       <div className={cl.buttons}>
-        <PlayBtn />
+        {playPauseBtn}
         <SpeedBtn rateChangeHandler={rateChangeHandler} />
       </div>
     </div>
