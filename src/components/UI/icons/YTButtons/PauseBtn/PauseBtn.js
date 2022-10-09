aed812b9902969c7ced4257a9dfcc02b464d2327
Igor Nikiforov <desola2@inbox.ru>
2022-10-09 15:59:40 +0300

    feat: rename playBtn class to pauseBtn

--- src/components/UI/icons/YTButtons/PauseBtn/PauseBtn.js
+++ src/components/UI/icons/YTButtons/PauseBtn/PauseBtn.js
@@ -3,7 +3,7 @@ import cl from './PauseBtn.module.scss';
 
 const PauseBtn = () => {
   return (
-    <button className={cl.playBtn}>
+    <button className={cl.pauseBtn}>
       <svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
         <use class='ytp-svg-shadow'></use>
         <path
