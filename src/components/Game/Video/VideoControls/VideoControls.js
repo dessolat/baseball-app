fb312294cbb13e048d1c9fe1be815f1d93fbb982
Igor Nikiforov <desola2@inbox.ru>
2022-10-10 22:33:01 +0300

    feat: import TimeLine comp

--- src/components/Game/Video/VideoControls/VideoControls.js
+++ src/components/Game/Video/VideoControls/VideoControls.js
@@ -4,17 +4,21 @@ import PlayBtn from 'components/UI/icons/YTButtons/PlayBtn/PlayBtn';
 import SpeedBtn from 'components/UI/icons/YTButtons/SpeedBtn/SpeedBtn';
 import { useSelector } from 'react-redux';
 import PauseBtn from 'components/UI/icons/YTButtons/PauseBtn/PauseBtn';
+import TimeLine from './TimeLine';
 
 const VideoControls = ({ controlsWrapper, rateChangeHandler, setPlayPause, currentMoment }, ref) => {
   const videoState = useSelector(state => state.game.videoState);
 
+
   const playPauseBtn =
     videoState === 1 || videoState === 3 ? (
-    // videoState === 1 || videoState === 3 || videoState === -1 ? (
+      // videoState === 1 || videoState === 3 || videoState === -1 ? (
       <PauseBtn setPlayPause={setPlayPause} />
     ) : (
       <PlayBtn setPlayPause={setPlayPause} />
     );
+
+  // {`${totalMinutes}:${remainSeconds}`}
   return (
     <div className={controlsWrapper}>
       <TimeLine cl={cl} currentMoment={currentMoment} ref={ref} />
