e1f433b94572013f4247e52de13aaa28775809c3
Igor Nikiforov <desola2@inbox.ru>
2022-10-19 13:51:41 +0300

    feat: add Galaxy comp p/holder

--- src/components/ThreePage/Content/CanvasComp.js
+++ src/components/ThreePage/Content/CanvasComp.js
@@ -1,10 +1,22 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import Galaxy from './Galaxy';
+import Buttons from './Buttons';
+import { useSelector } from 'react-redux';
+
 const CanvasComp = ({ cl }) => {
+  const count = useSelector(state => state.three.count);
+  const isStars = useSelector(state => state.three.isStars);
+  const isEvents = useSelector(state => state.three.isEvents);
+
   return (
     <>
       <Canvas camera={{ position: [0, 12, 24] }} style={{ position: 'absolute', left: 0, top: 0, pointerEvents: isEvents ? 'All' : 'none' }}>
+        <Suspense fallback={null}>
+          <Galaxy count={count} isStars={isStars} isEvents={isEvents} />
+        </Suspense>
       </Canvas>
+      <Buttons cl={cl} count={count} isStars={isStars} isEvents={isEvents} />
     </>
   );
 };
---
+++ src/components/ThreePage/Content/Galaxy.js
@@ -0,0 +1,9 @@
+import React, { useRef } from 'react';
+const Galaxy = ({ count, isStars, isEvents }) => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Galaxy;
