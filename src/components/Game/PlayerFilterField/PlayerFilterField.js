33d574ce7cbb46fad8774364773c947a9034dae2
Igor Nikiforov <desola2@inbox.ru>
2022-10-22 17:42:10 +0300

    feat: integrate classNames
    * add inputClasses

--- src/components/Game/PlayerFilterField/PlayerFilterField.js
+++ src/components/Game/PlayerFilterField/PlayerFilterField.js
@@ -1,11 +1,15 @@
+import classNames from 'classnames';
 import React, { useState } from 'react';
 import { useRef } from 'react';
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { setPlayerCardFilter } from 'redux/gameReducer';
 import cl from './PlayerFilterField.module.scss';
 
 const PlayerFilterField = () => {
   const [filterValue, setFilterValue] = useState('');
+
+	const isFilteredPlayer = useSelector(state => state.game.isFilteredPlayer)
 
   const dispatch = useDispatch();
 
@@ -20,12 +24,16 @@ const PlayerFilterField = () => {
       dispatch(setPlayerCardFilter(value));
     }, 400);
   };
+
+  const inputClasses = classNames([cl.filterPlayerField], {
+		[cl.noPlayerFiltered] : !isFilteredPlayer
+	});
   return (
     <input
       placeholder='Search of player'
       value={filterValue}
       onChange={changeHandler}
-      className={cl.filterPlayerField}
+      className={inputClasses}
     />
   );
 };
