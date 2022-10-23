6956900cfd21b145c916b6f851d787bb5b2f1fdd
Igor Nikiforov <desola2@inbox.ru>
2022-10-23 14:30:58 +0300

    feat: add basic func

--- src/components/Game/ModalCameraSelector/ModalCameraSelector.js
+++ src/components/Game/ModalCameraSelector/ModalCameraSelector.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import cl from './ModalCameraSelector.module.scss'
+import React from 'react';
+import cl from './ModalCameraSelector.module.scss';
+import ModalCross from 'images/modal_cross.png';
+import { setIsCameraSelector } from 'redux/gameReducer';
+import { useDispatch } from 'react-redux';
 
 const ModalCameraSelector = () => {
-	return (
-		<div>ModalCameraSelector</div>
-	)
-}
+  const dispatch = useDispatch();
 
-export default ModalCameraSelector
+  return (
+    <div className={cl.modalCameraSelectorWrapper}>
+      <div className={cl.camerasContainer}></div>
+      <button
+        className={cl.modalCrossBtn}
+        onClick={() => {
+          dispatch(setIsCameraSelector(false));
+        }}>
+        <img src={ModalCross} alt='modal-cross' />
+      </button>
+    </div>
+  );
+};
+
+export default ModalCameraSelector;
