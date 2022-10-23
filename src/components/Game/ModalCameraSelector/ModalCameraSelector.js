ef24688f717ef6b2219b911cf3d9f5e07b939b5e
Igor Nikiforov <desola2@inbox.ru>
2022-10-23 14:27:24 +0300

    feat: add ModalCameraSelector comp p/holder

---
+++ src/components/Game/ModalCameraSelector/ModalCameraSelector.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './ModalCameraSelector.module.scss'
+
+const ModalCameraSelector = () => {
+	return (
+		<div>ModalCameraSelector</div>
+	)
+}
+
+export default ModalCameraSelector
