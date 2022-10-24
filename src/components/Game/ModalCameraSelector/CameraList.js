28451d9c77ea03e9f6f9d21f0a385348ea1c5bb5
Igor Nikiforov <desola2@inbox.ru>
2022-10-24 11:41:39 +0300

    refactor: move code

--- src/components/Game/ModalCameraSelector/CameraList.js
+++ src/components/Game/ModalCameraSelector/CameraList.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import cl from './CameraList.module.scss';
+import classNames from 'classnames';
+
+const CAMERAS = [
+	{ number: 1, isActive: true, title: 'Base left main' },
+	{ number: 2, isActive: false, title: 'Base right main' },
+	{ number: 3, isActive: true, title: 'Base left add' },
+	{ number: 4, isActive: false, title: 'Base right add' },
+	{ number: 5, isActive: false, title: 'Bat left' },
+	{ number: 6, isActive: false, title: 'Bat right' },
+	{ number: 7, isActive: true, title: 'Pitch' },
+	{ number: 8, isActive: true, title: 'IP' }
+];
 
 const CameraList = () => {
-	return (
-		<div>CameraList</div>
-	)
-}
+  return (
+    <div className={cl.camerasContainer}>
+      {CAMERAS.map((camera, i) => {
+        const cameraClasses = classNames([cl.camera], {
+          [cl.active]: camera.isActive
+        });
+        return (
+          <button key={i} className={cameraClasses} data-before={camera.number}>
+            {camera.title}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
-export default CameraList
+export default CameraList;
--- src/components/Game/ModalCameraSelector/ModalCameraSelector.js
+++ src/components/Game/ModalCameraSelector/ModalCameraSelector.js
@@ -3,36 +3,14 @@ import cl from './ModalCameraSelector.module.scss';
 import ModalCross from 'images/modal_cross.png';
 import { setIsCameraSelector } from 'redux/gameReducer';
 import { useDispatch } from 'react-redux';
-import classNames from 'classnames';
+import CameraList from './CameraList';
 
 const ModalCameraSelector = () => {
   const dispatch = useDispatch();
 
-  const CAMERAS = [
-    { number: 1, isActive: true, title: 'Base left main' },
-    { number: 2, isActive: false, title: 'Base right main' },
-    { number: 3, isActive: true, title: 'Base left add' },
-    { number: 4, isActive: false, title: 'Base right add' },
-    { number: 5, isActive: false, title: 'Bat left' },
-    { number: 6, isActive: false, title: 'Bat right' },
-    { number: 7, isActive: true, title: 'Pitch' },
-    { number: 8, isActive: true, title: 'IP' }
-  ];
-
   return (
     <div className={cl.modalCameraSelectorWrapper}>
-      <div className={cl.camerasContainer}>
-        {CAMERAS.map((camera, i) => {
-          const cameraClasses = classNames([cl.camera], {
-            [cl.active]: camera.isActive
-          });
-          return (
-            <button key={i} className={cameraClasses} data-before={camera.number}>
-              {camera.title}
-            </button>
-          );
-        })}
-      </div>
+      <CameraList />
       <button
         className={cl.modalCrossBtn}
         onClick={() => {
