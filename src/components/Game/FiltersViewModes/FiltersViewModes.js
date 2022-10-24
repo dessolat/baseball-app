4476f039bb773a15076910e7698940ea3a9d8962
Igor Nikiforov <desola2@inbox.ru>
2022-10-24 11:30:42 +0300

    feat: add camera miniature titles

--- src/components/Game/FiltersViewModes/FiltersViewModes.js
+++ src/components/Game/FiltersViewModes/FiltersViewModes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setIsCameraSelector, setViewMode } from 'redux/gameReducer';
 import cl from './FiltersViewModes.module.scss';
+import GearIcon from 'icons/gear_icon.png';
 
 const FiltersViewModes = () => {
   const viewMode = useSelector(state => state.game.viewMode);
@@ -16,20 +17,21 @@ const FiltersViewModes = () => {
   return (
     <div className={cl.viewModes}>
       <button className={getClassName('mode-1')} name='mode-1' onClick={handleModeClick}>
-        <div className={cl.camera}></div>
+        <div className={cl.camera}>Auto</div>
       </button>
       <button className={getClassName('mode-2')} name='mode-2' onClick={handleModeClick}>
-        <div className={cl.camera}></div>
-        <div className={cl.camera}></div>
+        <div className={cl.camera}>1</div>
+        <div className={cl.camera}>2</div>
       </button>
       <button className={getClassName('mode-3')} name='mode-3' onClick={handleModeClick}>
         <div>
-          <div className={cl.camera}></div>
+          <div className={cl.camera}>1</div>
         </div>
         <div>
-          <div className={cl.camera}></div>
-          <div className={cl.camera}></div>
+          <div className={cl.camera}>2</div>
+          <div className={cl.camera}>3</div>
         </div>
+				<img src={GearIcon} alt='gear-icon'/>
       </button>
       {/* <button className={getClassName('mode-3')} name='mode-3' onClick={handleModeClick}>
         <div></div>
@@ -38,10 +40,10 @@ const FiltersViewModes = () => {
         <div></div>
       </button> */}
       <button className={getClassName('mode-4')} name='mode-4' onClick={handleModeClick}>
-        <div className={cl.camera}></div>
-        <div className={cl.camera}></div>
-        <div className={cl.camera}></div>
-        <div className={cl.camera}></div>
+        <div className={cl.camera}>1</div>
+        <div className={cl.camera}>2</div>
+        <div className={cl.camera}>3</div>
+        <div className={cl.camera}>4</div>
       </button>
     </div>
   );
