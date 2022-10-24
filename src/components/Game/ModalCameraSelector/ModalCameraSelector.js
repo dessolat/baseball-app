eda74828c6b05e805c379bc156d1b4e5209f4e24
Igor Nikiforov <desola2@inbox.ru>
2022-10-24 11:31:36 +0300

    feat: add CAMERAS arr

--- src/components/Game/ModalCameraSelector/ModalCameraSelector.js
+++ src/components/Game/ModalCameraSelector/ModalCameraSelector.js
@@ -7,6 +7,16 @@ import { useDispatch } from 'react-redux';
 const ModalCameraSelector = () => {
   const dispatch = useDispatch();
 
+  const CAMERAS = [
+    { number: 1, isActive: true, title: 'Base left main' },
+    { number: 2, isActive: false, title: 'Base right main' },
+    { number: 3, isActive: true, title: 'Base left add' },
+    { number: 4, isActive: false, title: 'Base right add' },
+    { number: 5, isActive: false, title: 'Bat left' },
+    { number: 6, isActive: false, title: 'Bat right' },
+    { number: 7, isActive: true, title: 'Pitch' },
+    { number: 8, isActive: true, title: 'IP' }
+  ];
   return (
     <div className={cl.modalCameraSelectorWrapper}>
       <div className={cl.camerasContainer}></div>
