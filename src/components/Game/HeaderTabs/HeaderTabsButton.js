78e90f49728ff7ef089164ebdedc0b07ac6d7fe1
Igor Nikiforov <desola2@inbox.ru>
2022-10-25 10:02:36 +0300

    feat: add itemName const

--- src/components/Game/HeaderTabs/HeaderTabsButton.js
+++ src/components/Game/HeaderTabs/HeaderTabsButton.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { capitalizeFirstLetter } from 'utils';
 
 const HeaderTabsButton = ({ item, handleClick, getClass }) => {
+  const itemName = item === 'hit' ? 'hitting' : item === 'run' ? 'running' : item;
+
   return (
     <li>
-      <button name={item} onClick={handleClick} className={getClass(item)}>
-				{capitalizeFirstLetter(item)}
+      <button name={itemName} onClick={handleClick} className={getClass(itemName)}>
+        {capitalizeFirstLetter(item)}
       </button>
     </li>
   );
