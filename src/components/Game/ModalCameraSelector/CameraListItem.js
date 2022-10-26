77808ec2a6847932c9a354415dae4b148faa1c37
Igor Nikiforov <desola2@inbox.ru>
2022-10-26 15:01:10 +0300

    feat: rework styles

--- src/components/Game/ModalCameraSelector/CameraListItem.js
+++ src/components/Game/ModalCameraSelector/CameraListItem.js
@@ -2,14 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 import cl from './CameraList.module.scss';
 
-const CameraListItem = ({ camera, index }) => {
+const CameraListItem = ({ camera, activeMode, handleCameraClick }) => {
   const cameraClasses = classNames([cl.camera], {
-    [cl.active]: camera.isActive,
-		[cl.selected]: index === 6
+    [cl.active]: activeMode.cams_values.includes(camera.number),
+		[cl.selected]: camera.number === activeMode.cams_values[activeMode.active_cam - 1]
   });
 
   return (
-    <button className={cameraClasses} data-before={camera.number}>
+    <button className={cameraClasses} data-before={camera.number} onClick={e => handleCameraClick(activeMode.number, camera.number, activeMode.active_cam)}>
       {camera.title}
     </button>
   );
