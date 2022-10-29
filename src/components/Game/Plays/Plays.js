d299a101212907a88a9ba920b015083fd0498042
Igor Nikiforov <desola2@inbox.ru>
2022-10-29 22:36:09 +0300

    feat: remove useEffect for moments
    * wrap PlaysEvent with onlyMobileLandscape class

--- src/components/Game/Plays/Plays.js
+++ src/components/Game/Plays/Plays.js
@@ -4,9 +4,9 @@ import PlaysEvents from '../PlaysEvents/PlaysEvents';
 // import PlaysFooter from '../PlaysFooter/PlaysFooter';
 import PlaysContent from '../PlaysContent/PlaysContent';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSearchParam,  } from 'utils';
+import { getSearchParam } from 'utils';
 // import { setSearchParam } from 'utils';
-import { setCurrentTab  } from 'redux/gameReducer';
+import { setCurrentTab } from 'redux/gameReducer';
 // import { setPitchState } from 'redux/gameReducer';
 // import MobilePitcherFilters from '../Content/MobilePitcherFilters';
 
@@ -14,30 +14,29 @@ const Plays = ({ isVideo }) => {
   // const ptab = getSearchParam('ptab');
   // const defaultPtab = ['pitch', 'hitting', 'running'].includes(ptab) ? ptab : 'pitch';
   // const [currentTab, setCurrentTab] = useState(defaultPtab);
-  const [moments, setMoments] = useState([]);
+  // const [moments, setMoments] = useState([]);
   const currentCard = useSelector(state => state.game.currentCard);
   const currentTab = useSelector(state => state.game.currentTab);
   const dispatch = useDispatch();
 
-	useLayoutEffect(() => {
-		const tab = getSearchParam('tab');
-		const defaultPtab = ['pitch', 'hitting', 'running'].includes(tab) ? tab : 'pitch';
-		dispatch(setCurrentTab(defaultPtab))
-		// const ptab = getSearchParam('ptab');
-		// const defaultPtab = ['pitch', 'hitting', 'running'].includes(ptab) ? ptab : 'pitch';
-		// dispatch(setCurrentTab(defaultPtab))
-		// eslint-disable-next-line
-}, [])
-
-
-  useEffect(() => {
-    const newMoments = [];
-    currentCard.type !== 'Replacement'
-      ? currentCard.moments?.forEach(moment => moment.icons && newMoments.push(moment))
-      : newMoments.push(currentCard.moments[0]);
-    setMoments(newMoments);
+  useLayoutEffect(() => {
+    const tab = getSearchParam('tab');
+    const defaultPtab = ['pitch', 'hitting', 'running'].includes(tab) ? tab : 'pitch';
+    dispatch(setCurrentTab(defaultPtab));
+    // const ptab = getSearchParam('ptab');
+    // const defaultPtab = ['pitch', 'hitting', 'running'].includes(ptab) ? ptab : 'pitch';
+    // dispatch(setCurrentTab(defaultPtab))
     // eslint-disable-next-line
-  }, [currentCard]);
+  }, []);
+
+  // useEffect(() => {
+  //   const newMoments = [];
+  //   currentCard.type !== 'Replacement'
+  //     ? currentCard.moments?.forEach(moment => moment.icons && newMoments.push(moment))
+  //     : newMoments.push(currentCard.moments[0]);
+  //   setMoments(newMoments);
+  //   // eslint-disable-next-line
+  // }, [currentCard]);
 
   const classes = [cl.plays];
   classes.push(
@@ -63,13 +62,15 @@ const Plays = ({ isVideo }) => {
           {/* <div className={cl.landscapeDisplayNone}>
             <PlaysFooter currentTab={currentTab} handleClick={handleTabClick} />
           </div> */}
-            {/* <PlaysFooter currentTab={currentTab} handleClick={handleTabClick} /> */}
-          <PlaysContent moments={moments} currentTab={currentTab} />
+          {/* <PlaysFooter currentTab={currentTab} handleClick={handleTabClick} /> */}
+          <PlaysContent currentTab={currentTab} />
         </>
       )}
       {/* <div className={cl.landscapeDisplayNone}>
       </div> */}
-        <PlaysEvents moments={moments} />
+      <div className={cl.onlyMobileLandscape}>
+        <PlaysEvents />
+      </div>
     </div>
   );
 };
