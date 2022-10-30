0a92b0154813939ce8e459eca796b1b8d6d93e0c
Igor Nikiforov <desola2@inbox.ru>
2022-10-30 19:45:09 +0300

    feat: create VideoOptions p/holder

--- src/components/Game/VideoOptions/VideoOptions.js
+++ src/components/Game/VideoOptions/VideoOptions.js
@@ -2,7 +2,11 @@ import React from 'react';
 import cl from './VideoOptions.module.scss';
 
 const VideoOptions = () => {
-  return <div>VideoOptions</div>;
+  return (
+    <div className={cl.wrapper}>
+      <div className={cl.videoOptions}></div>
+    </div>
+  );
 };
 
 export default VideoOptions;
