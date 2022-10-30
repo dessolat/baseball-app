f0639fab678e774209032af6c5da34d04656901c
Igor Nikiforov <desola2@inbox.ru>
2022-10-30 18:37:39 +0300

    feat: add lines with text to render

--- src/components/Game/Timeline/Timeline.js
+++ src/components/Game/Timeline/Timeline.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import cl from './Timeline.module.scss';
 
 const Timeline = () => {
-	const LINES = [
+  const LINES = [
     { color: '#1A4C96' },
     { color: '#FFAB00' },
     { color: '#BF8610' },
     { color: '#8D6004' },
     { color: '#5C4006' }
   ];
-	
-  return <div>Timeline</div>;
+
+  return (
+    <svg viewBox='0 0 876 52' className={cl.chart}>
+      {/* Horizontal lines */}
+      {LINES.map(({ color }, i) => (
+        <line key={i} x1='0' y1={(i + 1) * 9} x2='876' y2={(i + 1) * 9} stroke={color} strokeWidth='1' />
+      ))}
+
+      {/* Lines text */}
+      {LINES.map((_, i) => (
+        <Fragment key={i}>
+          <rect x={55 + i * 31} y={(i + 1) * 9 - 6} width='24' height='10' fill='#fff' />
+          <text x={57.87 + i * 31} y={(i + 1) * 9 + 2.9} className={cl.horizontalLineText}>
+            P-H
+          </text>
+          <rect x={55 + i * 31 + 273} y={(i + 1) * 9 - 6} width='24' height='10' fill='#fff' />
+          <text x={57.87 + i * 31 + 273} y={(i + 1) * 9 + 2.9} className={cl.horizontalLineText}>
+            P-H
+          </text>
+          <rect x={55 + i * 31 + 546} y={(i + 1) * 9 - 6} width='24' height='10' fill='#fff' />
+          <text x={57.87 + i * 31 + 546} y={(i + 1) * 9 + 2.9} className={cl.horizontalLineText}>
+            P-H
+          </text>
+        </Fragment>
+      ))}
+    </svg>
+  );
 };
 
 export default Timeline;
