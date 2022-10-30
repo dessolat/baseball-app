f95a4b9e916ace678ecd2f4e2af2cc4ab14d7779
Igor Nikiforov <desola2@inbox.ru>
2022-10-30 18:33:46 +0300

    feat: add LINES conts

--- src/components/Game/Timeline/Timeline.js
+++ src/components/Game/Timeline/Timeline.js
@@ -2,6 +2,14 @@ import React from 'react';
 import cl from './Timeline.module.scss';
 
 const Timeline = () => {
+	const LINES = [
+    { color: '#1A4C96' },
+    { color: '#FFAB00' },
+    { color: '#BF8610' },
+    { color: '#8D6004' },
+    { color: '#5C4006' }
+  ];
+	
   return <div>Timeline</div>;
 };
 
