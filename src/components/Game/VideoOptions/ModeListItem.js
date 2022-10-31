a9020dcdc1566bdb1681bdaf03a6487518e44046
Igor Nikiforov <desola2@inbox.ru>
2022-10-31 23:49:25 +0300

    feat: add item render
    * integrate classNames

--- src/components/Game/VideoOptions/ModeListItem.js
+++ src/components/Game/VideoOptions/ModeListItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import cl from './VideoOptions.module.scss';
+import classNames from 'classnames';
 
-const ModeListItem = () => {
-  return <div>ModeListItem</div>;
+const ModeListItem = ({ mode, currentMode }) => {
+  const itemClasses = classNames({
+    [cl.active]: mode === currentMode
+  });
+
+  return <li className={itemClasses}>{mode}</li>;
 };
 
 export default ModeListItem;
