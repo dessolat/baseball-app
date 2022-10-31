1d79e470aaf649bedd60d2841323151359988b14
Igor Nikiforov <desola2@inbox.ru>
2022-10-31 23:42:15 +0300

    feat: add state

--- src/components/Game/VideoOptions/ModeList.js
+++ src/components/Game/VideoOptions/ModeList.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cl from './VideoOptions.module.scss';
 
 const ModeList = () => {
+  const [mode, setMode] = useState('Full');
+	
+  const MODES = ['Full', 'Short', 'Super Short'];
   return (
     <ul className={cl.modeList}>
       <li>Full</li>
       <li>Short</li>
-      <li className={cl.active}>
-        Super
-        <br />
-        Short
-      </li>
+      <li>Super Short</li>
     </ul>
   );
 };
