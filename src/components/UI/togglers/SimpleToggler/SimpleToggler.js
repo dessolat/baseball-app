ba48f32fa52afd9497a3e0e703ceff694a9e6f23
Igor Nikiforov <desola2@inbox.ru>
2022-11-01 12:34:56 +0300

    feat: add functional

--- src/components/UI/togglers/SimpleToggler/SimpleToggler.js
+++ src/components/UI/togglers/SimpleToggler/SimpleToggler.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import cl from './SimpleToggler.module.scss';
+import React, { useState } from 'react';
+import './SimpleToggler.module.scss';
 
 const SimpleToggler = () => {
-  return <div>SimpleToggler</div>;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleClick = () => setIsChecked(prev => !prev);
+  return (
+    <label>
+      <input type='checkbox' checked={isChecked} onChange={handleClick} />
+    </label>
+  );
 };
 
 export default SimpleToggler;
