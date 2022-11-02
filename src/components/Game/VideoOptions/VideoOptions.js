968efab677e260bac5cbd84982f5cd61808c2489
Igor Nikiforov <desola2@inbox.ru>
2022-11-02 09:27:42 +0300

    feat: import SpeedSelector comp

--- src/components/Game/VideoOptions/VideoOptions.js
+++ src/components/Game/VideoOptions/VideoOptions.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Effects from './Effects';
 import ModeList from './ModeList';
+import SpeedSelector from './SpeedSelector';
 import cl from './VideoOptions.module.scss';
 
 const VideoOptions = () => {
   return (
     <div className={cl.wrapper}>
       <div className={cl.videoOptions}>
-				<ModeList />
-				<Effects />
-			</div>
+        <ModeList />
+        <Effects />
+        <SpeedSelector />
+      </div>
     </div>
   );
 };
