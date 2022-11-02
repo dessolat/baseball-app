c32515a5dfde0ae41e38917289ec4f9adc903fe8
Igor Nikiforov <desola2@inbox.ru>
2022-11-02 15:32:53 +0300

    feat: create fullscreen functional

--- src/components/Game/Videos/Videos.js
+++ src/components/Game/Videos/Videos.js
@@ -1,35 +1,35 @@
 // import useCurrentEvents from 'hooks/useCurrentEvents';
-import React from 'react';
+import React, { useRef } from 'react';
 // import PlaysEvents from '../PlaysEvents/PlaysEvents';
 import cl from './Videos.module.scss';
 import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 // import VideoEventsList from '../VideoEventsList/VideoEventsList';
 import VideoList from '../VideoList/VideoList';
+import FullscreenIcon from 'icons/fullscreen_icon.png';
+import CloseFullscreenIcon from 'icons/close_fullscreen_icon.png';
 
 const Videos = () => {
+  const wrapperRef = useRef();
+  const controlsWrapperRef = useRef();
+  const timerRef = useRef();
+
   const viewMode = useSelector(state => state.game.viewMode);
 
-//   const ref = useRef();
-
-//   useEffect(() => {
-//     function openFullscreen() {
-// console.log(ref.current.requestFullscreen);
-
-
-//       if (ref.current.webkitRequestFullscreen) {
-//         ref.current.webkitRequestFullscreen();
-//       } else if (ref.current.webkitRequestFullscreen) {
-//         /* Safari */
-//         ref.current.webkitRequestFullscreen();
-//       } else if (ref.current.msRequestFullscreen) {
-//         /* IE11 */
-//         ref.current.msRequestFullscreen();
-//       }
-//     }
-
-// 		openFullscreen()
-//   }, [viewMode]);
+  //   useEffect(() => {
+  // function openFullscreen() {
+  //   if (ref.current.webkitRequestFullscreen) {
+  //     ref.current.webkitRequestFullscreen();
+  //   } else if (ref.current.webkitRequestFullscreen) {
+  //     /* Safari */
+  //     ref.current.webkitRequestFullscreen();
+  //   } else if (ref.current.msRequestFullscreen) {
+  //     /* IE11 */
+  //     ref.current.msRequestFullscreen();
+  //   }
+  // }
+  // 		openFullscreen()
+  //   }, [viewMode]);
 
   const wrapperClasses = classNames(cl.wrapper, {
     [cl.videos1]: viewMode === 'mode-1',
@@ -38,12 +38,45 @@ const Videos = () => {
     [cl.videos4]: viewMode === 'mode-4'
   });
 
+  const openFullscreen = ref => () => {
+    if (ref.current.webkitRequestFullscreen) {
+      ref.current.webkitRequestFullscreen();
+    } else if (ref.current.webkitRequestFullscreen) {
+      /* Safari */
+      ref.current.webkitRequestFullscreen();
+    } else if (ref.current.msRequestFullscreen) {
+      /* IE11 */
+      ref.current.msRequestFullscreen();
+    }
+  };
+  const closeFullscreen = () => document.exitFullscreen();
+
+  const handleMouseMove = () => {
+    console.log(123);
+    clearTimeout(timerRef.current);
+
+    controlsWrapperRef.current.style.opacity = 1;
+
+    timerRef.current = setTimeout(() => {
+      controlsWrapperRef.current.style.opacity = 0;
+    }, 1000);
+  };
   return (
     <>
-      <div className={wrapperClasses}>
-      {/* <div className={wrapperClasses} ref={ref}> */}
+      <div className={wrapperClasses} onMouseMove={handleMouseMove} ref={wrapperRef}>
+        {/* <div className={wrapperClasses} ref={ref}> */}
         <VideoList viewMode={viewMode} />
         {/* <VideoEventsList /> */}
+        <div className={cl.controlsWrapper} ref={controlsWrapperRef}>
+          <div className={cl.controls}>
+            <button onClick={openFullscreen(wrapperRef)} className={cl.openFullscreen}>
+              <img src={FullscreenIcon} alt='fullscreen-icon' />
+            </button>
+            <button onClick={closeFullscreen} className={cl.closeFullscreen}>
+              <img src={CloseFullscreenIcon} alt='fullscreen-icon' />
+            </button>
+          </div>
+        </div>
       </div>
       {/* <div className={cl.eventsWrapper}>
         <PlaysEvents />
