8a2651996d93b036fede953a2f5cc4678ce395ad
Igor Nikiforov <desola2@inbox.ru>
2022-11-02 09:27:03 +0300

    feat: add image to SpeedSelector comp

--- src/components/Game/VideoOptions/SpeedSelector.js
+++ src/components/Game/VideoOptions/SpeedSelector.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import cl from './VideoOptions.module.scss';
+import TriangleIcon from 'icons/triangle_icon.svg';
 
 const SpeedSelector = () => {
-	return (
-		<div>SpeedSelector</div>
-	)
-}
+  return (
+    <div className={cl.speedSelectorWrapper}>
+      <p>Speed</p>
+      <div>
+        x2 <img src={TriangleIcon} />
+      </div>
+    </div>
+  );
+};
 
-export default SpeedSelector
+export default SpeedSelector;
