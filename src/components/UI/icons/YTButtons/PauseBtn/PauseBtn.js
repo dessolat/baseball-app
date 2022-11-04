4435b292315a735581531874b2e6b1ba7867c94a
Igor Nikiforov <desola2@inbox.ru>
2022-11-04 17:11:47 +0300

    fix: className issue

--- src/components/UI/icons/YTButtons/PauseBtn/PauseBtn.js
+++ src/components/UI/icons/YTButtons/PauseBtn/PauseBtn.js
@@ -9,9 +9,9 @@ const PauseBtn = ({ setPlayPause }) => {
   return (
     <button className={cl.pauseBtn} onClick={handleClickBtn}>
       <svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%'>
-        <use class='ytp-svg-shadow'></use>
+        <use className='ytp-svg-shadow'></use>
         <path
-          class='ytp-svg-fill'
+          className='ytp-svg-fill'
           d='M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z'
           id='ytp-id-592'></path>
       </svg>
