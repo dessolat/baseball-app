a7348eaca05a82d5a4f91fd9c46ba654e74cf8c6
Igor Nikiforov <desola2@inbox.ru>
2022-11-07 16:27:27 +0300

    feat: add totals to pitcher

--- src/components/Game/ContentPitcher/ContentPitcher.js
+++ src/components/Game/ContentPitcher/ContentPitcher.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import cl from './ContentPitcher.module.scss';
+// import PlaysEventsTotal from '../PlaysEvents/PlaysEventsTotal';
 
 const ContentPitcher = () => {
   const currentCard = useSelector(state => state.game.currentCard);
+  const moments = useSelector(state => state.game.moments);
+
+  const { balls_count, strikes_count } =
+    (moments.length > 0 ? moments.slice(-1)[0].pitcher : currentCard.moments?.slice(-1)[0].pitcher) || '';
 
   const pitcherName = currentCard.moments?.slice(-1)[0]?.pitcher.pitches_name;
   const isPitcher = currentCard.moments;
 
-  return <p className={cl.playerName}>Pitcher: {isPitcher && <span>{pitcherName}</span>}</p>;
+  return (
+    <div>
+      <p className={cl.playerName}>Pitcher: {isPitcher && <span>{pitcherName}</span>}</p>
+
+      {/* <PlaysEventsTotal moments={moments}/> */}
+      <p className={cl.playerName}>
+        Total:{' '}
+        {isPitcher && (
+          <>
+            <span>
+              {balls_count + strikes_count} ({balls_count} ball, {strikes_count} strike)
+            </span>
+          </>
+        )}
+      </p>
+    </div>
+  );
 };
 
 export default ContentPitcher;
