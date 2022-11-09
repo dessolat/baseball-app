4255dd21aa3fa5449b59c1156aa36ac720219207
Igor Nikiforov <desola2@inbox.ru>
2022-11-09 12:15:09 +0300

    feat: import PitchVideo & PitchValues comp

--- src/components/Game/PlaysPitch/PlaysPitch.js
+++ src/components/Game/PlaysPitch/PlaysPitch.js
@@ -3,6 +3,8 @@ import PlaysField from '../PlaysField/PlaysField';
 import PlaysSpin from '../PlaysSpin/PlaysSpin';
 import PlaysSpeed from '../PlaysSpeed/PlaysSpeed';
 import { useSelector } from 'react-redux';
+import PitchVideo from './PitchVideo/PitchVideo';
+import PitchValues from './PitchValues/PitchValues';
 
 const PlaysPitch = () => {
 	const currentMoment = useSelector(state => state.game.currentMoment)
@@ -11,6 +13,10 @@ const PlaysPitch = () => {
       <PlaysField currentMoment={currentMoment} />
       <PlaysSpeed currentMoment={currentMoment} />
       <PlaysSpin pitch={currentMoment?.metering?.pitch} />
+			<PitchVideo position='top-left'/>
+			<PitchVideo position='top-right'/>
+			<PitchVideo position='bottom'/>
+			<PitchValues />
     </>
   );
 };
