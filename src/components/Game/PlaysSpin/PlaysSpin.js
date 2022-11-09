137aeed55a0155a368e7ef943a40bfc3bab365fd
Igor Nikiforov <desola2@inbox.ru>
2022-11-09 12:11:13 +0300

    feat: rework spin chart

--- src/components/Game/PlaysSpin/PlaysSpin.js
+++ src/components/Game/PlaysSpin/PlaysSpin.js
@@ -1,19 +1,103 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import { useSelector } from 'react-redux';
 import cl from './PlaysSpin.module.scss';
 import PlaysSpinChart from './PlaysSpinChart';
-import PlaysSpinInfo from './PlaysSpinInfo';
+// import PlaysSpinInfo from './PlaysSpinInfo';
 
 const PlaysSpin = ({ pitch }) => {
+  const [chartData, setChartData] = useState([]);
+  const [currentDot, setCurrentDot] = useState({ type: null, offsetX: null, offsetY: null });
+
   const pitchState = useSelector(state => state.game.pitchState);
+  const currentCard = useSelector(state => state.game.currentCard);
+  const innings = useSelector(state => state.game.innings);
+  const currentMoment = useSelector(state => state.game.currentMoment);
+
+  useLayoutEffect(() => {
+    if (Object.keys(currentCard).length === 0) {
+      setChartData([]);
+      return;
+    }
+
+    const testData = [];
+
+    const { metering } = currentMoment;
+
+    let currentDotParams = {
+			type: metering?.pitch?.pitch_type,
+      offsetX: metering?.pitch?.offset_x,
+      offsetY: metering?.pitch?.offset_y
+    };
+		
+		console.log(currentDotParams);
+
+    innings.forEach(inning => {
+      inning['top/guests'].forEach(card =>
+        card.moments.forEach(moment => {
+          const pitchType = moment.metering.pitch?.pitch_type;
+
+          // if (
+          //   moment.inner.id === currentMoment?.inner?.id &&
+          //   moment.pitcher?.pitches_name === currentMoment?.pitcher?.pitches_name &&
+          //   moment.metering.pitch
+          // ) {
+          //   currentDotParams.type = pitchType;
+          //   currentDotParams.offsetX = moment.metering.pitch.offset_x;
+          //   currentDotParams.offsetY = moment.metering.pitch.offset_y;
+          //   currentDotParams.index = testData.length;
+          // }
+
+          if (
+            moment.pitcher?.pitches_name === currentMoment?.pitcher?.pitches_name &&
+            moment.metering.pitch
+          ) {
+            testData.push({
+              pitchType,
+              offsetX: moment.metering.pitch.offset_x,
+              offsetY: moment.metering.pitch.offset_y
+            });
+          }
+        })
+      );
+      inning['bottom/owners']?.forEach(card =>
+        card.moments.forEach(moment => {
+          const pitchType = moment.metering.pitch?.pitch_type;
+          // if (
+          //   moment.inner.id === currentMoment?.inner?.id &&
+          //   moment.pitcher?.pitches_name === currentMoment?.pitcher?.pitches_name &&
+          //   moment.metering.pitch
+          // ) {
+          //   currentDotParams.type = pitchType;
+          //   currentDotParams.offsetX = moment.metering.pitch.offset_x;
+          //   currentDotParams.offsetY = moment.metering.pitch.offset_y;
+          //   currentDotParams.index = testData.length;
+          // }
+
+          if (
+            moment.pitcher?.pitches_name === currentMoment?.pitcher?.pitches_name &&
+            moment.metering.pitch
+          ) {
+            testData.push({
+              pitchType,
+              offsetX: moment.metering.pitch.offset_x,
+              offsetY: moment.metering.pitch.offset_y
+            });
+          }
+        })
+      );
+    });
+
+    setChartData(testData);
+    setCurrentDot(currentDotParams);
+  }, [currentCard, innings, currentMoment]);
 
   const wrapperClasses = classNames(cl.spin, {
     [cl.dnone]: pitchState === 'Field'
   });
   return (
     <div className={wrapperClasses}>
-      <PlaysSpinChart pitch={pitch} />
+      <PlaysSpinChart chartData={chartData} currentDot={currentDot} />
       {/* <PlaysSpinInfo pitch={pitch} /> */}
     </div>
   );
--- src/components/Game/PlaysSpin/PlaysSpinChart.js
+++ src/components/Game/PlaysSpin/PlaysSpinChart.js
@@ -1,21 +1,29 @@
 import React, { useState, useLayoutEffect } from 'react';
 
-const PlaysSpinChart = ({ pitch }) => {
-  const { offset_x, offset_y } = pitch || 0;
+const DOT_RADIUS = 3;
+const COLORS = {
+  0: '#1A4C96',
+  1: 'red',
+  2: 'green',
+  3: 'olive'
+};
+
+const PlaysSpinChart = ({ chartData, currentDot }) => {
+  // const { offset_x, offset_y } = pitch || 0;
 
   const [currentDotRadius, setCurrentDotRadius] = useState(0);
 
   useLayoutEffect(() => {
-    if (pitch === null) return;
+    // if (pitch === null) return;
 
     setCurrentDotRadius(prev => (prev === 1 ? 0.99999 : 1));
     // eslint-disable-next-line
-  }, [offset_x, offset_y]);
+  }, [currentDot]);
 
   useLayoutEffect(() => {
-    if (pitch === null) return;
+    // if (pitch === null) return;
 
-    currentDotRadius < 7.5 &&
+    currentDotRadius < 5 &&
       setTimeout(() => {
         setCurrentDotRadius(prev => (prev + 0.45 > 7.5 ? 7.5 : prev + 0.45));
       }, 10);
@@ -23,14 +31,41 @@ const PlaysSpinChart = ({ pitch }) => {
     // eslint-disable-next-line
   }, [currentDotRadius]);
 
-  const isDrawBall = pitch;
-  const coordX = 50 + 50 * offset_x;
-  const coordY = 50 - 50 * offset_y;
+  const isCurrentDot = currentDot.offsetX !== undefined;
+  const currentCoordX = 50 + 50 * currentDot.offsetX;
+  const currentCoordY = 50 - 50 * currentDot.offsetY;
+
   return (
     <svg width='100' height='100' viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <line x1='49.5' y1='100' x2='49.5' stroke='#ACACAC' />
       <line y1='49.5' x2='100' y2='49.5' stroke='#ACACAC' />
-      {isDrawBall && <circle cx={coordX} cy={coordY} r={currentDotRadius} fill='#2B9D6A' />}
+      {chartData.map((dot, i) => {
+        const coordX = 50 + 50 * dot.offsetX;
+        const coordY = 50 - 50 * dot.offsetY;
+
+        return (
+          <circle
+            key={i}
+            cx={coordX}
+            cy={coordY}
+            r={DOT_RADIUS}
+            fill={COLORS[dot.pitchType - 1]}
+            stroke='black'
+            strokeWidth='0.5'
+          />
+        );
+      })}
+
+      {isCurrentDot && (
+        <circle
+          cx={currentCoordX}
+          cy={currentCoordY}
+          r={currentDotRadius}
+          fill={COLORS[currentDot.type - 1]}
+          stroke='black'
+          strokeWidth='0.5'
+        />
+      )}
     </svg>
   );
 };
