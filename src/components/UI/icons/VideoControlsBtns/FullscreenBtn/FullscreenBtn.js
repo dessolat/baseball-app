849a851b6c53f7b974b4a70ecae45b86f1f88e39
Igor Nikiforov <desola2@inbox.ru>
2022-11-09 12:08:41 +0300

    fix: fullscreen btn dimensions

--- src/components/UI/icons/VideoControlsBtns/FullscreenBtn/FullscreenBtn.js
+++ src/components/UI/icons/VideoControlsBtns/FullscreenBtn/FullscreenBtn.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FullscreenBtn = ({ isOff }) => {
   return (
-    <svg width='89' height='89' viewBox='0 0 89 89' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <svg width='100' height='100' viewBox='0 0 89 89' fill='none' xmlns='http://www.w3.org/2000/svg'>
       {isOff ? (
         <>
           <path
