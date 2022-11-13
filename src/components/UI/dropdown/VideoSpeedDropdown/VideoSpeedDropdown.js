829c168ad3966637f8451f73f9ddee66871544d5
Igor Nikiforov <desola2@inbox.ru>
2022-11-13 21:16:07 +0300

    feat: create VideoSpeedDropdown p/holder

---
+++ src/components/UI/dropdown/VideoSpeedDropdown/VideoSpeedDropdown.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from './VideoSpeedDropdown.module.scss';
+
+const VideoSpeedDropdown = () => {
+	return (
+		<div>VideoSpeedDropdown</div>
+	)
+}
+
+export default VideoSpeedDropdown
