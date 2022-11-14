7c1c9e3b4b26f81447cac123b9ddc0a8cf2014a1
Igor Nikiforov <desola2@inbox.ru>
2022-11-14 12:05:10 +0300

    feat: import VideoSpeedDropdown

--- src/components/Game/VideoOptions/SpeedSelector.js
+++ src/components/Game/VideoOptions/SpeedSelector.js
@@ -1,15 +1,22 @@
+import VideoSpeedDropdown from 'components/UI/dropdown/VideoSpeedDropdown/VideoSpeedDropdown';
+import ArrowDown from 'components/UI/icons/ArrowDown';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import cl from './VideoOptions.module.scss';
-import TriangleIcon from 'icons/triangle_icon.svg';
+// import TriangleIcon from 'icons/triangle_icon.svg';
 
 const SpeedSelector = () => {
+	const speedOptions = [0.25, 1, 2]
+	const videoPlaybackRate = useSelector(state => state.game.videoPlaybackRate)
+
   return (
-    <div className={cl.speedSelectorWrapper}>
-      <p>Speed</p>
-      <div>
-        x2 <img src={TriangleIcon} alt='triangle-icon' />
-      </div>
-    </div>
+    // <div className={cl.speedSelectorWrapper}>
+    //   <p>Speed</p>
+    //   <div>
+    //     x2 <ArrowDown />
+    //   </div>
+    // </div>
+		<VideoSpeedDropdown title='Speed x2 ' options={speedOptions} currentOption={videoPlaybackRate}/>
   );
 };
 
