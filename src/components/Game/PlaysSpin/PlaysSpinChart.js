2242412b75cd688221862e45cbd4ed5cf47b1eeb
Igor Nikiforov <desola2@inbox.ru>
2022-11-20 15:11:47 +0300

    feat: add GRAPH_START coords
    * add lines titles

--- src/components/Game/PlaysSpin/PlaysSpinChart.js
+++ src/components/Game/PlaysSpin/PlaysSpinChart.js
@@ -1,6 +1,9 @@
 import React, { useState, useLayoutEffect } from 'react';
+import cl from './PlaysSpin.module.scss'
 
 const DOT_RADIUS = 3;
+const GRAPH_START_X = 15;
+const GRAPH_START_Y = 10;
 const COLORS = {
   0: '#1A4C96',
   1: 'red',
@@ -36,9 +39,58 @@ const PlaysSpinChart = ({ chartData, currentDot }) => {
   const currentCoordY = 50 - 50 * currentDot.offsetY;
 
   return (
-    <svg width='100' height='100' viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <line x1='49.5' y1='100' x2='49.5' stroke='#ACACAC' />
-      <line y1='49.5' x2='100' y2='49.5' stroke='#ACACAC' />
+    <svg width='100%' height='100%' viewBox='0 0 115 135' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      {/* Render axis lines */}
+      <line x1={GRAPH_START_X + 49.5} y1={GRAPH_START_Y + 0} x2={GRAPH_START_X + 49.5} y2={GRAPH_START_Y + 100} stroke='#ACACAC' />
+      <line x1={GRAPH_START_X} y1={GRAPH_START_Y + 49.5} x2={GRAPH_START_X + 100} y2={GRAPH_START_Y + 49.5} stroke='#ACACAC' />
+
+      {/* Render horizontal grid lines */}
+      <line x1={GRAPH_START_X} y1={GRAPH_START_Y+0} x2={GRAPH_START_X + 100} y2={GRAPH_START_Y+0} stroke='#E3E1E1' stroke-dasharray='4 2' />
+      <line x1={GRAPH_START_X} y1={GRAPH_START_Y+25} x2={GRAPH_START_X + 100} y2={GRAPH_START_Y+25} stroke='#E3E1E1' stroke-dasharray='4 2' />
+      <line x1={GRAPH_START_X} y1={GRAPH_START_Y+75} x2={GRAPH_START_X + 100} y2={GRAPH_START_Y+75} stroke='#E3E1E1' stroke-dasharray='4 2' />
+      <line x1={GRAPH_START_X} y1={GRAPH_START_Y+100} x2={GRAPH_START_X + 100} y2={GRAPH_START_Y+100} stroke='#E3E1E1' stroke-dasharray='4 2' />
+
+      {/* Render horizontal lines text */}
+      <text x={GRAPH_START_X - 15} y='15' stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       10
+      </text>
+      <text x={GRAPH_START_X - 15} y='40' stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       5
+      </text>
+      <text x={GRAPH_START_X - 15} y='65' stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       0
+      </text>
+      <text x={GRAPH_START_X - 15} y='90' stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       -5
+      </text>
+      <text x={GRAPH_START_X - 15} y='115' stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       -10
+      </text>
+
+      {/* Render vertical grid lines */}
+      <line x1={GRAPH_START_X + 0} y1={GRAPH_START_Y} x2={GRAPH_START_X + 0} y2={GRAPH_START_Y+100} stroke='#E3E1E1' stroke-dasharray='4 2' />
+      <line x1={GRAPH_START_X + 25} y1={GRAPH_START_Y} x2={GRAPH_START_X + 25} y2={GRAPH_START_Y+100} stroke='#E3E1E1' stroke-dasharray='4 2' />
+      <line x1={GRAPH_START_X + 75} y1={GRAPH_START_Y} x2={GRAPH_START_X + 75} y2={GRAPH_START_Y+100} stroke='#E3E1E1' stroke-dasharray='4 2' />
+      <line x1={GRAPH_START_X + 100} y1={GRAPH_START_Y} x2={GRAPH_START_X + 100} y2={GRAPH_START_Y+100} stroke='#E3E1E1' stroke-dasharray='4 2' />
+
+			{/* Render vertical lines text */}
+      <text x={GRAPH_START_X + 0} y={GRAPH_START_Y+117} stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       10
+      </text>
+      <text x={GRAPH_START_X + 25} y={GRAPH_START_Y+117} stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       5
+      </text>
+      <text x={GRAPH_START_X + 50} y={GRAPH_START_Y+117} stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       0
+      </text>
+      <text x={GRAPH_START_X + 75} y={GRAPH_START_Y+117} stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       -5
+      </text>
+      <text x={GRAPH_START_X + 100} y={GRAPH_START_Y+117} stroke='black' textAnchor='middle' className={cl.graphTitle}>
+       -10
+      </text>
+
+      {/* Render dots */}
       {chartData.map((dot, i) => {
         const coordX = 50 + 50 * dot.offsetX;
         const coordY = 50 - 50 * dot.offsetY;
@@ -46,8 +98,8 @@ const PlaysSpinChart = ({ chartData, currentDot }) => {
         return (
           <circle
             key={i}
-            cx={coordX}
-            cy={coordY}
+            cx={coordX + GRAPH_START_X}
+            cy={coordY + GRAPH_START_Y}
             r={DOT_RADIUS}
             fill={COLORS[dot.pitchType - 1]}
             stroke='black'
@@ -56,10 +108,11 @@ const PlaysSpinChart = ({ chartData, currentDot }) => {
         );
       })}
 
+      {/* Render current dot  */}
       {isCurrentDot && (
         <circle
-          cx={currentCoordX}
-          cy={currentCoordY}
+          cx={currentCoordX + GRAPH_START_X}
+          cy={currentCoordY + GRAPH_START_Y}
           r={currentDotRadius}
           fill={COLORS[currentDot.type - 1]}
           stroke='black'
