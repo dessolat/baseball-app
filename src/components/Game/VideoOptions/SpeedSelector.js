02e9024b95a9a96588c6de484ef87b568a5fdd72
Igor Nikiforov <desola2@inbox.ru>
2022-11-20 14:17:24 +0300

    feat: add more video speeds

--- src/components/Game/VideoOptions/SpeedSelector.js
+++ src/components/Game/VideoOptions/SpeedSelector.js
@@ -1,13 +1,10 @@
 import VideoSpeedDropdown from 'components/UI/dropdown/VideoSpeedDropdown/VideoSpeedDropdown';
-// import ArrowDown from 'components/UI/icons/ArrowDown';
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setVideoPlaybackRate } from 'redux/gameReducer';
-// import cl from './VideoOptions.module.scss';
-// import TriangleIcon from 'icons/triangle_icon.svg';
 
 const SpeedSelector = () => {
-  const speedOptions = [0.25, 1, 2];
+  const speedOptions = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
   const videoPlaybackRate = useSelector(state => state.game.videoPlaybackRate);
 
   const dispatch = useDispatch();
@@ -16,12 +13,6 @@ const SpeedSelector = () => {
     dispatch(setVideoPlaybackRate(option));
   };
   return (
-    // <div className={cl.speedSelectorWrapper}>
-    //   <p>Speed</p>
-    //   <div>
-    //     x2 <ArrowDown />
-    //   </div>
-    // </div>
     <VideoSpeedDropdown
       title='Speed x2 '
       options={speedOptions}
