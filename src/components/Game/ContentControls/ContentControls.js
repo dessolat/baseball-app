c6d58a13530a209d47acd33ffd0620c748f94ed1
Igor Nikiforov <desola2@inbox.ru>
2022-11-21 15:25:02 +0300

    feat: import FirstLastMoment comp

--- src/components/Game/ContentControls/ContentControls.js
+++ src/components/Game/ContentControls/ContentControls.js
@@ -2,11 +2,13 @@ import React from 'react';
 import cl from './ContentControls.module.scss';
 import PlayOnline from 'components/UI/buttons/PlayOnline/PlayOnline';
 import { useDispatch, useSelector } from 'react-redux';
-import { setPlaybackMode } from 'redux/gameReducer';
+import { setIsLastMomentMode, setPlaybackMode } from 'redux/gameReducer';
 import ForwardRepeat from 'components/UI/buttons/ForwardRepeat/ForwardRepeat';
+import FirstLastMoment from 'components/UI/buttons/FirstLastMoment/FirstLastMoment';
 
 const ContentControls = ({ noPlayPause = false, isPlayOnline = true, ...props }) => {
   const playbackMode = useSelector(state => state.game.playbackMode);
+  const isLastMomentMode = useSelector(state => state.game.isLastMomentMode);
   const dispatch = useDispatch();
 
   const playbackModeClick = e => {
@@ -14,6 +16,8 @@ const ContentControls = ({ noPlayPause = false, isPlayOnline = true, ...props })
       e.currentTarget.name === 'play-pause' ? (playbackMode === 'play' ? 'pause' : 'play') : 'playOnline';
     dispatch(setPlaybackMode(newMode));
   };
+
+  const handlefirstLastBtn = () => dispatch(setIsLastMomentMode());
 
   const getClassName = name => {
     if (
@@ -27,12 +31,18 @@ const ContentControls = ({ noPlayPause = false, isPlayOnline = true, ...props })
   return (
     <div className={cl.controls} {...props}>
       {!noPlayPause && (
-        <ForwardRepeat
-          name='play-pause'
-          onClick={playbackModeClick}
-          className={getClassName('play-pause')}
-          playbackMode={playbackMode}
-        />
+        <>
+          <ForwardRepeat
+            name='play-pause'
+            onClick={playbackModeClick}
+            className={getClassName('play-pause')}
+            playbackMode={playbackMode}
+          />
+          <FirstLastMoment
+            onClick={handlefirstLastBtn}
+						isLastMomentMode={isLastMomentMode}
+          />
+        </>
       )}
       {isPlayOnline && (
         <PlayOnline name='play-online' onClick={playbackModeClick} className={getClassName('play-online')} />
