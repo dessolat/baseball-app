73b76b74338be61df04a6e7f344c70f2a7e9cd33
Igor Nikiforov <desola2@inbox.ru>
2022-11-22 17:51:54 +0300

    feat: render card stats if 2b,3b,hr !== undefined

--- src/components/Game/ContentCardTitle/ContentCardTitle.js
+++ src/components/Game/ContentCardTitle/ContentCardTitle.js
@@ -4,18 +4,21 @@ import cl from './ContentCardTitle.module.scss';
 const ContentCardTitle = ({ player }) => {
   const { h, '2b': b2, '3b': b3, ab, hr, pa } = player;
 
-  const getStats = () => {
-			let subStr = []
-			b2 && subStr.push(`${b2} 2B`)
-			b3 && subStr.push(`${b3} 3B`)
-			hr && subStr.push(`${hr} HR`)
-			subStr = subStr.join(', ')
-			const isSubstr = subStr.length > 0
+  const isStats = b2 !== undefined && b3 !== undefined && hr !== undefined;
 
-      return `PA:${pa}, ${h}of${ab}${isSubstr ? `(${subStr})` : ''}`;
+  const getStats = () => {
+    let subStr = [];
+    b2 && subStr.push(`${b2} 2B`);
+    b3 && subStr.push(`${b3} 3B`);
+    hr && subStr.push(`${hr} HR`);
+    subStr = subStr.join(', ');
+    const isSubstr = subStr.length > 0;
+
+    return `PA:${pa}, ${h}of${ab}${isSubstr ? `(${subStr})` : ''}`;
   };
 
-  return <p className={cl.playerName}>{`${player.hit_order}. ${player.who} ${getStats()}`}</p>;
+  const stats = isStats ? getStats() : '';
+  return <p className={cl.playerName}>{`${player.hit_order}. ${player.who} ${stats}`}</p>;
 };
 
 export default ContentCardTitle;
