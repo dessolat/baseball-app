933facdc29a7450b99523fa9974f398a7539331d
Igor Nikiforov <desola2@inbox.ru>
2022-11-23 14:54:33 +0300

    feat: add basic videos to pitch

--- src/components/Game/PlaysPitch/PitchVideo/PitchVideo.js
+++ src/components/Game/PlaysPitch/PitchVideo/PitchVideo.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import TopLeftVideoThumb from 'images/top_left_video_thumb.jpg';
-import TopRightVideoThumb from 'images/top_right_video_thumb.jpg';
-import BottomVideoThumb from 'images/bottom_video_thumb.jpg';
+import React, { useRef } from 'react';
+// import TopLeftVideoThumb from 'images/top_left_video_thumb.jpg';
+// import TopRightVideoThumb from 'images/top_right_video_thumb.jpg';
+// import BottomVideoThumb from 'images/bottom_video_thumb.jpg';
 import cl from './PitchVideo.module.scss';
 import classNames from 'classnames';
+import YouTube from 'react-youtube';
+import { useSelector } from 'react-redux';
 
-const PICTURES = {
-  'top-left': TopLeftVideoThumb,
-  'top-right': TopRightVideoThumb,
-  bottom: BottomVideoThumb
+// const PICTURES = {
+//   'top-left': TopLeftVideoThumb,
+//   'top-right': TopRightVideoThumb,
+//   bottom: BottomVideoThumb
+// };
+
+const POS_OPTIONS = {
+  'top-left': { x: -0.37, y: -0.25, delta: 0.09 },
+  'top-right': { x: -0.52, y: -0.26, delta: 0.09 },
+  bottom: { x: -0.31, y: -0.08, delta: 0.42 }
 };
 
-const PitchVideo = ({ position }) => {
+const PitchVideo = ({ videoId, position }) => {
+  const fullWidth = useSelector(state => state.shared.mobileWidth);
+
+  const videoWrapperRef = useRef(null);
+
   const videoClasses = classNames(cl.video, {
     [cl.topLeftVideo]: position === 'top-left',
     [cl.topRightVideo]: position === 'top-right',
     [cl.bottomVideo]: position === 'bottom'
   });
 
-  return <img className={videoClasses} src={PICTURES[position]} alt='video-1' />;
+  const xCoef = videoWrapperRef?.current?.clientWidth / ((1920 / 100) * (POS_OPTIONS[position].delta * 100));
+
+  return (
+    <div className={videoClasses} ref={videoWrapperRef}>
+      <div
+        style={{
+          position: 'absolute',
+          width: 1920 * xCoef,
+          height: 1080 * xCoef,
+          left: POS_OPTIONS[position].x * 1920 * xCoef,
+          top: POS_OPTIONS[position].y * 1080 * xCoef
+        }}>
+        <YouTube
+          videoId={videoId}
+          // videoId={'WCjLd7QAJq8'}
+          // onReady={onReady}
+          // onStateChange={onStateChange}
+          // onPlaybackRateChange={onPlaybackRateChange}
+          containerClassName={cl.YTContainer}
+          opts={{
+            width: '100%',
+            height: '100%',
+            playerVars: {
+              autoplay: 1,
+              controls: 0,
+              modestbranding: 1,
+              disablekb: 1
+              // loop: 1,
+              // playlist: '-GR52czEd-0'
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+  // return <img className={videoClasses} src={PICTURES[position]} alt='video-1' />;
 };
 
 export default PitchVideo;
--- src/components/Game/PlaysPitch/PlaysPitch.js
+++ src/components/Game/PlaysPitch/PlaysPitch.js
@@ -5,18 +5,27 @@ import PlaysSpeed from '../PlaysSpeed/PlaysSpeed';
 import { useSelector } from 'react-redux';
 import PitchVideo from './PitchVideo/PitchVideo';
 import PitchValues from './PitchValues/PitchValues';
+import getYouTubeID from 'get-youtube-id';
 
 const PlaysPitch = () => {
-	const currentMoment = useSelector(state => state.game.currentMoment)
+  const currentMoment = useSelector(state => state.game.currentMoment);
+  const { camera_info: cameraInfo } = useSelector(state => state.game.preview);
+
+  const { left_main_link: topLeftLink, right_main_link: topRightLink, pitch_link: bottomLink } = cameraInfo;
+
+  const videoId1 = getYouTubeID(topLeftLink) || 'WCjLd7QAJq8';
+  const videoId2 = getYouTubeID(topRightLink) || null;
+  const videoId3 = getYouTubeID(bottomLink) || null;
+
   return (
     <>
       <PlaysField currentMoment={currentMoment} />
       <PlaysSpeed currentMoment={currentMoment} />
       <PlaysSpin pitch={currentMoment?.metering?.pitch} />
-			<PitchVideo position='top-left'/>
-			<PitchVideo position='top-right'/>
-			<PitchVideo position='bottom'/>
-			<PitchValues />
+      <PitchVideo videoId={videoId1} position='top-left' />
+      <PitchVideo videoId={videoId2} position='top-right' />
+      <PitchVideo videoId={videoId3} position='bottom' />
+      <PitchValues />
     </>
   );
 };
