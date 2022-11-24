313a399718a0db7affe8f7f2cc6c2ee4addafe00
Igor Nikiforov <desola2@inbox.ru>
2022-11-24 15:07:31 +0300

    fix: title stats output format
    * add dot at the end of player name

--- src/components/Game/ContentCardTitle/ContentCardTitle.js
+++ src/components/Game/ContentCardTitle/ContentCardTitle.js
@@ -8,17 +8,17 @@ const ContentCardTitle = ({ player }) => {
 
   const getStats = () => {
     let subStr = [];
-    b2 && subStr.push(`${b2} 2B`);
-    b3 && subStr.push(`${b3} 3B`);
-    hr && subStr.push(`${hr} HR`);
+    b2 && subStr.push(`${b2 > 1 ? `${b2} ` : ''}2B`);
+    b3 && subStr.push(`${b3 > 1 ? `${b3} ` : ''}3B`);
+    hr && subStr.push(`${hr > 1 ? `${hr} ` : ''}HR`);
     subStr = subStr.join(', ');
     const isSubstr = subStr.length > 0;
 
-    return `PA:${pa}, ${h}of${ab}${isSubstr ? `(${subStr})` : ''}`;
+    return `PA:${pa}, ${h} of ${ab}${isSubstr ? ` (${subStr})` : ''}`;
   };
 
   const stats = isStats ? getStats() : '';
-  return <p className={cl.playerName}>{`${player.hit_order}. ${player.who} ${stats}`}</p>;
+  return <p className={cl.playerName}>{`${player.hit_order}. ${player.who}. ${stats}`}</p>;
 };
 
 export default ContentCardTitle;
