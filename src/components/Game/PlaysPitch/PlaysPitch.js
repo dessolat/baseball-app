0e962c258f7747d4f9973d946b7a794d36a104c2
Igor Nikiforov <desola2@inbox.ru>
2022-11-26 13:47:08 +0300

    refactor: move code

--- src/components/Game/PlaysPitch/PlaysPitch.js
+++ src/components/Game/PlaysPitch/PlaysPitch.js
@@ -3,28 +3,18 @@ import PlaysField from '../PlaysField/PlaysField';
 import PlaysSpin from '../PlaysSpin/PlaysSpin';
 import PlaysSpeed from '../PlaysSpeed/PlaysSpeed';
 import { useSelector } from 'react-redux';
-import PitchVideo from './PitchVideo/PitchVideo';
 import PitchValues from './PitchValues/PitchValues';
-import getYouTubeID from 'get-youtube-id';
+import PitchVideos from './PitchVideo/PitchVideos';
 
 const PlaysPitch = () => {
   const currentMoment = useSelector(state => state.game.currentMoment);
-  const { camera_info: cameraInfo } = useSelector(state => state.game.preview);
-
-  const { left_main_link: topLeftLink, right_main_link: topRightLink, pitch_link: bottomLink } = cameraInfo;
-
-  const videoId1 = getYouTubeID(topLeftLink) || 'WCjLd7QAJq8';
-  const videoId2 = getYouTubeID(topRightLink) || null;
-  const videoId3 = getYouTubeID(bottomLink) || null;
 
   return (
     <>
       <PlaysField currentMoment={currentMoment} />
       <PlaysSpeed currentMoment={currentMoment} />
       <PlaysSpin pitch={currentMoment?.metering?.pitch} />
-      <PitchVideo videoId={videoId1} position='top-left' />
-      <PitchVideo videoId={videoId2} position='top-right' />
-      <PitchVideo videoId={videoId3} position='bottom' />
+      <PitchVideos />
       <PitchValues />
     </>
   );
