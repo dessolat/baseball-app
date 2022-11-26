41eb48184d3631d6e923b7394cfb7f201a525928
Igor Nikiforov <desola2@inbox.ru>
2022-11-26 13:09:00 +0300

    feat: add ListDot icon comp

--- src/components/UI/icons/ListDot/ListDot.js
+++ src/components/UI/icons/ListDot/ListDot.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const ListDot = () => {
-	return (
-		<div>ListDot</div>
-	)
-}
+const ListDot = ({ fill = '#1A4C96' }) => {
+  return (
+    <svg width='6' height='6' viewBox='0 0 6 6' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <circle cx='3' cy='3' r='2.75' fill={fill} stroke='black' stroke-width='0.5' />
+    </svg>
+  );
+};
 
-export default ListDot
+export default ListDot;
