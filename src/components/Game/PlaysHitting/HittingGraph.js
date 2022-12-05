26c3417535788692dabbe33170826b4c374240e8
Igor Nikiforov <desola2@inbox.ru>
2022-12-05 18:40:25 +0300

    feat: add Header

--- src/components/Game/PlaysHitting/HittingGraph.js
+++ src/components/Game/PlaysHitting/HittingGraph.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import cl from './PlaysHitting.module.scss';
+const Header = ({ maxSpeed }) => {
+  const maxSpeedValue = maxSpeed ? `${maxSpeed} mph` : ' ';
+  const angleValue = maxSpeed ? `12.4 degrees` : ' ';
+
+  return (
+    <div className={cl.row}>
+      <div>
+        <span className={cl.title}>Attack Angle</span>
+        <span className={cl.value}>{angleValue}</span>
+      </div>
+      <div>
+        <span className={cl.title}>Max speed</span>
+        <span className={cl.value}>{maxSpeedValue}</span>
+      </div>
+    </div>
+  );
+};
 
 const HittingGraph = () => {
   return <div className={cl.graph}>HittingGraph</div>;
