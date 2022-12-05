dc431a9444d867614aafe733bfca542563376a34
Igor Nikiforov <desola2@inbox.ru>
2022-12-05 18:10:20 +0300

    feat: add HittingStats functional

--- src/components/Game/PlaysHitting/HittingStats.js
+++ src/components/Game/PlaysHitting/HittingStats.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import cl from './PlaysHitting.module.scss'
+import React from 'react';
+import cl from './PlaysHitting.module.scss';
 
-const HittingStats = () => {
-	return (
-		<div className={cl.stats}>HittingStats</div>
-	)
-}
+const HittingStats = ({ hit }) => {
+  const { init_speed_x, distance, angle } = hit || 0;
 
-export default HittingStats
+  const exitVelocityValue = init_speed_x ? init_speed_x.toFixed(1) + ' mph' : ' ';
+  const distanceValue = distance ? distance.toFixed(1) + ' m' : ' ';
+  const angleValue = angle ? angle.toFixed(0) + '°' : ' ';
+  return (
+    <div className={cl.stats}>
+      <div className={cl.leftSide}>
+        <div className={cl.row}>
+          <span className={cl.title}>Exit Velocity</span>
+          <span className={cl.value}>{exitVelocityValue}</span>
+        </div>
+        <div className={cl.row}>
+          <span className={cl.title}>Launch Angle</span>
+          <span className={cl.value}>{angleValue}</span>
+        </div>
+      </div>
+      <div className={cl.rightSide}>
+        <div className={cl.row}>
+          <span className={cl.title}>Height</span>
+          <span className={cl.value}>37.2 m</span>
+        </div>
+        <div className={cl.row}>
+          <span className={cl.title}>Distance</span>
+          <span className={cl.value}>{distanceValue}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HittingStats;
