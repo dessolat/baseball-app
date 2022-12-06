c76acf0876656189f34658f8811f3b6479308a8c
Igor Nikiforov <desola2@inbox.ru>
2022-12-06 11:25:00 +0300

    feat: add HittingVideos comp functional

--- src/components/Game/PlaysHitting/HittingVideos/HittingVideos.js
+++ src/components/Game/PlaysHitting/HittingVideos/HittingVideos.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
 import cl from '../PlaysHitting.module.scss';
+import TopImage from 'images/hit_top_image.jpg';
+import BottomImage from 'images/hit_bottom_image.jpg';
 
 const HittingVideos = () => {
-	return (
-		<div className={cl.videos}>HittingVideos</div>
-	)
-}
+  return (
+    <div className={cl.videos}>
+      <div>
+        <img src={TopImage} alt='hit-image' />
+      </div>
+      <div>
+        <img src={TopImage} alt='hit-image' />
+      </div>
+      <div>
+        <img src={BottomImage} alt='hit-image' />
+      </div>
+      <div>
+        <img src={BottomImage} alt='hit-image' />
+      </div>
+    </div>
+  );
+};
 
-export default HittingVideos
+export default HittingVideos;
