d027155a15ec15f2b8dbbddceda3235db25433e6
Igor Nikiforov <desola2@inbox.ru>
2022-12-27 20:18:59 +0300

    feat: add running info

--- src/components/Game/PlaysRunning/PlaysRunningInfoItem.js
+++ src/components/Game/PlaysRunning/PlaysRunningInfoItem.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { getFixedNumber } from 'utils';
 import cl from './PlaysRunning.module.scss';
 
+const BASES_BY_NUMBER = ['H', '1B', '2B', '3B'];
+
 const PlaysRunningInfoItem = ({ item }) => {
+  const {
+    max_speed: maxSpeed,
+    who,
+    run_from: runFrom,
+    run_to: runTo,
+    time_end: timeEnd,
+    time_start: timeStart
+  } = item;
+
+  const timeDelta = timeEnd - timeStart;
   return (
     <div className={cl.infoItem}>
-      <p className={cl.infoTitle}>{item.title}</p>
-      <p className={cl.infoValue}>{item.value}</p>
+      <p
+        className={
+          cl.infoTitle
+        }>{`${who} run from ${BASES_BY_NUMBER[runFrom]} to ${BASES_BY_NUMBER[runTo]} base:`}</p>
+      <p className={cl.infoValue}>{`S: ${getFixedNumber(maxSpeed, 1)} km/h T: ${getFixedNumber(
+        timeDelta,
+        1
+      )} s`}</p>
     </div>
   );
 };
--- src/components/Game/PlaysRunning/PlaysRunningInfoList.js
+++ src/components/Game/PlaysRunning/PlaysRunningInfoList.js
@@ -1,20 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import cl from './PlaysRunning.module.scss';
 import PlaysRunningInfoItem from './PlaysRunningInfoItem';
 
-const runningList = [
-  { title: 'Run to first base by Batter:', value: 'S: 5.7 m/s TB: 1.5 s' },
-  { title: 'Throwing ball to 2B by LF:', value: 'D: 20 m    HT: 1.5 s   AS: 100 %' },
-  { title: 'Run to second base by Runner 1:', value: 'S: 5.7 m/s TB: 2.0 s' },
-  { title: 'Run to second base by Batter:', value: 'S: 5.7 m/s TB: 1 s' },
-  { title: 'Throwing ball to 1B by 2B:', value: 'D: 20 m    HT: 3 s   AS: 100 %' },
-  { title: 'Run to first base by Batter:', value: 'S: 5.7 m/s TB: 1 s' }
-];
 
 const PlaysRunningInfoList = () => {
+  const { metering } = useSelector(state => state.game.currentMoment);
+
   return (
     <div className={cl.list}>
-      {runningList.map((item, i) => (
+      {metering?.field && metering.field.map((item, i) => (
         <PlaysRunningInfoItem key={i} item={item} />
       ))}
     </div>
