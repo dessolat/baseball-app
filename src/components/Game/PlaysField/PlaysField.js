7e8691b23bc87d794d1787d913afb2cedd8310e2
Igor Nikiforov <desola2@inbox.ru>
2023-01-14 16:06:15 +0300

    refactor: remove commented code

--- src/components/Game/PlaysField/PlaysField.js
+++ src/components/Game/PlaysField/PlaysField.js
@@ -4,18 +4,14 @@ import gridImg from 'images/grid.png';
 import PlaysFieldBalls from './PlaysFieldBalls';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPitchState } from 'redux/gameReducer';
-// import { setCurrentCard, setCurrentMoment, setPlaybackMode } from 'redux/gameReducer';
 import Arrow from 'components/UI/buttons/Arrow/Arrow';
-// import PlaysFieldValues from './PlaysFieldValues/PlaysFieldValues';
 import classNames from 'classnames';
 
 const PlaysField = ({ currentMoment }) => {
   const [coords, setCoords] = useState([]);
   const [count, setCount] = useState(1);
   const [coeff, setCoeff] = useState({ x: 1, y: 1, yScale: 1 });
-  // const currentCard = useSelector(state => state.game.currentCard);
-  // const playbackMode = useSelector(state => state.game.playbackMode);
-  // const filteredCards = useSelector(state => state.game.filteredCards);
+
   const pitchState = useSelector(state => state.game.pitchState);
   const dispatch = useDispatch();
 
@@ -60,42 +56,11 @@ const PlaysField = ({ currentMoment }) => {
       return;
     }
 
-    // const newMoments = [];
-    // currentCard.type !== 'Replacement'
-    //   ? currentCard.moments?.forEach(moment => moment.icons && newMoments.push(moment))
-    //   : newMoments.push(currentCard.moments[0]);
-
-    // const momentIndex = newMoments?.findIndex(moment => moment.inner?.id === currentMoment?.inner?.id);
-    // const cardIndex = filteredCards.findIndex(
-    //   card => card.moments[0].inner.id === currentCard.moments[0].inner.id
-    // );
-
-    // let playTimeout;
-    // if (playbackMode !== 'pause') {
-    //   playTimeout = setTimeout(() => {
-    //     if (
-    //       (momentIndex >= currentCard.moments.length - 1 && cardIndex >= filteredCards.length - 1) ||
-    //       momentIndex === -1
-    //     ) {
-    //       dispatch(setPlaybackMode('pause'));
-    //       return;
-    //     }
-    //     if (momentIndex >= currentCard.moments.length - 1) {
-    //       dispatch(setCurrentCard({ ...filteredCards[cardIndex + 1], manualMoment: true }));
-    //       return;
-    //     }
-    //     dispatch(setCurrentMoment(newMoments[momentIndex + 1]));
-    //   }, 2000);
-    // }
-
     return () => {
-      // console.log('cleared');
       clearTimeout(graphTimeout);
-      // clearTimeout(playTimeout);
     };
     // eslint-disable-next-line
   }, [count]);
-  // playbackMode]);
 
   useEffect(() => {
     setCount(1);
@@ -129,7 +94,6 @@ const PlaysField = ({ currentMoment }) => {
       <div className={cl.field} ref={parent}>
         <img className={cl.grid} src={gridImg} alt='grid' />
         <PlaysFieldBalls coords={coords} count={count} coeff={coeff} />
-        {/* <PlaysFieldValues currentMoment={currentMoment} cl={cl} /> */}
         <Arrow
           direction='right'
           onClick={handleArrowClick}
