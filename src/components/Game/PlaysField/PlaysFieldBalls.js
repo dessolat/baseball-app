5ec9172c1f99ee839529bb38f4412953588b1958
Igor Nikiforov <desola2@inbox.ru>
2023-01-17 18:43:49 +0300

    feat: change pitch balls animation conditions

--- src/components/Game/PlaysField/PlaysFieldBalls.js
+++ src/components/Game/PlaysField/PlaysFieldBalls.js
@@ -1,10 +1,38 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import PlaysFieldBall from './PlaysFieldBall';
 
 const PlaysFieldBalls = ({ coords, coeff, currentMoment }) => {
+  const [pauseBallsCount, setPauseBallsCount] = useState(0);
+
   const videoLengthMode = useSelector(state => state.game.videoLengthMode);
   const videoCurrentTime = useSelector(state => state.game.videoCurrentTime);
+  const preferredVideoState = useSelector(state => state.game.preferredVideoState);
+
+  useEffect(() => {
+    if (preferredVideoState !== 2) return;
+
+    setPauseBallsCount(0);
+
+    setTimeout(() => {
+      setPauseBallsCount(1);
+    }, 40);
+  }, [currentMoment]);
+
+  useEffect(() => {
+    if (pauseBallsCount === 0) return;
+
+    let graphTimeout;
+    if (pauseBallsCount < coords.length) {
+      graphTimeout = setTimeout(() => setPauseBallsCount(prev => prev + 1), 40);
+      return;
+    }
+
+    return () => {
+      clearTimeout(graphTimeout);
+    };
+    // eslint-disable-next-line
+  }, [pauseBallsCount]);
 
   const videoLengthPrefix = videoLengthMode.toLowerCase().replace(' ', '_');
 
@@ -26,8 +54,7 @@ const PlaysFieldBalls = ({ coords, coeff, currentMoment }) => {
   const timePerBall = totalPitchTime / coords.length;
 
   const isCount = pitchTimeStart < videoCurrentTime && SECONDS_SRC.pitch?.timeEnd >= videoCurrentTime;
-  const count = isCount ? Math.ceil((videoCurrentTime - pitchTimeStart) / timePerBall) : 0;
-
+  const count = preferredVideoState === 2 ? pauseBallsCount : isCount ? Math.ceil((videoCurrentTime - pitchTimeStart) / timePerBall) : coords.length;
   return (
     <>
       {useMemo(
@@ -37,7 +64,7 @@ const PlaysFieldBalls = ({ coords, coeff, currentMoment }) => {
             .map((coord, i) => (
               <PlaysFieldBall key={i} coord={coord} index={i} coeff={coeff} coordsLength={coords.length} />
             )),
-        [count]
+        [count, coords]
       )}
     </>
   );
