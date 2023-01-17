b24ec3aba52d8d446d09564107fb7634bca35299
Igor Nikiforov <desola2@inbox.ru>
2023-01-17 12:20:59 +0300

    feat: rework curve for 2d_data
    * add animation

--- src/components/Game/PlaysHitting/HittingField/HittingField.js
+++ src/components/Game/PlaysHitting/HittingField/HittingField.js
@@ -1,10 +1,11 @@
-import React, { Suspense, useMemo, useState, useRef } from 'react';
+import React, { Suspense, useMemo, useState, useRef, useEffect, useLayoutEffect } from 'react';
 import cl from './HittingField.module.scss';
 import { Canvas } from '@react-three/fiber';
-import { TextureLoader } from 'three';
+import { CatmullRomCurve3, TextureLoader } from 'three';
 import { OrbitControls } from '@react-three/drei';
 import FieldBg from 'images/field_right.jpg';
 import classNames from 'classnames';
+import * as THREE from 'three';
 
 // extend({ TextGeometry });
 
@@ -28,6 +29,7 @@ const OptionsBar = ({ isAutoRotate, handleAutoRotateClick, handleResetClick }) =
 const HittingField = ({ hit }) => {
   const { data_2d } = hit || 0;
   const [isAutoRotate, setAutoRotate] = useState(false);
+  const [curveCount, setCurveCount] = useState(null);
 
   const textureRef = useMemo(() => new TextureLoader().load(FieldBg), []);
 
@@ -47,10 +49,51 @@ const HittingField = ({ hit }) => {
   //   setCameraCoords([e.point.x, e.point.y, e.point.z]);
   // };
 
+  useLayoutEffect(() => {
+    setCurveCount(0);
+
+    if (!data_2d) return;
+
+    setTimeout(() => {
+      setCurveCount(1);
+    }, 150);
+  }, [data_2d]);
+
+  useEffect(() => {
+    if (!data_2d || curveCount === 0) return;
+
+    const length = data_2d.length;
+		
+		let timeout;
+
+    if (curveCount < length) {
+      timeout = setTimeout(() => {
+        setCurveCount(prev => prev + 1);
+      }, 3);
+    }
+
+		return () => {
+			clearTimeout(timeout)
+		}
+  }, [curveCount]);
+
   const handleAutoRotateClick = () => setAutoRotate(prev => !prev);
   const handleResetClick = () => controlsRef.current.reset();
 
-	// const font = new FontLoader().parse(ComfortaaFont)
+  // const font = new FontLoader().parse(ComfortaaFont)
+
+  let points, curve;
+
+  if (data_2d && curveCount > 0) {
+    points = data_2d?.slice(0, curveCount).reduce((sum, coord) => {
+      const newCoord = new THREE.Vector3(coord[0] / 3.5 - 450, coord[2] * 20, (coord[1] - 1490) / 3 + 150);
+      sum.push(newCoord);
+      return sum;
+    }, []);
+
+    curve = new CatmullRomCurve3(points);
+  }
+
   return (
     <div className={cl.field}>
       <Canvas
@@ -62,20 +105,24 @@ const HittingField = ({ hit }) => {
             <planeGeometry args={[1280, 1090]} />
             <meshBasicMaterial map={textureRef} toneMapped={false} />
           </mesh>
-          {/* <mesh position={[-500, 0, 0]}>
-						<textGeometry args={['test', {font, size:500, height: 200}]}/>
-            <meshBasicMaterial color={'lightblue'} toneMapped={false} />
-          </mesh> */}
-          {data_2d?.map(coord => (
+
+          {curveCount > 1 && (
+            <mesh position={[-70, 0, 220]}>
+              <tubeGeometry args={[curve, 70, 3, 50, false]} />
+              <meshBasicMaterial color={'blue'} side={THREE.DoubleSide} />
+            </mesh>
+          )}
+          {/* {data_2d?.map(coord => (
             <mesh position={[coord[0] / 3.5 - 450, coord[2] * 20, (coord[1] - 1490) / 3 + 150]}>
               <sphereGeometry args={[5, 40, 40]} />
               <meshStandardMaterial color={'blue'} />
             </mesh>
-          ))}
+          ))} */}
           {data_2d
-            ?.filter(coords => coords[3] === 1)
+            ?.slice(0, curveCount)
+            .filter(coords => coords[3] === 1)
             .map(coord => (
-              <mesh position={[coord[0] / 3.5 - 450, coord[2] * 20, (coord[1] - 1490) / 3 + 150]}>
+              <mesh position={[coord[0] / 3.5 - 450 - 70, coord[2] * 20, (coord[1] - 1490) / 3 + 150 + 220]}>
                 <sphereGeometry args={[5, 40, 40]} />
                 <meshStandardMaterial color={'red'} />
               </mesh>
