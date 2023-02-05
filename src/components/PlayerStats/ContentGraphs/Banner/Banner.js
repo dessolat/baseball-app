961a3fa515fd10f1d33af516f3939819b119aff3
Igor Nikiforov <desola2@inbox.ru>
2023-02-05 17:30:25 +0300

    feat: rework animation
    * move getParentObj func

--- src/components/PlayerStats/ContentGraphs/Banner/Banner.js
+++ src/components/PlayerStats/ContentGraphs/Banner/Banner.js
@@ -1,13 +1,21 @@
-import React, { useEffect, useRef, useState, createContext } from 'react';
+import { AnimationContext, PlayerYearsContext } from 'context';
+import React, { useEffect, useRef, useState, useContext } from 'react';
+import { useSelector } from 'react-redux';
 import cl from './Banner.module.scss';
 import BannerColumns from './BannerColumns';
-
-export const AnimationContext = createContext(0);
 
 const Banner = () => {
   const [valueCoef, setValueCoef] = useState(0);
 
   const wrapperRef = useRef();
+  const firstMountRef = useRef(true);
+
+	const { playerYears } = useContext(PlayerYearsContext);
+
+  const { playerStatsData: statsData, playerCurrentTeam: currentTeam } = useSelector(s => s.playerStats);
+  const { currentLeague } = useSelector(state => state.games);
+
+  const { total, total_annual, teams, leagues } = statsData.pitcher_banner;
 
   useEffect(() => {
     let observer = new IntersectionObserver(entries => {
@@ -49,15 +57,39 @@ const Banner = () => {
     };
   }, [valueCoef]);
 
-  
+	useEffect(() => {
+		if (firstMountRef.current === true) {
+			firstMountRef.current = false
+			return
+		}
 
-  return (
+    setValueCoef(0);
+
+    setTimeout(() => {
+      setValueCoef(prev => prev + 0.01);
+    }, 10);
+  }, [playerYears, currentTeam, currentLeague.id]);
+
+	const getParentObj = () => {
+    if (playerYears === 'All years' && currentTeam === 'All teams') return total;
+    if (playerYears === 'All years' && currentTeam !== 'All teams')
+      return teams.find(team => team.name === currentTeam);
+    if (currentLeague.id === -1 && currentTeam === 'All teams') return total_annual[playerYears];
+    if (currentLeague.id === -1 && currentTeam !== 'All teams')
+      return teams.find(team => team.name === currentTeam).annual[playerYears];
+
+    const curLeague = leagues.find(league => league.id === currentLeague.id);
+    return curLeague.teams.find(team => team.name === currentTeam);
+  };
+
+  const parentObj = getParentObj();
+  return parentObj ? (
     <AnimationContext.Provider value={valueCoef}>
       <div className={cl.bannerWrapper} ref={wrapperRef}>
-        <BannerColumns />
+        <BannerColumns setAnimationCoef={setValueCoef} parentObj={parentObj}/>
       </div>
     </AnimationContext.Provider>
-  );
+  ) : <></>;
 };
 
 export default Banner;
