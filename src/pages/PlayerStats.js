d3fa0f32539128f00b5dd5da80cbd67f3c653fe5
Igor Nikiforov <desola2@inbox.ru>
2023-02-19 15:06:32 +0300

    feat: integrate useFetch
    * add graphs data fetching

--- src/pages/PlayerStats.js
+++ src/pages/PlayerStats.js
@@ -4,36 +4,46 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentLeague } from 'redux/gamesReducer';
 import { setPlayerStatsData, setPlayerCurrentTeam as setCurrentTeam } from 'redux/playerStatsReducer';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
 import Header from 'components/PlayerStats/Header/Header';
 import Content from 'components/PlayerStats/Content/Content';
 import Loader from 'components/UI/loaders/Loader/Loader';
 import { setTableMode } from 'redux/statsReducer';
 import { PlayerYearsContext } from 'context';
+import useFetch from 'hooks/useFetch';
 
 const PlayerStats = () => {
-  // eslint-disable-next-line
-  const [isStatsLoading, setIsStatsLoading] = useState(false);
-  const [error, setError] = useState('');
+  const { playerId } = useParams();
+
   const currentYear = useSelector(state => state.shared.currentYear);
   const [playerYears, setPlayerYears] = useState(currentYear);
+  const [pitchesData, setPitchesData] = useState(null);
+
   const currentLeague = useSelector(state => state.games.currentLeague);
   const { tableType: playerTableMode, playerStatsData } = useSelector(state => state.playerStats);
 
-  const cancelTokenRef = useRef();
+  const dispatch = useDispatch();
+
   const firstMountRef = useRef(true);
 
-  const { playerId } = useParams();
+  const {
+    fetchData: fetchStats,
+    isLoading: isStatsLoading,
+    error: statsError,
+    cancelToken: statsCancelToken
+  } = useFetch(`http://baseball-gametrack.ru/api/player?id=${playerId}`);
 
-  // const playerStatsData = useSelector(state => state.playerStats.playerStatsData);
-  // const games = useSelector(state => state.games.games);
-  const dispatch = useDispatch();
+  const urlPlayerYears = playerYears !== 'All years' ? playerYears : -1;
+
+  const { fetchData: fetchPitches, cancelToken: pitchesCancelToken } = useFetch(
+    `http://baseball-gametrack.ru/api/pitcher_metrix?id=${playerId}&year=${urlPlayerYears}`
+  );
 
   useEffect(
     () => () => {
       dispatch(setPlayerStatsData({}));
       dispatch(setCurrentTeam(null));
-      cancelTokenRef.current.cancel(null);
+      statsCancelToken.current.cancel(null);
+      pitchesCancelToken.current.cancel(null);
     },
     // eslint-disable-next-line
     []
@@ -49,29 +59,16 @@ const PlayerStats = () => {
   useEffect(() => {
     const isLeague = leagues => leagues.find(league => league.id === currentLeague.id);
 
-    const fetchStats = async () => {
-      cancelTokenRef.current = axios.CancelToken.source();
-
+    (async () => {
       try {
-        setIsStatsLoading(true);
-        const response = await axios.get(`http://baseball-gametrack.ru/api/player?id=${playerId}`, {
-          cancelToken: cancelTokenRef.current.token,
-          timeout: 10000
-        });
-        setError('');
-        console.log(response.data);
-        !isLeague(response.data.leagues) && dispatch(setCurrentLeague({ id: -1, name: 'All', title: 'All' }));
+        const response = await fetchStats();
 
+        !isLeague(response.data.leagues) && dispatch(setCurrentLeague({ id: -1, name: 'All', title: 'All' }));
         dispatch(setPlayerStatsData(response.data));
-      } catch (err) {
-        if (err.message === null) return;
-        console.log(err.message);
-        setError(err.message);
-      } finally {
-        setIsStatsLoading(false);
+      } catch ({ message }) {
+        console.warn(message);
       }
-    };
-    fetchStats();
+    })();
 
     if (firstMountRef.current === true) {
       firstMountRef.current = false;
@@ -82,46 +79,20 @@ const PlayerStats = () => {
 
     // eslint-disable-next-line
   }, [playerId]);
-  // useEffect(() => {
-  //   const fetchGamesData = async () => {
-  //     cancelTokenRef.current = axios.CancelToken.source();
+	
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetchPitches();
 
-  //     try {
-  //       setIsLoading(true);
-  //       const response = await axios.get(`http://baseball-gametrack.ru/api/main/year-${currentYear}`, {
-  //         cancelToken: cancelTokenRef.current.token,
-  //         timeout: 5000
-  //       });
-  //       console.log(response.data);
-  //       setError('');
-  //       dispatch(setGamesAndLeagues(response.data));
+        setPitchesData(response.data);
+      } catch ({ message }) {
+        console.warn(message);
+      }
+    })();
 
-  //       if (games === null) {
-  //         console.log('dispatched after fetching');
-  //         dispatch(setCurrentLeague({ id: -1, name: 'All' }));
-  //       }
-  //     } catch (err) {
-  //       if (err.message === null) return;
-  //       console.log(err.message);
-  //       setError(err.message);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
-  //   fetchGamesData();
-
-  //   if (firstMountRef.current === true) {
-  //     firstMountRef.current = false;
-  //     return;
-  //   }
-
-  //   dispatch(setCurrentLeague({ id: -1, name: 'All' }));
-
-  //   return () => {
-  //     cancelTokenRef.current.cancel(null);
-  //   };
-  // 	// eslint-disable-next-line
-  // }, [currentYear]);
+    // eslint-disable-next-line
+  }, [playerYears]);
 
   function calculateTeamsArray(tableMode) {
     const selectedLeague = playerStatsData.leagues.find(league => league.id === currentLeague.id);
@@ -159,8 +130,8 @@ const PlayerStats = () => {
 
   return (
     <>
-      {error !== '' ? (
-        <ErrorLoader error={error} />
+      {statsError !== '' ? (
+        <ErrorLoader error={statsError} />
       ) : isStatsLoading ? (
         <Loader />
       ) : Object.keys(playerStatsData).length === 0 ? (
@@ -168,7 +139,7 @@ const PlayerStats = () => {
       ) : (
         <PlayerYearsContext.Provider value={{ playerYears, setPlayerYears, calculateTeamsArray }}>
           <Header />
-          <Content />
+          <Content pitchesData={pitchesData} />
         </PlayerYearsContext.Provider>
       )}
     </>
