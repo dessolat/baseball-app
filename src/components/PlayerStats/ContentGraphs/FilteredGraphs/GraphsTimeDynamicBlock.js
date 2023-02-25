fd69c8af804576b92a915e6c39d6d26151f6c96c
Igor Nikiforov <desola2@inbox.ru>
2023-02-25 15:57:10 +0300

    feat: add calculatedDefOptions2

--- src/components/PlayerStats/ContentGraphs/FilteredGraphs/GraphsTimeDynamicBlock.js
+++ src/components/PlayerStats/ContentGraphs/FilteredGraphs/GraphsTimeDynamicBlock.js
@@ -2,8 +2,22 @@ import { useState } from 'react';
 import cl from './FilteredGraphs.module.scss';
 
 const GraphsTimeDynamicBlock = ({ defaultOption, defaultOption2, children, ...props }) => {
+  const calculatedDefOptions2 = defaultOption2.reduce(
+    (sum, option) => {
+      sum.push({ name: option, checked: true });
+
+      return sum;
+    },
+    [
+      {
+        name: 'All Pitches',
+        checked: true
+      }
+    ]
+  );
+
   const [currentOption, setCurrentOption] = useState(defaultOption);
-  const [currentOption2, setCurrentOption2] = useState(defaultOption2);
+  const [currentOption2, setCurrentOption2] = useState(calculatedDefOptions2);
 
   return (
     <div className={cl.graphsBlock} {...props}>
