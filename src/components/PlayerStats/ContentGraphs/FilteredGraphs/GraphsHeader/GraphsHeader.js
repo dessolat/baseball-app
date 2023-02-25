3a66176be29e085be9e4a436ddf7579e8a5d2161
Igor Nikiforov <desola2@inbox.ru>
2023-02-25 15:51:20 +0300

    feat: integrate TimeDynamicCheckbox

--- src/components/PlayerStats/ContentGraphs/FilteredGraphs/GraphsHeader/GraphsHeader.js
+++ src/components/PlayerStats/ContentGraphs/FilteredGraphs/GraphsHeader/GraphsHeader.js
@@ -1,25 +1,66 @@
 import cl from './GraphsHeader.module.scss';
 import OptionsToggler from 'components/UI/togglers/OptionsToggler/OptionsToggler';
+import classNames from 'classnames';
+import TimeDynamicCheckbox from 'components/UI/checkboxes/TimeDynamicCheckbox/TimeDynamicCheckbox';
 
-const GraphsHeader = ({ optionsArr, title, subTitle, currentOption, setCurrentOption, noSelector = false, ...props }) => {
+const GraphsHeader = ({
+  optionsArr,
+	availableOptions,
+  title,
+  subTitle,
+  currentOption,
+  setCurrentOption,
+  currentOption2 = null,
+  setCurrentOption2 = null,
+  noSelector = false,
+  ...props
+}) => {
   const optionsTogglerStyles = {
     position: 'absolute',
     right: '.4375rem',
-    top: '50%',
-    transform: 'translateY(-50%)'
+    top: currentOption2 !== null ? '30%' : '50%',
+    transform: currentOption2 !== null ? 'translateY(-55%)' : 'translateY(-50%)'
+  };
+
+  const optionsToggler2Styles = {
+    position: 'absolute',
+    right: '.4375rem',
+    bottom: '30%',
+    transform: 'translateY(55%)'
   };
 
   const handleOptionClick = option => () => setCurrentOption(option);
+  const handleOption2Click = option => () => {
+    setCurrentOption2(prev =>
+      prev.map(curOption =>
+        curOption.name === option.name ? { ...curOption, checked: !curOption.checked } : curOption
+      )
+    );
+  };
+
+  const titleClasses = classNames(cl.title, {
+    [cl.highTitle]: currentOption2 !== null
+  });
   return (
     <div className={cl.graphsHeader} {...props}>
       <h3>{title}</h3>
-      <p className={cl.title}>{subTitle}</p>
-      {!noSelector && <OptionsToggler
-        style={optionsTogglerStyles}
-        optionsArr={optionsArr}
-        currentOption={currentOption}
-        handleOptionClick={handleOptionClick}
-      />}
+      <p className={titleClasses}>{subTitle}</p>
+      {!noSelector && (
+        <OptionsToggler
+          style={optionsTogglerStyles}
+          optionsArr={optionsArr}
+          currentOption={currentOption}
+          handleOptionClick={handleOptionClick}
+        />
+      )}
+      {currentOption2 !== null && (
+        <TimeDynamicCheckbox
+          style={optionsToggler2Styles}
+          optionsArr={currentOption2}
+					availableOptions={availableOptions}
+          handleOptionClick={handleOption2Click}
+        />
+      )}
     </div>
   );
 };
