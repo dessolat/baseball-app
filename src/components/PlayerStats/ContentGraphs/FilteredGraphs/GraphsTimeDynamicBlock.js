3f5796bec3dbec93ca9dae4eb2e43477270ff6b9
Igor Nikiforov <desola2@inbox.ru>
2023-02-27 17:40:54 +0300

    feat: add type to default options

--- src/components/PlayerStats/ContentGraphs/FilteredGraphs/GraphsTimeDynamicBlock.js
+++ src/components/PlayerStats/ContentGraphs/FilteredGraphs/GraphsTimeDynamicBlock.js
@@ -3,14 +3,15 @@ import cl from './FilteredGraphs.module.scss';
 
 const GraphsTimeDynamicBlock = ({ defaultOption, defaultOption2, children, ...props }) => {
   const calculatedDefOptions2 = defaultOption2.reduce(
-    (sum, option) => {
-      sum.push({ name: option, checked: true });
+    (sum, option, index) => {
+      sum.push({ name: option, type: index, checked: true });
 
       return sum;
     },
     [
       {
         name: 'All Pitches',
+				type: -1,
         checked: true
       }
     ]
