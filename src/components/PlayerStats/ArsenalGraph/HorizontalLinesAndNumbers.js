82c1c0b8bd429226ce9cd07d7cf784cbbd096bd8
Igor Nikiforov <desola2@inbox.ru>
2023-03-10 20:23:50 +0300

    feat: add VerticalBreak exception

--- src/components/PlayerStats/ArsenalGraph/HorizontalLinesAndNumbers.js
+++ src/components/PlayerStats/ArsenalGraph/HorizontalLinesAndNumbers.js
@@ -1,31 +1,37 @@
 import { Fragment } from 'react';
 import cl from './ArsenalGraph.module.scss';
 
-const HorizontalLinesAndNumbers = ({ PARAMS, marks }) => {
+const HorizontalLinesAndNumbers = ({ PARAMS, marks, graphType }) => {
   const isLeftMarks = marks.some(value => !isNaN(value));
 
   return (
     <>
-      {marks.map((number, i) => (
-        <Fragment key={i}>
-          {/* Horizontal lines */}
-          <line
-            x1={PARAMS.ZERO_COORDS.X}
-            y1={PARAMS.ZERO_COORDS.Y - PARAMS.HORIZONTAL_GRID_LINES_STEP * (i + 1)}
-            x2={PARAMS.ZERO_COORDS.X + PARAMS.HORIZONTAL_GRID_LINES_WIDTH}
-            y2={PARAMS.ZERO_COORDS.Y - PARAMS.HORIZONTAL_GRID_LINES_STEP * (i + 1)}
-            stroke='#E3E1E1'
-            strokeDasharray='4 2'
-          />
-          {/* Left numbers */}
-          {isLeftMarks && <text
-            x={PARAMS.LEFT_PADDING}
-            y={PARAMS.ZERO_COORDS.Y - PARAMS.HORIZONTAL_GRID_LINES_STEP * i + 5}
-            className={cl.leftNumber}>
-            {number}
-          </text>}
-        </Fragment>
-      ))}
+      {marks.map((number, i) => {
+        const leftMarkValue = graphType !== 'VerticalBreak' ? number : (-number).toFixed(1);
+
+        return (
+          <Fragment key={i}>
+            {/* Horizontal lines */}
+            <line
+              x1={PARAMS.ZERO_COORDS.X}
+              y1={PARAMS.ZERO_COORDS.Y - PARAMS.HORIZONTAL_GRID_LINES_STEP * (i + 1)}
+              x2={PARAMS.ZERO_COORDS.X + PARAMS.HORIZONTAL_GRID_LINES_WIDTH}
+              y2={PARAMS.ZERO_COORDS.Y - PARAMS.HORIZONTAL_GRID_LINES_STEP * (i + 1)}
+              stroke='#E3E1E1'
+              strokeDasharray='4 2'
+            />
+            {/* Left numbers */}
+            {isLeftMarks && (
+              <text
+                x={PARAMS.LEFT_PADDING}
+                y={PARAMS.ZERO_COORDS.Y - PARAMS.HORIZONTAL_GRID_LINES_STEP * i + 5}
+                className={cl.leftNumber}>
+                {leftMarkValue}
+              </text>
+            )}
+          </Fragment>
+        );
+      })}
     </>
   );
 };
