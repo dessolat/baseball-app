e7c30c77616bed4e6259e699bad5045bac579a12
Igor Nikiforov <desola2@inbox.ru>
2023-03-18 20:12:22 +0300

    feat: create Curves comp

---
+++ src/components/PlayerStats/ContentGraphs/FilteredGraphs/PitchesTrajectories/Curves/Curves.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { getRndValue } from 'utils';
+import Curve from './Curve';
+// import CurveTexture from 'images/blue_ball_curve.jpg';
+// import { TextureLoader } from 'three';
+
+const Curves = ({ hitsData }) => {
+  const maxLength = hitsData.reduce((sum, hit) => {
+    const curLength = hit.hit_info.data_3d.length;
+    if (curLength > sum) sum = curLength;
+
+    return sum;
+  }, 0);
+
+  const [curveCount, setCurveCount] = useState(maxLength);
+
+  const isRenderCurves = curveCount > 1;
+  return (
+    <>
+      {isRenderCurves &&
+        hitsData.map((hit, i) => {
+          const { data_3d: data3D } = hit.hit_info;
+
+          const curCurveCount = curveCount > data3D.length ? data3D.length : curveCount;
+
+          return (
+            <Curve
+              key={'curve-' + getRndValue(0, 100000)}
+              number={i}
+              data3D={data3D}
+              curveCount={curCurveCount}
+              // curveTextureBlue={curveTextureBlue}
+            />
+          );
+        })}
+    </>
+  );
+};
+
+export default Curves;
