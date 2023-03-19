39707ae762f69efee10bbb2a1704fbdf1d92e11c
Igor Nikiforov <desola2@inbox.ru>
2023-03-19 19:12:55 +0300

    feat: add intersection observer

--- src/components/PlayerStats/ContentGraphs/FilteredGraphs/PitchesTrajectories/PitchesTrajectories.js
+++ src/components/PlayerStats/ContentGraphs/FilteredGraphs/PitchesTrajectories/PitchesTrajectories.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { memo, Suspense, useMemo, useState, useRef, useEffect } from 'react';
+import React, { memo, Suspense, useMemo, useState, useRef, useEffect, forwardRef } from 'react';
 import { useSelector } from 'react-redux';
 import cl from './PitchesTrajectories.module.scss';
 import { Canvas } from '@react-three/fiber';
@@ -32,6 +32,34 @@ const OptionsBar = ({ isAutoRotate, handleAutoRotateClick, handleResetClick }) =
 const PitchesTrajectories = ({ data }) => {
   const [isAutoRotate, setAutoRotate] = useState(false);
   const [zoomCoef, setZoomCoef] = useState(1);
+  const [isGraphVisible, setGraphVisibility] = useState(false);
+
+  const graphRef = useRef();
+
+  useEffect(() => {
+    let options = {
+      root: null,
+      rootMargin: '300px 0px',
+      threshold: 0
+    };
+
+    let observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const { isIntersecting } = entry;
+
+        if (isIntersecting) {
+          setGraphVisibility(true);
+        } else {
+          setGraphVisibility(false);
+        }
+      });
+    }, options);
+    observer.observe(graphRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   const currentMoment = useSelector(state => state.game.currentMoment);
 
@@ -56,40 +84,43 @@ const PitchesTrajectories = ({ data }) => {
     <div className={cl.fieldWrapper} ref={wrapperRef}>
       <Canvas
         className={cl.canvas}
-        camera={{ position: [0, 1000, 0], far: 3000, zoom: 0.45 * zoomCoef }}
+        ref={graphRef}
+        camera={{ position: [350, 1000, 1500], far: 3000, zoom: 0.45 * zoomCoef }}
         shadows={true}
         orthographic={true}>
-        <Suspense fallback={null}>
-          <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow={true}>
-            <planeGeometry args={[1280, 1090]} />
-            <meshStandardMaterial map={textureRef} toneMapped={false} shadowSide={FrontSide} />
-          </mesh>
+        {isGraphVisible && (
+          <Suspense fallback={null}>
+            <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow={true}>
+              <planeGeometry args={[1280, 1090]} />
+              <meshStandardMaterial map={textureRef} toneMapped={false} shadowSide={FrontSide} />
+            </mesh>
 
-          <Curves hitsData={hitsData} />
+            <Curves hitsData={hitsData} />
 
-          <directionalLight
-            position={[0, 400, 0]}
-            intensity={0.5}
-            castShadow
-            shadow-camera-left={-640}
-            shadow-camera-right={640}
-            shadow-camera-top={640}
-            shadow-camera-bottom={-640}
-          />
-          <ambientLight intensity={0.5} />
+            <directionalLight
+              position={[0, 400, 0]}
+              intensity={0.5}
+              castShadow
+              shadow-camera-left={-640}
+              shadow-camera-right={640}
+              shadow-camera-top={640}
+              shadow-camera-bottom={-640}
+            />
+            <ambientLight intensity={0.5} />
 
-          <OrbitControls
-            enableZoom={true}
-            autoRotate={isAutoRotate}
-            maxPolarAngle={Math.PI / 2.2}
-            minZoom={0.22}
-            maxZoom={3}
-            zoomSpeed={1.5}
-            enableDamping={false}
-            rotateSpeed={1.2}
-            ref={controlsRef}
-          />
-        </Suspense>
+            <OrbitControls
+              enableZoom={true}
+              autoRotate={isAutoRotate}
+              maxPolarAngle={Math.PI / 2.2}
+              minZoom={0.22}
+              maxZoom={3}
+              zoomSpeed={1.5}
+              enableDamping={false}
+              rotateSpeed={1.2}
+              ref={controlsRef}
+            />
+          </Suspense>
+        )}
       </Canvas>
 
       <OptionsBar
