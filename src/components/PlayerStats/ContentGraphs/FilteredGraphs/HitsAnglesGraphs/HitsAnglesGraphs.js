c7bbaf5c93227afcf4c7ed35511fc37684aaf02b
Igor Nikiforov <desola2@inbox.ru>
2023-03-20 09:48:11 +0300

    feat: create HitsAnglesGraphs comp
    * add data filtering

---
+++ src/components/PlayerStats/ContentGraphs/FilteredGraphs/HitsAnglesGraphs/HitsAnglesGraphs.js
@@ -0,0 +1,17 @@
+import HitsAnglesGraph from './HitsAnglesGraph';
+import cl from './HitsAnglesGraphs.module.scss';
+
+const HitsAnglesGraphs = ({ data }) => {
+  const notNullAngleData = data.filter(({ hit_info: { angle } }) => angle !== null);
+
+  console.log(notNullAngleData);
+  return (
+    <div className={cl.wrapper}>
+      <HitsAnglesGraph title='Hits by angle (hits)' />
+      <HitsAnglesGraph title='Exit velocity by angle (mph)' />
+      <HitsAnglesGraph title='Distance by angle (m)' />
+    </div>
+  );
+};
+
+export default HitsAnglesGraphs;
