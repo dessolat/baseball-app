f95dfcd35df07c565c49acfac2212a16ca6ab13f
Igor Nikiforov <desola2@inbox.ru>
2023-03-20 13:24:37 +0300

    feat: add segments

--- src/components/PlayerStats/ContentGraphs/FilteredGraphs/HitsAnglesGraphs/HitsAnglesGraph.js
+++ src/components/PlayerStats/ContentGraphs/FilteredGraphs/HitsAnglesGraphs/HitsAnglesGraph.js
@@ -5,28 +5,62 @@ import cl from './HitsAnglesGraph.module.scss';
 const PARAMS = {
   GRAPH_WIDTH: 307,
   GRAPH_HEIGHT: 423,
-  LINE_WIDTH: 180,
+  LINE_WIDTH: 179,
   ZERO_X: 98.45,
   ZERO_Y: 230.35
   // GRAPH_WIDTH: 313,
   // GRAPH_HEIGHT: 428
 };
 
-const HitsAnglesGraph = ({ title, angleValues, dataField }) => {
+const Segment = ({ angle, dataField, minMaxValue }) => {
+  const { min, max } = minMaxValue;
+  const { deg } = angle;
+
+  const minMaxDelta = max - min;
+  const rel = (angle[dataField] - min) / minMaxDelta;
+
   let path = `M${PARAMS.ZERO_X},${PARAMS.ZERO_Y}`;
 
-  let secondDotXCoord = PARAMS.LINE_WIDTH * 0.55 * Math.cos(degToRad(20)) + PARAMS.ZERO_X;
-  let secondDotYCoord = PARAMS.ZERO_Y - PARAMS.LINE_WIDTH * 0.55 * Math.sin(degToRad(20));
+  let secondDotXCoord = PARAMS.LINE_WIDTH * rel * Math.cos(degToRad(deg + 10)) + PARAMS.ZERO_X;
+  let secondDotYCoord = PARAMS.ZERO_Y - PARAMS.LINE_WIDTH * rel * Math.sin(degToRad(deg + 10));
   path += `L${secondDotXCoord},${secondDotYCoord}`;
 
-  let centerDotXCoord = PARAMS.LINE_WIDTH * 0.5555 * Math.cos(degToRad(15)) + PARAMS.ZERO_X;
-  let centerDotYCoord = PARAMS.ZERO_Y - PARAMS.LINE_WIDTH * 0.5555 * Math.sin(degToRad(15));
+  let centerDotXCoord = PARAMS.LINE_WIDTH * rel * 1.005 * Math.cos(degToRad(deg + 5)) + PARAMS.ZERO_X;
+  let centerDotYCoord = PARAMS.ZERO_Y - PARAMS.LINE_WIDTH * rel * 1.005 * Math.sin(degToRad(deg + 5));
 
-  let thirdDotXCoord = PARAMS.LINE_WIDTH * 0.55 * Math.cos(degToRad(10)) + PARAMS.ZERO_X;
-  let thirdDotYCoord = PARAMS.ZERO_Y - PARAMS.LINE_WIDTH * 0.55 * Math.sin(degToRad(10));
+  let thirdDotXCoord = PARAMS.LINE_WIDTH * rel * Math.cos(degToRad(deg)) + PARAMS.ZERO_X;
+  let thirdDotYCoord = PARAMS.ZERO_Y - PARAMS.LINE_WIDTH * rel * Math.sin(degToRad(deg));
 
   path += `Q${centerDotXCoord},${centerDotYCoord} ${thirdDotXCoord},${thirdDotYCoord}Z`;
+  return (
+    <path
+      d={path}
+      fill={`hsla(${169 + 0.41 * rel * 100}, 30%, ${88 - 0.15 * rel * 100}%, 1)`}
+    />
+  );
+};
 
+const Segments = ({ angleValues, dataField }) => {
+  const minMaxValue = angleValues.reduce(
+    (sum, value) => {
+      if (value[dataField] > sum.max) sum.max = value[dataField];
+      if (value[dataField] < sum.min) sum.min = value[dataField];
+
+      return sum;
+    },
+    { min: 0, max: 0 }
+  );
+
+  return (
+    <>
+      {angleValues.map((angle, i) => (
+        <Segment key={i} angle={angle} dataField={dataField} minMaxValue={minMaxValue} />
+      ))}
+    </>
+  );
+};
+
+const HitsAnglesGraph = ({ title, angleValues, dataField }) => {
   return (
     <svg
       width='307'
@@ -44,7 +78,9 @@ const HitsAnglesGraph = ({ title, angleValues, dataField }) => {
         {title}
       </text>
 
-      <path d={path} fill='lightblue' />
+      <Segments angleValues={angleValues} dataField={dataField} />
+
+      {/* <path d={path} fill='lightblue' /> */}
     </svg>
   );
 };
