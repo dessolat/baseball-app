ddb4e46f1e50c8d3df92db798a48d0f9435f515d
Igor Nikiforov <desola2@inbox.ru>
2023-03-28 13:05:01 +0300

    feat: add all batter balls

--- src/components/Game/PlaysHitting/HittingField/HittingField.js
+++ src/components/Game/PlaysHitting/HittingField/HittingField.js
@@ -1,6 +1,6 @@
-import React, { Suspense, useMemo, useState, useRef, useEffect, useLayoutEffect } from 'react';
+import React, { Suspense, useMemo, useState, useRef, useLayoutEffect, memo } from 'react';
 import cl from './HittingField.module.scss';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { CatmullRomCurve3, FrontSide, TextureLoader } from 'three';
 import { OrbitControls } from '@react-three/drei';
 import FieldBg from 'images/field_right.jpg';
@@ -11,6 +11,7 @@ import ArrowDown from 'components/UI/icons/ArrowDown';
 import CameraIcon from 'images/video_camera_icon.png';
 import FieldIcon from 'images/field_icon.png';
 import CameraView from './CameraView';
+import { useSelector } from 'react-redux';
 
 // extend({ TextGeometry });
 
@@ -34,29 +35,56 @@ const OptionsBar = ({ isAutoRotate, handleAutoRotateClick, handleResetClick }) =
   );
 };
 
-const Curve = ({ data, coef, curveCount }) => {
-  const points = data.slice(0, curveCount).reduce((sum, coord) => {
-    const newCoord = new THREE.Vector3(coord[0] * coef - 320, coord[2] * coef, coord[1] * -coef + 217);
-    // const newCoord = new THREE.Vector3(-320, coord[2] * 5, 217);
-    // const newCoord = new THREE.Vector3(coord[0]-325, coord[2] * 20, (coord[1] - 1490) / 3 + 150);
-    // const newCoord = new THREE.Vector3(coord[0] / 3.5 - 450, coord[2] * 20, (coord[1] - 1490) / 3 + 150);
-    sum.push(newCoord);
-    return sum;
-  }, []);
+const Curve = ({ moment, coef, setDrawPoints, currentMoment }) => {
+  const { data_3d: data } = moment?.metering?.hit;
+
+  const cashedStepTotal = useRef(0);
+  const tubeRef = useRef(null);
+
+  let stepTotalRef = cashedStepTotal.current;
+
+  useLayoutEffect(() => {
+    setDrawPoints(false);
+
+    stepTotalRef = 0;
+    cashedStepTotal.current = 0;
+  }, [data]);
+
+  const points = data
+    // .slice(0, curveCount)
+    .reduce((sum, coord) => {
+      const newCoord = new THREE.Vector3(coord[0] * coef - 320, coord[2] * coef, coord[1] * -coef + 217);
+
+      sum.push(newCoord);
+      return sum;
+    }, []);
 
   const curveCoords = new CatmullRomCurve3(points);
 
-  const textureRef = useMemo(() => new TextureLoader().load(CurveTexture), []);
+  // const textureRef = useMemo(() => new TextureLoader().load(CurveTexture), []);
+
+  useFrame(() => {
+    if (stepTotalRef > 30510) return;
+    if (stepTotalRef > 30110) {
+      setDrawPoints(true);
+      cashedStepTotal.current = stepTotalRef;
+      return;
+    }
+    cashedStepTotal.current = stepTotalRef;
+
+    stepTotalRef += 400;
+    tubeRef.current.setDrawRange(0, stepTotalRef);
+  });
+  const isCurrentTube = currentMoment.inner.id === moment.inner.id;
   return (
     <>
-      {/* <mesh position={[0, 0, 0]}> */}
-
       <mesh position={[-70, 0, 220]} castShadow>
-        <tubeGeometry args={[curveCoords, 70, 3, 50, false]} />
+        <tubeGeometry args={[curveCoords, 70, 3, 50, false]} ref={tubeRef} />
         <meshPhongMaterial
-          // color={'blue'}
-          map={textureRef}
+          color={isCurrentTube ? 'blue' : '#0099E6'}
+          // map={textureRef}
           side={FrontSide}
+
           //  shadowSide={DoubleSide}
           //  metalness={0}
 
@@ -74,15 +102,43 @@ const Curve = ({ data, coef, curveCount }) => {
   );
 };
 
-const TouchPoints = ({ data, coef, curveCount }) => (
+const CurveAndPoints = memo(({ moment, coef, currentMoment }) => {
+  const [drawPoints, setDrawPoints] = useState(false);
+  const { data_3d: data3D } = moment?.metering?.hit;
+
+  return (
+    <>
+      <Curve moment={moment} coef={coef} setDrawPoints={setDrawPoints} currentMoment={currentMoment} />
+      {drawPoints && <TouchPoints data={data3D} coef={coef} />}
+    </>
+  );
+}, propsComparison);
+
+function propsComparison(prevProps, nextProps) {
+  return JSON.stringify(prevProps) === JSON.stringify(nextProps);
+}
+
+const Curves = memo(
+  ({ batterMoments, coef, currentMoment }) => (
+    <>
+      {batterMoments.map((moment, i) => {
+        return (
+          <CurveAndPoints key={moment.inner.id} moment={moment} coef={coef} currentMoment={currentMoment} />
+        );
+        // return <CurveAndPoints key={'curve-points-' + i} data3D={data3D} coef={coef} />;
+      })}
+    </>
+  ),
+  propsComparison
+);
+
+const TouchPoints = ({ data, coef }) => (
   <>
     {data
-      .slice(0, curveCount)
+      // .slice(0, curveCount)
       .filter(coords => coords[3] === 1)
-      .map(coord => (
-        <mesh position={[coord[0] * coef - 320 - 70, coord[2] * coef, coord[1] * -coef + 217 + 220]}>
-          {/* <mesh position={[coord[0] * coef - 320, coord[2] * coef, coord[1] * -coef + 217]}> */}
-          {/* <mesh position={[coord[0] / 3.5 - 450 - 70, coord[2] * 20, (coord[1] - 1490) / 3 + 150 + 220]}> */}
+      .map((coord, i) => (
+        <mesh key={i} position={[coord[0] * coef - 320 - 70, coord[2] * coef, coord[1] * -coef + 217 + 220]}>
           <sphereGeometry args={[5, 40, 40]} />
           <meshStandardMaterial color={'red'} />
         </mesh>
@@ -90,59 +146,49 @@ const TouchPoints = ({ data, coef, curveCount }) => (
   </>
 );
 
-const HittingField = ({ hit }) => {
-  const { data_3d, camera_2d: camera2D } = hit || 0;
+const HittingField = () => {
   const [isAutoRotate, setAutoRotate] = useState(true);
-  const [curveCount, setCurveCount] = useState(null);
+  // const [curveCount, setCurveCount] = useState(null);
   const [isCameraView, setCameraView] = useState(false);
+
+  const { innings, currentCard, currentMoment } = useSelector(state => state.game);
+
+  const { hit } = currentMoment.metering || {};
+  const { data_3d, camera_2d: camera2D } = hit || 0;
 
   const textureRef = useMemo(() => new TextureLoader().load(FieldBg), []);
 
   const controlsRef = useRef();
 
-  // let ballPath = data_2d ? `M${data_2d[0][0]} ${data_2d[0][1]}` : '';
-  // data_2d && data_2d.slice(1).forEach(coords => (ballPath += `L${coords[0]} ${coords[1]}`));
+  // useLayoutEffect(() => {
+  //   if (isCameraView) return;
 
-  // const intermediatePoints = data_2d
-  //   ? data_2d
-  //       .filter(coords => coords[3] === 1)
-  //       .map((coords, i) => <circle key={i} cx={coords[0]} cy={coords[1]} r={18} fill='red'></circle>)
-  //   : '';
+  //   setCurveCount(0);
 
-  // const setCords = e => {
-  // 	console.log(e);
-  //   setCameraCoords([e.point.x, e.point.y, e.point.z]);
-  // };
+  //   if (!data_3d) return;
 
-  useLayoutEffect(() => {
-    if (isCameraView) return;
+  //   setTimeout(() => {
+  //     setCurveCount(1);
+  //   }, 150);
+  // }, [data_3d, isCameraView]);
 
-    setCurveCount(0);
+  // useEffect(() => {
+  //   if (!data_3d || curveCount === 0) return;
 
-    if (!data_3d) return;
+  //   const length = data_3d.length;
 
-    setTimeout(() => {
-      setCurveCount(1);
-    }, 150);
-  }, [data_3d, isCameraView]);
+  //   let timeout;
 
-  useEffect(() => {
-    if (!data_3d || curveCount === 0) return;
+  //   if (curveCount < length) {
+  //     timeout = setTimeout(() => {
+  //       setCurveCount(prev => prev + 1);
+  //     }, 3);
+  //   }
 
-    const length = data_3d.length;
-
-    let timeout;
-
-    if (curveCount < length) {
-      timeout = setTimeout(() => {
-        setCurveCount(prev => prev + 1);
-      }, 3);
-    }
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [curveCount]);
+  //   return () => {
+  //     clearTimeout(timeout);
+  //   };
+  // }, [curveCount]);
 
   const handleAutoRotateClick = () => setAutoRotate(prev => !prev);
   const handleResetClick = () => controlsRef.current.reset();
@@ -150,18 +196,49 @@ const HittingField = ({ hit }) => {
   // const font = new FontLoader().parse(ComfortaaFont)
 
   const coef = 925 / 90;
-  // const yCoef = 909 / 90;
-  // xStart = -320 (1)
-  // xEnd = 605 (1)
-  // yStart = 217 (3)
-  // yEnd = -692 (3)
 
-  // if (data_3d && curveCount > 1) {
-  // }
+  const batterCards = useMemo(
+    () =>
+      innings.reduce((sum, inning, i) => {
+        inning['bottom/owners']
+          ?.filter(card => card.who_id === currentCard.who_id)
+          .forEach(card => {
+            sum.push(card);
+          });
+        inning['top/guests']
+          ?.filter(card => card.who_id === currentCard.who_id)
+          .forEach(card => {
+            sum.push(card);
+          });
+
+        return sum;
+      }, []),
+    [innings, currentCard]
+  );
+
+  const batterMoments = useMemo(
+    () =>
+      batterCards.reduce((sum, card) => {
+        card.moments
+          .filter(
+            moment => moment.metering?.hit?.data_3d
+            // && moment.inner.id !== currentMoment.inner?.id
+          )
+          .forEach(moment => sum.push(moment));
+
+        return sum;
+      }, []),
+    [
+      batterCards
+      // , currentMoment
+    ]
+  );
 
   const cameraSwitchBtnHandler = () => setCameraView(prev => !prev);
 
-  const isRenderPath = data_3d && curveCount > 1;
+  // const isCurrentTrajectory =  !!data_3d;
+  const isOtherTrajectories = true;
+  // const isOtherTrajectories = curveCount > 1;
   const btnIcon = isCameraView ? FieldIcon : CameraIcon;
   const isBtnIcon = camera2D !== null;
   return (
@@ -180,24 +257,13 @@ const HittingField = ({ hit }) => {
                 <meshStandardMaterial map={textureRef} toneMapped={false} shadowSide={THREE.DoubleSide} />
               </mesh>
 
-              {isRenderPath && <Curve data={data_3d} coef={coef} curveCount={curveCount} />}
-              {/* {data_2d?.map(coord => (
-            <mesh position={[coord[0] / 3.5 - 450, coord[2] * 20, (coord[1] - 1490) / 3 + 150]}>
-              <sphereGeometry args={[5, 40, 40]} />
-              <meshStandardMaterial color={'blue'} />
-            </mesh>
-          ))} */}
+              {/* CurrentMoment trajectory */}
+              {/* {isCurrentTrajectory && <CurveAndPoints data3D={data_3d} coef={coef} />} */}
+              {/* {isCurrentTrajectory && <Curve data={data_3d} coef={coef} />}
+              {isCurrentTrajectory && <TouchPoints data={data_3d} coef={coef} />} */}
 
-              {isRenderPath && <TouchPoints data={data_3d} coef={coef} curveCount={curveCount} />}
-
-              {/* <spotLight
-            position={[0, 1500, 0]}
-            // target={new THREE.Object3D(500,500,100)}
-            intensity={1}
-            castShadow={true}
-						distance={15000}
-						power={2.5}
-						/> */}
+              {/* Other trajectories */}
+              <Curves batterMoments={batterMoments} coef={coef} currentMoment={currentMoment} />
 
               <directionalLight
                 position={[0, 400, 0]}
@@ -229,18 +295,6 @@ const HittingField = ({ hit }) => {
             handleAutoRotateClick={handleAutoRotateClick}
             handleResetClick={handleResetClick}
           />
-
-          {/* <div className={cl.fieldBg}>
-        <svg width='100%' height='100%' viewBox='0 0 2560 2560' fill='none' preserveAspectRatio='none'>
-          <path
-            d={ballPath}
-            stroke='#4AA0F0'
-            strokeWidth='20'
-            strokeLinejoin='round'
-            strokeLinecap='round'></path>
-          {intermediatePoints}
-        </svg>
-      </div> */}
         </>
       )}
       {isCameraView && camera2D !== null && <CameraView camera2D={camera2D} />}
@@ -253,4 +307,4 @@ const HittingField = ({ hit }) => {
   );
 };
 
-export default HittingField;
+export default memo(HittingField);
