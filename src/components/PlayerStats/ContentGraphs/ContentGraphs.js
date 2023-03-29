1e7f95100b2f7d83395515b74477c905a5c6e4a5
Igor Nikiforov <desola2@inbox.ru>
2023-03-29 12:03:10 +0300

    refactor: remove commented code

--- src/components/PlayerStats/ContentGraphs/ContentGraphs.js
+++ src/components/PlayerStats/ContentGraphs/ContentGraphs.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import cl from './ContentGraphs.module.scss';
-// import FrequencySpeedGraph from '../FrequencySpeedGraph/FrequencySpeedGraph';
-// import BreakGraph from '../BreakGraph/BreakGraph';
-// import TypesGraph from '../TypesGraph/TypesGraph';
-// import ArsenalGraph from '../ArsenalGraph/ArsenalGraph';
-// import DotsGraph from '../DotsGraph/DotsGraph';
-// import PlayerImgLeft from '../../../images/player_left.png';
-// import PlayerImgRight from '../../../images/player_right.png';
 import Banner from './Banner/Banner';
 import FilteredGraphs from './FilteredGraphs/FilteredGraphs';
 
@@ -17,31 +10,6 @@ const ContentGraphs = ({ pitchesData }) => {
     <div className={cl.graphsWrapper}>
       <Banner />
       {isFilteredGraphs && <FilteredGraphs pitchesData={pitchesData} />}
-
-      {/* <div className={cl.graphRow}>
-        <FrequencySpeedGraph />
-        <BreakGraph />
-        <TypesGraph />
-      </div>
-      <div className={cl.graphRow} style={{ marginTop: '30px' }}>
-        <ArsenalGraph />
-      </div>
-      <div className={cl.graphRow} style={{ marginTop: '80px', padding: '0 35px 50px' }}>
-        <img src={PlayerImgRight} alt='player' />
-        <DotsGraph color='red' />
-        <DotsGraph color='green' />
-        <DotsGraph color='blue' />
-        <DotsGraph color='yellow' />
-        <DotsGraph color='olive' />
-      </div>
-      <div className={cl.graphRow} style={{ marginTop: '50px', padding: '0 35px 50px' }}>
-        <img src={PlayerImgLeft} alt='player' />
-        <DotsGraph color='red' />
-        <DotsGraph color='green' />
-        <DotsGraph color='blue' />
-        <DotsGraph color='yellow' />
-        <DotsGraph color='olive' />
-      </div> */}
     </div>
   );
 };
