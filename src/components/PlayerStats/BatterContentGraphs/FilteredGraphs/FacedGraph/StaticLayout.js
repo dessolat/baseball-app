4030eeed4a2760c1fec693db909509f7ce3b159d
Igor Nikiforov <desola2@inbox.ru>
2023-04-06 17:31:18 +0300

    feat: add titles

--- src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/FacedGraph/StaticLayout.js
+++ src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/FacedGraph/StaticLayout.js
@@ -1,3 +1,5 @@
+import { leftTitle, bottomTitle } from './FacedGraph.module.scss';
+
 const HorizontalLines = ({ PARAMS }) => {
   const linesArr = new Array(PARAMS.ROWS_NUMBER + 1).fill('');
   const rowDelta = PARAMS.GRAPH_HEIGHT / PARAMS.ROWS_NUMBER;
@@ -40,11 +42,30 @@ const VerticalLines = ({ PARAMS }) => {
   );
 };
 
+const Titles = ({ PARAMS }) => {
+  const leftTitleCoords = { x: PARAMS.PADDING_LEFT - 40, y: PARAMS.PADDING_TOP + PARAMS.GRAPH_HEIGHT + 15 };
+  const bottomTitleCoords = { x: PARAMS.PADDING_LEFT - 1, y: PARAMS.PADDING_TOP + PARAMS.GRAPH_HEIGHT + 34 };
+  return (
+    <>
+      {/* Left title */}
+      <text x={leftTitleCoords.x} y={leftTitleCoords.y} className={leftTitle}>
+        Pitches
+      </text>
+
+      {/* Bottom title */}
+      <text x={bottomTitleCoords.x} y={bottomTitleCoords.y + 15} className={bottomTitle}>
+        Speed, mph
+      </text>
+    </>
+  );
+};
+
 const StaticLayout = ({ PARAMS }) => {
   return (
     <>
       <HorizontalLines PARAMS={PARAMS} />
       <VerticalLines PARAMS={PARAMS} />
+      <Titles PARAMS={PARAMS} />
     </>
   );
 };
