9befab4b22689cd44b70c763f2d656d40e1be299
Igor Nikiforov <desola2@inbox.ru>
2023-04-06 16:05:53 +0300

    feat: add basic bg and style

--- src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/FacedGraph/FacedGraph.js
+++ src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/FacedGraph/FacedGraph.js
@@ -1,8 +1,21 @@
-import GraphImg from 'images/faced_graph.jpg';
 import cl from './FacedGraph.module.scss';
 
+const PARAMS = {
+  GRAPH_WIDTH: 940,
+  GRAPH_HEIGHT: 365
+};
+
 const FacedGraph = () => {
-  return <img src={GraphImg} className={cl.graphThumb} alt='' />;
+  return (
+    <svg
+      viewBox={`0 0 ${PARAMS.GRAPH_WIDTH} ${PARAMS.GRAPH_HEIGHT}`}
+      width='100%'
+      fill='none'
+      className={cl.graph}
+      xmlns='http://www.w3.org/2000/svg'>
+				
+			</svg>
+  );
 };
 
 export default FacedGraph;
