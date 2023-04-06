38ee397e51ac0a0caecd8d21269870d49ee7174d
Igor Nikiforov <desola2@inbox.ru>
2023-04-06 16:31:32 +0300

    feat: add VerticalLines comp

--- src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/FacedGraph/StaticLayout.js
+++ src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/FacedGraph/StaticLayout.js
@@ -1,12 +1,12 @@
-const HorizontalRows = ({ PARAMS }) => {
-  const rowsArr = new Array(PARAMS.ROWS_NUMBER + 1).fill('');
-	const rowDelta = PARAMS.GRAPH_HEIGHT / PARAMS.ROWS_NUMBER
+const HorizontalLines = ({ PARAMS }) => {
+  const linesArr = new Array(PARAMS.ROWS_NUMBER + 1).fill('');
+  const rowDelta = PARAMS.GRAPH_HEIGHT / PARAMS.ROWS_NUMBER;
 
   return (
     <>
-      {rowsArr.map((_, i) => (
+      {linesArr.map((_, i) => (
         <line
-          key={i}
+          key={'hor-line-' + i}
           x1={PARAMS.PADDING_LEFT}
           y1={PARAMS.PADDING_TOP + i * rowDelta}
           x2={PARAMS.PADDING_LEFT + PARAMS.GRAPH_WIDTH}
@@ -19,10 +19,32 @@ const HorizontalRows = ({ PARAMS }) => {
   );
 };
 
+const VerticalLines = ({ PARAMS }) => {
+  const linesArr = new Array(PARAMS.COLS_NUMBER + 1).fill('');
+  const colDelta = PARAMS.GRAPH_WIDTH / PARAMS.COLS_NUMBER;
+
+  return (
+    <>
+      {linesArr.map((_, i) => (
+        <line
+          key={'vert-line-' + i}
+          x1={PARAMS.PADDING_LEFT + i * colDelta}
+          y1={PARAMS.PADDING_TOP}
+          x2={PARAMS.PADDING_LEFT + i * colDelta}
+          y2={PARAMS.PADDING_TOP + PARAMS.GRAPH_HEIGHT}
+          stroke='#ACACAC'
+          strokeDasharray='4 2'
+        />
+      ))}
+    </>
+  );
+};
+
 const StaticLayout = ({ PARAMS }) => {
   return (
     <>
-      <HorizontalRows PARAMS={PARAMS} />
+      <HorizontalLines PARAMS={PARAMS} />
+      <VerticalLines PARAMS={PARAMS} />
     </>
   );
 };
