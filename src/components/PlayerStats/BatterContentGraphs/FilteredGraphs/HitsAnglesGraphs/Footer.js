87c250ecb784e6e384f9581408b13e92a04daf47
Igor Nikiforov <desola2@inbox.ru>
2023-04-10 16:09:59 +0300

    feat: add footer

---
+++ src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/HitsAnglesGraphs/Footer.js
@@ -0,0 +1,38 @@
+import { footerText } from './HitsAnglesGraph.module.scss';
+
+const Footer = ({
+  PARAMS: { GRAPH_WIDTH: graphWidth, GRAPH_HEIGHT: graphHeight },
+  angleValues,
+  footerField,
+  footerUnits
+}) => {
+  const minMaxAvgValues = angleValues
+    .filter(valueGroup => valueGroup[`min${footerField}`] !== undefined)
+    .reduce(
+      (sum, valueGroup) => {
+        if (sum.min === undefined || valueGroup[`min${footerField}`] < sum.min)
+          sum.min = valueGroup[`min${footerField}`];
+        if (sum.max === undefined || valueGroup[`max${footerField}`] > sum.max)
+          sum.max = valueGroup[`max${footerField}`];
+
+        sum.sumValues += valueGroup[`sum${footerField}`];
+        sum.sumHits += valueGroup.hitsCount;
+
+        return sum;
+      },
+      { sumValues: 0, sumHits: 0 }
+    );
+
+  minMaxAvgValues.avg = minMaxAvgValues.sumValues / minMaxAvgValues.sumHits;
+
+  const minValue = `${Math.round(minMaxAvgValues.min * 10) / 10} ${footerUnits}`;
+  const avgValue = `${Math.round(minMaxAvgValues.avg * 10) / 10} ${footerUnits}`;
+  const maxValue = `${Math.round(minMaxAvgValues.max * 10) / 10} ${footerUnits}`;
+  return (
+    <text x={graphWidth / 2} y={graphHeight - 8} className={footerText}>
+      MIN: {minValue} AVG: {avgValue} MAX: {maxValue}
+    </text>
+  );
+};
+
+export default Footer;
