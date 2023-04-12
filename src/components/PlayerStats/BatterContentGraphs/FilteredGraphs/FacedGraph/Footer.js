c42c5a0317c6477c263232279611ee1bb1873e29
Igor Nikiforov <desola2@inbox.ru>
2023-04-12 14:52:14 +0300

    fix: footer values
    * remove empty pitch class from legend

--- src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/FacedGraph/Footer.js
+++ src/components/PlayerStats/BatterContentGraphs/FilteredGraphs/FacedGraph/Footer.js
@@ -12,38 +12,43 @@ const Footer = ({ totalClasses, PARAMS, data, pitchClasses }) => {
       return minMax;
     }
 
-		if (speed < minMax[pitchClasses[pitchType]].min) {
-			minMax[pitchClasses[pitchType]].min = speed
-		}
-		if (speed > minMax[pitchClasses[pitchType]].max) {
-			minMax[pitchClasses[pitchType]].max = speed
-		}
+    if (speed < minMax[pitchClasses[pitchType]].min) {
+      minMax[pitchClasses[pitchType]].min = speed;
+    }
+    if (speed > minMax[pitchClasses[pitchType]].max) {
+      minMax[pitchClasses[pitchType]].max = speed;
+    }
 
-		minMax[pitchClasses[pitchType]].sum += speed
-		minMax[pitchClasses[pitchType]].count++
+    minMax[pitchClasses[pitchType]].sum += speed;
+    minMax[pitchClasses[pitchType]].count++;
 
-		return minMax
+    return minMax;
   }, {});
+
   return (
     <>
-      {totalClasses.map((classTitle, i) => {
-        const xCoord = PARAMS.PADDING_LEFT + 135 + 207 * i;
-				const avgValue = Math.round(minMaxSum[classTitle].sum / minMaxSum[classTitle].count)
-        return (
-          <Fragment key={i}>
-            <rect
-              x={xCoord}
-              y={yCoord}
-              width='6'
-              height='6'
-              fill={getPitchСlassPrimaryColorByName(classTitle)}
-            />
-            <text x={xCoord + 10} y={yCoord + 7} className={footerValue}>
-              {classTitle} (Min:{minMaxSum[classTitle].min}, Avg:{avgValue}, Max:{minMaxSum[classTitle].max})
-            </text>
-          </Fragment>
-        );
-      })}
+      {totalClasses
+        .filter(classTitle => classTitle !== '')
+        .map((classTitle, i) => {
+          const xCoord = PARAMS.PADDING_LEFT + 135 + 207 * i;
+          const minValue = Math.round(minMaxSum[classTitle].min * 2.24);
+          const avgValue = Math.round((minMaxSum[classTitle].sum / minMaxSum[classTitle].count) * 2.24);
+          const maxValue = Math.round(minMaxSum[classTitle].max * 2.24);
+          return (
+            <Fragment key={i}>
+              <rect
+                x={xCoord}
+                y={yCoord}
+                width='6'
+                height='6'
+                fill={getPitchСlassPrimaryColorByName(classTitle)}
+              />
+              <text x={xCoord + 10} y={yCoord + 7} className={footerValue}>
+                {classTitle} (Min:{minValue}, Avg:{avgValue}, Max:{maxValue})
+              </text>
+            </Fragment>
+          );
+        })}
     </>
   );
 };
