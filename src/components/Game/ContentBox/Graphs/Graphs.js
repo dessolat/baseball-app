63ffee4848f212f615696d7afb516378aac6378f
Igor Nikiforov <desola2@inbox.ru>
2023-04-22 13:31:15 +0300

    feat: add Graphs comp to desktop box

---
+++ src/components/Game/ContentBox/Graphs/Graphs.js
@@ -0,0 +1,22 @@
+// import GraphsBlock from './GraphsBlock';
+// import PitchesTrajectories from './PitchesTrajectories/PitchesTrajectories';
+// import GraphsHeader from './GraphsHeader/GraphsHeader';
+// import { useSelector } from 'react-redux';
+
+const Graphs = () => {
+  // const { preview, boxActiveButton } = useSelector(s => s.game);
+
+  return (
+    <div className='container'>
+      {/* <GraphsBlock noSelector>
+        <GraphsHeader title='Machine vision statistics' subTitle={`${preview[boxActiveButton].name} Batters Hits`} noSelector />
+        <PitchesTrajectories data={[]} />
+      </GraphsBlock> */}
+      {/* <GraphsBlock noSelector>
+        
+      </GraphsBlock> */}
+    </div>
+  );
+};
+
+export default Graphs;
