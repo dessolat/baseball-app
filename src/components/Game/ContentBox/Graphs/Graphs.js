06e69fb8dda476e9dbe7c2e1b6456aa590ab00c3
Igor Nikiforov <desola2@inbox.ru>
2023-04-22 14:19:04 +0300

    feat: add to graphs

--- src/components/Game/ContentBox/Graphs/Graphs.js
+++ src/components/Game/ContentBox/Graphs/Graphs.js
@@ -2,6 +2,7 @@ import GraphsBlock from './GraphsBlock';
 import PitchesTrajectories from './PitchesTrajectories/PitchesTrajectories';
 import GraphsHeader from './GraphsHeader/GraphsHeader';
 import { useSelector } from 'react-redux';
+import BattersTable from './BattersTable/BattersTable';
 
 const Graphs = () => {
   const { preview, boxActiveButton } = useSelector(s => s.game);
@@ -9,12 +10,16 @@ const Graphs = () => {
   return (
     <div className='container'>
       <GraphsBlock noSelector>
-        <GraphsHeader title='Machine vision statistics' subTitle={`${preview[boxActiveButton].name} Batters Hits`} noSelector />
+        <GraphsHeader
+          title='Machine vision statistics'
+          subTitle={`${preview[boxActiveButton].name} Batters Hits`}
+          noSelector
+        />
         <PitchesTrajectories data={[]} />
       </GraphsBlock>
-      {/* <GraphsBlock noSelector>
-        
-      </GraphsBlock> */}
+      <GraphsBlock noSelector style={{ lineHeight: 'unset' }}>
+        <BattersTable />
+      </GraphsBlock>
     </div>
   );
 };
