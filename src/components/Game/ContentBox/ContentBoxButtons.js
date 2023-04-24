23f34dfecb9271b3a7d519e3a86eeb4f2882155a
Igor Nikiforov <desola2@inbox.ru>
2023-04-24 14:38:19 +0300

    feat: add buttons scrolling with page

--- src/components/Game/ContentBox/ContentBoxButtons.js
+++ src/components/Game/ContentBox/ContentBoxButtons.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import cl from './ContentBox.module.scss';
+import { useEffect, useState, useRef } from 'react';
+import { buttons } from './ContentBox.module.scss';
 import ContentBoxButton from './ContentBoxButton';
 
-const ContentBoxButtons = () => (
-  <div className={cl.buttons}>
-    <ContentBoxButton team={'guests'} />
-    <ContentBoxButton team={'owners'} />
-  </div>
-);
+const ContentBoxButtons = () => {
+  const [buttonsTopPosition, setButtonsTopPosition] = useState({ left: 0, top: 0 });
+  const buttonsRef = useRef();
+
+  useEffect(() => {
+    function handleWindowResize() {
+      const parentElement = buttonsRef.current.parentElement;
+
+      setButtonsTopPosition({
+        top: parentElement.getBoundingClientRect().top,
+        left: parentElement.getBoundingClientRect().left
+      });
+    }
+
+		handleWindowResize()
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <div
+      className={buttons}
+      style={{ top: `calc(${buttonsTopPosition.top}px + 6.3rem)`, left: `${buttonsTopPosition.left}px` }}
+      ref={buttonsRef}
+      // style={{top: `calc(6.3rem + ${buttonsTopPosition}px)`}}
+    >
+      <ContentBoxButton team={'guests'} />
+      <ContentBoxButton team={'owners'} />
+    </div>
+  );
+};
 
 export default ContentBoxButtons;
