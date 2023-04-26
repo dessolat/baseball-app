982d3c304cb5903161f53789bf7548b5c7153130
Igor Nikiforov <desola2@inbox.ru>
2023-04-26 11:19:27 +0300

    feat: remove DotRadiusContext

--- src/context/index.js
+++ src/context/index.js
@@ -2,6 +2,5 @@ import { createContext } from "react";
 
 export const PlayerYearsContext = createContext()
 export const AnimationContext = createContext();
-export const DotRadiusContext = createContext();
 export const GamesLoadingContext = createContext();
 export const StatsLoadingContext = createContext();
