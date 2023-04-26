b589de1bc154ab5c010d933802aa9d884560802a
Igor Nikiforov <desola2@inbox.ru>
2023-04-26 10:48:19 +0300

    feat: add progress bar to loader

--- src/components/UI/loaders/Loader/Loader.js
+++ src/components/UI/loaders/Loader/Loader.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import cl from './Loader.module.scss';
 
+// const ProgressTitle = ({ loadedPercents }) => (
+//   <span style={{ position: 'absolute', bottom: '-4.5rem', left: '50%', translate: '-50% 0' }}>
+//     {Math.round(loadedPercents)}%
+//   </span>
+// );
+
+const ProgressBar = ({ loadedPercents }) => (
+  <progress
+    value={Math.round(loadedPercents)}
+    max='100'
+    style={{
+      position: 'absolute',
+      bottom: '-4.5rem',
+      left: '50%',
+      translate: '-50% 0',
+      borderRadius: '2px',
+      height: '4px',
+      width: 100
+    }}></progress>
+);
+
 const Loader = ({ styles = null, loadedPercents = null }) => {
   const isPercentsTitle = loadedPercents && loadedPercents >= 10;
 
@@ -14,11 +35,7 @@ const Loader = ({ styles = null, loadedPercents = null }) => {
       <div></div>
       <div></div>
       <div></div>
-      {isPercentsTitle && (
-        <span style={{ position: 'absolute', bottom: '-4.5rem', left: '50%', translate: '-50% 0' }}>
-          {Math.round(loadedPercents)}%
-        </span>
-      )}
+      {isPercentsTitle && <ProgressBar loadedPercents={loadedPercents} />}
     </div>
   );
 };
