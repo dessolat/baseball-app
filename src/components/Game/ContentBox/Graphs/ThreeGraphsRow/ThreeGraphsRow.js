54d13f145ec5ee89183d88c4022251fc8f7b0128
Igor Nikiforov <desola2@inbox.ru>
2023-05-01 23:05:25 +0300

    feat: add functional

--- src/components/Game/ContentBox/Graphs/ThreeGraphsRow/ThreeGraphsRow.js
+++ src/components/Game/ContentBox/Graphs/ThreeGraphsRow/ThreeGraphsRow.js
@@ -1,15 +1,42 @@
+import { getPitchColorByName } from 'utils';
 import PitchesSpeedField from '../PitchesSpeedField/PitchesSpeedField';
 import TwinPitchesGraph from '../TwinPitchesGraph/TwinPitchesGraph';
 import cl from './ThreeGraphsRow.module.scss';
 
-const ThreeGraphsRow = ({ currentOption, setCurrentOption }) => {
+const ThreeGraphsRow = ({ currentOption, setCurrentOption, pitcher }) => {
+  const { pitch_types: pitchTypes } = pitcher.preview;
+
+	const relValuesData = pitcher.pitches_all.reduce((sum, pitch) => {
+    const { speed, pitch_type, pitchGraphCoords } = pitch.pitch_info;
+    const pitchType = pitchTypes[pitch_type];
+
+    if (sum[pitchType] !== undefined) {
+      sum[pitchType].count += 1;
+      sum[pitchType].speeds.push(speed * 2.24);
+      sum[pitchType].pitchGraphCoords.push({ ...pitchGraphCoords, color: getPitchColorByName(pitchType) });
+
+      return sum;
+    }
+
+    sum[pitchType] = {
+      count: 1,
+      speeds: [speed * 2.24],
+      pitchGraphCoords: [{ ...pitchGraphCoords, color: getPitchColorByName(pitchType) }]
+    };
+
+    return sum;
+  }, {});
+
   return (
     <div className={cl.wrapper}>
-      <TwinPitchesGraph style={{ flex: 278 }} />
+      <TwinPitchesGraph filteredData={pitcher.pitches_all} preview={pitcher.preview} style={{ flex: 278 }} />
       <PitchesSpeedField
         optionsArr={['Types', 'All Pitches']}
         currentOption={currentOption}
         setCurrentOption={setCurrentOption}
+        filteredData={pitcher.pitches_all}
+        preview={pitcher.preview}
+				relValuesData={relValuesData}
       />
     </div>
   );
