6dc779286662dea25d0fde46268b337794251812
Igor Nikiforov <desola2@inbox.ru>
2023-05-02 14:42:39 +0300

    feat: add teamName

--- src/components/Game/ContentBox/Graphs/Graphs.js
+++ src/components/Game/ContentBox/Graphs/Graphs.js
@@ -1,22 +1,19 @@
 import GraphsBlock from './GraphsBlock';
 import PitchesTrajectories from './PitchesTrajectories/PitchesTrajectories';
 import GraphsHeader from './GraphsHeader/GraphsHeader';
-import { useSelector } from 'react-redux';
 import BattersTable from './BattersTable/BattersTable';
 import PitchersReleaseSpeeds from './PitchersReleaseSpeeds/PitchersReleaseSpeeds';
 import PitchersGraphRows from './PitchersGraphRows';
 
 const Graphs = ({ graphsData }) => {
-  const { batters_metrix: battersMetrix, pitchers_metrix: pitchersMetrix } = graphsData;
-
-  const { preview, boxActiveButton } = useSelector(s => s.game);
+  const { batters_metrix: battersMetrix, pitchers_metrix: pitchersMetrix, name: teamName } = graphsData;
 
   return (
     <div className='container'>
       <GraphsBlock noSelector>
         <GraphsHeader
           title='Machine vision statistics'
-          subTitle={`${preview[boxActiveButton].name} Batters Hits`}
+          subTitle={`${teamName} Batters Hits`}
           noSelector
         />
         <PitchesTrajectories metrix={battersMetrix} />
@@ -25,7 +22,7 @@ const Graphs = ({ graphsData }) => {
         <BattersTable metrix={battersMetrix} />
       </GraphsBlock>
       <GraphsBlock noSelector>
-        <GraphsHeader subTitle={`${preview[boxActiveButton].name} pitchers Release speed, mph`} noSelector />
+        <GraphsHeader subTitle={`${teamName} pitchers Release speed, mph`} noSelector />
         <PitchersReleaseSpeeds metrix={pitchersMetrix} />
       </GraphsBlock>
       <PitchersGraphRows metrix={pitchersMetrix} />
