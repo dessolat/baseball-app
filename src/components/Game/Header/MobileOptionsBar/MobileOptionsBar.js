563e4b69ade815bc61cc8c9ce14ca7b1328a039b
Igor Nikiforov <desola2@inbox.ru>
2023-05-09 20:54:44 +0300

    refactor: decompose

--- src/components/Game/Header/MobileOptionsBar/MobileOptionsBar.js
+++ src/components/Game/Header/MobileOptionsBar/MobileOptionsBar.js
@@ -1,25 +1,14 @@
 import classNames from 'classnames';
 import cl from './MobileOptionsBar.module.scss';
-import { shallowEqual, useSelector } from 'react-redux';
 import OptionsBtn from './OptionsBtn';
+import ScoreInfo from './ScoreInfo';
 
 const MobileOptionsBar = () => {
-  const preview = useSelector(s => s.game.preview, shallowEqual);
-
   const optionsBarClasses = classNames(cl.onlyPortraitMobile, cl.optionsBar);
 
   return (
     <div className={optionsBarClasses}>
-      <div className={cl.scoreInfo}>
-        <div className={cl.team}>
-          <span className={cl.teamName}>{preview.guests.name}</span>
-          <span className={cl.teamScore}>{preview.guests.score}</span>
-        </div>
-        <div className={cl.team}>
-          <span className={cl.teamName}>{preview.owners.name}</span>
-          <span className={cl.teamScore}>{preview.owners.score}</span>
-        </div>
-      </div>
+      <ScoreInfo />
       <OptionsBtn />
     </div>
   );
