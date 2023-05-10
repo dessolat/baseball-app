15f044f2b2efcda8f9be3fbb6a9c06ff1e06ed0b
Igor Nikiforov <desola2@inbox.ru>
2023-05-10 14:37:18 +0300

    feat: options panel placeholder thumb

--- src/components/Game/Header/MobileOptionsBar/OptionsDropdown.js
+++ src/components/Game/Header/MobileOptionsBar/OptionsDropdown.js
@@ -2,6 +2,9 @@ import BtnImg from 'icons/options_button_icon.svg';
 import cl from './MobileOptionsBar.module.scss';
 import useComponentVisible from 'hooks/useComponentVisible';
 import CrossClose from 'components/UI/buttons/CrossClose/CrossClose';
+import SimpleToggler from 'components/UI/togglers/SimpleToggler/SimpleToggler';
+import FirstLastMoment from 'components/UI/buttons/FirstLastMoment/FirstLastMoment';
+import ForwardRepeat from 'components/UI/buttons/ForwardRepeat/ForwardRepeat';
 
 const OptionsDropdown = () => {
   const {
@@ -19,6 +22,63 @@ const OptionsDropdown = () => {
       </button>
       {isOpen && (
         <div className={cl.optionsPanel}>
+          <div className={cl.componentsTogglers}>
+            <div className={cl.row}>
+              <span className={cl.title}>Scoreboard</span>
+              <SimpleToggler checked={false} onChange={() => {}} />
+            </div>
+            <div className={cl.row}>
+              <span className={cl.title}>Timeline</span>
+              <SimpleToggler checked={false} onChange={() => {}} />
+            </div>
+          </div>
+          <div className={cl.playModesTogglers}>
+            <div className={cl.buttonWrapper}>
+              <ForwardRepeat
+                playbackMode='play'
+                style={{
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  flexGap: 5
+                }}
+              />
+            </div>
+            <div className={cl.buttonWrapper}>
+              <FirstLastMoment isLastMomentMode={false} />
+            </div>
+          </div>
+          <div className={cl.speedSelectors}>
+            <div className={cl.playbackSpeed}>
+              <p>Speed</p>
+              <div className={cl.speedValues}>
+                <div>0.25</div>
+                <div style={{ borderBottom: '2px solid var(--side-color)', width: 14 }}>1</div>
+                <div>2</div>
+              </div>
+            </div>
+            <div className={cl.timeIntervalTogglers}>
+              <label className={cl.row}>
+                <input type='radio' checked={false} />
+                <p>Full</p>
+              </label>
+              <label className={cl.row}>
+                <input type='radio' checked={true} />
+                <p>Short</p>
+              </label>
+              <label className={cl.row}>
+                <input type='radio' checked={false} />
+                <p>Super Short</p>
+              </label>
+            </div>
+          </div>
+          <div className={cl.effectsToggler}>
+            <div className={cl.row}>
+              <span className={cl.title}>Effects</span>
+              <SimpleToggler checked={false} onChange={() => {}} />
+            </div>
+          </div>
           <CrossClose handleCrossClick={handleCrossClick} style={{ left: 10, top: 10 }} />
         </div>
       )}
