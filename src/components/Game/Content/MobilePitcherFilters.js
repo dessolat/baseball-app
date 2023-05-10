b0f60a9f1824808686a978749b5bc825b7db5f17
Igor Nikiforov <desola2@inbox.ru>
2023-05-10 11:19:14 +0300

    feat: add events line

--- src/components/Game/Content/MobilePitcherFilters.js
+++ src/components/Game/Content/MobilePitcherFilters.js
@@ -1,14 +1,21 @@
 // import { useSelector } from 'react-redux';
 // import ContentControls from '../ContentControls/ContentControls';
+import classNames from 'classnames';
 import ContentPitcher from '../ContentPitcher/ContentPitcher';
+import PlaysEvents from '../PlaysEvents/PlaysEvents';
 import cl from './Content.module.scss';
 
 const MobilePitcherFilters = () => {
   // const isVideo = useSelector(state => state.game.isVideo);
 
+	const eventsWrapperClasses = classNames(cl.mobileOnlyPortrait, cl.eventsWrapper)
   return (
     <div className={cl.mobilePitcherFilters}>
       <ContentPitcher />
+
+      <div className={eventsWrapperClasses}>
+        <PlaysEvents />
+      </div>
       {/* <ContentControls noPlayPause={!isVideo} isPlayOnline={false} /> */}
     </div>
   );
