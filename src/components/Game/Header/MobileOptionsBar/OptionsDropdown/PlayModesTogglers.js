8d3861e90ade2c80c1a32bfc70a84b7841e1f8ed
Igor Nikiforov <desola2@inbox.ru>
2023-05-14 15:56:15 +0300

    feat: add PlayModesTogglers functional

--- src/components/Game/Header/MobileOptionsBar/OptionsDropdown/PlayModesTogglers.js
+++ src/components/Game/Header/MobileOptionsBar/OptionsDropdown/PlayModesTogglers.js
@@ -1,13 +1,40 @@
 import cl from './OptionsDropdown.module.scss';
 import FirstLastMoment from 'components/UI/buttons/FirstLastMoment/FirstLastMoment';
 import ForwardRepeat from 'components/UI/buttons/ForwardRepeat/ForwardRepeat';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIsLastMomentMode, setPlaybackMode } from 'redux/gameReducer';
 
 const PlayModesTogglers = () => {
+  const playbackMode = useSelector(s => s.game.playbackMode);
+  const isLastMomentMode = useSelector(s => s.game.isLastMomentMode);
+
+  const dispatch = useDispatch();
+
+  const playbackModeClick = e => {
+    console.log(e);
+    const newMode =
+      e.currentTarget.name === 'play-pause' ? (playbackMode === 'play' ? 'pause' : 'play') : 'playOnline';
+    dispatch(setPlaybackMode(newMode));
+  };
+
+  const handlefirstLastBtn = () => dispatch(setIsLastMomentMode());
+
+  const getClassName = name => {
+    if (
+      (playbackMode !== 'playOnline' && name === 'play-pause') ||
+      (playbackMode === 'playOnline' && name === 'play-online')
+    )
+      return cl.active;
+    return '';
+  };
   return (
     <div className={cl.playModesTogglers}>
-      <div className={cl.buttonWrapper}>
+      <label className={cl.buttonWrapper}>
         <ForwardRepeat
-          playbackMode='play'
+          playbackMode={playbackMode}
+          className={getClassName('play-pause')}
+          onClick={playbackModeClick}
+          name='play-pause'
           style={{
             height: '100%',
             display: 'flex',
@@ -16,10 +43,10 @@ const PlayModesTogglers = () => {
             flexGap: 5
           }}
         />
-      </div>
-      <div className={cl.buttonWrapper}>
-        <FirstLastMoment isLastMomentMode={false} />
-      </div>
+      </label>
+      <label className={cl.buttonWrapper}>
+        <FirstLastMoment isLastMomentMode={isLastMomentMode} onClick={handlefirstLastBtn} />
+      </label>
     </div>
   );
 };
