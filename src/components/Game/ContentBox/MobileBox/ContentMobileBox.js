ce78ae449fb4ca934b5751fb9d8bd842febbd203
Igor Nikiforov <desola2@inbox.ru>
2023-06-04 16:12:29 +0300

    feat: add team switching

--- src/components/Game/ContentBox/MobileBox/ContentMobileBox.js
+++ src/components/Game/ContentBox/MobileBox/ContentMobileBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContentBoxFooter from '../../ContentBoxFooter/ContentBoxFooter';
+// import ContentBoxFooter from '../../ContentBoxFooter/ContentBoxFooter';
 import cl from './ContentMobileBox.module.scss';
 import MobileBoxTable from './MobileBoxTable';
 import { useSelector } from 'react-redux';
@@ -8,12 +8,12 @@ const ContentMobileBox = ({ tableData, footer }) => {
   const currentMode = useSelector(s => s.game.boxMode);
 
   const isTable = currentMode !== 'Info';
-  const isFooter = currentMode === 'Info';
+  // const isFooter = currentMode === 'Info' && footer !== '';
   return (
     <div className={cl.mobileBox}>
       {/* <MobileBoxHeader currentMode={currentMode} /> */}
       {isTable && <MobileBoxTable currentMode={currentMode} tableData={tableData} />}
-      {isFooter && <ContentBoxFooter footer={footer} />}
+      {/* {isFooter && <ContentBoxFooter footer={footer} />} */}
     </div>
   );
 };
