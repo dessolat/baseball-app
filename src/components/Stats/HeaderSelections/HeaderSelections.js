25e2ff2e16073c7a2bc0761c75a0e27cf4718e5c
Igor Nikiforov <desola2@inbox.ru>
2023-06-29 12:05:47 +0300

    feat: new years array method

--- src/components/Stats/HeaderSelections/HeaderSelections.js
+++ src/components/Stats/HeaderSelections/HeaderSelections.js
@@ -3,10 +3,11 @@ import cl from './HeaderSelections.module.scss';
 import Dropdown from 'components/UI/dropdown/GamesDropdown/Dropdown';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentYear, setCurrentDate, setCurrentGameType } from 'redux/sharedReducer';
-import { Link, useParams } from 'react-router-dom';
+import { setCurrentLeague } from 'redux/gamesReducer';
 import { setTableMode } from 'redux/statsReducer';
+import { Link, useParams } from 'react-router-dom';
 import ContentPlayerFilterField from '../ContentPlayerTable/ContentPlayerFilterField';
-import { getYears } from 'utils';
+// import { getYears } from 'utils';
 
 const TABLE_MODES = ['Batting', 'Fielding / Running', 'Pitching'];
 
@@ -16,12 +17,14 @@ const HeaderSelections = () => {
   const currentYear = useSelector(state => state.shared.currentYear);
   const currentGameType = useSelector(state => state.shared.currentGameType);
   const tableMode = useSelector(state => state.stats.tableMode);
+  const statsData = useSelector(state => state.stats.statsData);
   const dispatch = useDispatch();
 
   const handleClick = option => {
     const tempDate = new Date(option, 0, 1);
     tempDate.setHours(0, tempDate.getTimezoneOffset() * -1, 0, 0);
 
+		dispatch(setCurrentLeague({ id: -1, name: 'All' }));
     dispatch(setCurrentDate(tempDate));
     dispatch(setCurrentYear(option));
   };
@@ -32,7 +35,10 @@ const HeaderSelections = () => {
   const handleModeClick = mode => dispatch(setTableMode(mode));
   const handleGameTypeClick = gameType => dispatch(setCurrentGameType(gameType));
 
-  const yearsArr = getYears();
+  // const yearsArr = getYears();
+	const yearsArr = Object.keys(statsData)
+    .map(year => +year)
+    .reverse();
   return (
     <div className={cl.selections}>
       <div className={cl.types}>
