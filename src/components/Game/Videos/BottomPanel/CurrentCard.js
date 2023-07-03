a9ae9b02ff81ad5d6d2dc91f282a701767235a6a
Igor Nikiforov <desola2@inbox.ru>
2023-07-03 11:46:35 +0300

    feat: remove excessive code

--- src/components/Game/Videos/BottomPanel/CurrentCard.js
+++ src/components/Game/Videos/BottomPanel/CurrentCard.js
@@ -6,42 +6,9 @@ import ContentCardSimple from '../../ContentCardSimple/ContentCardSimple';
 const CurrentCard = ({ currentCard }) => {
   const situationsArr = [];
   currentCard.moments?.forEach(moment => moment.icons?.rect_text && situationsArr.push(moment));
-
-  const scrollVertically = e => {
-		console.log(e);
-    const start = e.target.scrollTop,
-      change = 580,
-      increment = 10;
-    let currentTime = 0;
-
-    Math.easeInOutQuad = function (t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return (c / 2) * t * t + b;
-      t--;
-      return (-c / 2) * (t * (t - 2) - 1) + b;
-    };
-
-    const animateScroll = function (name) {
-      currentTime += increment;
-      const val = Math.easeInOutQuad(currentTime, start, name === 'scroll-left' ? -change : change, 300);
-      e.target.scrollTop = val;
-      if (currentTime < 300) {
-        setTimeout(() => animateScroll(name), increment);
-      }
-    };
-
-    animateScroll(e.target.name);
-  };
-  const handleCardWheel = e => {
-		// console.log(e.currentTarget);
-		if (e.deltaY < 0) {
-			// console.log(e);
-		}
-// console.log(e);
-// 		e.currentTarget.scrollTo(0,100);
-	}
+	
   return (
-    <div className={cl.currentCard} onScrollCapture={scrollVertically} >
+    <div className={cl.currentCard}>
       {currentCard.type === 'Replacement' ? (
         <ContentCardReplacement events={currentCard.moments[0].events} />
       ) : situationsArr.length > 0 ? (
