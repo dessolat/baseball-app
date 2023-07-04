48dc406aa8061f2605c7ba7b1ee83a6984d4f62b
Igor Nikiforov <desola2@inbox.ru>
2023-07-04 17:10:16 +0300

    feat: destructurize bottom panel for video

--- src/components/Game/Video/BottomPanel.js
+++ src/components/Game/Video/BottomPanel.js
@@ -2,21 +2,14 @@ import React from 'react';
 import cl from './Video.module.scss';
 import Timeline from '../Timeline/Timeline';
 import { useSelector } from 'react-redux';
-import HeaderLogo from '../HeaderLogo/HeaderLogo';
-import Outs from 'components/UI/icons/Outs/Outs';
-import BallsStrikes from 'components/UI/icons/BallsStrikes/BallsStrikes';
-import Bases from 'components/UI/icons/Bases/Bases';
+import CurrentCard from '../Videos/BottomPanel/CurrentCard';
 
 const BottomPanel = () => {
-  const { guests, owners } = useSelector(state => state.game.preview);
-  const imagesData = useSelector(state => state.game.imagesData);
   const currentCard = useSelector(state => state.game.currentCard);
   const currentMoment = useSelector(state => state.game.currentMoment);
 
   const situationsArr = [];
   currentCard.moments?.forEach(moment => moment.icons?.rect_text && situationsArr.push(moment));
-
-  const { r1, r2, r3, outs, balls, strikes } = situationsArr[situationsArr.length - 1]?.table || {};
 
   const pitcherName = currentCard?.moments
     ?.slice(-1)[0]
@@ -28,21 +21,7 @@ const BottomPanel = () => {
   return (
     <div className={cl.bottomPanelFull}>
       <Timeline forFullscreen={true} />
-      <div className={cl.signs}>
-        <BallsStrikes balls={balls} strikes={strikes} />
-        <Bases r1={r1} r2={r2} r3={r3} />
-        <Outs outs={outs} />
-      </div>
-      <div className={cl.teamInfo}>
-        <HeaderLogo teamName={guests.name} images={imagesData} />
-        <span className={cl.teamTitle}>{guests.name}</span>
-        <h3 className={cl.scoreTitle}>{guests.score}</h3>
-      </div>
-      <div className={cl.teamInfo}>
-        <HeaderLogo teamName={owners.name} images={imagesData} side='right' />
-        <span className={cl.teamTitle}>{owners.name}</span>
-        <h3 className={cl.scoreTitle}>{owners.score}</h3>
-      </div>
+      {currentCard?.moments && <CurrentCard currentCard={currentCard} />}
       <div className={cl.playersInfo}>
         <div className={cl.players}>
           <div>
