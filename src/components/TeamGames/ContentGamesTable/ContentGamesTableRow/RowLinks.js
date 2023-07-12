771daace92f2c4ac47eef2871ed8521efff6dbed
Igor Nikiforov <desola2@inbox.ru>
2023-07-12 14:06:07 +0300

    feat: add list_inn check for links

--- src/components/TeamGames/ContentGamesTable/ContentGamesTableRow/RowLinks.js
+++ src/components/TeamGames/ContentGamesTable/ContentGamesTableRow/RowLinks.js
@@ -1,11 +1,14 @@
-import React from 'react';
 import RowLink from './RowLink';
 
 const RowLinks = ({ game }) => (
   <div>
     <div>
-      <RowLink to='box' id={game.id} />
-      <RowLink to='plays' id={game.id} />
+      {game.last_inn !== null && (
+        <>
+          <RowLink to='box' id={game.id} />
+          <RowLink to={game.has_records ? 'pitch' : 'plays'} id={game.id} />
+        </>
+      )}
       {game.has_records && <RowLink to='videos' id={game.id} />}
     </div>
   </div>
