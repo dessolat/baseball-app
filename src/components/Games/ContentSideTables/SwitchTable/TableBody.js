6c734cca16c907fd3b05b0539cc45d8a8b1f94ce
Igor Nikiforov <desola2@inbox.ru>
2023-08-09 13:13:01 +0300

    feat: decompose TableBodyRow

--- src/components/Games/ContentSideTables/SwitchTable/TableBody.js
+++ src/components/Games/ContentSideTables/SwitchTable/TableBody.js
@@ -1,8 +1,7 @@
 import { useSelector } from 'react-redux';
 import { useMemo } from 'react';
-import { Link } from 'react-router-dom';
-import { getShortName } from 'utils';
 import cl from '../ContentSideTables.module.scss';
+import TableBodyRow from './TableBodyRow';
 
 const REPLACES = { 'FLD%': 'FLD', '%SB': 'SB_pr' };
 
@@ -27,46 +26,17 @@ const TableBody = ({ tableMode, dropdownValue }) => {
   const isToFixed = ['AVG', 'SLG', 'OBP', 'OPS', 'ERA', '%SB', 'FLD%'].includes(dropdownValue[tableMode]);
   return (
     <div className={cl.tableBody}>
-      {sortedPlayers.map(row => {
-        const teams = row.teams.map((team, i, arr) => {
-          if (i !== 0)
-            return (
-              <>
-                /
-                <Link key={i} to={`/games/team/${currentGameType.toLowerCase()}/${team.name}`}>
-                  {getShortName(team.name, arr.length > 1 ? 3 : 6)}
-                </Link>
-              </>
-            );
-          return (
-            <Link key={i} to={`/games/team/${currentGameType.toLowerCase()}/${team.name}`}>
-              {getShortName(team.name, arr.length > 1 ? 3 : 6)}
-            </Link>
-          );
-        });
-
-        return (
-          <div key={row.id} className={cl.tableRow}>
-            <div className={cl.underlineHover}>
-              <Link to={`/stats/player/${row.id}`}>
-                {row.surname} {row.name}
-              </Link>
-            </div>
-            <div>
-              <div>{teams}</div>
-              <div>
-                {REPLACES[dropdownValue[tableMode]]
-                  ? !isToFixed
-                    ? row[REPLACES[dropdownValue[tableMode]]]
-                    : row[REPLACES[dropdownValue[tableMode]]].toFixed(3)
-                  : !isToFixed
-                  ? row[dropdownValue[tableMode]]
-                  : row[dropdownValue[tableMode]].toFixed(3)}
-              </div>
-            </div>
-          </div>
-        );
-      })}
+      {sortedPlayers.map(row => (
+        <TableBodyRow
+          key={row.id}
+					row={row}
+          tableMode={tableMode}
+          dropdownValue={dropdownValue}
+          REPLACES={REPLACES}
+          currentGameType={currentGameType}
+					isToFixed={isToFixed}
+        />
+      ))}
     </div>
   );
 };
