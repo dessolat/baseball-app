2d03f0324a1e0bb652c406043f4a7d5823252965
Igor Nikiforov <desola2@inbox.ru>
2023-08-10 20:31:32 +0300

    feat: add test car model to three page

--- src/components/ThreePage/Content/CanvasComp.js
+++ src/components/ThreePage/Content/CanvasComp.js
@@ -1,9 +1,21 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Galaxy from './Galaxy';
 import Buttons from './Buttons';
 import { useSelector } from 'react-redux';
+// import { Model } from './Car';
+import Loader from 'components/UI/loaders/Loader/Loader';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+// (async () => {
 
+const Model = lazy(() => import('./Car'));
+// })();
+
+// (async () => {
+//   const { Component1 } = await import('./Component1');
+// })();
+
+// const MyComponent = lazy(() => import('./MyComponent'))
 const CanvasComp = ({ cl }) => {
   const count = useSelector(state => state.three.count);
   const isStars = useSelector(state => state.three.isStars);
@@ -11,12 +23,18 @@ const CanvasComp = ({ cl }) => {
 
   return (
     <>
-      <Canvas camera={{ position: [0, 12, 24] }} style={{ position: 'absolute', left: 0, top: 0, pointerEvents: isEvents ? 'All' : 'none' }}>
-        <Suspense fallback={null}>
-          <Galaxy count={count} isStars={isStars} isEvents={isEvents} />
-        </Suspense>
-      </Canvas>
-      <Buttons cl={cl} count={count} isStars={isStars} isEvents={isEvents} />
+      <Suspense fallback={<Loader />}>
+        <Canvas
+          camera={{ position: [0, 12, 24], zoom: 10 }}
+          style={{ position: 'absolute', left: 0, top: 0, pointerEvents: isEvents ? 'All' : 'none' }}>
+          {/* <Galaxy count={count} isStars={isStars} isEvents={isEvents} /> */}
+          <Model />
+          <ambientLight position={[0, 1.5, 0]} intensity={2} />
+          <directionalLight position={[10, 50, 0]} />
+          <OrbitControls enableZoom={true} />
+        </Canvas>
+      </Suspense>
+      {/* <Buttons cl={cl} count={count} isStars={isStars} isEvents={isEvents} /> */}
     </>
   );
 };
