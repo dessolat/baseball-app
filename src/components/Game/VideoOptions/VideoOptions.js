6906303be01f20aabceae60ea8a0575d0a056fbf
Igor Nikiforov <desola2@inbox.ru>
2023-08-23 15:46:45 +0300

    feat: disable mode list on broadcast

--- src/components/Game/VideoOptions/VideoOptions.js
+++ src/components/Game/VideoOptions/VideoOptions.js
@@ -7,13 +7,14 @@ import cl from './VideoOptions.module.scss';
 
 const VideoOptions = ({ currentTab }) => {
   const currentMoment = useSelector(state => state.game.currentMoment);
+  const isBroadcast = useSelector(state => state.game.isBroadcast);
 
   const timeData = {
     pitch: currentMoment.metering?.pitch?.time_start_pitch_window,
     hitting: currentMoment.metering?.hit?.time_start_hit_window
   };
 
-  const isModeListDisabled = timeData[currentTab];
+  const isModeListDisabled = timeData[currentTab] || isBroadcast;
   return (
     <div className={cl.wrapper}>
       <div className={cl.videoOptions}>
