bc2261260cbed6fb1e713d92dd13cfb80acb7882
Igor Nikiforov <desola2@inbox.ru>
2023-08-23 15:45:55 +0300

    feat: add isBroadcast checking for pitch videos

--- src/components/Game/PlaysPitch/PlaysPitch.js
+++ src/components/Game/PlaysPitch/PlaysPitch.js
@@ -5,16 +5,19 @@ import PlaysSpeed from '../PlaysSpeed/PlaysSpeed';
 import { useSelector } from 'react-redux';
 import PitchValues from './PitchValues/PitchValues';
 import PitchVideos from './PitchVideo/PitchVideos';
+import BroadcastPitchVideos from './PitchVideo/BroadcastPitchVideos';
 
 const PlaysPitch = () => {
   const currentMoment = useSelector(state => state.game.currentMoment);
+  const isBroadcast = useSelector(state => state.game.isBroadcast);
 
   return (
     <>
       <PlaysField currentMoment={currentMoment} />
       <PlaysSpeed currentMoment={currentMoment} />
       <PlaysSpin pitch={currentMoment?.metering?.pitch} />
-      <PitchVideos />
+      {!isBroadcast && <PitchVideos />}
+      {isBroadcast && <BroadcastPitchVideos />}
       <PitchValues />
     </>
   );
