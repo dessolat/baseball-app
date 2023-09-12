62651dae22b56684ae7127dadabcb954332b645d
Igor Nikiforov <desola2@inbox.ru>
2023-09-12 16:17:23 +0300

    feat: rework render
    * integrate classnames

--- src/components/Game/HeaderScoresList/HeaderScoresListItem.js
+++ src/components/Game/HeaderScoresList/HeaderScoresListItem.js
@@ -1,29 +1,33 @@
-import React from 'react';
+import classNames from 'classnames';
 
 const HeaderScoresListItem = ({ inning, inningNumber, maxInnings, cl, handleClick, currentTab }) => {
-  const outerClass = inning.number === inningNumber && currentTab !== 'box' ? cl.active : '';
-  const innerClass = inning['bottom/owners'] === undefined ? cl.hidden : '';
+  const outerClasses = classNames({ [cl.active]: inning.number === inningNumber && currentTab !== 'box' });
+  const innerClasses = classNames({ [cl.hidden]: inning['bottom/owners'] === undefined });
 
   const topScore = inning['top/guests'] ? inning.top_runs || 0 : 0;
   const botScore = inning['bottom/owners'] ? inning.bot_runs || 0 : 0;
 
-  const renderedComp =
-    inning.number <= maxInnings ? (
-      <div className={outerClass}>
+  // Render main innings
+  if (inning.number <= maxInnings) {
+    return (
+      <div className={outerClasses}>
         <span>{inning.number}</span>
         <span onClick={handleClick(inning.number, 'top')}>{topScore}</span>
-        <span onClick={handleClick(inning.number, 'bottom')} className={innerClass}>
+        <span onClick={handleClick(inning.number, 'bottom')} className={innerClasses}>
           {botScore}
         </span>
       </div>
-    ) : (
-      <div>
-        <span>{inning.number}</span>
-        <span className={cl.hidden}>0</span>
-        <span className={cl.hidden}>0</span>
-      </div>
     );
-  return <>{renderedComp}</>;
+  }
+
+  // Render empty innings
+  return (
+    <div>
+      <span>{inning.number}</span>
+      <span className={cl.hidden}>0</span>
+      <span className={cl.hidden}>0</span>
+    </div>
+  );
 };
 
 export default HeaderScoresListItem;
